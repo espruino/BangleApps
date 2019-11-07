a1c33d16b53c8880328ec59c831c7365de9e8231
Gordon Williams <gw@pur3.co.uk>
2019-11-07 08:44:08 +0000

    New files and animals apps

---
+++ apps/animals.js
@@ -0,0 +1,29 @@
+var current="";
+var images="snake,duck,swan,fox,camel,pig,sheep,mouse".split(",");
+var lastPress = 0;
+
+function next(e) {
+  Bangle.setLCDPower(1);
+  if (e.time<(lastPress+0.5)) return;
+  lastPress = e.time;
+  var last = current;
+  do {
+    var n = 0|(Math.random()*images.length);
+    current = images[n];
+  } while (current && current==last);
+  g.clear();
+  var n = 1 + (0|(Math.random()*3.9));
+  var img = require("Storage").read("*"+current);
+  if (n==1)
+    g.drawImage(img,120,120,{scale:4,rotate:Math.random()-0.5});
+  else
+    for (var i=0;i<n;i++) {
+      var a = Math.PI*2*i/n;
+      g.drawImage(img,120+60*Math.cos(a),120+60*Math.sin(a),{scale:1.5,rotate:Math.random()-0.5});
+    }
+  g.flip();
+}
+
+setWatch(next,BTN4,{repeat:true});
+setWatch(next,BTN5,{repeat:true});
+next({time:getTime()});
---
+++ apps/files.js
@@ -0,0 +1,102 @@
+const storage = require('Storage');
+
+const boolFormat = (v) => v ? "On" : "Off";
+
+let m;
+
+function showMainMenu() {
+  const mainmenu = {
+    '': {
+      'title': 'App Manager',
+    },
+    'Free': {
+      value: undefined,
+      format: (v) => {
+        return storage.getFree();
+      },
+      onchange: () => {}
+    },
+    'Compact': () => {
+      E.showMessage('Compacting...');
+      try {
+        storage.compact();
+      } catch (e) {
+      }
+      m = showMainMenu();
+    },
+    'Apps': ()=> m = showApps(),
+    '< Back': load
+  };
+  return Bangle.menu(mainmenu);
+}
+
+function eraseApp(app) {
+  E.showMessage('Erasing ' + app.name + '...');
+  storage.erase(app['']);
+  storage.erase(app.icon);
+  storage.erase(app.src);
+}
+
+function showAppMenu(app) {
+  const appmenu = {
+    '': {
+      'title': app.name,
+    },
+    '< Back': () => m = showApps(),
+    'Erase': () => {
+      E.showPrompt('Erase ' + app.name + '?').then((v) => {
+        if (v) {
+          Bangle.buzz(100, 1);
+          eraseApp(app);
+          m = showApps();
+        } else {
+          m = showAppMenu(app)
+        }
+      });
+    }
+  };
+  return Bangle.menu(appmenu);
+}
+
+function showApps() {
+  const appsmenu = {
+    '': {
+      'title': 'Apps',
+    },
+    '< Back': () => m = showMainMenu(),
+  };
+
+  var list = storage.list().filter((a)=> {
+    return a[0]=='+' && a !== '+setting';
+  }).sort().map((app) => {
+    var ret = storage.readJSON(app);
+    ret[''] = app;
+    return ret;
+  });
+
+  if (list.length > 0) {
+    list.reduce((menu, app) => {
+      menu[app.name] = () => m = showAppMenu(app);
+      return menu;
+    }, appsmenu);
+    appsmenu['Erase All'] = () => {
+      E.showPrompt('Erase all?').then((v) => {
+        if (v) {
+          Bangle.buzz(100, 1);
+          for (var n = 0; n < list.length; n++)
+            eraseApp(list[n]);
+        }
+        m = showApps();
+      });
+    };
+  } else {
+    appsmenu['...No Apps...'] = {
+      value: undefined,
+      format: ()=> '',
+      onchange: ()=> {}
+    };
+  }
+  return Bangle.menu(appsmenu);
+}
+
+m = showMainMenu();
