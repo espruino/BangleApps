ad1d27463b1e0c32eca0f38bf0bd5c819ae61382
James M Snell <jasnell@gmail.com>
2019-11-08 12:10:49 -0800

    NCEU Start Sequence

--- apps/start.js
+++ apps/start.js
@@ -66,74 +66,6 @@ function logos() {
       clearInterval(i);
       clearWatch();
       res();
-    }, BTN2, {repeat:false});var g = Graphics.getInstance();
-
-const d = g.getWidth() - 18;
-function c(a) {
-  return {
-    width: 8,
-    height: a.length,
-    bpp: 1,
-    buffer: (new Uint8Array(a)).buffer
-  };
-}
-
-function welcome() {
-  var welcomes = [
-    'Welcome',
-    'Failte',
-    'Bienvenue',
-    'Willkommen',
-    'Bienvenido'
-  ];
-  function next() {
-    var n = welcomes.shift();
-    E.showMessage(n);
-    g.drawImage(c([0,8,12,14,255,14,12,8]),d,116);
-    welcomes.push(n);
-  }
-  return new Promise((res) => {
-    next();
-    var i = setInterval(next, 2000);
-    setWatch(() => {
-      clearInterval(i);
-      clearWatch();
-      E.showMessage('Loading...');
-      res();
-    }, BTN2, {repeat:false});
-  });
-}
-
-function logos() {
-  var logos = [
-    ['nfr', 20, 90, ()=>{}],
-    ['nceu', 20, 90, ()=>{
-      g.setFont("6x8", 2);
-      g.setColor(0,0,1);
-      g.drawString('Welcome To', 160, 110);
-      g.drawString('NodeConfEU', 160, 130);
-      g.drawString('2019', 200, 150);
-    }],
-    ['bangle', 70, 90, ()=>{}],
-    ['nodew', 20, 90, ()=>{}],
-    ['tf', 5, 60, ()=>{}],
-  ];
-  function next() {
-    var n = logos.shift();
-    var img = require("Storage").read("*"+n[0]);
-    g.clear();
-    g.drawImage(img, n[1], n[2]);
-    n[3]();
-    g.drawImage(c([0,8,12,14,255,14,12,8]),d,116);
-    logos.push(n);
-  }
-  return new Promise((res) => {
-    next();
-    var i = setInterval(next, 2000);
-    setWatch(() => {
-      clearInterval(i);
-      clearWatch();
-      res();
     }, BTN2, {repeat:false});
   });
 }
@@ -183,51 +115,18 @@ function info() {
   });
 }
 
-welcome()
-  .then(logos)
-  .then(info)
-  .then(()=>E.showMessage('done'));
-
-
-  });
-}
-
-function info() {
-  var slides = [
-    () => {
-      g.clear();
-      g.setFont('6x8',2);
-      g.setColor(1,1,1);
-      g.drawString('Visit', 120, 80);
-      g.drawString('nodewatch.dev', 120, 110);
-      g.drawString('for info', 115, 140);
-    },
-    () => {
-      g.clear();
-      g.setFont('6x8',2);
-      g.setColor(1,1,1);
-      g.drawString('Remember', 120, 80);
-      g.drawString('to charge', 120, 110);
-      g.drawString('your watch!', 125, 140);
-    },
-    () => {
-    }
-  ];
-  function next() {
-    var n = slides.shift();
-    n();
-    g.drawImage(c([0,8,12,14,255,14,12,8]),d,116);
-    slides.push(n);
-  }
-  return new Promise((res) => {
-    next();
-    var i = setInterval(next, 2000);
-    setWatch(()=>{
-      clearInterval(i);
-      clearWatch();
-      res();
-    }, BTN2, {repeat:false});
-  });
+function cleanup() {
+  E.showMessage('Loading...');
+  var s = require('Storage');
+  s.erase('*nfr');
+  s.erase('*nceu');
+  s.erase('*bangle');
+  s.erase('*nodew');
+  s.erase('*tf');
+  s.erase('+start');
+  s.erase('-start');
+  s.erase('*start');
+  return Promise.resolve();
 }
 
 welcome()
@@ -235,4 +134,3 @@ welcome()
   .then(info)
   .then(cleanup)
   .then(load);
-
