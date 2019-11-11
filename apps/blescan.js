953ee0471b3470d364847ab63ce18a02c04dfabc
Shane T Odlum <shane.odlum@consensys.net>
2019-11-11 16:59:02 +0000

    Add BLE Scanner app

---
+++ apps/blescan.js
@@ -0,0 +1,52 @@
+// ble-scanner
+// Scan the airwaves every three seconds (which seems safe for a large number of devices)
+// Using the menu feature, display a scrollable list of BLE devices on the watch
+
+// Dummy menu item to display until we find something
+const NODEVICE = 'No devices found';
+
+const SCAN_INTERVAL = 3000;
+
+const menu = {
+};
+
+menu[NODEVICE] = {
+  value : "",
+  onchange : () => {}
+};
+
+  
+function draw() {
+  Bangle.menu(menu);
+}
+
+function scan() {
+  NRF.findDevices(devices => {
+      for (let device of devices) {
+
+        // Only display devices that advertise a name
+        
+        if (device.name) {
+          // Remove no devices found message if it is present
+          if (menu[NODEVICE]) {
+            delete menu[NODEVICE];
+          }
+          menu[device.name] = {
+            value : device.rssi,
+            onchange : () => {}
+          };
+        }
+      }
+      draw();
+  }, { active: true });
+}
+
+
+function waitMessage() {
+  E.showMessage('scanning');
+}
+
+scan();
+waitMessage();
+
+setInterval(scan, SCAN_INTERVAL);
