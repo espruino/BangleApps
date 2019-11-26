913440c59673a18cf79a27456f2c9cfcb98768f1
MaBecker <mark.becker@sbit-gmbh.de>
2019-11-26 17:51:23 +0100

    minor fixes
    - add drawWidgets()

--- apps/_example_app/app.js
+++ apps/_example_app/app.js
@@ -6,6 +6,7 @@
     // special function to handle display switch on
     Bangle.on('lcdPower', (on) => {
         if (on) {
+        	drawWidgets();
             // call your app function here
     }});
 
