d976cd2d94a94c3c1c49e6d4bc8a57ed56818d3a
Stefan Kuehnel <eska_2007@gmx.de>
2019-12-14 19:14:07 +0100

    Created icon as .png and .js files

--- apps/berlinc/berlin-clock-icon.js
+++ apps/berlinc/berlin-clock-icon.js
@@ -1,1 +1,4 @@
-require("heatshrink").decompress(atob("mEwwJC/AH4A/AH4AgA=="));
+var img = {
+  width : 48, height : 48, bpp : 4,
+  buffer : require("heatshrink").decompress(atob("xAAWxOZAAeP/4ADApX4C/4X/C/4XvxAAWF4o1QI4wX/C/4X/C9OIAC2L3e7AQQvBzAEBFAP4FoYEFC/4X/C/4XvxAAWFIIADApYsBAAX4C/4X/C/4XXf54AH"))
+}
