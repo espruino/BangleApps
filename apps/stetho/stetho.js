24c6eaeea641aac26fecdddc516a2a0031d0e2e7
Kristin Galvin <kristin.galvin@nearform.com>
2020-01-19 17:51:08 -0800

    audio test

--- apps/stetho/stetho.js
+++ apps/stetho/stetho.js
@@ -28,9 +28,8 @@ function readHRM() {
   }
   g.setColor(1,1,1);
   g.lineTo(x,y);
-  console.log("a ", y);
-  freq((Math.abs(y) * 10000.0) + 500.0);
-  console.log("freq ", (y * 1000.0) + 100.0);
+  freq((Math.abs(a) * 10000.0) + 500.0);
+  console.log("freq ", (a * 10000.0) + 500.0);
   if ((max-min)>0.005) {
     if (4*a > (min+3*max)) { // high
       g.setColor(1,0,0);
