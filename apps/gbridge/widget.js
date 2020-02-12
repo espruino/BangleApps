899579e1b1b9e991f22d764d1e6e913e20971642
Gordon Williams <gw@pur3.co.uk>
2020-02-12 10:55:53 +0000

    gadgetbridge widget now shows connection state (fix #77)

--- apps/gbridge/widget.js
+++ apps/gbridge/widget.js
@@ -106,13 +106,22 @@
     }
   };
 
-
+function draw() {
+  g.setColor(-1);
+  if (NRF.getSecurityStatus().connected)
+    g.drawImage(require("heatshrink").decompress(atob("i0WwgHExAABCIwJCBYwJEBYkIBQ2ACgvzCwoECx/z/AKDD4WD+YLBEIYKCx//+cvnAKCBwU/mc4/8/HYv//Ev+Y4EEAePn43DBQkzn4rCEIoABBIwKHO4cjmczK42I6mqlqEEBQeIBQaDED4IgDUhi6KaBbmIA==")),xpos+1,1);
+  else
+    g.drawImage(require("heatshrink").decompress(atob("i0WwQFC1WgAgYFDAgIFClQFCwEK1W/AoIPB1f+CAMq1f7/WqwQPB/fq1Gq1/+/4dC/2/CAIaB/YbBAAO///qAoX/B4QbBDQQ7BDQQrBAAWoIIIACIIIVC0ECB4cACAZiBAoRtCAoIDBA")),xpos+1,1);
+}
+function changed() {
+  draw();
+  g.flip();// turns screen on
+}
+NRF.on('connected',changed);
+NRF.on('disconnected',changed);
 
 var xpos = WIDGETPOS.tl;
 WIDGETPOS.tl+=24;
-WIDGETS["gbridgew"]={draw:function() {
-  g.drawImage(require("heatshrink").decompress(atob("i0WwgHExAABCIwJCBYwJEBYkIBQ2ACgvzCwoECx/z/AKDD4WD+YLBEIYKCx//+cvnAKCBwU/mc4/8/HYv//Ev+Y4EEAePn43DBQkzn4rCEIoABBIwKHO4cjmczK42I6mqlqEEBQeIBQaDED4IgDUhi6KaBbmIA==")),xpos+1,1);
-  g.setColor(1,1,1);
-}};
+WIDGETS["gbridgew"]={draw:draw};
 
 })();
