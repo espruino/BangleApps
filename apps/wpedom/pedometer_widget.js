2b94bdea41bb3ecab1f13e0d872da3a3512c3359
Gordon Williams <gw@pur3.co.uk>
2020-02-13 10:39:57 +0000

    Pedometer: Add foot icon, use tidier font, and move to the left of the screen

--- apps/wpedom/pedometer_widget.js
+++ apps/wpedom/pedometer_widget.js
@@ -2,41 +2,40 @@
   const PEDOMFILE = "@wpedom";
   // add the width
   // WIDGETPOS.tr is originally 208 without any widgets
-  var xpos = WIDGETPOS.tr; // draw string with right alignment
-  var width = 48;
-  WIDGETPOS.tr -= (width + 2);
+  var xpos = WIDGETPOS.tl;
+  var width = 24;
+  WIDGETPOS.tl -= (width + 2);
 
   let lastUpdate = new Date();
   let stp_today = 0;
-
-  function erase() {
-    g.clearRect(xpos-width, 0, xpos, 23);
-  }
 
   // draw your widget at xpos
   function draw() {
     // Widget	(0,0,239,23)
     if (stp_today > 99999){
       stp_today = stp_today % 100000; // cap to five digits + comma = 6 characters
-      erase();
     }
     let stps = stp_today.toString();
+    g.reset();
     if (stps.length > 3){
       stps = stps.slice(0,-3) + "," + stps.slice(-3);
+      g.setFont("4x6", 1); // if big, shrink text to fix
+    } else {
+      g.setFont("6x8", 1);
     }
-    g.setColor(-1);
-    g.setFont("4x6", 2);
-    g.setFontAlign(1, 0); // align to x: right, y: center
-    g.drawString(stps, xpos, 11, true); // 6 * 4*2 = 48
+    g.setFontAlign(0, 0); // align to x: right, y: center
+    g.clearRect(xpos,15,xpos+width,24); // erase background
+    g.drawString(stps, xpos+width/2, 19);
+    g.drawImage(atob("CgoBM5znv+9z3OABznOMwA=="),xpos+(width-10)/2,2);
   }
 
   Bangle.on('step', (up) => {
     let date = new Date();
     if (lastUpdate.getDate() == date.getDate()){
-      stp_today += 1;
+      stp_today ++;
     } else {
+      // TODO: could save this to PEDOMFILE for lastUpdate's day?
       stp_today = 1;
-      erase();
     }
     lastUpdate = date;
     //console.log("up: " + up + " stp: " + stp_today + " " + date.toString());
