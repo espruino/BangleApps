2c1397ab929827328bde8e72eb2870c6201b7149
MaBecker <mark.becker@sbit-gmbh.de>
2020-03-04 20:59:27 +0100

    widid

---
+++ apps/widid/widget.js
@@ -0,0 +1,15 @@
+/* */
+(() => {
+  var xpos = WIDGETPOS.tr-16;
+  WIDGETPOS.tr-= 16;
+  id = NRF.getAddress().split(":");
+
+  // draw your widget at xpos
+  function draw() {
+    var x = xpos, y = 0;
+    g.setColor(0,0.5,1).setFont("6x8",1);
+	g.drawString(addr[4],x,0,true);
+    g.drawString(addr[5],x,10),true;
+  }
+  WIDGETS["widid"]={draw:draw};
+})()
