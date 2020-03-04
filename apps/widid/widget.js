09f146cf63fc40087218dfed27d7f26e6f6c3f70
MaBecker <mark.becker@sbit-gmbh.de>
2020-03-04 21:14:49 +0100

    widid

--- apps/widid/widget.js
+++ apps/widid/widget.js
@@ -8,8 +8,8 @@
   function draw() {
     var x = xpos, y = 0;
     g.setColor(0,0.5,1).setFont("6x8",1);
-	g.drawString(addr[4],x,0,true);
-    g.drawString(addr[5],x,10),true;
+	g.drawString(id[4],x,0,true);
+    g.drawString(id[5],x,10),true;
   }
   WIDGETS["widid"]={draw:draw};
 })()
