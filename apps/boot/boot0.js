d07dc1de6fe9948edd0418c69352200f28d07f6a
Gordon Williams <gw@pur3.co.uk>
2020-03-12 16:46:39 +0000

    Stop users calling save() (fix #125)       If Debug info is set to 'show' don't move to Terminal if connected!

--- apps/boot/boot0.js
+++ apps/boot/boot0.js
@@ -12,7 +12,7 @@ if (s.blerepl===false) { // If not programmable, force terminal off Bluetooth
   if (s.log) Terminal.setConsole(true); // if showing debug, force REPL onto terminal
   else E.setConsole(null,{force:true}); // on new (2v05+) firmware we have E.setConsole which allows a 'null' console
 } else {
-  if (s.log) Terminal.setConsole(); // if showing debug, put REPL on terminal (until connection)
+  if (s.log && !NRF.getSecurityStatus().connected) Terminal.setConsole(); // if showing debug, put REPL on terminal (until connection)
   else Bluetooth.setConsole(true); // else if no debug, force REPL to Bluetooth
 }
 // we just reset, so BLE should be on.
@@ -25,6 +25,8 @@ Bangle.setLCDTimeout(s.timeout);
 if (!s.timeout) Bangle.setLCDPower(1);
 E.setTimeZone(s.timezone);
 delete s;
+// stop users doing bad things!
+global.save = function() { throw new Error("You can't use save() on Bangle.js without overwriting the bootloader!"); }
 // check for alarms
 var alarms = require('Storage').readJSON('alarm.json',1)||[];
 var time = new Date();
