ae5f8e8f086ce7d64552742c3f2daaf3d4c0a1fd
MaBecker <mark.becker@sbit-gmbh.de>
2020-04-02 13:41:28 +0200

    widget hand wash timer

--- apps/widhwt/widget.js
+++ apps/widhwt/widget.js
@@ -2,7 +2,6 @@
 (() => {
     var icon = require("heatshrink").decompress(atob("jEYwIKHgwCBhwCBh4CEggPCkACBmAXDBwVZ+EB+F4gEsjl8EgMP+EChk/gEMh+ehkA+YIBxwxBnF/4HggH/wEAj0AA=="));
     var color = 0x4A69;
-    var isActive = false;
 
     function draw() {
         g.reset().setColor(color).drawImage(icon, this.x + 1, 0);
@@ -10,18 +9,15 @@
 
     WIDGETS["widhwt"] = { area: "tr", width: 26, draw: draw };
 
-    Bangle.on('twist', function() {
-        if (!isActive) {
-            isActive = true;
-            color = 0x41f;
-            Bangle.buzz();
+    Bangle.on('swipe', function() {
+        color = 0x41f;
+        Bangle.buzz();
+        Bangle.drawWidgets();
+        setTimeout(() => {
+            color = 0x4A69;
+            Bangle.buzz(1E3, 1);
             Bangle.drawWidgets();
-            setTimeout(() => {
-                color = 0x4A69;
-                Bangle.buzz(1E3, 1);
-                Bangle.drawWidgets();
-                setTimeout(() => { isActive = false; }, 1E10);
-            }, 35E3);
-        }
+        }, 35E3);
+
     });
 })();
