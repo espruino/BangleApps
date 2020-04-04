8bb7c24b81ff4ee91ea59af2928e402e02fe8bd4
Christian Hemker <hemker@busitec.de>
2020-04-04 17:13:39 +0200

    refactor 2

--- apps/daysl/widget.js
+++ apps/daysl/widget.js
@@ -1,38 +1,38 @@
 const storage = require('Storage');
 let settings;
-var dd = settings.day,
-    mm = settings.month-1, //-1 because month is zero-based
-    yy = settings.year;
 
-//update file
+//write settings to file
 function updateSettings() {
     storage.write('daysleft.json', settings);
-  }
+}
 
-//set standard settings
+//Define standard settings
 function resetSettings() {
   settings = {
     day : 17,
     month : 6,
     year: 2020
   };
-  updateSettings(); //update file
+  updateSettings();
 }
 
-settings = storage.readJSON('daysleft.json',1); //read file
-if (!settings) resetSettings(); //if file does not exist, reset settings
+settings = storage.readJSON('daysleft.json',1); //read storage
+if (!settings) resetSettings(); //if settings file was not found, set to standard
+
+var dd = settings.day,
+    mm = settings.month-1, //-1 because month is zero-based
+    yy = settings.year;
 
 const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-const targetDate = new Date(yy, mm, dd); //time is 00:00 then
-const today = new Date();
+const targetDate = new Date(yy, mm, dd); //is 00:00
+const today = new Date(); //includes current time
 
-//create date object with today, but 00:00:00
 const currentYear = today.getFullYear();
 const currentMonth = today.getMonth();
 const currentDay = today.getDate();
-const todayMorning = new Date (currentYear, currentMonth, currentDay, 0, 0, 0); //set time to 00:00
+const todayMorning = new Date (currentYear, currentMonth, currentDay, 0, 0, 0); //create date object with today, but 00:00:00
 
-const diffDays = (targetDate - todayMorning) / oneDay; //calculate difference of days
+const diffDays = (targetDate - todayMorning) / oneDay; //calculate day difference
 
 //draw widget
 WIDGETS["daysl"]={area:"tl",width:40,draw:function(){
