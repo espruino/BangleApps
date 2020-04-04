10b044997dc2bca2a3639f736f222142482a9f4d
Christian Hemker <hemker@busitec.de>
2020-04-04 17:59:01 +0200

    change string sizes and positions

--- apps/daysl/widget.js
+++ apps/daysl/widget.js
@@ -1,5 +1,9 @@
 const storage = require('Storage');
 let settings;
+let height = 23;
+let width = 34;
+
+var debug = 0; //1 = show debug info
 
 //write settings to file
 function updateSettings() {
@@ -34,8 +38,47 @@ const todayMorning = new Date (currentYear, currentMonth, currentDay, 0, 0, 0); 
 
 const diffDays = (targetDate - todayMorning) / oneDay; //calculate day difference
 
+function drawWidget() {
+  if (debug == 1) g.drawRect(this.x,this.y,this.x+width,this.y+height); //draw rectangle around widget area
+  g.reset();
+
+  //define font size and string position
+  //small if number has more than 3 digits (positive number)
+  if (diffDays >= 1000) {
+    g.setFont("6x8", 1);
+    g.drawString(diffDays,this.x+10,this.y+7);
+  }
+  //large if number has 3 digits (positive number)
+  if (diffDays <= 999 && diffDays >= 100) {
+    g.setFont("6x8", 2);
+    g.drawString(diffDays,this.x,this.y+4);
+  }
+  //large if number has 2 digits (positive number)
+  if (diffDays <= 99 && diffDays >= 10) {
+    g.setFont("6x8", 2);
+    g.drawString(diffDays,this.x+6,this.y+4);
+  }
+  //large if number has 1 digit (positive number)
+  if (diffDays <= 9 && diffDays >= 0) {
+    g.setFont("6x8", 2);
+    g.drawString(diffDays,this.x+13,this.y+4);
+  }
+  //large if number has 1 digit (negative number)
+  if (diffDays <= -1 && diffDays >= -9) {
+    g.setFont("6x8", 2);
+    g.drawString(diffDays,this.x+5,this.y+4);
+  }
+  //large if number has 2 digits (negative number)
+  if (diffDays <= -10 && diffDays >= -99) {
+    g.setFont("6x8", 2);
+    g.drawString(diffDays,this.x,this.y+4);
+  }
+  //large if number has 3 digits or more (negative number)
+  if (diffDays <= -100) {
+    g.setFont("6x8", 1);
+    g.drawString(diffDays,this.x,this.y+7);
+  }
+}
+
 //draw widget
-WIDGETS["daysl"]={area:"tl",width:40,draw:function(){
-  g.setFont("6x8", 1);
-  g.drawString(diffDays,this.x+12,this.y+12);
-}};
+WIDGETS["daysl"]={area:"tl",width:width,draw:drawWidget};
