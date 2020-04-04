df97a75200e3e5eb4ca1b7c409b32f6ef0cdfbca
Christian Hemker <hemker@busitec.de>
2020-04-04 16:02:34 +0200

    refactor and comments

--- apps/daysl/widget.js
+++ apps/daysl/widget.js
@@ -1,38 +1,40 @@
 const storage = require('Storage');
 let settings;
+var dd = settings.day,
+    mm = settings.month-1, //-1 because month is zero-based
+    yy = settings.year;
 
+//update file
 function updateSettings() {
     storage.write('daysleft.json', settings);
   }
 
-  function resetSettings() {
-    settings = {
-      day : 17,
-      month : 6,
-      year: 2020
-    };
-    updateSettings();
-  }
+//set standard settings
+function resetSettings() {
+  settings = {
+    day : 17,
+    month : 6,
+    year: 2020
+  };
+  updateSettings(); //update file
+}
 
-  settings = storage.readJSON('daysleft.json',1);
-  if (!settings) resetSettings();
+settings = storage.readJSON('daysleft.json',1); //read file
+if (!settings) resetSettings(); //if file does not exist, reset settings
 
-  var dd = settings.day,
-      mm = settings.month-1, //month is zero-based
-      yy = settings.year;
+const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+const targetDate = new Date(yy, mm, dd); //time is 00:00 then
+const today = new Date();
 
-  const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-  const targetDate = new Date(yy, mm, dd);
-  const today = new Date();
+//create date object with today, but 00:00:00
+const currentYear = today.getFullYear();
+const currentMonth = today.getMonth();
+const currentDay = today.getDate();
+const todayMorning = new Date (currentYear, currentMonth, currentDay, 0, 0, 0); //set time to 00:00
 
-  //create date object with today, but 00:00:00
-  const currentYear = today.getFullYear();
-  const currentMonth = today.getMonth();
-  const currentDay = today.getDate();
-  const todayMorning = new Date (currentYear, currentMonth, currentDay, 0, 0, 0);
+const diffDays = (targetDate - todayMorning) / oneDay; //calculate difference of days
 
-  const diffDays = (targetDate - todayMorning) / oneDay;
-
+//draw widget
 WIDGETS["daysl"]={area:"tl",width:40,draw:function(){
   g.setFont("6x8", 1);
   g.drawString(diffDays,this.x+12,this.y+12);
