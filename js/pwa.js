dfd98b2d3fa012a11e7552d150c374fb86b20073
OmegaRogue <thatomegarogue@gmail.com>
2020-04-04 12:23:53 +0200

    fixed paths
    Signed-off-by: OmegaRogue <thatomegarogue@gmail.com>

--- js/pwa.js
+++ js/pwa.js
@@ -4,7 +4,7 @@ const butInstall = document.getElementById('butInstall');
 
 /* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js');
+    navigator.serviceWorker.register('js/service-worker.js');
 }
 
 /**
