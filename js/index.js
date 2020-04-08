36a89579421686c954f42cb9ac35000ac112c308
Gordon Williams <gw@pur3.co.uk>
2020-04-08 08:53:40 +0100

    Remove 2v04 version warning, add links in About to official/developer versionsRemove

--- js/index.js
+++ js/index.js
@@ -465,6 +465,19 @@ librarySearchInput.addEventListener('input', evt => {
 
 // =========================================== About
 
+if (window.location.host=="banglejs.com") {
+  document.getElementById("apploaderlinks").innerHTML =
+    'This is the official Bangle.js App Loader - you can also try the <a href="https://espruino.github.io/BangleApps/">Development Version</a> for the most recent apps.';
+} else if (window.location.host=="espruino.github.io") {
+  document.title += " [Development]";
+  document.getElementById("apploaderlinks").innerHTML =
+    'This is the development Bangle.js App Loader - you can also try the <a href="https://banglejs.com/apps/">Official Version</a> for stable apps.';
+} else {
+  document.title += " [Unofficial]";
+  document.getElementById("apploaderlinks").innerHTML =
+    'This is not the official Bangle.js App Loader - you can try the <a href="https://banglejs.com/apps/">Official Version</a> here.';
+}
+
 document.getElementById("settime").addEventListener("click",event=>{
   Comms.setTime().then(()=>{
     showToast("Time set successfully","success");
