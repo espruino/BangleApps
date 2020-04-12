16bcd463bbbcb1d340e56c4fce21b0542e205386
Francesco Bedussi <francesco.bedussi@abstract.it>
2020-04-12 18:21:20 +0200

    feat: big clock

---
+++ apps/bigclock/bigclock.js
@@ -0,0 +1,53 @@
+function drawTime() {
+  if (!Bangle.isLCDOn()) return;
+  var d = new Date();
+  var da = d.toString().split(" ");
+  var time = da[4].substr(0, 5).split(":");
+  var dow = da[0],
+    month = da[1],
+    day = da[2],
+    year = da[3],
+    hours = time[0],
+    minutes = time[1];
+  g.clearRect(0, 24, 239, 239);
+  g.setColor(1, 1, 1);
+  g.setFont("Vector", 100);
+  g.drawString(hours, 50, 24, true);
+  g.drawString(minutes, 50, 135, true);
+  g.setFont("Vector", 20);
+  g.setRotation(3);
+  g.drawString(`${dow} ${day} ${month}`, 50, 20, true);
+  g.drawString(year, 85, 205, true);
+}
+
+Bangle.on("lcdPower", function(on) {
+  if (on) {
+    g.clear();
+    Bangle.drawWidgets();
+    drawTime();
+  }
+});
+
+Bangle.setLCDBrightness(1);
+g.clear();
+Bangle.loadWidgets();
+Bangle.drawWidgets();
+drawTime();
+intervalRefMin = setInterval(drawTime, 60 * 1000);
+// Show launcher when middle button pressed
+setWatch(Bangle.showLauncher, BTN2, { repeat: false, edge: "falling" });
+// change watch
+setWatch(
+  function() {
+    load("anaclock.app.js");
+  },
+  BTN1,
+  { repeat: false, edge: "rising" }
+);
+setWatch(
+  function() {
+    load("digiclock.app.js");
+  },
+  BTN3,
+  { repeat: false, edge: "rising" }
+);
