6b908542c38dc3838c162b369605f78278980898
Francesco Bedussi <francesco.bedussi@abstract.it>
2020-04-18 19:59:58 +0200

    feat: bigclock: add moon phase

--- apps/bigclock/bigclock.js
+++ apps/bigclock/bigclock.js
@@ -2,8 +2,78 @@ const REFRESH_RATE = 10000;
 
 let interval;
 
-function drawTime() {
-  const d = new Date();
+function drawMoon(d) {
+  const BLACK = 0,
+    MOON = 0x41f,
+    MC = 29.5305882,
+    NM = 694039.09;
+  var r = 12,
+    mx = 215,
+    my = 50;
+
+  var moon = {
+    0: () => {
+      g.reset()
+        .setColor(BLACK)
+        .fillRect(mx - r, my - r, mx + r, my + r);
+    },
+    1: () => {
+      moon[0]();
+      g.setColor(MOON).drawCircle(mx, my, r);
+    },
+    2: () => {
+      moon[3]();
+      g.setColor(BLACK).fillEllipse(mx - r / 2, my - r, mx + r / 2, my + r);
+    },
+    3: () => {
+      moon[0]();
+      g.setColor(MOON)
+        .fillCircle(mx, my, r)
+        .setColor(BLACK)
+        .fillRect(mx - r, my - r, mx, my + r);
+    },
+    4: () => {
+      moon[3]();
+      g.setColor(MOON).fillEllipse(mx - r / 2, my - r, mx + r / 2, my + r);
+    },
+    5: () => {
+      moon[0]();
+      g.setColor(MOON).fillCircle(mx, my, r);
+    },
+    6: () => {
+      moon[7]();
+      g.setColor(MOON).fillEllipse(mx - r / 2, my - r, mx + r / 2, my + r);
+    },
+    7: () => {
+      moon[0]();
+      g.setColor(MOON)
+        .fillCircle(mx, my, r)
+        .setColor(BLACK)
+        .fillRect(mx, my - r, mx + r + r, my + r);
+    },
+    8: () => {
+      moon[7]();
+      g.setColor(BLACK).fillEllipse(mx - r / 2, my - r, mx + r / 2, my + r);
+    }
+  };
+
+  function moonPhase(d) {
+    var tmp,
+      month = d.getMonth(),
+      year = d.getFullYear(),
+      day = d.getDate();
+    if (month < 3) {
+      year--;
+      month += 12;
+    }
+    tmp = (365.25 * year + 30.6 * ++month + day - NM) / MC;
+    return Math.round((tmp - (tmp | 0)) * 7 + 1);
+  }
+
+  moon[moonPhase(d)]();
+}
+
+function drawTime(d) {
   const da = d.toString().split(" ");
   const time = da[4].substr(0, 5).split(":");
   const dow = da[0];
@@ -25,12 +95,18 @@ function drawTime() {
   g.setRotation(0);
 }
 
+function drawClockFace() {
+  const d = new Date();
+  drawTime(d);
+  drawMoon(d);
+}
+
 Bangle.on("lcdPower", function(on) {
   if (on) {
     g.clear();
     Bangle.drawWidgets();
-    drawTime();
-    interval = setInterval(drawTime, REFRESH_RATE, settings.drawMode);
+    drawClockFace();
+    interval = setInterval(drawClockFace, REFRESH_RATE, settings.drawMode);
   } else {
     clearInterval(interval);
   }
@@ -44,8 +120,8 @@ setWatch(Bangle.showLauncher, BTN2, { repeat: false, edge: "falling" });
 
 g.clear();
 clearInterval();
-drawTime();
-interval = setInterval(drawTime, REFRESH_RATE);
+drawClockFace();
+interval = setInterval(drawClockFace, REFRESH_RATE);
 
 Bangle.loadWidgets();
 Bangle.drawWidgets();
