7735a2ba97d6156c79450869a7f716d52cd9cffb
fredericrous <frederic.rousseau.fr@gmail.com>
2020-04-21 11:18:29 +0100

    New game: Pong🕹

---
+++ apps/pong/app-icon.js
@@ -0,0 +1,1 @@
+require("heatshrink").decompress(atob("mEwgIEBgOABQYFD8AUEApoXFDqIXV4BYGKZIANsIRE+IFE/IFEvCIFGrgXLDqIAOgc/9/2hv+g8///3AoUwvE3xuABYP4m3NzwFB7E2tu/CIMYm09wYFDjoFCj4pB/8HkEP+EBFII7EAosDJxYA="))
