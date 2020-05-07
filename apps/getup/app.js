74eb86d376ea5227649ab9e2f91b7fe7e240f06e
v1nc <github@reckendrees.systems>
2020-05-07 20:59:04 +0200

    Add comments

--- apps/getup/app.js
+++ apps/getup/app.js
@@ -1,19 +1,25 @@
+//init settings
 const storage = require("Storage");
 const SETTINGS_FILE = 'getup.settings.json';
 
 function setting(key) {
-const DEFAULTS = {
-    'sitTime' : 20,
-    'moveTime' : 1
-};
-if (!settings) { loadSettings(); }
-return (key in settings) ? settings[key] : DEFAULTS[key];
-}
-let settings;
-function loadSettings() {
-settings = storage.readJSON(SETTINGS_FILE, 1) || {};
+    const DEFAULTS = {
+        'sitTime' : 20,
+        'moveTime' : 1
+    }
+    if (!settings) {
+        loadSettings();
+    }
+    return (key in settings) ? settings[key] : DEFAULTS[key];
 }
 
+let settings;
+
+function loadSettings() {
+    settings = storage.readJSON(SETTINGS_FILE, 1) || {};
+}
+
+//vibrate, draw move message and start timer for sitting message
 function remind() {
     Bangle.buzz(1000,1);
     g.clear();
@@ -21,13 +27,14 @@ function remind() {
     g.drawString("MOVE!", g.getWidth()/2, g.getHeight()/2);
     setTimeout(print_message,moveTime * 60000);
 }
-
+//draw sitting message and start timer for reminder
 function print_message(){
 	g.clear();
 	g.setColor(0xF800);
 	g.drawString("sitting is dangerous!", g.getWidth()/2, g.getHeight()/2);
         setTimeout(remind,settings.sitTime * 60000);
 }
+
 //init graphics
 require("Font8x12").add(Graphics);
 g.setFont("8x12",2);
