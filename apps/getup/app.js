dbf57058d0fe156ea9a721b4584f41999f7c4e6b
v1nc <github@reckendrees.systems>
2020-05-07 19:38:34 +0200

    change colors and interval time

--- apps/getup/app.js
+++ apps/getup/app.js
@@ -7,16 +7,16 @@ function remind() {
 }
 
 function print_message(){
-	g.setColor(0xF800);
+	g.clear();
+	g.setColor(0x03E0);
 	g.drawString("sitting is dangerous!", g.getWidth()/2, g.getHeight()/2);
 }
 //init graphics
-g.clear();
 require("Font8x12").add(Graphics);
 g.setFont("8x12",2);
 g.setFontAlign(0,0);
 g.flip();
 
 print_message();
-setInterval(remind,60000);
+setInterval(remind,1200000);
 
