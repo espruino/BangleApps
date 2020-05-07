c091db0d680ecbd1b42f60735141c7909b7214e7
v1nc <github@reckendrees.systems>
2020-05-07 20:03:11 +0200

    add settings for move and sit time

--- apps/getup/app.js
+++ apps/getup/app.js
@@ -1,9 +1,25 @@
+const storage = require("Storage");
+const SETTINGS_FILE = 'getup.settings.json';
+
+function setting(key) {
+const DEFAULTS = {
+    'sitTime' : 20,
+    'moveTime' : 1
+};
+if (!settings) { loadSettings(); }
+return (key in settings) ? settings[key] : DEFAULTS[key];
+}
+let settings;
+function loadSettings() {
+settings = storage.readJSON(SETTINGS_FILE, 1) || {};
+}
+
 function remind() {
     Bangle.buzz(1000,1);
     g.clear();
     g.setColor(0xF800);
     g.drawString("MOVE!", g.getWidth()/2, g.getHeight()/2);
-    setTimeout(print_message,60000);
+    setTimeout(print_message,moveTime * 60000);
 }
 
 function print_message(){
@@ -18,5 +34,5 @@ g.setFontAlign(0,0);
 g.flip();
 
 print_message();
-setInterval(remind,1200000);
+setInterval(remind,settings.sitTime * 60000);
 
