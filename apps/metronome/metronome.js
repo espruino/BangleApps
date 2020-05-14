09ba504289ae95c113ed2f2beb95c67b39dcecab
bengwalker <michael.bengfort@posteo.de>
2020-05-14 07:51:55 +0200

    add settings to apps.json

--- apps/metronome/metronome.js
+++ apps/metronome/metronome.js
@@ -14,6 +14,7 @@ function setting(key) {
   //define default settings
   const DEFAULTS = {
     'beatsperbar': 4,
+    'buzzintens': 0.75,
   };
   if (!settings) { loadSettings(); }
   return (key in settings) ? settings[key] : DEFAULTS[key];
@@ -52,7 +53,7 @@ function updateScreen() {
   g.clearRect(0, 50, 250, 150);
   changecolor();
   try {
-    Bangle.buzz(50, 0.75);
+    Bangle.buzz(50, setting('buzzintens'));
   }
   catch(err) {
   }
