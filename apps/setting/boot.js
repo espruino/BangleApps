297188624e7ef8e55bb45beb5934e6b32888b986
Gordon Williams <gw@pur3.co.uk>
2020-05-15 16:14:17 +0100

    Move BLE into its own menu, add passkey pairing setup. Doesn't seem great when working via web bluetooth

--- apps/setting/boot.js
+++ apps/setting/boot.js
@@ -3,4 +3,5 @@
   if (!settings) return;
   if (settings.options) Bangle.setOptions(settings.options);
   if (settings.brightness && settings.brightness!=1) Bangle.setLCDBrightness(settings.brightness);
+  if (settings.passkey!==undefined && settings.passkey.length==6) NRF.setSecurity({passkey:settings.passkey, mitm:1, display:1});
 })()
