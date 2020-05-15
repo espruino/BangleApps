d073c4735e3bf1b700a9ee9497a06258914ca815
Stefan Weitlaner <steff.weiti@gmail.com>
2020-05-15 14:37:37 +0200

    ctracker fixes

--- apps/ctracker/app.js
+++ apps/ctracker/app.js
@@ -1,34 +1,58 @@
 // place your const, vars, functions or classes here
 var calories = 0;
-var leftMargin = g.getWidth()/2;
-var topMargin = g.getHeight()/2;
+var leftMargin = g.getWidth()/4;
+var topMargin = g.getHeight()/8;
 var previousHr = {
   timestamp: 0
 };
+var hr = 0;
+var status = "stopped";
+var startTime = 0;
+var interval;
 
 //personal data (settings file?)
 var age = 27;
 var weight = 96;
 var sex = "Male";
 
-
-
 function draw(){
   g.clear();
+  g.setColor(1,1,1);
+  //time
   g.setFontAlign(0,0);
+  g.setFont("6x8",3);
+  g.drawString(getTimeString(),leftMargin*2,topMargin);
   
-  g.setFont("6x8",4);
-  g.drawString(getTimeString(),leftMargin,topMargin*1.6);
-
-  g.setFont("6x8",8);
-  g.drawString(calories.toFixed(0),leftMargin,topMargin);
-  g.setFont("6x8",4);
-  g.drawString("kCal",leftMargin,topMargin*1.6);
+  //calories
+  g.setFontAlign(0,0);
+  g.setFont("6x8",6);
+  g.drawString(calories.toFixed(0),leftMargin*3,topMargin*3);
+  g.setFontAlign(0,1);
+  g.setFont("6x8",3);
+  g.drawString("kCal",leftMargin*3,topMargin*6);
+  
+  //heartrate
+  g.setFontAlign(0,0);
+  g.setFont("6x8",6);
+  g.drawString(hr.toFixed(0),leftMargin,topMargin*3);
+  g.setFontAlign(0,1);
+  g.setFont("6x8",3);
+  g.drawString("bpm",leftMargin,topMargin*6);
+  
+  //status
+  g.setFontAlign(0,1);
+  g.setFont("6x8",3);
+  if(status == "running")
+    g.setColor(0,1,0);
+  g.drawString(status,leftMargin*2,topMargin*7);
+  
+  //keep screen active
   g.flip();
 }
 
 function onHrm(hrm){
   if(hrm.confidence > 95){
+    hr = hrm.bpm;
     if(previousHr.timestamp == 0){
       previousHr.timestamp = getTime();
     } else {
@@ -47,30 +71,45 @@ function onHrm(hrm){
   }
 }
 
-function calcCalories(){
-  if(hrArray.length>0){
-    var total = 0;
-    for(var i =0;i<hrArray.length;i++){
-      total += hrArray[i];
-    }
-    var bpmAverage = total/hrArray.length;
-    console.log("Total: " + total + "; Average: " + bpmAverage + ";\n");
-    hrArray = [];
-    var acCal = 0; 
-    if(sex === "Male"){
-      acCal = ((age*0.2017) + (weight*0.1988) + (bpmAverage*0.6309) - 55.0969) * 0.5/4.184;
-    } else if (sex === "Female"){
-      acCal = ((age*0.074) + (weight*0.1263) + (bpmAverage*0.4472) - 20.4022) * 0.5/4.184;
-    }
-    calories += acCal;
-    console.log("Calories: " + acCal + "; Sum Cal: "+calories);
-  }
+//get time string formated to hh:mm in 24h format
+function getTimeString(){
+  if(startTime == 0)
+    return "00:00:00";
+  
+  var secs = getTime()- startTime;
+  var hours = Math.floor(secs/3600);
+  secs %= 3600;
+  var minutes = Math.floor(secs/60);
+  secs = Math.floor(secs%60);
+  
+  var str = ((hours>9) ? hours : "0" + hours) +":";
+  str += ((minutes>9) ? minutes : "0" + minutes) +":";
+  str += ((secs>9) ? secs : "0" + secs);
+  return str;
 }
 
-// special function to handle display switch on
-Bangle.setHRMPower(1);
-Bangle.on('HRM',onHrm);
-setInterval(draw,1000);
-//setInterval(calcCalories,30*1000);
+//start and stop
+function start(){
+  calories = 0;
+  previousHr.timestamp = 0;
+  startTime = getTime();
+  interval = setInterval(draw,1000);
+  Bangle.setHRMPower(1);
+  Bangle.on('HRM',onHrm);
+  status = "running";
+}
+function stop(){
+  Bangle.setHRMPower(0);
+  Bangle.removeListener('HRM',onHrm);
+  status = "stopped";
+  draw();  
+  clearInterval(interval);
+  startTime=0;
+}
 
-// call your app function here
+//intervals and eventhandlers
+
+setWatch(start, BTN1, {repeat:true,edge:"falling"});
+setWatch(stop, BTN3, {repeat:true,edge:"falling"});
+
+draw();
