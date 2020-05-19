09b83b99cfef05de2603de0525099b35376914de
Stefan Weitlaner <steff.weiti@gmail.com>
2020-05-19 08:49:30 +0200

    fixed drawing

--- apps/sswatch/app.js
+++ apps/sswatch/app.js
@@ -16,7 +16,6 @@ function draw(){
   g.setFont("6x8",5);
   g.drawString(time,colWidth,rowHeight,true);
   g.setFont("6x8",3);
-  g.clear();
   g.drawString(dateArray[2]+"-"+dateArray[1]+"-"+dateArray[3],colWidth,rowHeight*2);
 }
 // special function to handle display switch on
