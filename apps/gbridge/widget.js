1b2f8e740479f4d13f9b76d2881032604cb860a6
Richard de Boer <richard@tubul.net>
2020-07-07 21:41:24 +0200

    gbridge: Nicer display of alarm clock notifications
    My phone generates these events for alarms (even labelled ones):
    {"t":"notify","id":1592721702,"src":"ALARMCLOCKRECEIVER"}
    {"t":"notify-","id":1592721702} // when dismissing the alarm
    
    This commit shows a nice alarm-clock icon and friendlier title instead,
    but preserves event title/body if it is provided after all.
    
    Lifted the icon from apps/alarm/app-icon.js

--- apps/gbridge/widget.js
+++ apps/gbridge/widget.js
@@ -24,9 +24,23 @@
     Bluetooth.println(JSON.stringify(message));
   }
 
+  function prettifyNotificationEvent(event) {
+    switch (event.src) {
+      case "ALARMCLOCKRECEIVER":
+        return {
+          id: event.id,
+          title: event.title || "Alarm",
+          body: event.body,
+          // same icon as apps/alarm/app-icon.js
+          icon: require("heatshrink").decompress(atob("mEwwkGswAhiMRCCAREAo4eHBIQLEAgwYHsIJDiwHB5gACBpIhHCoYZEGA4gFCw4ABGA4HEjgXJ4IXGAwcUB4VEmf//8zogICoJIFAodMBoNDCoIADmgJB4gXIFwXDCwoABngwFC4guB4k/CQXwh4EC+YMCC44iBp4qDC4n/+gNBC41sEIJCEC4v/GAPGC4dhXYRdFC4xhCCYIXCdQRdDC5HzegQXCsxGHC45IDCwQXCUgwXHJAIXGRogXJSIIXcOw4XIPAYXcBwv/mEDBAwXOgtQC65QGC5vzoEAJAx3Nmk/mEABIiPN+dDAQIwFC4zXGFwKRCGAjvMFwQECGAgXI4YuGGAUvAgU8C4/EFwwGCAgdMC4p4EFwobFOwoXDJAIoEAApGBC4xIEABJGHGAapEAAqNBFwwXD4heI+YuBC5BIBVQhdHIw4wD5inFS4IKCCxFmigNCokzCoMzogICoIWIsMRjgPCAA3BiMWC48RBQIXJEgMRFxAJCCw4lEC44IECooOIBAaBJKwhgIAH4ACA==")),
+        };
+      default:
+        return event;
+    }
+  }
   function handleNotificationEvent(event) {
     if (event.t === "notify") {
-      require("notify").show(event);
+      require("notify").show(prettifyNotificationEvent(event));
       Bangle.buzz();
     } else { // notify-
       require("notify").hide(event);
