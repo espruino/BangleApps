785b48f47a3a460a991ab2160573bb45899c71ac
Gordon Williams <gw@pur3.co.uk>
2020-10-21 14:48:30 +0100

    * Added progress bar on Bangle.js for uploads * Provide a proper error message in case JSON decode fails * Check you're connecting with a Bangle.js of the correct version

--- loader.js
+++ loader.js
@@ -11,10 +11,20 @@ if (window.location.host=="banglejs.com") {
     'This is not the official Bangle.js App Loader - you can try the <a href="https://banglejs.com/apps/">Official Version</a> here.';
 }
 
-var APP_SOURCECODE_URL;
+var RECOMMENDED_VERSION = "2v08";
+// could check http://www.espruino.com/json/BANGLEJS.json for this
+
 (function() {
   let username = "espruino";
   let githubMatch = window.location.href.match(/\/(\w+)\.github\.io/);
   if (githubMatch) username = githubMatch[1];
-  APP_SOURCECODE_URL = `https://github.com/${username}/BangleApps/tree/master/apps`;
+  Const.APP_SOURCECODE_URL = `https://github.com/${username}/BangleApps/tree/master/apps`;
 })();
+
+function onFoundDeviceInfo(deviceId, deviceVersion) {
+  if (deviceId != "BANGLEJS") {
+    showToast(`You're using ${deviceId}, not a Bangle.js. Did you want <a href="https://espruino.com/apps">espruino.com/apps</a> instead?` ,"warning", 20000);
+  } else if (versionLess(deviceVersion, RECOMMENDED_VERSION)) {
+    showToast(`You're using an old Bangle.js firmware (${deviceVersion}). You can <a href="https://www.espruino.com/Bangle.js#firmware-updates" target="_blank">update with the instructions here</a>` ,"warning", 20000);
+  }
+}
