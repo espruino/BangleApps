caec113a0b24330353616a0f8feef6692a886157
Gordon Williams <gw@pur3.co.uk>
2020-10-22 17:02:35 +0100

    Added automatic charge animation

---
+++ apps/chargeanim/app-icon.js
@@ -0,0 +1,1 @@
+require("heatshrink").decompress(atob("mEwxH+AH4A/AH4ASwoAIF/4pZABYuuGDIv/F/4v/F/4vt1Ivt6Zft1Ivu6fNF9nT6a+JF8SNBF9ouBXxQvhFwQvrRoTuLF8BeDXxQvfFwYAFGgwvdRoYAGd840GX84AC5rCLF8ReLF8wMJF8K+CRpAvmNhQvhdwIuKF8SNLF8guLF8JdML8Yv/F/4v/F/4v/GDguYAH4A/AGYA=="))
---
+++ apps/chargeanim/boot.js
@@ -0,0 +1,1 @@
+Bangle.on("charging", isCharging => { if (isCharging) load("chargeanim.app.js"); });
