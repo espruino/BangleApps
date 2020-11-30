48beacf01728bfe8e3b14ef167a5bdf5013b9bbf
Gordon Williams <gw@pur3.co.uk>
2020-11-30 11:51:03 +0000

    Use HRM data and calculations from Bangle.js (don't access hardware directly)

--- apps/hrm/heartrate.js
+++ apps/hrm/heartrate.js
@@ -1,74 +1,60 @@
 Bangle.setLCDPower(1);
 Bangle.setLCDTimeout(0);
-Bangle.ioWr(0x80,0)
-x=0;
+Bangle.setHRMPower(1);
+var hrmInfo, hrmOffset = 0;
+var hrmInterval;
+function onHRM(h) {
+  // this is the first time we're called
+  if (counter!==undefined) {
+    counter = undefined;
+    g.clear();
+  }
+  hrmInfo = h;
+  hrmOffset = 0;
+  if (hrmInterval) clearInterval(hrmInterval);
+  hrmInterval = setInterval(readHRM,40);
+
+  var px = g.getWidth()/2;
+  g.setFontAlign(0,0);
+  g.clearRect(0,24,239,90);
+  g.setFont("6x8").drawString("Confidence "+hrmInfo.confidence+"%", px, 75);
+  var str = hrmInfo.bpm;
+  g.setFontVector(40).drawString(str,px,45);
+  px += g.stringWidth(str)/2;
+  g.setFont("6x8");
+  g.drawString("BPM",px+15,45);
+}
+Bangle.on('HRM', onHRM);
+
+// It takes 5 secs for us to get the first HRM event
+var counter = 5;
+function countDown() {
+  E.showMessage("Please wait...\n"+counter--);
+  if (counter) setTimeout(countDown, 1000);
+}
+countDown();
+
+
 var min=0,max=0;
 var wasHigh = 0, wasLow = 0;
 var lastHigh = getTime();
 var hrmList = [];
-var hrm;
+var hrmInfo;
 
 function readHRM() {
-  var a = analogRead(D29);
-  var h = getTime();
-  min=Math.min(min*0.97+a*0.03,a);
-  max=Math.max(max*0.97+a*0.03,a);
-  y = E.clip(170 - (a*960*4),100,230);
-  if (x==0) {
+  if (!hrmInfo) return;
+
+  if (hrmOffset==0) {
     g.clearRect(0,100,239,239);
     g.moveTo(-100,0);
   }
-  /*g.setColor(0,1,0);
-  var z = 170 - (min*960*4); g.fillRect(x,z,x,z);
-  var z = 170 - (max*960*4); g.fillRect(x,z,x,z);*/
-  g.setColor(1,1,1);
-  g.lineTo(x,y);
-  if ((max-min)>0.005) {
-    if (4*a > (min+3*max)) { // high
-      g.setColor(1,0,0);
-      g.fillRect(x,230,x,239);
-      g.setColor(1,1,1);
-      if (!wasHigh && wasLow) {
-        var currentHrm = 60/(h-lastHigh);
-        lastHigh = h;
-        if (currentHrm<250) {
-          while (hrmList.length>12) hrmList.shift();
-          hrmList.push(currentHrm);
-          // median filter
-          var t = hrmList.slice(); // copy
-          t.sort();
-          // average the middle 3
-          var mid = t.length>>1;
-          if (mid+2<t.length)
-            hrm = (t[mid]+t[mid+1]+t[mid+2])/3;
-          else if (mid<t.length)
-            hrm = t[mid];
-          else
-            hrm = 0;
-          g.setFontVector(40);
-          g.setFontAlign(0,0);
-          g.clearRect(0,0,239,100);
-          var str = hrm ? Math.round(hrm) : "?";
-          var px = 120;
-          g.drawString(str,px,40);
-          px += g.stringWidth(str)/2;
-          g.setFont("6x8");
-          g.drawString("BPM",px+20,60);
-        }
-      }
-      wasLow = 0;
-      wasHigh = 1;
-    } else if (4*a < (max+3*min)) { // low
-      wasLow = 1;
-    } else { // middle
-      g.setColor(0.5,0,0);
-      g.fillRect(x,230,x,239);
-      g.setColor(1,1,1);
-      wasHigh = 0;
-    }
+  for (var i=0;i<2;i++) {
+    var a = hrmInfo.raw[hrmOffset];
+    hrmOffset++;
+    min=Math.min(min*0.97+a*0.03,a);
+    max=Math.max(max*0.97+a*0.03,a);
+    y = E.clip(170 - (a*4),100,230);
+    g.setColor(1,1,1);
+    g.lineTo(hrmOffset, y);
   }
-  x++;
-  if (x>239)x=0;
 }
-
-setInterval(readHRM,50);
