4ec41d6d4c0c6e2b2ee5c0e09dd4f15b11e4c239
RenaudG <34276398+renaudgweb@users.noreply.github.com>
2020-12-01 13:50:13 +0100

    Update gps-info.js

--- apps/gpsinfo/gps-info.js
+++ apps/gpsinfo/gps-info.js
@@ -1,4 +1,4 @@
-var img = require("heatshrink").decompress(atob("mEwghC/AH4AKg9wC6t3u4uVC6wWBI6t3uJeVuMQCqcBLisAi4XLxAABFxAXKgc4DBAuBRhQXEDAq7MmYXEwBHEXZYXFGAOqAAKDMmczC4mIC62CC50PC4JIBkQABiIvRmURAAUSjQXSFwMoxGKC6CRFwUSVYgXLPIgXXwMYegoXLJAYXCGBnzGA0hPQIwMgYwGC6gwCC4ZIMC4gYBC604C4ZISmcRVgapQAAMhC6GIJIwXCMBcIxGDDBAuLC4IwGAARGMAAQWGmAXPJQoWMC4pwCCpoXJAB4XXAH4A/ABQA="))
+var img = require("heatshrink").decompress(atob("mEwghC/AH4AKg9wC6t3u4uVC6wWBI6t3uJeVuMQCqcBLisAi4XLxAABFxAXKgc4DBAuBRhQXEDAq7MmYXEwBHEXZYXFGAOqAAKDMmczC4mIC62CC50PC4JIBkQABiIvRmURAAUSjQXSFwMoxGKC6CRFwUSVYgXLPIgXXwMYegoXLJAYXCGBnzGA0hPQIwMgYwGC6gwCC4ZIMC4gYBC604C4ZISmcRVgapQAAMhC6GIJIwXCMBcIxGDDBAuLC4IwGAARGMAAQWGmAXPJQoWMC4pwCCpoXJAB4XXAH4A/ABQA="));
 
 Bangle.setGPSPower(1);
 Bangle.setLCDMode("doublebuffered");
@@ -9,6 +9,7 @@ var lastFix = {
   alt: 0,
   lat: 0,
   lon: 0,
+  course: 0,
   speed: 0,
   time: 0,
   satellites: 0
@@ -19,7 +20,7 @@ function formatTime(now) {
   var fd = now.toUTCString().split(" ");
   var time = fd[4].substr(0, 5);
   var date = [fd[0], fd[1], fd[2]].join(" ");
-  return time + " - " + date;
+  return time + "UTC " + date;
 }
 
 function onGPS(fix) {
@@ -35,6 +36,7 @@ function onGPS(fix) {
     var alt = fix.alt;
     var lat = fix.lat;
     var lon = fix.lon;
+    var course = fix.course;
     var speed = fix.speed;
     var time = formatTime(fix.time);
     var satellites = fix.satellites;
@@ -42,11 +44,12 @@ function onGPS(fix) {
     var s = 15;
     g.setFontVector(s);
     g.drawString("Altitude: "+alt+" m",10,44);
-    g.drawString("Lat: "+lat,10,44+20);
-    g.drawString("Lon: "+lon,10,44+40);
-    g.drawString("Speed: "+speed.toFixed(1)+" km/h",10,44+60);
-    g.drawString("Time: "+time,10,44+80);
-    g.drawString("Satellites: "+satellites,10,44+100);
+    g.drawString("Lat: "+lat.toFixed(6),10,44+20);
+    g.drawString("Lon: "+lon.toFixed(6),10,44+40);
+    g.drawString("course: "+(isNaN(course) ? "---" : course.toFixed(0))+"°",10,44+60);
+    g.drawString("Speed: "+speed.toFixed(1)+" km/h",10,44+80);
+    g.drawString("Time: "+time,10,44+100);
+    g.drawString("Satellites: "+satellites,10,44+120);
   } else {
     g.setFontAlign(0, 1);
     g.setFont("6x8", 2);
