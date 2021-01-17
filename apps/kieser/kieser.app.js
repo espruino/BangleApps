d20279884f06d69b692d30cf9d618880af3c131b
Tobias Neben <tobias.neben@studieren.fernuni-hagen.de>
2021-01-17 22:18:31 +0100

    first steps for Kieser App

---
+++ apps/kieser/kieser.app.js
@@ -0,0 +1,42 @@
+var counter = 30;
+var counterInterval;
+
+function outOfTime() {
+  if (counterInterval) return;
+  E.showMessage("Out of Time", "My Timer");
+  Bangle.buzz();
+  Bangle.beep(200, 4000)
+    .then(() => new Promise(resolve => setTimeout(resolve,200)))
+    .then(() => Bangle.beep(200, 3000));
+  // again, 10 secs later
+  setTimeout(outOfTime, 10000);
+}
+
+function countDown() {
+  counter--;
+  // Out of time
+  if (counter<=0) {
+    clearInterval(counterInterval);
+    counterInterval = undefined;
+    setWatch(startTimer, BTN2);
+    outOfTime();
+    return;
+  }
+
+  g.clear();
+  g.setFontAlign(0,0); // center font
+  g.setFont("Vector",80); // vector font, 80px  
+  // draw the current counter value
+  g.drawString(counter,120,120);
+  // optional - this keeps the watch LCD lit up
+  g.flip();
+}
+
+function startTimer() {
+  counter = 30;
+  countDown();
+  if (!counterInterval)
+    counterInterval = setInterval(countDown, 1000);
+}
+
+startTimer();
---
+++ apps/kieser/kieser.html
@@ -0,0 +1,1 @@
+Hier könnte Ihre Werbung stehen
