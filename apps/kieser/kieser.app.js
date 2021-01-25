4b10688cd20ad6b18b75132f818c4b89c867aaf6
Tobias Neben <tobias.neben@studieren.fernuni-hagen.de>
2021-01-25 08:32:24 +0100

    Update kieser.app.js

--- apps/kieser/kieser.app.js
+++ apps/kieser/kieser.app.js
@@ -1,4 +1,7 @@
 var machineArray = require("Storage").readJSON("kieser-trainingplan.json", false);
+var startDate = new Date().toUTCString();
+
+var trainingTimes = [];
 
 //array to temporary skip machine
 var tempArray = machineArray.slice();
@@ -42,14 +45,15 @@ function showSettings(machine){
 
 function startTraining(machine){
   clearWatch();
-  var time = 120;
+  var time = 0;
   console.log("start training for " + machine.machine);
-
+  
+  function draw(){
   g.clear();
   g.setFontAlign(0,0); // center font
-  g.setFont("Vector",200); // vector font, 80px  
+  g.setFont("Vector",100); // vector font, 80px  
   // draw the current counter value
-  E.showMessage("Timer starten", machine.machine);
+  g.drawString(time, 120, 120);
   
   g.setFont("6x8", 2);
     g.setFontAlign(0, 0, 3);
@@ -57,22 +61,66 @@ function startTraining(machine){
     g.drawString("Stop", 230, 110);
     g.setFont("Vector", 35);
     g.setFontAlign(-1, -1);
+  }
+  
+  setInterval(function () {
+    time++;
+    draw();
+  }, 1000);
+  
   
   setWatch(function() {
-    Bangle.buzz();
+    vibrate();
   }, BTN1, {repeat:true});
 
 
   setWatch(function() {
+    clearInterval();
      setFinished(machine, time);
   }, BTN2, {repeat:true});
   
+}
+
+function vibrate() {
+   
+  //Bangle.buzz()
+  //  .then(() => new Promise(resolve => setTimeout(resolve, 4000)))
+  //  .then(() => Bangle.buzz())
+  //  .then(() => new Promise(resolve => setTimeout(resolve, 2000)))
+   // .then(() => Bangle.buzz());
+
+  setTimeout(function () {
+  Bangle.buzz();
+}, 4000);
+  setTimeout(function () {
+  Bangle.buzz();
+}, 2000);
+  //setTimeout(vibrate, 2000);
 }
 
 function setFinished(machine, time){
   console.log("finished "+ machine.machine); 
   clearWatch();
   machine.finished = true;
+  oldWeight = machine.weight;
+  fiveProcent = Math.ceil(oldWeight / 100 * 5);
+  
+  console.log("alt" + oldWeight);
+  console.log("prozent" + fiveProcent);
+  if (time < 90){
+    newWeight = oldWeight - fiveProcent;
+  } else if (time > 120) {
+    newWeight = oldWeight + fiveProcent;
+  } else {
+    newWeight = oldWeight;
+  }
+  if (newWeight%2 != 0){
+    newWeight++;
+  }
+  console.log("neu" + newWeight);
+  machine.weight = newWeight;
+  require("Storage").writeJSON("kieser-trainingplan.json", machineArray);
+  trainingTimes.push(machine.machine+ " "  + oldWeight + " " + time);
   
   g.clear();
   g.setFontAlign(0,0); // center font
@@ -100,30 +148,33 @@ function showNext(){
   var nextMachine;
   for (let i = 0; i < tempArray.length; i++){
     if (tempArray[i].finished == false){
-      nextMachine = tempArray[i];
+      nextMachine = machineArray[i];
       break;
     }
   }
+  if (nextMachine == undefined) {showFinished();}
   console.log(nextMachine);
   showSettings(nextMachine);
 }
 
+function showFinished(){
+       var dateString = (new Date()).toISOString().substr(0,16).replace("T","_");
+    
+  var resultFile = require("Storage").open("kieser-results.json", a);
+  resultFile.dataString = trainingTimes;
+
+}
+
 function showMenu(){
+  clearWatch();
 
   function createMenuItems(){
     menuObjekt = {};
     menuObjekt[""] = {"title" : "machines left"};
-    /* for (var i = 0; i < tempArray.length; i++){
-      if (tempArray[i].finished == false){
-        key = tempArray[i].machine;
-                
-        menuObjekt[key] = function(){showSettings(tempArray[i])};
-      }
-      
-    } */
+
     tempArray.forEach(function(m, i) {
        if (m.finished == false){
-          menuObjekt[m.machine] = function(){showSettings(m)};
+          menuObjekt[m.machine] = function(){showSettings(m);};
           }
     });
     
@@ -142,6 +193,7 @@ function init(){
    var finished = false;
    machine.finished = finished;
   }
+  console.log(startDate);
   
   g.clear();
   g.setFontAlign(0,0); // center font
