54603ca47be2eb6b7da88dee528f7466b8969a74
nujw <mike@kereru.com>
2021-01-29 11:16:28 +1300

    Update settings.js

--- apps/speedalt/settings.js
+++ apps/speedalt/settings.js
@@ -1,6 +1,6 @@
 (function(back) {
   let settings = require('Storage').readJSON('speedalt.json',1)||{};
-  var vibrate = settings.buzz||1;
+  settings.buzz = settings.buzz||1;
   
    function setUnits(m,u) {
     settings['spd'] = m;
@@ -25,9 +25,9 @@
     'Units' : function() { E.showMenu(unitsMenu); },
     'Colours' : function() { E.showMenu(colMenu); },
     'Vibrate' : {
-    value : vibrate,
+    value : settings.buzz,
     format : v => v?"On":"Off",
-    onchange : v => { settings.buzz = v; require('Storage').write('speedalt.json',settings); }
+    onchange : () => { settings.buzz = !settings.buzz; require('Storage').write('speedalt.json',settings); }
   };
   
   const unitsMenu = {
