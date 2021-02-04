3d7ddfca15adf4a294a9d016ad0f6776d115a646
adrian w kirk <adrianwkirk@gmail.com>
2021-02-04 00:29:35 +0000

    - Bug fix to onTap behviour - Generaization so that more languages can be added

--- apps/xclock/xclock.js
+++ apps/xclock/xclock.js
@@ -16,22 +16,37 @@ const tensStr = ["ZERO", "TEN", "TWENTY", "THIRTY", "FOURTY",
 */
 class ShiftText {
   constructor(x,y,txt,font_name,
-              font_size,speed_x,speed_y,freq_millis){
+              font_size,speed_x,speed_y,freq_millis, color){
     this.x = x;
     this.tgt_x = x;
+    this.init_x = x;
     this.y = y;
     this.tgt_y = y;
+    this.init_y = y;
     this.txt = txt;
+    this.init_txt = txt;
     this.font_name = font_name;
     this.font_size = font_size;
     this.speed_x = Math.abs(speed_x);
     this.speed_y = Math.abs(speed_y);
     this.freq_millis = freq_millis;
+    this.colour = color;
     this.finished_callback=null;
+    this.timeoutId = null;
+  }
+  reset(){
+    this.hide();
+    this.x = this.init_x;
+    this.y = this.init_y;
+    this.txt = this.init_txt;
+    this.show();
+    if(this.timeoutId != null){
+       clearTimeout(this.timeoutId);
+    }
   }
   show() {
     g.setFont(this.font_name,this.font_size);
-    g.setColor(1,1,1);
+    g.setColor(this.colour[0],this.colour[1],this.colour[2]);
     g.drawString(this.txt, this.x, this.y);
   }
   hide(){
@@ -49,8 +64,22 @@ class ShiftText {
     this.txt = txt;
     this.show();
   }
+  setTextXPosition(txt,x){
+    this.hide();
+    this.x = x;
+    this.txt = txt;
+    this.show();
+  }
   moveTo(new_x,new_y){
     this.tgt_x = new_x;
+    this.tgt_y = new_y;
+    this._doMove();
+  }
+  moveToX(new_x){
+    this.tgt_x = new_x;
+    this._doMove();
+  }
+  moveToY(new_y){
     this.tgt_y = new_y;
     this._doMove();
   }
@@ -91,9 +120,10 @@ class ShiftText {
       }
     }
     this.show();
+    this.timeoutId = null;
     finished = finished_x & finished_y;
     if(!finished){
-      setTimeout(this._doMove.bind(this), this.freq_millis);
+      this.timeoutId = setTimeout(this._doMove.bind(this), this.freq_millis);
     } else if(this.finished_callback != null){
       this.finished_callback.call();
       this.finished_callback = null;
@@ -102,87 +132,83 @@ class ShiftText {
 }
 
 
-let hour_shift_txt = new ShiftText(240,50,'',"Vector",40,5,5,100);
-let min_shift_txt = new ShiftText(240,100,'',"Vector",20,5,5,100);
-let min_remainder_shift_txt = new ShiftText(240,120,'',"Vector",20,5,5,125);
+class DateFormatter { 
+  formatDate(date){
+    return ["","",""];
+  }
+}
+
+class EnglishDateFormatter extends DateFormatter{
+  formatDate(date){
+    // First display the hours as a text number
+    let hours = date.getHours();
+    if(hours == 0){
+      hours = 12;
+    } else if(hours > 12){
+      hours = hours - 12;
+    }
+    new_hours = numberStr[hours]; 
+    // If the mins is over 20 we have to display the text on 2 lines
+    // Otherwise we just output our defined numbers from 1 to 20
+    let mins = date.getMinutes();
+    new_mins = '';
+    new_mins_remainder = '';
+    if(mins > 20){
+      tens = (mins / 10 | 0);
+      new_mins = tensStr[tens];
+      let remainder = mins - tens * 10;
+      if(remainder > 0){
+        new_mins_remainder = numberStr[remainder];
+      }
+    } else if(mins > 0) {
+      new_mins = numberStr[mins];
+    }
+    return [new_hours,new_mins,new_mins_remainder];
+  }
+}
+
+let row_displays = [ new ShiftText(240,50,'',"Vector",40,10,10,50,[1,1,1]),
+  new ShiftText(240,100,'',"Vector",20,10,10,50,[0.85,0.85,0.85]),
+  new ShiftText(240,120,'',"Vector",20,10,10,50,[0.85,0.85,0.85])
+];
+
+let date_formatter = new EnglishDateFormatter();
+
+function reset_clock(){
+  console.log("reset_clock");
+  this.hour_shift_txt.reset();
+  this.min_shift_txt.reset();
+  this.min_remainder_shift_txt.reset();
+}
 
 function draw_clock(){
   console.log("draw_clock");
   let date = new Date();
-  // First display the hours as a text number
-  let hours = date.getHours();
-  if(hours == 0 ){
-    hours = 12;
-  } else if(hours > 12){
-    hours = hours - 12;
-  }
-  // If the hour string has changed then we move out the old
-  // hours and move in the new hour string
-  // If its the first time through the text is empty
-  // so we just display without movement (otherewise the user 
-  // will think its not working
-  new_hours = numberStr[hours];
-  if(new_hours != hour_shift_txt.txt && hour_shift_txt.txt != ''){
-    hour_shift_txt.moveTo(-100,50);
-    hour_shift_txt.onFinished(
-      function(){
-        hour_shift_txt.setTextPosition(new_hours,240,50);
-        hour_shift_txt.moveTo(20,50);
-        hour_shift_txt.onFinished(function(){console.log("hour finished");});
-      }
-    );
-  } else {
-    hour_shift_txt.setTextPosition(new_hours,20,50);
-  }
-  // If the mins is over 20 we have to display the text on 2 lines
-  // Otherwise we just output our defined numbers from 1 to 20
-  let mins = date.getMinutes();
-  new_mins = '';
-  new_mins_remainder = '';
-  if(mins > 20){
-    tens = (mins / 10 | 0);
-    new_mins = tensStr[tens];
-    let remainder = mins - tens * 10;
-    if(remainder > 0){
-      new_mins_remainder = numberStr[remainder];
-    }
-  } else if(mins > 0) {
-    new_mins = numberStr[mins];
-  }
-  // If its the first time through the the text is moved in from 
-  // the right
-  // If the minute text has changed we move the old text
-  // off screen and the new text on. 
-  if(min_shift_txt.txt == ''){
-    min_shift_txt.setTextPosition(new_mins,240,100);
-    min_shift_txt.moveTo(20,100);
-  } else if(new_mins != min_shift_txt.txt){
-    min_shift_txt.moveTo(-100,100);
-    min_shift_txt.onFinished(
-      function(){
-        min_shift_txt.setTextPosition(new_mins,240,100);
-        min_shift_txt.moveTo(20,100);
-      }
-    );
-  } else {
-    min_shift_txt.setTextPosition(new_mins,20,100);
-  }
-  // finally we deal with the remainder line in the same way.
-  if(min_remainder_shift_txt.txt == ''){
-    min_remainder_shift_txt.setTextPosition(new_mins_remainder,240,125);
-    min_remainder_shift_txt.moveTo(20,125);
-  } else if(new_mins_remainder != min_remainder_shift_txt.txt){
-    min_remainder_shift_txt.moveTo(-100,125);
-    min_remainder_shift_txt.onFinished(
-      function(){
-        min_remainder_shift_txt.setTextPosition(new_mins_remainder,240,125);
-        min_remainder_shift_txt.moveTo(20,125);
-      }
-    );
-  } else {
-    min_remainder_shift_txt.setTextPosition(new_mins_remainder,240,125);
+  rows = date_formatter.formatDate(date);
+  var i;
+  for (i = 0; i < rows.length; i++) {
+    display = row_displays[i];
+    txt = rows[i];
+    display_row(display,txt);
   }
   console.log(date);
+}
+
+function display_row(display,txt){
+  if(display.txt == ''){
+    display.setTextXPosition(txt,240);
+    display.moveToX(20);
+  } else if(txt != display.txt){
+    display.moveToX(-100);
+    display.onFinished(
+      function(){
+        display.setTextXPosition(txt,240);
+        display.moveToX(20);
+      }
+    );
+  } else {
+    display.setTextXPosition(txt,20);
+  }
 }
 
 function clearTimers(){
@@ -212,9 +238,11 @@ Bangle.on('lcdPower', (on) => {
   if (on) {
     console.log("lcdPower: on");
     Bangle.drawWidgets();
+    reset_clock();
     startTimers();
   } else {
     console.log("lcdPower: off");
+    reset_clock();
     clearTimers();
   }
 });
