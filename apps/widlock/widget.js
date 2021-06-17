ac751105047f71923d07accd800653c509d15259
Gordon Williams <gw@pur3.co.uk>
2021-06-17 14:14:09 +0100

    widlock: Handle new firmwares with 'lock' event

--- apps/widlock/widget.js
+++ apps/widlock/widget.js
@@ -1,10 +1,12 @@
 (function(){
-  Bangle.on('lcdPower', function(on) {
-    WIDGETS["lock"].width = Bangle.isLCDOn()?0:16;
+  Bangle.on(Bangle.isLocked?'lock':'lcdPower', function(on) {
+    WIDGETS["lock"].width = Bangle.isLocked()?0:16;
     Bangle.drawWidgets();
   });
-  WIDGETS["lock"]={area:"tl",width:Bangle.isLCDOn()?0:16,draw:function(w) {
-    if (!Bangle.isLCDOn())
+  if (Bangle.isLocked===undefined)
+    Bangle.isLocked = Bangle.isLCDOn;
+  WIDGETS["lock"]={area:"tl",width:Bangle.isLocked()?0:16,draw:function(w) {
+    if (!Bangle.isLocked())
       g.reset().drawImage(atob("DhABH+D/wwMMDDAwwMf/v//4f+H/h/8//P/z///f/g=="), w.x+1, w.y+4);
   }};
 })()
