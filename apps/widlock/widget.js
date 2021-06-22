50c7c15a503f2e1c10805d047c13b9c1e3f9e294
Gordon Williams <gw@pur3.co.uk>
2021-06-22 09:36:57 +0100

    tweaks for low BPP screens

--- apps/widlock/widget.js
+++ apps/widlock/widget.js
@@ -1,12 +1,11 @@
 (function(){
-  Bangle.on(Bangle.isLocked?'lock':'lcdPower', function(on) {
-    WIDGETS["lock"].width = Bangle.isLocked()?0:16;
+  if (!Bangle.isLocked) return; // bail out on old firmware
+  Bangle.on("lock", function(on) {
+    WIDGETS["lock"].width = Bangle.isLocked()?16:0;
     Bangle.drawWidgets();
   });
-  if (Bangle.isLocked===undefined)
-    Bangle.isLocked = Bangle.isLCDOn;
-  WIDGETS["lock"]={area:"tl",width:Bangle.isLocked()?0:16,draw:function(w) {
-    if (!Bangle.isLocked())
+  WIDGETS["lock"]={area:"tl",width:Bangle.isLocked()?16:0,draw:function(w) {
+    if (Bangle.isLocked())
       g.reset().drawImage(atob("DhABH+D/wwMMDDAwwMf/v//4f+H/h/8//P/z///f/g=="), w.x+1, w.y+4);
   }};
 })()
