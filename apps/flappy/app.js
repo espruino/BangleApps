466fef2baf3b198113a7e376a9171f052265cac9
Gordon Williams <gw@pur3.co.uk>
2021-08-26 16:53:43 +0100

    Bangle 2 support and misc game tweaks

--- apps/flappy/app.js
+++ apps/flappy/app.js
@@ -1,3 +1,21 @@
+b = Graphics.createArrayBuffer(120,120,8);
+var gimg = {
+    width:120,
+    height:104,
+    bpp:8,
+    buffer:b.buffer
+  };
+
+if (process.env.HWVERSION==2) {
+  b.flip = function() {
+    g.drawImage(gimg,28,50);
+  };
+} else {
+  b.flip = function() {
+    g.drawImage(gimg,0,24,{scale:2});
+  };
+}
+
 var BIRDIMG = E.toArrayBuffer(atob("EQyI/v7+/v7+/gAAAAAAAP7+/v7+/v7+/gYG0tLS0gDXAP7+/v7+/v4A0tLS0tIA19fXAP7+/v4AAAAA0tLS0gDX1wDXAP7+ANfX19cA0tLSANfXANcA/v4A19fX19cA0tLSANfX1wD+/gDS19fX0gDS0tLSAAAAAAD+/gDS0tIA0tLS0gDAwMDAwAD+/gAAAM3Nzc0AwAAAAAAA/v7+/v4Azc3Nzc0AwMDAwAD+/v7+/v4AAM3Nzc0AAAAAAP7+/v7+/v7+AAAAAP7+/v7+/g=="))
 var FLOORIMG = require("heatshrink").decompress(atob("iEKxH+kklABuLAAlgAAwNFB34OLmAAO0YAO5wAOA"));
 
@@ -33,26 +51,26 @@ function gameStop() {
 
 function draw() {
   "ram"
-  var H = g.getHeight()-24;
-  g.setColor("#71c6cf");
-  g.fillRect(0,0,g.getWidth(),H-1);
+  var H = b.getHeight()-24;
+  b.setColor("#71c6cf");
+  b.fillRect(0,0,b.getWidth(),H-1);
   floorpos++;
-  for (var x=-(floorpos&15);x<g.getWidth();x+=16)
-    g.drawImage(FLOORIMG,x,H);
+  for (var x=-(floorpos&15);x<b.getWidth();x+=16)
+    b.drawImage(FLOORIMG,x,H);
 
 
   if (!running) {
-    var x = g.getWidth()/2;
-    g.setColor("#000000");
-    g.setFontAlign(0,0);
-    g.setFont("4x6",2);
-    g.drawString("GAME OVER!",x,20);
-    g.setFont("6x8",1);
-    g.drawString("Score",x,40);
-    g.drawString(score,x,56);
-    g.drawString("Tap screen to",x,76);
-    g.drawString("restart and flap",x,84);
-    g.flip();
+    var x = b.getWidth()/2;
+    b.setColor("#000000");
+    b.setFontAlign(0,0);
+    b.setFont("4x6",2);
+    b.drawString("GAME OVER!",x,20);
+    b.setFont("6x8",1);
+    b.drawString("Score",x,40);
+    b.drawString(score,x,56);
+    b.drawString("Tap screen to",x,76);
+    b.drawString("restart and flap",x,84);
+    b.flip();
     return;
   }
 
@@ -63,30 +81,30 @@ function draw() {
   if (birdy > H)
     gameStop();
   // draw bird
-  g.drawImage(BIRDIMG, 6,birdy, {rotate:Math.atan2(birdvy,15)});
+  b.drawImage(BIRDIMG, 6,birdy, {rotate:Math.atan2(birdvy,15)});
   // draw barriers
-  barriers.forEach(function(b) {
-    b.x1--;
-    b.x2--;
-    var btop = b.y-b.gap;
-    var bbot = b.y+b.gap;
-    g.setColor("#73bf2f"); // middle
-    g.fillRect(b.x1+4, 0, b.x2-4, btop-1);
-    g.fillRect(b.x1+4, bbot, b.x2-4, H-1);
-    g.setColor("#c0f181"); // left
-    g.fillRect(b.x1+1, 0, b.x1+3, btop-1);
-    g.fillRect(b.x1+1, bbot, b.x1+3, H-1);
-    g.setColor("#538917"); // right
-    g.fillRect(b.x2-3, 0, b.x2-1, btop-1);
-    g.fillRect(b.x2-3, bbot, b.x2-1, H-1);
-    g.setColor("#808080"); // outlines
-    g.drawRect(b.x1, btop-5, b.x2, btop); // top
-    g.drawLine(b.x1+1, 0, b.x1+1, btop-6);
-    g.drawLine(b.x2-2, 0, b.x2-2, btop-6);
-    g.drawRect(b.x1, bbot, b.x2, bbot+5); // bottom
-    g.drawLine(b.x1+1, bbot+6, b.x1+1, H-1);
-    g.drawLine(b.x2-1, bbot+6, b.x2-1, H-1);
-    if (b.x1<6 && (birdy-3<btop || birdy+3>bbot))
+  barriers.forEach(function(r) {
+    r.x1--;
+    r.x2--;
+    var btop = r.y-r.gap;
+    var bbot = r.y+r.gap;
+    b.setColor("#73bf2f"); // middle
+    b.fillRect(r.x1+4, 0, r.x2-4, btop-1);
+    b.fillRect(r.x1+4, bbot, r.x2-4, H-1);
+    b.setColor("#c0f181"); // left
+    b.fillRect(r.x1+1, 0, r.x1+3, btop-1);
+    b.fillRect(r.x1+1, bbot, r.x1+3, H-1);
+    b.setColor("#538917"); // right
+    b.fillRect(r.x2-3, 0, r.x2-1, btop-1);
+    b.fillRect(r.x2-3, bbot, r.x2-1, H-1);
+    b.setColor("#808080"); // outlines
+    b.drawRect(r.x1, btop-5, r.x2, btop); // top
+    b.drawLine(r.x1+1, 0, r.x1+1, btop-6);
+    b.drawLine(r.x2-2, 0, r.x2-2, btop-6);
+    b.drawRect(r.x1, bbot, r.x2, bbot+5); // bottom
+    b.drawLine(r.x1+1, bbot+6, r.x1+1, H-1);
+    b.drawLine(r.x2-1, bbot+6, r.x2-1, H-1);
+    if (r.x1<6 && (birdy-3<btop || birdy+3>bbot))
       gameStop();
   });
   while (barriers.length && barriers[0].x2<=0) {
@@ -94,7 +112,7 @@ function draw() {
     newBarrier(g.getWidth());
   }
 
-  g.flip();
+  b.flip();
 }
 
 Bangle.on('touch', function(button) {
@@ -105,11 +123,9 @@ Bangle.on('touch', function(button) {
   }
 });
 
-// Finally, start everything going
-setTimeout(()=>{
-  Bangle.setLCDMode("120x120");
-  g.setBgColor("#e3db9d");
-  g.clear();
-  gameStart();
-  setInterval(draw, 100);
-},10);
+Bangle.loadWidgets();
+g.clear();
+Bangle.drawWidgets();
+b.setBgColor("#e3db9d");
+gameStart();
+setInterval(draw, 100);
