12ee7f830323475b039b4307404d168397b61f4b
Smooklu <37220586+Smooklu@users.noreply.github.com>
2021-09-07 10:53:23 -0500

    Update FD6FDetect.app.js

--- apps/fd6fdetect/FD6FDetect.app.js
+++ apps/fd6fdetect/FD6FDetect.app.js
@@ -2,7 +2,7 @@
 //Bangle.setLCDTimeout(0); // debugging purposes
 g.clear(); // clear screen
 let amount = 'global value'; // make amount global
-function FindCOVIDAwareBeacons() { // function for finding COVID beacons
+function FindFD6FBeacons() { // function for finding COVID beacons
 NRF.findDevices(function(devices) { // function for searching for devices with the UUID of FD6F
   g.setFont('Vector', 75); // set size of font to 75x
   g.setFontAlign(0,0); // align font
@@ -11,15 +11,15 @@ NRF.findDevices(function(devices) { // function for searching for devices with t
   g.drawString(amount, 125, 100); // draw amount of devices matching FD6F
   if (amount == 1) { // check to see if we need to use the word users or user
   g.setFont('Vector', 25); // set size of font to 25x
-  g.drawString('COVIDAwareMN', 125, 150); // draw string
-  g.drawString('user', 125, 175); // draw string
+  g.drawString('FD6F', 125, 150); // draw string
+  g.drawString('beacon', 125, 175); // draw string
   g.drawString('nearby', 125, 200); // draw string
   } else{ // if more than one change user to users
   g.setFont('Vector', 25); // set size of font to 25x
-  g.drawString('COVIDAwareMN', 125, 150); // draw string
-  g.drawString('users', 125, 175); // draw string
+  g.drawString('FD6F', 125, 150); // draw string
+  g.drawString('beacons', 125, 175); // draw string
   g.drawString('nearby', 125, 200); // draw string
   }
 }, {timeout : 1000, filters : [{services: ['fd6f'] }] }); // set filters to target only FD6F beacons
 } 
-setInterval(FindCOVIDAwareBeacons, 2000); // poll for new devices
+setInterval(FindFD6FBeacons, 2000); // poll for new devices
