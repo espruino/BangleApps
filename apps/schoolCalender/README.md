135165c983beca48190d091e355436100beee41d
Ronin0000 <89286474+Ronin0000@users.noreply.github.com>
2021-09-16 18:37:52 -0700

    Add files via upload

---
+++ apps/schoolCalender/README.md
@@ -0,0 +1,9 @@
+# Bangle.js Calendar
+
+School Calender is a calender that you can see your upcoming classes or schedule.
+
+## Versions:
+
+Version 1.00: Get Design Working
+
+Version 2.00: Update Graphics
---
+++ apps/schoolCalender/comments.js
@@ -0,0 +1,25 @@
+/*
+if(currentHour >= schedule[i].startingTimeHour && currentMinute >= schedule[i].startingTimeMinute){
+  if(currentHour <= schedule[i].endingTimeHour){
+    console.log("Time of Day     "+schedule[i].description);
+    g.drawString(i + ": "+schedule[i].description, 10, 10*i-100);
+  }
+
+  //console.log("DayOfWeek:"+currentDayOfWeek+", Hour:"+ currentHour + ", Minute:" + currentMinute);
+    //console.log("DayOfWeek:"+schedule[i].dayOfWeek+", StartHour:"+ schedule[i].startingTimeHour +", EndHour:" + schedule[i].endingTimeHour + ", StartMinute:" + schedule[i].startingTimeMinute + ", EndMinute:" + schedule[i].endingTimeMinute);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+ */
