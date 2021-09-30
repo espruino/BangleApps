c8e86dc78ac1e325874aacac7b93ce321635a63e
Gordon Williams <gw@pur3.co.uk>
2021-09-30 16:14:44 +0100

    fix after recent layout tweaks

--- apps/accellog/app.js
+++ apps/accellog/app.js
@@ -94,9 +94,9 @@ function startRecord(force) {
   var Layout = require("Layout");
   var layout = new Layout({ type: "v", c: [
       {type:"txt", font:"6x8", label:"Samples", pad:2},
-      {type:"txt", id:"samples", font:"6x8:2", label:"  -  ", pad:5},
+      {type:"txt", id:"samples", font:"6x8:2", label:"  -  ", pad:5, bgCol:g.theme.bg},
       {type:"txt", font:"6x8", label:"Time", pad:2},
-      {type:"txt", id:"time", font:"6x8:2", label:"  -  ", pad:5},
+      {type:"txt", id:"time", font:"6x8:2", label:"  -  ", pad:5, bgCol:g.theme.bg},
       {type:"txt", font:"6x8:2", label:"RECORDING", bgCol:"#f00", pad:5, fillx:1},
     ]
   },[ // Buttons...
