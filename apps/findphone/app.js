6817cafba4afac0d21c432c6f29587642ccb4c55
hughbarney <hughbarney@googlemail.com>
2021-10-02 14:13:20 +0100

    Findphone - fix for Bangle 2

--- apps/findphone/app.js
+++ apps/findphone/app.js
@@ -1,13 +1,15 @@
 //notify your phone
 
+const fontSize = g.getWidth() / 8;
 var finding = false;
 
 function draw() {
   // show message
-  g.clear(1);
-  require("Font8x12").add(Graphics);
-  g.setFont("8x12",3);
+  g.clear(g.theme.bg); 
+  g.setColor(g.theme.fg);
+  g.setFont("Vector", fontSize);
   g.setFontAlign(0,0);
+  
   if (finding) {
     g.drawString("Finding...", g.getWidth()/2, (g.getHeight()/2)-20);
     g.drawString("Click to stop", g.getWidth()/2, (g.getHeight()/2)+20);
@@ -27,7 +29,26 @@ draw();
 
 //register all buttons and screen to find phone
 setWatch(find, BTN1, {repeat:true});
-setWatch(find, BTN2, {repeat:true});
-setWatch(find, BTN3, {repeat:true});
-setWatch(find, BTN4, {repeat:true});
-setWatch(find, BTN5, {repeat:true});
+
+if (process.env.HWVERSION == 1) {
+  setWatch(find, BTN2, {repeat:true});
+  setWatch(find, BTN3, {repeat:true});
+  setWatch(find, BTN4, {repeat:true});
+  setWatch(find, BTN5, {repeat:true});
+}
+
+if (process.env.HWVERSION == 2) {
+  Bangle.on('touch', function(button, xy) {
+   
+    // click top part of the screen to stop start
+    if (xy.y < g.getHeight() / 2) {
+      find();
+      return;
+    } else {
+      // exit app
+      Bluetooth.println("\n"+JSON.stringify({t:"findPhone", n:false}));
+      load();
+    }
+  });
+}
+
