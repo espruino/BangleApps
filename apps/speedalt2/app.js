633552e0a3ad455b01e784fb4de8c20b6413a05f
nujw <mike@kereru.com>
2021-10-22 08:30:07 +1300

    Update app.js

--- apps/speedalt2/app.js
+++ apps/speedalt2/app.js
@@ -5,7 +5,7 @@ Mike Bennett mike[at]kereru.com
 0.06 : Add Posn screen
 0.07 : Add swipe to change screens same as BTN3
 */
-var v = '1.01b';
+var v = '1.02';
 
 /*kalmanjs, Wouter Bulten, MIT, https://github.com/wouterbulten/kalmanjs */
 var KalmanFilter = (function () {
