474d705c300b29be00ce91b5d3cfeef1542d9015
nujw <mike@kereru.com>
2021-10-25 08:55:35 +1300

    Update settings.js

--- apps/slomoclock/settings.js
+++ apps/slomoclock/settings.js
@@ -6,11 +6,29 @@
     require('Storage').write('slomoclock.json',settings);
   }
   
+  function setColour(c) {
+    settings.colour = c;
+    writeSettings();
+  }
+  
   const appMenu = {
     '': {'title': 'SloMo Clock'},
     '< Back': back,
-    'Widget Space Top' : {value : settings.widTop, format : v => v?"On":"Off",onchange : () => { settings.widTop = !settings.widTop; writeSettings(); },
-    'Widget Space Bottom' : {value : settings.widBot, format : v => v?"On":"Off",onchange : () => { settings.widBot = !settings.widBot; writeSettings(); }
+    'Colours' : function() { E.showMenu(colMenu); }
+    //,'Widget Space Top' : {value : settings.widTop, format : v => v?"On":"Off",onchange : () => { settings.widTop = !settings.widTop; writeSettings(); }
+    //,'Widget Space Bottom' : {value : settings.widBot, format : v => v?"On":"Off",onchange : () => { settings.widBot = !settings.widBot; writeSettings(); }
+  };
+  
+  const colMenu = {
+    '': {'title': 'Colours'},
+    '< Back': function() { E.showMenu(appMenu); },
+    'Surprise' : function() { setColour(0); },
+    'Red' : function() { setColour(1); },
+    'Orange' : function() { setColour(2); },
+    'Yellow' : function() { setColour(3); },
+    'Green' : function() { setColour(4); },
+    'Blue' : function() { setColour(5); },
+    'Violet' : function() { setColour(6); }
   };
   
   E.showMenu(appMenu);
