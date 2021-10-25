fbcf33961614544fd603a1da1bf56c468eb5bbbb
qucchia <yijods>
2021-10-25 16:52:52 +0200

    Take boot out of unnecessary wrapper

--- apps/hardalarm/boot.js
+++ apps/hardalarm/boot.js
@@ -1,26 +1,24 @@
 // Check for alarms
-(function () {
-  const alarms = require("Storage").readJSON("hardalarm.json", 1) || [];
-  const time = new Date();
-  let active = alarms.filter((a) => a.on);
-  if (active.length) {
-    active = active.sort((a, b) => a.hr - b.hr + (a.last - b.last) * 24);
-    const hr =
-      time.getHours() + time.getMinutes() / 60 + time.getSeconds() / 3600;
-    if (!require("Storage").read("hardalarm.js")) {
-      console.log("No alarm app!");
-      require("Storage").write("hardalarm.json", "[]");
-    } else {
-      let t = 3600000 * (active[0].hr - hr);
-      if (active[0].last == time.getDate() || t < 0) t += 86400000;
-      if (t < 1000) t = 1000;
-      /* execute alarm at the correct time. We avoid execing immediately
+const alarms = require("Storage").readJSON("hardalarm.json", 1) || [];
+const time = new Date();
+let active = alarms.filter((a) => a.on);
+if (active.length) {
+  active = active.sort((a, b) => a.hr - b.hr + (a.last - b.last) * 24);
+  const hr =
+    time.getHours() + time.getMinutes() / 60 + time.getSeconds() / 3600;
+  if (!require("Storage").read("hardalarm.js")) {
+    console.log("No alarm app!");
+    require("Storage").write("hardalarm.json", "[]");
+  } else {
+    let t = 3600000 * (active[0].hr - hr);
+    if (active[0].last == time.getDate() || t < 0) t += 86400000;
+    if (t < 1000) t = 1000;
+    /* execute alarm at the correct time. We avoid execing immediately
       since this code will get called AGAIN when hardalarm.js is loaded. alarm.js
       will then clearInterval() to get rid of this call so it can proceed
       normally. */
-      setTimeout(function () {
-        load("hardalarm.js");
-      }, t);
-    }
+    setTimeout(function () {
+      load("hardalarm.js");
+    }, t);
   }
-})();
+}
