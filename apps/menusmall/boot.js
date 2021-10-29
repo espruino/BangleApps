f2f500e474109e91b8f83d5d2e76ee79bb09c194
Richard de Boer <git@tubul.net>
2021-10-29 18:35:55 +0200

    menusmall: add `wrap` option

--- apps/menusmall/boot.js
+++ apps/menusmall/boot.js
@@ -100,8 +100,10 @@ E.showMenu = function(items) {
       if (l.selectEdit) {
         var item = l.selectEdit;
         item.value -= (dir||1)*(item.step||1);
-        if (item.min!==undefined && item.value<item.min) item.value = item.min;
-        if (item.max!==undefined && item.value>item.max) item.value = item.max;
+        if (item.min!==undefined && item.value<item.min)
+          item.value = (item.wrap && item.max!==undefined) ? item.max : item.min;
+        if (item.max!==undefined && item.value>item.max)
+          item.value = (item.wrap && item.min!==undefined) ? item.min : item.max;
         if (item.onchange) item.onchange(item.value);
         l.draw(options.selected,options.selected);
       } else {
