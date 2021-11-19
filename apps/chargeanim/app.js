a04ca73b0b6d715a1d2ac9e7137af0bfa6e55926
andrewmk <1872642+andrewmk@users.noreply.github.com>
2021-11-19 11:47:28 +0000

    Bangle.js 2 compatibility

--- apps/chargeanim/app.js
+++ apps/chargeanim/app.js
@@ -6,6 +6,7 @@ var imgbubble = require("heatshrink").decompress(atob("i0UhAebgoAFCaYXNBocjAAIWN
 var W=g.getWidth(),H=g.getHeight();
 var b2v = (W != 240)?-1:1;
 var b2rot = (W != 240)?Math.PI:0;
+var b2scale = W/240.0;
 var bubbles = [];
 for (var i=0;i<10;i++) {
   bubbles.push({y:Math.random()*H,ly:0,x:(0.5+(i<5?i:i+8))*W/18,v:0.6+Math.random(),s:0.5+Math.random()});
@@ -22,9 +23,9 @@ function anim() {
       f.y=H+8;
     else if (f.y > (H+8))
       f.y=0;
-    g.drawImage(imgbubble,f.y,f.x,{scale:f.s, rotate:b2rot});
+    g.drawImage(imgbubble,f.y,f.x,{scale:f.s * b2scale, rotate:b2rot});
   });
-  g.drawImage(imgbat, mx,my,{scale:W/240.0, rotate:Math.sin(getTime()*2)*0.5-Math.PI/2 + b2rot});
+  g.drawImage(imgbat, mx,my,{scale:b2scale, rotate:Math.sin(getTime()*2)*0.5-Math.PI/2 + b2rot});
   g.flip();
 }
 
