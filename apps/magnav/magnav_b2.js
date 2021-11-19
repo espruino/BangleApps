2fe779d02ba15a7646a9f9e7db9077f358f781a5
jeffmer <jeffreymagee@gmail.com>
2021-11-19 15:08:45 +0000

    update multiclock + initial B2 magnav

--- apps/magnav/magnav_b2.js
+++ apps/magnav/magnav_b2.js
@@ -1,50 +1,42 @@
 
-const Yoff = 80;
-var pal2color = new Uint16Array([0x0000,0xffff,0x07ff,0xC618],0,2);
-var buf = Graphics.createArrayBuffer(240,60,2,{msb:true});
-Bangle.setLCDTimeout(30);
-
-function flip(b,y) {
- g.drawImage({width:240,height:60,bpp:2,buffer:b.buffer, palette:pal2color},0,y);
- b.clear();
-}
+const Ypos = 40;
 
 const labels = ["N","NE","E","SE","S","SW","W","NW"];
 var brg=null;
 
 function drawCompass(course) {
-  buf.setColor(1);
-  buf.setFont("Vector",24);
+  "ram"
+  g.setColor(g.theme.fg);
+  g.setFont("Vector",18);
   var start = course-90;
   if (start<0) start+=360;
-  buf.fillRect(28,45,212,49);
-  var xpos = 30;
+  g.fillRect(16,Ypos+45,160,Ypos+49);
+  var xpos = 16;
   var frag = 15 - start%15;
-  if (frag<15) xpos+=frag; else frag = 0;
+  if (frag<15) xpos+=Math.floor((frag*4)/5); else frag = 0;
   for (var i=frag;i<=180-frag;i+=15){
     var res = start + i;
     if (res%90==0) {
-      buf.drawString(labels[Math.floor(res/45)%8],xpos-8,0);
-      buf.fillRect(xpos-2,25,xpos+2,45);
+      g.drawString(labels[Math.floor(res/45)%8],xpos-6,Ypos+6);
+      g.fillRect(xpos-2,Ypos+25,xpos+2,Ypos+45);
     } else if (res%45==0) {
-      buf.drawString(labels[Math.floor(res/45)%8],xpos-12,0);
-      buf.fillRect(xpos-2,30,xpos+2,45);
+      g.drawString(labels[Math.floor(res/45)%8],xpos-9,Ypos+6);
+      g.fillRect(xpos-2,Ypos+30,xpos+2,Ypos+45);
     } else if (res%15==0) {
-      buf.fillRect(xpos,35,xpos+1,45);
+      g.fillRect(xpos,Ypos+35,xpos+1,Ypos+45);
     }
-    xpos+=15;
+    xpos+=12;
   }
   if (brg) {
     var bpos = brg - course;
     if (bpos>180) bpos -=360;
     if (bpos<-180) bpos +=360;
-    bpos+=120;
-    if (bpos<30) bpos = 14;
-    if (bpos>210) bpos = 226;
-    buf.setColor(2);
-    buf.fillCircle(bpos,40,8);
+    bpos= Math.floor((bpos*4)/5)+88;
+    if (bpos<16) bpos = 8;
+    if (bpos>160) bpos = 170;
+    g.setColor(g.theme.fg2);
+    g.fillCircle(bpos,Ypos+45,6);
     }
-  flip(buf,Yoff);
 }
 
 var heading = 0;
@@ -63,7 +55,7 @@ var candraw = false;
 var CALIBDATA = require("Storage").readJSON("magnav.json",1)||null;
 
 function tiltfixread(O,S){
-  var start = Date.now();
+  "ram"
   var m = Bangle.getCompass();
   var g = Bangle.getAccel();
   m.dx =(m.x-O.x)*S.x; m.dy=(m.y-O.y)*S.y; m.dz=(m.z-O.z)*S.z;
@@ -82,19 +74,24 @@ function tiltfixread(O,S){
 
 // Note actual mag is 360-m, error in firmware
 function reading() {
+  "ram"
+  g.clearRect(0,24,175,175);
   var d = tiltfixread(CALIBDATA.offset,CALIBDATA.scale);
   heading = newHeading(d,heading);
   drawCompass(heading);
-  buf.setColor(1);
-  buf.setFont("6x8",2);
-  buf.setFontAlign(-1,-1);
-  buf.drawString("o",170,0);
-  buf.setFont("Vector",54);
+  g.setColor(g.theme.fg);
+  g.setFont("6x8",2);
+  g.setFontAlign(-1,-1);
+  g.drawString("o",120,Ypos+80);
+  g.setFont("Vector",40);
   var course = Math.round(heading);
   var cs = course.toString();
   cs = course<10?"00"+cs : course<100 ?"0"+cs : cs;
-  buf.drawString(cs,70,10);
-  flip(buf,Yoff+80);
+  g.drawString(cs,50,Ypos+90);
+  g.setColor(g.theme.fg2);
+  g.fillPoly([88,Ypos+60,78,Ypos+80,98,Ypos+80]);
+  g.setColor(g.theme.fg);
+  g.flip();
 }
 
 function calibrate(){
@@ -117,54 +114,49 @@ function calibrate(){
        var avg = (delta.x+delta.y+delta.z)/3;
        var scale = {x:avg/delta.x, y:avg/delta.y, z:avg/delta.z};
        resolve({offset:offset,scale:scale});
-     },30000);
+     },20000);
   });
 }
 
-function docalibrate(e,first){
+var calibrating=false;
+function docalibrate(first){
+    calibrating=true;
     const title = "Calibrate";
-    const msg = "takes 30 seconds";
+    const msg = "takes 20 seconds";
+    function restart() {
+        calibrating=false;
+        setButtons();
+        startdraw();
+    }
     function action(b){
         if (b) {
-          buf.setColor(1);
-          buf.setFont("Vector",24);
-          buf.setFontAlign(0,-1);
-          buf.drawString("Fig 8s to",120,0);
-          buf.drawString("Calibrate",120,26);
-          flip(buf,Yoff);
+          g.clearRect(0,24,175,175);
+          g.setColor(g.theme.fg);
+          g.setFont("Vector",18);
+          g.setFontAlign(0,-1);
+          g.drawString("Fig 8s to",88,Ypos);
+          g.drawString("Calibrate",88,Ypos+18);
+          g.flip();
           calibrate().then((r)=>{
             require("Storage").write("magnav.json",r);
-            CALIBDATA = r;
-            startdraw();
-            setButtons();
+            restart();
           });
         } else {
-          startdraw();
-          setTimeout(setButtons,1000);
+          restart();
         }
     }   
     if (first===undefined) first=false;
     stopdraw();
-    clearWatch();
     if (first) 
         E.showAlert(msg,title).then(action.bind(null,true));
     else 
         E.showPrompt(msg,{title:title,buttons:{"Start":true,"Cancel":false}}).then(action);
 }
 
-Bangle.on('touch', function(b) { 
-    if(!candraw) return;
-    if(b==1) brg=heading;
-    if(b==2) brg=null;
- });
-
 var intervalRef;
 
 function startdraw(){
-  g.clear();
-  g.setColor(1,0.5,0.5);
-  g.fillPoly([120,Yoff+60,110,Yoff+80,130,Yoff+80]);
-  g.setColor(1,1,1);
+  g.clear(1);
   Bangle.drawWidgets();
   candraw = true;
   intervalRef = setInterval(reading,200);
@@ -176,45 +168,24 @@ function stopdraw() {
 }
 
 function setButtons(){
-  setWatch(()=>{load();}, BTN1, {repeat:false,edge:"falling"});
-  setWatch(Bangle.showLauncher, BTN2, {repeat:false,edge:"falling"});
-  setWatch(docalibrate, BTN3, {repeat:false,edge:"falling"});
-}
-
-var SCREENACCESS = {
-      withApp:true,
-      request:function(){
-        this.withApp=false;
-        stopdraw();
-        clearWatch();
-      },
-      release:function(){
-        this.withApp=true;
-        startdraw(); 
-        setButtons();
-      }
-};
- 
-Bangle.on('lcdPower',function(on) {
-  if (!SCREENACCESS.withApp) return;
-  if (on) {
-    startdraw();
-  } else {
-    stopdraw();
+  function actions(v){
+    if (!v) docalibrate(false);
+    else if (v==1) brg=null;
+    else brg=heading;
   }
-});
+  Bangle.setUI("updown",actions);
+}
 
 Bangle.on('kill',()=>{Bangle.setCompassPower(0);});
 
 Bangle.loadWidgets();
 Bangle.setCompassPower(1);
 if (!CALIBDATA) 
-  docalibrate({},true);
+  docalibrate(true);
 else {  
   startdraw();
   setButtons();
 }
-
 
 
 
