cb6e14f6c12816224bbc3dc127e9817d0df7ae78
Gordon Williams <gw@pur3.co.uk>
2021-11-23 20:20:37 +0000

    messages, start of call handling and message responses, fix for scroll issue when only 2 menu items

--- apps/android/boot.js
+++ apps/android/boot.js
@@ -12,7 +12,7 @@
     /* TODO: Call handling, fitness */
     var HANDLERS = {
       // {t:"notify",id:int, src,title,subject,body,sender,tel:string} add
-      "notify" : function() { event.t="add";require("messages").pushMessage(event); },
+      "notify" : function() { Object.assign(event,{t:"add",positive:true, negative:true});require("messages").pushMessage(event); },
       // {t:"notify~",id:int, title:string} // modified
       "notify~" : function() { event.t="modify";require("messages").pushMessage(event); },
       // {t:"notify-",id:int} // remove
@@ -36,8 +36,11 @@
       },
       // {"t":"call","cmd":"incoming/end","name":"Bob","number":"12421312"})
       "call" : function() {
-        event.t=t.cmd=="incoming"?"add":"remove";
-        event.id="call";
+        Object.assign(event, {
+          t:event.cmd=="incoming"?"add":"remove",
+          id:"call", src:"Phone",
+          positive:true, negative:true,
+          title:event.name||"Call", body:"Incoming call\n"+event.number});
         require("messages").pushMessage(event);
       },
     };
@@ -58,5 +61,11 @@
   Bangle.musicControl = cmd => {
     // play/pause/next/previous/volumeup/volumedown
     gbSend({ t: "music", n:cmd });
-  }
+  };
+  // Message response
+  Bangle.messageResponse = (msg,response) => {
+    if (msg.id=="call") return gbSend({ t: "call", n:response?"ACCEPT":"REJECT" });
+    if (isFinite(msg.id)) return gbSend({ t: "notify", n:response?"OPEN":"DISMISS" });
+    // error/warn here?
+  };
 })();
--- apps/ios/boot.js
+++ apps/ios/boot.js
@@ -96,7 +96,13 @@ Bangle.musicControl = cmd => {
   // play, pause, playpause, next, prev, volup, voldown, repeat, shuffle, skipforward, skipback, like, dislike, bookmark
   NRF.amsCommand(cmd);
 };
-NRF.on("disconnect", () => require("messages").clearAll()); // remove all messages on disconnect
+// Message response
+Bangle.messageResponse = (msg,response) => {
+  if (isFinite(msg.id)) return NRF.sendANCSAction(msg.id, response);//true/false
+  // error/warn here?
+};
+// remove all messages on disconnect
+NRF.on("disconnect", () => require("messages").clearAll());
 
 /*
 // For testing...
