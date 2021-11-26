181a8dd45fe144b10fdd0dbf5434d45d34fe45b8
t0m1o1 <94725994+t0m1o1@users.noreply.github.com>
2021-11-26 04:37:15 +0000

    Update settings.js
    Added font to settings
    Changed scalevalue to vector font size

--- apps/launch/settings.js
+++ apps/launch/settings.js
@@ -1,18 +1,24 @@
 // make sure to enclose the function in parentheses
-(function(back) {
+
   let settings = require('Storage').readJSON('launch.json',1)||{};
+  let fonts = g.getFonts();
   function save(key, value) {
     settings[key] = value;
     require('Storage').write('launch.json',settings);
   }
   const appMenu = {
     '': {'title': 'Launcher Settings'},
-    '< Back': back,
-    'Scale Value': {
-      value: settings.scaleval || 1,
-      min:0.1,max:2,step:0.05,wrap:true,
-      onchange: (m) => {save('scaleval', m)}
-    }   
+    '< Back': false,
+    'Font': {
+      value: settings.font || 2,
+      min:0, max:fonts.length-1, step:1,wrap:true,
+      onchange: (m) => {save('font', fonts[m])},
+      format: v => fonts[v]
+     },
+    'Vector font size': {
+      value: settings.vectorsize || 1,
+      min:10, max: 20,step:1,wrap:true,
+      onchange: (m) => {save('vectorsize', m)}
+    }
   };
   E.showMenu(appMenu)
-})
