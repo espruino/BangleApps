6fd7a0eb633ac47eea07c2befe8f0a96bda19a02
leer10 <brandon.abbott@charter.net>
2021-11-27 22:11:06 -0800

    Update app.js
    battery icon actually works now

--- apps/93dub/app.js
+++ apps/93dub/app.js
@@ -77,11 +77,14 @@ g.setColor(1,1,1);
 // draw battery indicator
 var batPercent = E.getBattery();
 if (Bangle.isCharging()) {selBattery = imgBattery_Charge;}
-else {selBattery=imgBattery;}
+else {
+selBattery=imgBattery;
+g.setColor(0,0,0);
+g.fillRect(129,61,(129 + batPercent / 10),65);
+g.setColor(1,1,1);}
 g.drawImage(selBattery,127,59);
 if (batPercent != null) {
 g.drawImage(imgPercent, 118,60);
-
 }
 
 //draw 24 hr indicator and 12 hr specific behavior
