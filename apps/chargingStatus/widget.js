86626ce80531b1557f6b05a6e5664fa5250cc148
Sebastian Di Luzio <sebidiluzio@gmail.com>
2021-12-04 19:53:43 +0100

    test: activate widget always

--- apps/chargingStatus/widget.js
+++ apps/chargingStatus/widget.js
@@ -11,9 +11,9 @@
 		width: 16,
 		draw() {
 			g.reset();
-			if (Bangle.isCharging()) {
+			//if (Bangle.isCharging()) {
 				g.setColor('#0f0').drawImage(atob("EBCBAf9//3/+f/x//P/4//gH8A/wD+Af/x//P/4//n/+//7/"), this.x, this.y);
-			}
+			//}
 		},
 	};
 })();
