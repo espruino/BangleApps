67c72d337e7f7e70c67d0c3627eb5257192f3cfd
Sebastian Di Luzio <sebidiluzio@gmail.com>
2021-12-04 20:44:17 +0100

    attempt to use correct image for rendering

--- apps/widChargingStatus/widget.js
+++ apps/widChargingStatus/widget.js
@@ -7,17 +7,25 @@
 		g.flip();
 	});
 
+	const chargingSymbol = {
+		width: 16,
+		height: 16,
+		bpp: 1,
+		transparent: 1,
+		buffer: require("heatshrink").decompress(atob("/9/AIP+v/8AIP//ABBg/wh4BB8Ef/4BBn/+AIIXB/4BB"))
+	};
+
 	function draw() {
 		g.reset();
-		g.setColor("#FD0"); // on = amber
+		g.setColor("#FD0");
 		//if (Bangle.isCharging()) {
-			g.setColor('#0f0').drawImage(atob("EBCBAf9//3/+f/x//P/4//gH8A/wD+Af/x//P/4//n/+//7/"), this.x, this.y);
+		g.drawImage(chargingSymbol, this.x + 2, this.y);
 		//}
 	}
 
 	WIDGETS.chargingStatus = {
 		area: 'tr',
-		width: 32,
+		width: 20,
 		draw: draw,
 	};
 
