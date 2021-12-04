6b8c201ff22bf37c85b07ae060dc53aeb248add4
Sebastian Di Luzio <sebidiluzio@gmail.com>
2021-12-04 21:08:40 +0100

    use different export technique

--- apps/widChargingStatus/widget.js
+++ apps/widChargingStatus/widget.js
@@ -7,19 +7,11 @@
 		g.flip();
 	});
 
-	const chargingIcon = {
-		width: 32,
-		height: 32,
-		bpp: 1,
-		transparent: 1,
-		buffer: require("heatshrink").decompress(atob("///34CB/8/AYv+AYX8AYd/AYP4AY3wAY3gAYXAAYcAEAOAAYgMBHwYDC/0AgIyBAYkDIYQDDg4DGh4DCj4DGL45DCAYhyC/oCBA="))
-	}
-
 	function draw() {
 		g.reset();
-		g.setColor("#FD0");
+		//g.setColor("#FD0");
 		//if (Bangle.isCharging()) {
-		g.drawImage(chargingIcon, this.x /* + 2 */ , this.y);
+		g.drawImage(atob("kEgwMB///34CB/8/AYv+AYX8AYd/AYP4AY3wAY3gAYXAAYcAEAOAAYgMBIQIPBAYX+gBGB/gDEgZDCAYcHAY0PAYUfAYxfHIYQDEOQX9AQI="), this.x /* + 2 */ , this.y);
 		//g.drawString('X',this.x,this.y)
 		//}
 	}
