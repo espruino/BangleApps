78800e63d0fa688006d9e6a8f472fca96497e000
Sebastian Di Luzio <sebidiluzio@gmail.com>
2021-12-04 19:56:41 +0100

    Update widget.js

--- apps/chargingStatus/widget.js
+++ apps/chargingStatus/widget.js
@@ -12,7 +12,8 @@
 		draw() {
 			g.reset();
 			//if (Bangle.isCharging()) {
-				g.setColor('#0f0').drawImage(atob("EBCBAf9//3/+f/x//P/4//gH8A/wD+Af/x//P/4//n/+//7/"), this.x, this.y);
+				g.drawString("X", this.x, this.y);
+				//g.setColor('#0f0').drawImage(atob("EBCBAf9//3/+f/x//P/4//gH8A/wD+Af/x//P/4//n/+//7/"), this.x, this.y);
 			//}
 		},
 	};
