c2b731a972699307063fdf106af66bdab43f3c13
Sebastian Di Luzio <sebidiluzio@gmail.com>
2021-12-04 20:34:45 +0100

    attempt to draw what we want to see

--- apps/widChargingStatus/widget.js
+++ apps/widChargingStatus/widget.js
@@ -11,8 +11,7 @@
 		g.reset();
 		g.setColor("#FD0"); // on = amber
 		//if (Bangle.isCharging()) {
-		g.drawImage(atob("GBiBAAAAAAAAAAAAAA//8B//+BgYGBgYGBgYGBgYGBgYGBgYGB//+B//+BgYGBgYGBgYGBgYGBgYGBgYGB//+A//8AAAAAAAAAAAAA=="), this.x, 2 + this.y);
-		//g.setColor('#0f0').drawImage(atob("EBCBAf9//3/+f/x//P/4//gH8A/wD+Af/x//P/4//n/+//7/"), this.x, this.y);
+			g.setColor('#0f0').drawImage(atob("EBCBAf9//3/+f/x//P/4//gH8A/wD+Af/x//P/4//n/+//7/"), this.x, this.y);
 		//}
 	}
 
@@ -22,17 +21,6 @@
 		draw: draw,
 	};
 
-	function gpsDraw() {
-		g.reset();
-		g.setColor("#FD0"); // on = amber
 
-		g.drawImage(atob("GBiBAAAAAAAAAAAAAA//8B//+BgYGBgYGBgYGBgYGBgYGBgYGB//+B//+BgYGBgYGBgYGBgYGBgYGBgYGB//+A//8AAAAAAAAAAAAA=="), this.x, 2 + this.y);
-
-	}
-	WIDGETS.gpsT = {
-		area: "tr",
-		width: 24,
-		draw: gpsDraw
-	};
 
 })();
