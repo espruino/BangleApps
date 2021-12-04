cf36400faa5f05e4a10a5546cbcc247a68c48edf
Sebastian Di Luzio <sebidiluzio@gmail.com>
2021-12-04 22:37:16 +0100

    prepare first release

--- apps/widChargingStatus/widget.js
+++ apps/widChargingStatus/widget.js
@@ -1,30 +1,30 @@
 (() => {
-	Bangle.on('charging', (charging) => {
-		if (charging) {
-			Bangle.buzz();
-		}
-		Bangle.drawWidgets(); // re-layout widgets
-		g.flip();
-	});
-
 	const icon = require("heatshrink").decompress(atob("kEggIEBoAIC4ADFgIDCgYDDwADBg4DGh4DGj4DCn4DD/4gBv4DEBgP//4PBAYUB//+GQIDE/hDCAYf4AY3wAYXgAYxfHIYQDEOQUCAQI"));
 
 	function draw() {
 		g.reset();
-		g.setColor("#FD0");
-		//if (Bangle.isCharging()) {
-		g.drawImage(icon, this.x, this.y + 2, {
-			scale: 0.8125
-		});
-		//}
+		if (Bangle.isCharging()) {
+			g.setColor("#FD0");
+			g.drawImage(icon, this.x, this.y + 1, {
+				scale: 0.6875
+			});
+		}
 	}
 
 	WIDGETS.chargingStatus = {
 		area: 'tr',
-		width: 26,
+		width: 22,
 		draw: draw,
 	};
 
-
-
+	Bangle.on('charging', (charging) => {
+		if (charging) {
+			Bangle.buzz();
+			WIDGETS.chargingStatus.width = 22;
+		} else {
+			WIDGETS.chargingStatus.width = 0;
+		}
+		Bangle.drawWidgets(); // re-layout widgets
+		g.flip();
+	});
 })();
