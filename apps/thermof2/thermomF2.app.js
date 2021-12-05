14a6817e6aba775e9d39d6d74620c8b6e9bb699b
elrod16 <95036593+elrod16@users.noreply.github.com>
2021-12-04 19:59:22 -0500

    Add files via upload

---
+++ apps/thermof2/thermomF2.app.js
@@ -0,0 +1,25 @@
+function onTemperature(p) {
+  g.reset(1).clearRect(0,24,g.getWidth(),g.getHeight());
+  g.setFont("6x8",2).setFontAlign(0,0);
+  var x = g.getWidth()/2;
+  var y = g.getHeight()/2 + 10;
+  g.drawString("Temp", x, y - 45);
+  g.setFontVector(70).setFontAlign(0,0);
+  g.drawString(p.temperature.toFixed(1), x, y);
+}
+
+function drawTemperature() {
+    onTemperature({
+      temperature : E.getTemperature() * 9 / 5 + 32
+    });
+
+}
+
+
+setInterval(function() {
+  drawTemperature();
+}, 20000);
+drawTemperature();
+E.showMessage("Loading...");
+Bangle.loadWidgets();
+Bangle.drawWidgets();
