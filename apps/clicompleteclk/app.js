f8628dd26d587edff90037bcd255aacbdddaafcc
Marco H <myxor@users.noreply.github.com>
2021-12-10 08:58:44 +0100

    Update app.js
    Load steps from Health Tracking app (if installed)

--- apps/clicompleteclk/app.js
+++ apps/clicompleteclk/app.js
@@ -68,7 +68,7 @@ function drawInfo(now) {
   */
 
   // weather
-  var weatherJson = getWeather();
+  const weatherJson = getWeather();
   if(weatherJson && weatherJson.weather){
     const currentWeather = weatherJson.weather;
 
@@ -84,9 +84,9 @@ function drawInfo(now) {
   }
 
   // steps
-  if (stepsWidget() != undefined) {
+  const steps = getSteps();
+  if (steps != undefined) {
     writeLineTopic("STEP", i);
-    const steps = stepsWidget().getSteps();
     writeLine(steps, i);
     i++;
   }
@@ -128,6 +128,30 @@ function writeLine(str,line,pColor){
   g.drawString(str,marginLeftData,y);
 }
 
+
+function getSteps() {
+  var steps = 0;
+  let health;
+  try {
+    health = require("health");
+  } catch (e) {
+    // Module health not found
+  }
+  if (health != undefined) {
+    health.readDay(new Date(), h=>steps+=h.steps);
+  } else if (WIDGETS.wpedom !== undefined) {
+    return WIDGETS.wpedom.getSteps();
+  } else if (WIDGETS.activepedom !== undefined) {
+    return WIDGETS.activepedom.getSteps();
+  }
+  return steps;
+}
+
+function getWeather() {
+  let jsonWeather = storage.readJSON('weather.json');
+  return jsonWeather;
+}
+
 // EVENTS:
 
 // turn on HRM when the LCD is unlocked
@@ -165,21 +189,6 @@ Bangle.on('HRM', function(hrm) {
   //  hrtValue = undefined;
   //}
 });
-
-
-function stepsWidget() {
-  if (WIDGETS.activepedom !== undefined) {
-    return WIDGETS.activepedom;
-  } else if (WIDGETS.wpedom !== undefined) {
-    return WIDGETS.wpedom;
-  }
-  return undefined;
-}
-
-function getWeather() {
-  let jsonWeather = storage.readJSON('weather.json');
-  return jsonWeather;
-}
 
 g.clear();
 Bangle.setUI("clock");
