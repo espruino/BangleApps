5b16eb20f871f7e95018be62936f3b7ed6162d94
Marco H <myxor@users.noreply.github.com>
2021-12-11 13:12:30 +0100

    Update settings.js
    Extend settings

--- apps/clicompleteclk/settings.js
+++ apps/clicompleteclk/settings.js
@@ -11,8 +11,42 @@
       value: "battery" in settings ? settings.battery : false,
       format: () => (settings.battery ? 'Yes' : 'No'),
       onchange: () => {
-        settings.battery = !settings.battery
+        settings.battery = !settings.battery;
         save('battery', settings.battery);
+      },
+    },
+    'Battery warn level': {
+      value: "batteryLvl" in settings ? settings.batteryLvl : 30,
+      min: 0,
+      max : 100,
+      step: 10,
+      format: x => {
+        return x + "%";
+      },
+      onchange: x => save('batteryLvl', x),
+    },
+    'Show weather': {
+      value: "weather" in settings ? settings.weather : false,
+      format: () => (settings.batweathertery ? 'Yes' : 'No'),
+      onchange: () => {
+        settings.weather = !settings.weather;
+        save('weather', settings.weather);
+      },
+    },
+    'Show steps': {
+      value: "steps" in settings ? settings.steps : false,
+      format: () => (settings.steps ? 'Yes' : 'No'),
+      onchange: () => {
+        settings.steps = !settings.steps;
+        save('steps', settings.steps);
+      },
+    },
+    'Show heartrate': {
+      value: "heartrate" in settings ? settings.heartrate : false,
+      format: () => (settings.heartrate ? 'Yes' : 'No'),
+      onchange: () => {
+        settings.heartrate = !settings.heartrate;
+        save('heartrate', settings.heartrate);
       },
     },
     '< Back': back,
