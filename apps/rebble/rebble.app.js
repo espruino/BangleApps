0cd7a577a46d6c55f4ca80bcb18a1cb85259f6bf
hughbarney <hughbarney@googlemail.com>
2021-12-12 17:36:59 +0000

    Rebble: ensure widget area is also cleared when we hide widgets

--- apps/rebble/rebble.app.js
+++ apps/rebble/rebble.app.js
@@ -263,9 +263,10 @@ g.clear();
 Bangle.loadWidgets();
 /*
  * we are not drawing the widgets as we are taking over the whole screen
- * so we will blank out the draw() functions of each widget
+ * so we will blank out the draw() functions of each widget and change the
+ * area to the top bar doesn't get cleared.
  */
-for (let wd of WIDGETS) {wd.draw=()=>{};}
+for (let wd of WIDGETS) {wd.draw=()=>{};wd.area="";}
 loadSettings();
 loadLocation();
 draw();  // queues the next draw for a minutes time
