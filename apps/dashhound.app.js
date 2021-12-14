c45624970d9f9ddd49faf69df0af2106f4ba2106
Andy O'Callaghan <andrew.ocallaghan@ucdconnect.ie>
2021-12-14 23:28:01 +0000

    Create dashhound.app.js
    Adding app that has Title, Icon and Author for opening page 😄

---
+++ apps/dashhound.app.js
@@ -0,0 +1,9 @@
+showOpeningPage = () => {
+  title = "Dash Hound"
+  author = "Andy OC"
+  iconProvider = "Icon8"
+  E.showMessage(`App by ${author}\nImgs by ${iconProvider}`, title);
+  g.drawImage(require("Storage").read("dashhound.img"), 100, 50)  
+}
+
+showOpeningPage()
