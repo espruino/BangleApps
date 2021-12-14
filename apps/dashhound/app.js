08d713ca34245e01669ca925537f3c9c0500605b
Andy O'Callaghan <andrew.ocallaghan@ucdconnect.ie>
2021-12-14 23:58:07 +0000

    Create app.js
    Adding dashhound folder to contain app and its assets.
    Adding app.js which includes the opening screen with the title.
    The image is not uploaded yet

---
+++ apps/dashhound/app.js
@@ -0,0 +1,8 @@
+showOpeningPage = () => {
+  title = "Dash Hound"
+  author = "Andy OC"
+  iconProvider = "Icon8"
+  E.showMessage(`App by ${author}\nImgs by ${iconProvider}`, title);
+}
+
+showOpeningPage()
