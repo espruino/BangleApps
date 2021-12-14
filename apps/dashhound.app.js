f3ff58ca8167c5b243b3b17a183e3d838a7b8ca0
Andy O'Callaghan <andrew.ocallaghan@ucdconnect.ie>
2021-12-14 23:54:50 +0000

    Delete dashhound.app.js
    need to create folder not file

--- apps/dashhound.app.js
+++
@@ -1,9 +0,0 @@
-showOpeningPage = () => {
-  title = "Dash Hound"
-  author = "Andy OC"
-  iconProvider = "Icon8"
-  E.showMessage(`App by ${author}\nImgs by ${iconProvider}`, title);
-  g.drawImage(require("Storage").read("dashhound.img"), 100, 50)  
-}
-
-showOpeningPage()
