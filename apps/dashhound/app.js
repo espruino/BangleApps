ace2e9c8a227dcc7d8c138bc5eda0dc14c9f144b
Andy O'Callaghan <andrew.ocallaghan@ucdconnect.ie>
2021-12-15 23:10:42 +0000

    adding dog image
    adding dog image to opening screen

--- apps/dashhound/app.js
+++ apps/dashhound/app.js
@@ -3,6 +3,7 @@ showOpeningPage = () => {
   author = "Andy OC"
   iconProvider = "Icon8"
   E.showMessage(`App by ${author}\nImgs by ${iconProvider}`, title);
+  g.drawImage(require("Storage").read("dashhound.img"), 100, 50)
 }
 
 showOpeningPage()
