8e0ba23aa8ff485b402df652e9b748475a4bf76f
RomanistHere <RomanistHere@gmail.com>
2021-12-15 22:23:38 +0300

    fix: play with syntax

--- apps/pebbleDistance/pebble.settings.js
+++ apps/pebbleDistance/pebble.settings.js
@@ -1,8 +1,8 @@
 (function(back) {
-    const SETTINGS_FILE = "pebbleDistance.json";
+    const SETTINGS_FILE = "pbd.json";
 
     // initialize with default settings...
-    let s = {'bg': '#0f0', 'color': 'Green', 'avStep': 0.75}};
+    let s = {'bg': '#0f0', 'color': 'Green', 'avStep': 0.75};
 
     // ...and overwrite them with any saved values
     // This way saved values are preserved if a new version adds more settings
@@ -10,13 +10,13 @@
     let settings = storage.readJSON(SETTINGS_FILE, 1) || s;
     const saved = settings || {};
     for (const key in saved) {
-        s[key] = saved[key]
-    };
+        s[key] = saved[key];
+    }
 
     function save() {
         settings = s;
         storage.write(SETTINGS_FILE, settings);
-    };
+    }
 
     var color_options = ['Green','Orange','Cyan','Purple','Red','Blue'];
     var bg_code = ['#0f0','#ff0','#0ff','#f0f','#f00','#00f'];
