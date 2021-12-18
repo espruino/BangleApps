51422c44c0fc89874b96fc893b3d06014de0f771
Adam Schmalhofer <code@adaschma.name>
2021-12-18 18:46:16 +0100

    hwt: Reduce line lenght

--- apps/widhwt/app.js
+++ apps/widhwt/app.js
@@ -2,9 +2,9 @@
 // with our own message
 g.clearRect(38, 68, 138, 108);
 g.drawRect(38, 68, 138, 108);
-g.setFontVector(13).setFontAlign(0, 0, 0).drawString("Wash...",
-                                                     g.getWidth()/2,
-                                                     g.getHeight()/2);
+g.setFontVector(13);
+g.setFontAlign(0, 0, 0);
+g.drawString("Wash...", g.getWidth()/2, g.getHeight()/2);
 
 Bangle.buzz();
 setTimeout(() => {
