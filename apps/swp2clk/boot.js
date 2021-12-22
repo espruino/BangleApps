c664e45eb054eeaddf9ac29ff89a314951fb5996
crazysaem <crazysaem@fastmail.com>
2021-12-22 20:58:26 +0000

    swp2clk: Remove unused app icon (since we are settings-only). Add comment explaining boot.js code. Add credits and motivation.

--- apps/swp2clk/boot.js
+++ apps/swp2clk/boot.js
@@ -1,3 +1,20 @@
+/**
+ * How does this work?
+ * 
+ * Every *boot.js file is executed everytime any app is loaded, including this one.
+ * We just need to figure out which app is currently loaded, in case we are in the white list / black list mode,
+ * to figure out if the swipe handler should be attached or not.
+ * It does not seem to be the case that this can be done easily, but we can work around it.
+ * It seems that every app is loaded via the global "load" function, which takes a fileName as it's first parameter to load any app
+ * or the default clock when the fileName is undefined.
+ * To be able to use this for us, we wrap the global "load" function, and determine before loading the next app,
+ * whether the swipe handler should be added or not, since we now know which app will be loaded.
+ * Note: We cannot add the swipe handler inside the wrapped "load" function, because once the "load" function is complete
+ * everything is cleaned up. That's why we merely save a flag, whether the swipe handler should be attached or not,
+ * which is evaluated once this file is executed again, which will be right after the load function completes
+ * (since every *boot.js file is executed everytime any app is loaded).
+ */
+
 (function () {
   var DEBUG = true;
   var FILE = "swp2clk.data.json";
