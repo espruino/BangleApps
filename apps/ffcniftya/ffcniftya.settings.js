fcf8a0b1f097466f8e6688e645b1977f2ef8cee7
Danny <31635744+DDDanny@users.noreply.github.com>
2021-12-25 23:58:46 +0100

    added ISO weeknumber
    * update changelog, screenshots and readme
    * created a settings dialog

---
+++ apps/ffcniftya/ffcniftya.settings.js
@@ -0,0 +1,25 @@
+(function(back) {
+  var FILE = "\ffcniftya.json";
+  // Load settings
+  var cfg = Object.assign({
+    showWeekNum: true,
+  }, require('Storage').readJSON(FILE, true) || {});
+
+  function writeSettings() {
+    require('Storage').writeJSON(FILE, cfg);
+  }
+
+  // Show the menu
+  E.showMenu({
+    "" : { "title" : "Nifty-A Clock" },
+    "< Back" : () => back(),
+    'week number?': {
+      value: cfg.showWeekNum,
+      format: v => v?"On":"Off",
+      onchange: v => {
+        cfg.showWeekNum = v;
+        writeSettings();
+      }
+    }
+  });
+})
