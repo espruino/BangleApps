4112df5f6d8cb2c1129cfdfcd51800593ef75a0c
Kendell R <KTibow@users.noreply.github.com>
2021-12-28 08:56:54 -0800

    Fix app icons to be one line

--- apps/fclock/app-icon.js
+++ apps/fclock/app-icon.js
@@ -1,5 +1,1 @@
-require("heatshrink").decompress(
-  atob(
-    "mEwgRC/AH4AfgkBBA0BqADBAQQAEhfwAQP+BY1/BYM///ABQkC//wgm///gBYkT+4XB/sDAYIXEwfwgP4qFQlMAlBGCCYMB/47B/sA3gYCoYLC//R90R/kUikAC4f/8Mdifslm9BYf9g/wl0LnkYloLD/FC+kdh/+jttF4PxiP4qX0t1G3suvAXD/kC+v/6E8KYWn+2Q/1b+OqyMYO4REB8F/CoKDFKAUv/+AqtAXweq1WQgWof+IA/AE0GsNRiqnEBYe21Vq1WABY2p1WKXAILFgNUqFFqtQHBw"
-  )
-);
+require("heatshrink").decompress(atob("mEwgRC/AH4AfgkBBA0BqADBAQQAEhfwAQP+BY1/BYM///ABQkC//wgm///gBYkT+4XB/sDAYIXEwfwgP4qFQlMAlBGCCYMB/47B/sA3gYCoYLC//R90R/kUikAC4f/8Mdifslm9BYf9g/wl0LnkYloLD/FC+kdh/+jttF4PxiP4qX0t1G3suvAXD/kC+v/6E8KYWn+2Q/1b+OqyMYO4REB8F/CoKDFKAUv/+AqtAXweq1WQgWof+IA/AE0GsNRiqnEBYe21Vq1WABY2p1WKXAILFgNUqFFqtQHBw"));
