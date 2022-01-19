3670ff2170005dcdde8e47b9df2a7ac6bf12204e
hughbarney <hughbarney@googlemail.com>
2022-01-19 23:09:52 +0000

    set widget sortorder through code for widpa,widpb,widbata,widlock

--- apps/widlock/widget.js
+++ apps/widlock/widget.js
@@ -4,7 +4,7 @@
     WIDGETS["lock"].width = Bangle.isLocked()?16:0;
     Bangle.drawWidgets();
   });
-  WIDGETS["lock"]={area:"tl",sortorder:-10,width:Bangle.isLocked()?16:0,draw:function(w) {
+  WIDGETS["lock"]={area:"tl",sortorder:10,width:Bangle.isLocked()?16:0,draw:function(w) {
     if (Bangle.isLocked())
       g.reset().drawImage(atob("DhABH+D/wwMMDDAwwMf/v//4f+H/h/8//P/z///f/g=="), w.x+1, w.y+4);
   }};
