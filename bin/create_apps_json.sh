2a813e221afb49ac9e358bafc15c84c57b317883
Gordon Williams <gw@pur3.co.uk>
2022-01-19 16:21:07 +0000

    Updated docs!

--- bin/create_apps_json.sh
+++ bin/create_apps_json.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# ================================================================
+# apps.json used to contain the metadata for every app. Now the
+# metadata is stored in each apps's directory - app/yourapp/metadata.js
+#
+# The app loader still wants all the data in one file, so normally
+# with GitHub pages, Jekyll is automatically run and creates a working
+# apps.json
+#
+# However if you're hosting locally, or not on GitHub pages then you
+# will want to create apps.json yourself. You can do that by installing
+# and running Jekyll, OR the easier option is just to run this script.
+#
+# If you do this, please do not attempt to commit your modified
+# apps.json back into the main BangleApps repository!
 
 cd `dirname $0`/..
 echo "[" > apps.json
