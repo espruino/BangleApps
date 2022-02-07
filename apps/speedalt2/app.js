3e9df1a711c3e138ef6364e83a10186bf29082e7
nujw <mike@kereru.com>
2022-02-08 08:17:31 +1300

    Update app.js

--- apps/speedalt2/app.js
+++ apps/speedalt2/app.js
@@ -6,7 +6,7 @@ Mike Bennett mike[at]kereru.com
 1.34 : Add bluetooth data stream for Droidscript
 1.43 : Keep GPS in SuperE mode while using Droiscript screen mirroring
 */
-var v = '1.46';
+var v = '1.47';
 var vDroid = '1.46';    // Required DroidScript program version
 
 /*kalmanjs, Wouter Bulten, MIT, https://github.com/wouterbulten/kalmanjs */
@@ -645,7 +645,7 @@ function btSend(dat) {
   var dur = getTime() - btLast;
   if ( dur < 1.0 ) return;                  // Don't need to transmit more than every 1.0 secs.
   btLast = getTime();
-  console.log(JSON.stringify(dat));         // transmit the data
+  Bluetooth.println(JSON.stringify(dat));         // transmit the data
 }
 
 // == Events
