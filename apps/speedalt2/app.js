af0a286dcdf79e2f295922578c35962cdd206382
nujw <mike@kereru.com>
2022-02-09 16:03:18 +1300

    Update app.js

--- apps/speedalt2/app.js
+++ apps/speedalt2/app.js
@@ -6,7 +6,7 @@ Mike Bennett mike[at]kereru.com
 1.34 : Add bluetooth data stream for Droidscript
 1.43 : Keep GPS in SuperE mode while using Droiscript screen mirroring
 */
-var v = '1.48';
+var v = '1.49';
 var vDroid = '1.50';    // Required DroidScript program version
 
 /*kalmanjs, Wouter Bulten, MIT, https://github.com/wouterbulten/kalmanjs */
@@ -175,16 +175,16 @@ var KalmanFilter = (function () {
 
 var buf = Graphics.createArrayBuffer(240,160,2,{msb:true});
 
-/*
+
 let LED = // LED as minimal and only definition (as instance / singleton)
 { isOn: false // status on / off, not needed if you don't need to ask for it
 , set: function(v) { // turn on w/ no arg or truey, else off
-   g.setColor((this.isOn=(v===undefined||!!v))?1:0,0,0).fillCircle(40,10,10); }
+   g.setColor((this.isOn=(v===undefined||!!v))?1:0,0,0).fillCircle(120,10,10); }
 , reset: function() { this.set(false); } // turn off
 , write: function(v) { this.set(v); }  // turn on w/ no arg or truey, else off
 , toggle: function() { this.set( ! this.isOn); } // toggle the LED
 }, LED1 = LED; // LED1 as 'synonym' for LED 
-*/
+
 
 var lf = {fix:0,satellites:0};
 var showMax = 0;        // 1 = display the max values. 0 = display the cur fix
