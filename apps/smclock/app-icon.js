a07b502e0fb1bbf56f617c3bcfe776f7bf4a656c
smekras <stergios.mekras@gmail.com>
2022-02-22 16:43:04 +0100

    Update app-icon.js

--- apps/smclock/app-icon.js
+++ apps/smclock/app-icon.js
@@ -1,1 +1,1 @@
-require("heatshrink").decompress(atob("mYAK+YLKERUCm93ABIX/C/4X/C/4X/C/4X/C/4XP+czACk/mAwKABUDC/4X/C/4X/C/4X/C/4X/C54A="))
+require("heatshrink").decompress(atob("mYAJn4LKiIAJkd3ABI4KC34W/C34W/C34W/C393iIAJCxczACsxFxQAJicxLhQAJC34W/C34W/C34W/C34WM"))
