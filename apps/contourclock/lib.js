1af14f3f2c9d7f76c09f38e273e0045eda3366a6
copoer <c@ooper.space>
2022-02-25 17:11:08 -0400

    Updated to use suggested function

--- apps/contourclock/lib.js
+++ apps/contourclock/lib.js
@@ -1,3 +1,11 @@
+var is12;
+function getHours(d) {
+  var h = d.getHours();
+  if (is12===undefined) is12 = (require('Storage').readJSON('setting.json',1)||{})["12hour"];
+  if (!is12) return h;
+  return (h%12==0) ? 12 : h%12;
+}
+
 exports.drawClock = function(fontIndex) {
   var digits = [];
   fontFile=require("Storage").read("contourclock-"+Math.abs(parseInt(fontIndex+0.5))+".json");
@@ -13,15 +21,13 @@ exports.drawClock = function(fontIndex) {
     if (n!=10) return (false); //font file seems to be invalid
     var x=0;
     var y = g.getHeight()/2-digits[0].height/2;
-    var date = require('locale').time(new Date(),1);
-    var hours = date.split(":")[0];
-    var minutes = date.split(":")[1];
+    var date = new Date();
     g.clearRect(0,38,g.getWidth()-1,138);
-    d1=parseInt(hours/10);
-    d2=parseInt(hours%10);
+    d1=parseInt(getHours(date)/10);
+    d2=parseInt(getHours(date)%10);
     d3=10;
-    d4=parseInt(minutes/10);
-    d5=parseInt(minutes%10);
+    d4=parseInt(date.getMinutes()/10);
+    d5=parseInt(date.getMinutes()%10);
     w1=digits[d1].width;
     w2=digits[d2].width;
     w3=digits[d3].width;
