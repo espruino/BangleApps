ae221a9cd7cd890d00e33b0beb0b1b4828363f7b
foostuff <97034053+foostuff@users.noreply.github.com>
2022-03-05 11:46:56 +0100

    Update settings.js

--- apps/clockcal/settings.js
+++ apps/clockcal/settings.js
@@ -1,107 +1,92 @@
-// Settings menu for the enhanced Anton clock
+(function (back) {
+    var FILE = "clockcal.json";
 
-(function(back) {
-  var FILE = "clockcal.json";
-  // Load settings
-  var settings = Object.assign({
-    secondsOnUnlock: false,
-  }, require('Storage').readJSON(FILE, true) || {});
+    settings = Object.assign({
+        CAL_ROWS: 4, //number of calendar rows.(weeks) Shouldn't exceed 5 when using widgets.
+        BUZZ_ON_BT: true, //2x slow buzz on disconnect, 2x fast buzz on connect. Will be extra widget eventually
+        MODE24: true, //24h mode vs 12h mode
+        FIRSTDAY: 6, //First day of the week: mo, tu, we, th, fr, sa, su
+        REDSUN: true, // Use red color for sunday?
+        REDSAT: true, // Use red color for saturday?
+    }, require('Storage').readJSON(FILE, true) || {});
 
-  function writeSettings() {
-    require('Storage').writeJSON(FILE, settings);
-  }
 
-  // Helper method which uses int-based menu item for set of string values
-  function stringItems(startvalue, writer, values) {
-    return {
-      value: (startvalue === undefined ? 0 : values.indexOf(startvalue)),
-      format: v => values[v],
-      min: 0,
-      max: values.length - 1,
-      wrap: true,
-      step: 1,
-      onchange: v => {
-        writer(values[v]);
-        writeSettings();
-      }
-    };
-  }
+    function writeSettings() {
+        require('Storage').writeJSON(FILE, settings);
+    }
 
-  // Helper method which breaks string set settings down to local settings object
-  function stringInSettings(name, values) {
-    return stringItems(settings[name], v => settings[name] = v, values);
-  }
-
-  var mainmenu = {
-    "": {
-      "title": "clockcal clock"
-    },
-    "< Back": () => back(),
-    "Seconds...": () => E.showMenu(secmenu),
-    "Date": stringInSettings("dateOnMain", ["Long", "Short", "ISO8601"]),
-    "Show Weekday": {
-      value: (settings.weekDay !== undefined ? settings.weekDay : true),
-      format: v => v ? "On" : "Off",
-      onchange: v => {
-        settings.weekDay = v;
-        writeSettings();
-      }
-    },
-    "Show CalWeek": {
-      value: (settings.calWeek !== undefined ? settings.calWeek : false),
-      format: v => v ? "On" : "Off",
-      onchange: v => {
-        settings.calWeek = v;
-        writeSettings();
-      }
-    },
-    "Uppercase": {
-      value: (settings.upperCase !== undefined ? settings.upperCase : true),
-      format: v => v ? "On" : "Off",
-      onchange: v => {
-        settings.upperCase = v;
-        writeSettings();
-      }
-    },
-    "Vector font": {
-      value: (settings.vectorFont !== undefined ? settings.vectorFont : false),
-      format: v => v ? "On" : "Off",
-      onchange: v => {
-        settings.vectorFont = v;
-        writeSettings();
-      }
-    },
-  };
-
-  // Submenu
-  var secmenu = {
-    "": {
-      "title": "Show seconds..."
-    },
-    "< Back": () => E.showMenu(mainmenu),
-    "Show": stringInSettings("secondsMode", ["Never", "Unlocked", "Always"]),
-    "With \":\"": {
-      value: (settings.secondsWithColon !== undefined ? settings.secondsWithColon : true),
-      format: v => v ? "On" : "Off",
-      onchange: v => {
-        settings.secondsWithColon = v;
-        writeSettings();
-      }
-    },
-    "Color": {
-      value: (settings.secondsColoured !== undefined ? settings.secondsColoured : true),
-      format: v => v ? "On" : "Off",
-      onchange: v => {
-        settings.secondsColoured = v;
-        writeSettings();
-      }
-    },
-    "Date": stringInSettings("dateOnSecs", ["Year", "Weekday", "No"])
-  };
-
-  // Actually display the menu
-  E.showMenu(mainmenu);
-
-});
-
-// end of file
+    menu = {
+        "": { "title": "Clock & Calendar" },
+        "< Back": () => back(),
+        'Buzz(dis)conn.?': {
+            value: settings.BUZZ_ON_BT,
+            format: v => v ? "On" : "Off",
+            onchange: v => {
+                settings.BUZZ_ON_BT = v;
+                writeSettings();
+            }
+        },
+        '#Calendar Rows': {
+            value: settings.CAL_ROWS,
+            min: 0, max: 6,
+            onchange: v => {
+                settings.CAL_ROWS = v;
+                writeSettings();
+            }
+        },
+        'Clock mode': {
+            value: settings.MODE24,
+            format: v => v ? "24h" : "12h",
+            onchange: v => {
+                settings.MODE24 = v;
+                writeSettings();
+            }
+        },
+        'First Day': {
+            value: settings.FIRSTDAY,
+            min: 0, max: 6,
+            format: v => ["Sun", "Sat", "Fri", "Thu", "Wed", "Tue", "Mon"][v],
+            onchange: v => {
+                settings.FIRSTDAY = v;
+                writeSettings();
+            }
+        },
+        'Red Saturday?': {
+            value: settings.REDSAT,
+            format: v => v ? "On" : "Off",
+            onchange: v => {
+                settings.REDSAT = v;
+                writeSettings();
+            }
+        },
+        'Red Sunday?': {
+            value: settings.REDSUN,
+            format: v => v ? "On" : "Off",
+            onchange: v => {
+                settings.REDSUN = v;
+                writeSettings();
+            }
+        },
+        'Load deafauls?': {
+            value: 0,
+            min: 0, max: 1,
+            format: v => ["No", "Yes"][v],
+            onchange: v => {
+                if (v == 1) {
+                    settings = {
+                        CAL_ROWS: 4, //number of calendar rows.(weeks) Shouldn't exceed 5 when using widgets.
+                        BUZZ_ON_BT: true, //2x slow buzz on disconnect, 2x fast buzz on connect. 
+                        MODE24: true, //24h mode vs 12h mode
+                        FIRSTDAY: 6, //First day of the week: mo, tu, we, th, fr, sa, su
+                        REDSUN: true, // Use red color for sunday?
+                        REDSAT: true, // Use red color for saturday?
+                    };
+                    writeSettings();
+                    load()
+                }
+            }
+        },
+    }
+    // Show the menu
+    E.showMenu(menu);
+})
