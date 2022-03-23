1b9dbd58afb2c7978cd86b10b2644092f17ab27f
ELIARENZONI <102217389+ELIARENZONI@users.noreply.github.com>
2022-03-23 11:11:46 +0100

    Create metadata.js

---
+++ apps/metadata.js
@@ -0,0 +1,13 @@
+{ "id": "dentigrossi",
+  "name": "My app monitors the teeth brush",
+  "shortName":"Short Name",
+  "version":"0.01",
+  "description": "A detailed description of my great app",
+  "icon": "brush-teeth.png",
+  "tags": "",
+  "supports" : ["BANGLEJS1"],  
+  "storage": [
+    {"name":"denti.app.js","url":"app.js"},
+    {"name":"brush-teeth.img","url":"app-icon.js","evaluate":true}
+  ]
+}
