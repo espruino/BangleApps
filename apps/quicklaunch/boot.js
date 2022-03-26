6ea588646e16b7cd728a378f4513041304136b2e
frigis1 <63980066+frigis1@users.noreply.github.com>
2022-03-25 23:45:53 -0700

    Update boot.js

--- apps/quicklaunch/boot.js
+++ apps/quicklaunch/boot.js
@@ -1,11 +1,25 @@
 (function() {
+    var settings = Object.assign(require("Storage").readJSON("quicklaunch.json", true) || {});
+    
+    if (!settings.leftapp) {
+      settings["leftapp"] = {"name":"(none)"};
+      require("Storage").write("quicklaunch.json",settings);
+    }
+    if (!settings.rightapp) {
+      settings["rightapp"] = {"name":"(none)"};
+      require("Storage").write("quicklaunch.json",settings);
+    }
+
     var sui = Bangle.setUI;
     Bangle.setUI = function(mode, cb) {
         sui(mode,cb);
         if(!mode) return;
         if ("object"==typeof mode) mode = mode.mode;
         if (!mode.startsWith("clock")) return;
-        Bangle.swipeHandler = dir => { if (dir<0) Bangle.showLauncher(); };
+        Bangle.swipeHandler = dir => { 
+          if (dir<0) load(settings.leftapp.src); 
+          else load(settings.rightapp.src);
+          };
         Bangle.on("swipe", Bangle.swipeHandler);
     };
 })();
