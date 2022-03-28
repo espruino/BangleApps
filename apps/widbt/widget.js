bacc9e80af40ec2940fd9fb901647e682a998728
KungPhoo <gf@glbasic.com>
2022-03-28 08:24:12 +0200

    Update widget.js
    -Avoid too many notifications on connection loss.
    -Change non-connected icon color.

--- apps/widbt/widget.js
+++ apps/widbt/widget.js
@@ -1,12 +1,14 @@
 WIDGETS.bluetooth = {
     area: "tr",
     width: 15,
+    warningEnabled: 1,
     draw: function() {
         g.reset();
         if (NRF.getSecurityStatus().connected) {
             g.setColor((g.getBPP() > 8) ? "#07f" : (g.theme.dark ? "#0ff" : "#00f"));
         } else {
-            g.setColor(g.theme.dark ? "#666" : "#999");
+            // g.setColor(g.theme.dark ? "#666" : "#999");
+            g.setColor("#f00"); // red is easier to distinguish from blue
         }
         g.drawImage(atob("CxQBBgDgFgJgR4jZMawfAcA4D4NYybEYIwTAsBwDAA=="), 2 + this.x, 2 + this.y);
     },
@@ -14,7 +16,11 @@ WIDGETS.bluetooth = {
         WIDGETS.bluetooth.draw();
     },
     diconnect: function() {
-        Bangle.buzz(1000, 1); // buzz on connection loss
+        if(warningEnabled == 1){
+            Bangle.buzz(700, 1); // buzz on connection loss
+            warningEnabled = 0;
+            setTimeout('WIDGETS.bluetooth.warningEnabled = 1;', 30000); // re-notify only after 30 seconds.
+        }
         WIDGETS.bluetooth.draw();
     }
 };
