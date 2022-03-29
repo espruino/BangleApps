6389b27d41577544a09719c96aad2f9d0138874d
sir-indy <53864146+sir-indy@users.noreply.github.com>
2022-03-29 10:51:29 +0100

    Update pebble.app.js
    First stab at adding theme selection

--- apps/pebble/pebble.app.js
+++ apps/pebble/pebble.app.js
@@ -12,7 +12,7 @@ const SETTINGS_FILE = "pebble.json";
 let settings;
 
 function loadSettings() {
-  settings = require("Storage").readJSON(SETTINGS_FILE,1)|| {'bg': '#0f0', 'color': 'Green'};
+  settings = require("Storage").readJSON(SETTINGS_FILE,1)|| {'bg': '#0f0', 'color': 'Green', 'theme':'System'};
 }
 
 var img = require("heatshrink").decompress(atob("oFAwkEogA/AH4A/AH4A/AH4A/AE8AAAoeXoAfeDQUBmcyD7A+Dh///8QD649CiAfaHwUvD4sEHy0DDYIfEICg+Cn4fHICY+DD4nxcgojOHwgfEIAYfRCIQaDD4ZAFD5r7DH4//kAfRCIZ/GAAnwD5p9DX44fTHgYSBf4ofVDAQEBl4fFUAgfOXoQzBgIfFBAIfPP4RAEAoYAB+cRiK/SG4h/WIBAfXIA7CBAAswD55AHn6fUIBMCD65AHl4gCmcziAfQQJqfQQJpiDgk0IDXxQLRAEECaBM+QgRYRYgUIA0CD4ggSQJiDCiAKBICszAAswD55AHABKBVD7BAFABIqBD5pAFABPxD55AOD6BADiIAJQAyxLABwf/gaAPAH4A/AH4ARA=="));
@@ -107,6 +107,17 @@ function getSteps() {
   return '????';
 }
 
+function set_colors() {
+  if (settings.theme == 'Light') {
+    pass
+  } else if (settings.theme == 'Dark') {
+    pass
+  } else {
+    let color_bg = g.theme.bg
+    let color_fg = g.theme.fg 
+  }
+}
+
 g.clear();
 Bangle.loadWidgets();
 /*
@@ -116,6 +127,7 @@ Bangle.loadWidgets();
  */
 for (let wd of WIDGETS) {wd.draw=()=>{};wd.area="";}
 loadSettings();
+set_colors();
 setInterval(draw, 15000); // refresh every 15s
 draw();
 Bangle.setUI("clock");
