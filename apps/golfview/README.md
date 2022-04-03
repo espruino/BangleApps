e3cc8717e6933cfcddc9174e0570e6941460e01e
Jason Dekarske <dekarskej@gmail.com>
2022-04-02 22:23:48 -0700

    emulator mvp course parsed and displayed!

---
+++ apps/golfview/README.md
@@ -0,0 +1,21 @@
+# App Name
+
+Describe the app...
+
+Add screen shots (if possible) to the app folder and link then into this file with ![](<name>.png)
+
+## Usage
+
+Select your course of interest upon loading this app.
+
+## Contributions
+
+The performance of this app depends on the accuracy and consistency of user-submitted maps. Please contribute to Open Street Map using these guidelines and provide input in ways to support this application.
+
+## Controls
+
+Swipe to change holes and tap to see a green closeup.
+
+## Requests/Creator
+
+[Jason Dekarske](https://github.com/jdekarske)
---
+++ apps/golfview/golfview.js
@@ -0,0 +1,51 @@
+let course = require("Storage").readJSON("course_data_hole1.json");
+
+console.log(Object.keys(course));
+
+g.clear();
+
+for (var feature of course.features) {
+  //console.log(Object.keys(feature));
+  if (feature.type === "fairway"){
+    g.setColor(0,0,1);
+  } else if ( feature.type === "tee"){
+    g.setColor(1,0,0);
+  } else if (feature.type === "green"){
+    g.setColor(0,1,0);
+  } else if (feature.type === "bunker"){
+    g.setColor(1,1,0);
+  } else {
+    continue;
+  }
+  
+  var nodelist = [];
+  feature.nodesXY.forEach(function (node) {
+    nodelist.push(node.x);
+    nodelist.push(node.y);
+  });
+  newnodelist = g.transformVertices(nodelist,{
+    x: 150, // x offset (default 0)
+    y: 150, // y offset (default 0)
+    scale: 0.45, // scale factor (default 1)
+    rotate: course.angle - 1.57, // angle in radians (default 0)
+  });
+  
+  g.fillPoly(newnodelist, true);
+  console.log(feature.type);
+  console.log(nodelist);
+}
+
+var nodelist = [];
+course.nodesXY.forEach(function (node) {
+  nodelist.push(node.x);
+  nodelist.push(node.y);
+});
+
+newnodelist = g.transformVertices(nodelist,{
+  x: 150, // x offset (default 0)
+  y: 150, // y offset (default 0)
+  scale: 0.45, // scale factor (default 1)
+  rotate: course.angle - 1.57, // angle in radians (default 0)
+});
+g.setColor(0,1,1);
+g.drawPoly(newnodelist);
