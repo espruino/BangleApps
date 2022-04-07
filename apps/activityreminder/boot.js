919175671dea80321c5d68ab710a582681965914
Stiralbios <aurelien.vezin.dev@gmail.com>
2022-04-07 18:31:11 +0200

    Try to preserve stepsArray

--- apps/activityreminder/boot.js
+++ apps/activityreminder/boot.js
@@ -7,7 +7,7 @@ global.activityreminder = Object.assign({
   minSteps: 50,
 }, require("Storage").readJSON("activityreminder.json", true) || {});
 
-var stepsArray = [];
+global.stepsArray = [];
 
 
 if (global.activityreminder) {
@@ -19,18 +19,18 @@ if (global.activityreminder) {
       run: function(){
         var now = new Date();
         var h = now.getHours();
-        console.log(stepsArray);
+        console.log(global.stepsArray);
         if(h >= activityreminder.startHour && h < activityreminder.endHour)
         {
           var health = Bangle.getHealthStatus("day");
-          stepsArray.unshift(health.steps);
-          stepsArray = stepsArray.slice(0, activityreminder.maxInnactivityMin);
+          global.stepsArray.unshift(health.steps);
+          global.stepsArray = global.stepsArray.slice(0, activityreminder.maxInnactivityMin);
         }
         else{
-          stepsArray = [];
+          global.stepsArray = [];
         }
-        if(stepsArray.length == activityreminder.maxInnactivityMin){
-          if (stepsArray[0] - stepsArray[stepsArray.length-1] < activityreminder.minSteps)
+        if(global.stepsArray.length == activityreminder.maxInnactivityMin){
+          if (global.stepsArray[0] - global.stepsArray[stepsArray.length-1] < activityreminder.minSteps)
           {
               load('activityreminder.app.js');
           }
