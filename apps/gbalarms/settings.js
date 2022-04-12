1caf5c6d8cba3f3be8373b7e9facf468c791bfc3
frigis1 <63980066+frigis1@users.noreply.github.com>
2022-04-11 23:04:18 -0700

    Update settings.js

--- apps/gbalarms/settings.js
+++ apps/gbalarms/settings.js
@@ -1,21 +1,42 @@
 (function(back) {
   var settings = require("Storage").readJSON("gbalarms.json", 1) || {};
-  E.showMenu({
-    "" : { "title": "GB Alarms" },
-    "Vibrate": require("buzz_menu").pattern(settings.vibrate, v => settings.vibrate=v),
-    "Repeat": {
-      value: settings.rp,
-      format: v => v ? "Yes" : "No",
-      onchange: v => settings.rp = v,
-    },
-    "Auto snooze": {
-       value: settings.as,
-       format: v => v ? "Yes" : "No",
-       onchange: v => settings.as = v,
-    },
+  var pattern = ["Off", ".", "..", "-", "--", "-.-", "---"];
+
+  function showMainMenu() {
+    var mainMenu = {
+      "" : { "title": "GB Alarms" },
+      "Vibrate": {
+        value: settings.vibrate,
+        onchange: function() { E.showMenu(vibMenu); },
+      },
+      "Repeat": {
+        value: settings.rp,
+        format: v => v ? "Yes" : "No",
+        onchange: v => settings.rp = v,
+      },
+      "Auto snooze": {
+        value: settings.as,
+        format: v => v ? "Yes" : "No",
+        onchange: v => settings.as = v,
+      },
+      "< Back": function() {
+        require('Storage').writeJSON("gbalarms.json", settings);
+        back();
+      }
+    };
+  E.showMenu(mainMenu);
+  }
+  var vibMenu = {
     "< Back": function() {
-      require('Storage').writeJSON("gbalarms.json", settings);
-      back();
+      showMainMenu();
     }
+  };
+  pattern.forEach((a, idx) => {
+    vibMenu[pattern[idx]] = function() {
+      settings.vibrate = a;
+      require("buzz").pattern(a);
+      showMainMenu();
+    };
   });
+  showMainMenu();
 });
