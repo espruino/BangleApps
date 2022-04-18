2e5f198d666379ae7dac0efbedcba7861ee0da77
Alessandro Cocco <alessandrococco@users.noreply.github.com>
2022-04-18 22:03:37 +0200

    [Launcher] Update labels
    - Shorten settings title
    - Change menu items to Camel Case

--- apps/launch/settings.js
+++ apps/launch/settings.js
@@ -8,7 +8,7 @@
     require("Storage").write("launch.json",settings);
   }
   const appMenu = {
-    "": {"title": /*LANG*/"Launcher Settings"},
+    "": { "title": /*LANG*/"Launcher" },
     /*LANG*/"< Back": back,
     /*LANG*/"Font": {
       value: fonts.includes(settings.font)? fonts.indexOf(settings.font) : fonts.indexOf("12x20"),
@@ -16,12 +16,12 @@
       onchange: (m) => {save("font", fonts[m])},
       format: v => fonts[v]
      },
-    /*LANG*/"Vector font size": {
+    /*LANG*/"Vector Font Size": {
       value: settings.vectorsize || 10,
       min:10, max: 20,step:1,wrap:true,
       onchange: (m) => {save("vectorsize", m)}
     },
-    /*LANG*/"Show clocks": {
+    /*LANG*/"Show Clocks": {
       value: settings.showClocks == true,
       format: v => v ? /*LANG*/"Yes" : /*LANG*/"No",
       onchange: (m) => {save("showClocks", m)}
