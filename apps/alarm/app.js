6c8c3805bf806643d690b72f9d02c3c9d25958f7
Alessandro Cocco <alessandrococco@users.noreply.github.com>
2022-04-25 15:24:05 +0200

    [Alarms & Timers] Automatically save the alarm/timer when the user returns to the main menu using the back arrow

--- apps/alarm/app.js
+++ apps/alarm/app.js
@@ -92,7 +92,10 @@ function editAlarm(alarmIndex, alarm) {
 
   const menu = {
     '': { 'title': /*LANG*/'Alarm' },
-    /*LANG*/'< Back' : () => showMainMenu(),
+    /*LANG*/'< Back': () => {
+      saveAlarm(newAlarm, alarmIndex, a, t);
+      showMainMenu();
+    },
     /*LANG*/'Hours': {
       value: t.hrs, min : 0, max : 23, wrap : true,
       onchange: v => t.hrs=v
@@ -126,22 +129,31 @@ function editAlarm(alarmIndex, alarm) {
       onchange: v => a.as = v
     }
   };
-  menu[/*LANG*/"Save"] = function() {
-    a.t = require("sched").encodeTime(t);
-    a.last = (a.t < getCurrentTime()) ? (new Date()).getDate() : 0;
-    if (newAlarm) alarms.push(a);
-    else alarms[alarmIndex] = a;
-    saveAndReload();
-    showMainMenu();
-  };
+
+  menu[/*LANG*/"Cancel"] = () => showMainMenu();
+
   if (!newAlarm) {
-    menu[/*LANG*/"Delete"] = function() {
-      alarms.splice(alarmIndex,1);
+    menu[/*LANG*/"Delete"] = function () {
+      alarms.splice(alarmIndex, 1);
       saveAndReload();
       showMainMenu();
     };
   }
+
   return E.showMenu(menu);
+}
+
+function saveAlarm(newAlarm, alarmIndex, a, t) {
+  a.t = require("sched").encodeTime(t);
+  a.last = (a.t < getCurrentTime()) ? (new Date()).getDate() : 0;
+
+  if (newAlarm) {
+    alarms.push(a);
+  } else {
+    alarms[alarmIndex] = a;
+  }
+
+  saveAndReload();
 }
 
 function editTimer(alarmIndex, alarm) {
@@ -153,7 +165,10 @@ function editTimer(alarmIndex, alarm) {
 
   const menu = {
     '': { 'title': /*LANG*/'Timer' },
-    /*LANG*/'< Back' : () => showMainMenu(),
+    /*LANG*/'< Back': () => {
+      saveTimer(newAlarm, alarmIndex, a, t);
+      showMainMenu();
+    },
     /*LANG*/'Hours': {
       value: t.hrs, min : 0, max : 23, wrap : true,
       onchange: v => t.hrs=v
@@ -169,15 +184,9 @@ function editTimer(alarmIndex, alarm) {
     },
     /*LANG*/'Vibrate': require("buzz_menu").pattern(a.vibrate, v => a.vibrate=v ),
   };
-  menu[/*LANG*/"Save"] = function() {
-    a.timer = require("sched").encodeTime(t);
-    a.t = getCurrentTime() + a.timer;
-    a.last = 0;
-    if (newAlarm) alarms.push(a);
-    else alarms[alarmIndex] = a;
-    saveAndReload();
-    showMainMenu();
-  };
+
+  menu[/*LANG*/"Cancel"] = () => showMainMenu();
+
   if (!newAlarm) {
     menu[/*LANG*/"Delete"] = function() {
       alarms.splice(alarmIndex,1);
@@ -186,6 +195,20 @@ function editTimer(alarmIndex, alarm) {
     };
   }
   return E.showMenu(menu);
+}
+
+function saveTimer(newAlarm, alarmIndex, a, t) {
+  a.timer = require("sched").encodeTime(t);
+  a.t = getCurrentTime() + a.timer;
+  a.last = 0;
+
+  if (newAlarm) {
+    alarms.push(a);
+  } else {
+    alarms[alarmIndex] = a;
+  }
+
+  saveAndReload();
 }
 
 function enableAll(on) {
