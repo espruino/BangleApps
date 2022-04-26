87e72910952a8b491553f2ed9d3b8350d76cc774
nomusictuesdays <104293511+nomusictuesdays@users.noreply.github.com>
2022-04-25 17:34:25 -0700

    Create app.js
    A watch face based on the classic Seiko model worn by one of my favorite authors. I didn't follow the original lcd layout exactly, opting for larger font for more easily readable time, and adding date, battery level, and step count read from the device.

---
+++ apps/bradbury/app.js
@@ -0,0 +1,1 @@
+A watch face based on the classic Seiko model worn by one of my favorite authors. I didn't follow the original lcd layout exactly, opting for larger font for more easily readable time, and adding date, battery level, and step count read from the device.
