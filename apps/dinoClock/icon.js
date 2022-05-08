c7aba47f5d4508714193ad1952840ffa62388c80
Iakov Davydov <idavydov@users.noreply.github.com>
2022-05-08 18:06:44 +0200

    add icon.js

---
+++ apps/dinoClock/icon.js
@@ -0,0 +1,1 @@
+require("heatshrink").decompress(atob("mEwgP/AC+eg/A4AFCg1AAoP+j/mvnMAoXn/gFB/+f84XDAowXDAov+g4RDAooAfgEf/F///8gEH++/AoNAg/v44LD5/zAoe//YFDn4FDg1//IFD//9AoMD/wFDgE+CIO//EPHoMD3/gAoIABAouPwDCDw0AAosJAoWAgAXDApfAAongOwIFC+EAPQIAB/AFE/0IRof+jgEC8YFEzsAAAd+/08DogFEA="))
