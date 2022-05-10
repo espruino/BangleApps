1c8ab449ea74bc9fbf6565485a0da48f5d2662d3
Iakov Davydov <idavydov@users.noreply.github.com>
2022-05-10 19:28:52 +0200

    invert icon

--- apps/dinoClock/icon.js
+++ apps/dinoClock/icon.js
@@ -1,1 +1,1 @@
-require("heatshrink").decompress(atob("mEwgP/AC+eg/A4AFCg1AAoP+j/mvnMAoXn/gFB/+f84XDAowXDAov+g4RDAooAfgEf/F///8gEH++/AoNAg/v44LD5/zAoe//YFDn4FDg1//IFD//9AoMD/wFDgE+CIO//EPHoMD3/gAoIABAouPwDCDw0AAosJAoWAgAXDApfAAongOwIFC+EAPQIAB/AFE/0IRof+jgEC8YFEzsAAAd+/08DogFEA="))
+require("heatshrink").decompress(atob("mEwgP/ABXACYcDAuFh/E/hgFC/O/AoPB8EZwc8AoUYgYFB4FgjAXDAowXBAo/B/ARBn4FGIIvBmAFE2AEBhwFEiIXEkGD4PvC4ntgfwoARCgRmBAoUD/ARCwAFBu9jAoWAiND4Ohg+Au8D4uhh+AogFC3/ABYf/wAjB4/hAofDwH/IIeAvwFEmkAhEj77nC8ePI4M2aLQiCABAA="))
