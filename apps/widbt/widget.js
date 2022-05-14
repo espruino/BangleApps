71ebe9bca52239b4e8a1820c9c59d12f3909bcc1
Iakov Davydov <idavydov@users.noreply.github.com>
2022-05-14 10:38:37 +0200

    widbt: option to hide when no connection

--- apps/widbt/widget.js
+++ apps/widbt/widget.js
@@ -1,12 +1,28 @@
-WIDGETS["bluetooth"]={area:"tr",width:15,draw:function() {
+WIDGETS["bluetooth"]={area:"tr",draw:function() {
+  if (WIDGETS.bluetooth.width==0)
+    return;
   g.reset();
   if (NRF.getSecurityStatus().connected)
     g.setColor((g.getBPP()>8) ? "#07f" : (g.theme.dark ? "#0ff" : "#00f"));
   else
     g.setColor(g.theme.dark ? "#666" : "#999");
   g.drawImage(atob("CxQBBgDgFgJgR4jZMawfAcA4D4NYybEYIwTAsBwDAA=="),2+this.x,2+this.y);
+},getWidth:function(){
+  if (!NRF.getSecurityStatus().connected) {
+    const settings = require('Storage').readJSON("widbt.json", true) || {};
+    if (settings.hideDisconnected)
+      return 0;
+  }
+  return 15;
 },changed:function() {
-  WIDGETS["bluetooth"].draw();
+  const newWidth = WIDGETS.bluetooth.getWidth();
+  if (WIDGETS.bluetooth.width != newWidth) {
+    WIDGETS.bluetooth.width = newWidth;
+    Bangle.drawWidgets();
+  } else {
+    WIDGETS.bluetooth.draw();
+  }
 }};
-NRF.on('connect',WIDGETS["bluetooth"].changed);
-NRF.on('disconnect',WIDGETS["bluetooth"].changed);
+WIDGETS.bluetooth.width = WIDGETS.bluetooth.getWidth();
+NRF.on('connect',WIDGETS.bluetooth.changed);
+NRF.on('disconnect',WIDGETS.bluetooth.changed);
