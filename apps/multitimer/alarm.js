15f395f5768a5385d98537875858d299bf0b9eba
frigis1 <63980066+frigis1@users.noreply.github.com>
2022-05-15 08:49:06 -0700

    Update alarm.js

--- apps/multitimer/alarm.js
+++ apps/multitimer/alarm.js
@@ -14,10 +14,6 @@ function hardMode(tries, max) {
     hardMode(tries, max);
   }
 
-  function random(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
   if (tries < max) {
     g.clear();
     g.reset();
