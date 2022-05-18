2c1a4e30035a0aa689c0fe5497973ab800e73cc7
Iakov Davydov <idavydov@users.noreply.github.com>
2022-05-18 19:10:02 +0200

    widbt: recover original widbt

---
+++ apps/widbt/widget.js
@@ -0,0 +1,12 @@
+WIDGETS["bluetooth"]={area:"tr",width:15,draw:function() {
+  g.reset();
+  if (NRF.getSecurityStatus().connected)
+    g.setColor((g.getBPP()>8) ? "#07f" : (g.theme.dark ? "#0ff" : "#00f"));
+  else
+    g.setColor(g.theme.dark ? "#666" : "#999");
+  g.drawImage(atob("CxQBBgDgFgJgR4jZMawfAcA4D4NYybEYIwTAsBwDAA=="),2+this.x,2+this.y);
+},changed:function() {
+  WIDGETS["bluetooth"].draw();
+}};
+NRF.on('connect',WIDGETS["bluetooth"].changed);
+NRF.on('disconnect',WIDGETS["bluetooth"].changed);
