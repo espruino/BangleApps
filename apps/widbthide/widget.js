63aa03bbdbcdbeeabaf89e23d8cc672a40a15a41
Iakov Davydov <idavydov@users.noreply.github.com>
2022-05-18 19:31:52 +0200

    widbthide: fix typo

--- apps/widbthide/widget.js
+++ apps/widbthide/widget.js
@@ -5,7 +5,7 @@ WIDGETS["bluetooth"]={area:"tr",draw:function() {
   g.setColor((g.getBPP()>8) ? "#07f" : (g.theme.dark ? "#0ff" : "#00f"));
   g.drawImage(atob("CxQBBgDgFgJgR4jZMawfAcA4D4NYybEYIwTAsBwDAA=="),2+this.x,2+this.y);
 },changed:function() {
-  WIDGET.bluetooth.width = NRF.getSecurityStatus().connected?15:0;
+  WIDGETS.bluetooth.width = NRF.getSecurityStatus().connected?15:0;
   Bangle.drawWidgets();
 },width:NRF.getSecurityStatus().connected?15:0
 };
