1fb256b9def3cf47df62d79b1f8f1ae563848e5f
Lubomir <lubomir.yudenko@gmail.com>
2022-05-24 22:24:38 +1000

    messages: Replace mail icon with color notif icons

--- apps/messages/widget.js
+++ apps/messages/widget.js
@@ -1,3 +1,15 @@
+(() => {
+
+function getBaseMessages() {
+  if ("undefined"!=typeof MESSAGES) return MESSAGES;
+  return require("Storage").readJSON("messages.json",1)||[];
+}
+
+function getMessages() {
+  return getBaseMessages().filter(msg => msg.new && msg.id != "music")
+    .filter((msg, i, arr) => arr.findIndex(nmsg => msg.src == nmsg.src) == i);
+}
+
 WIDGETS["messages"]={area:"tl", width:0, iconwidth:24,
 draw:function(recall) {
   // If we had a setTimeout queued from the last time we were called, remove it
@@ -11,8 +23,18 @@ draw:function(recall) {
   let settings = require('Storage').readJSON("messages.settings.json", true) || {};
   if (settings.flash===undefined) settings.flash = true;
   if (recall !== true || settings.flash) {
+    var msgs = getMessages();
+    var msgsShown = E.clip(msgs.length, 0, 3);
     g.reset().clearRect(this.x, this.y, this.x+this.width, this.y+23);
-    g.drawImage(settings.flash && (c&1) ? atob("GBiBAAAAAAAAAAAAAAAAAAAAAB//+DAADDAADDAADDwAPD8A/DOBzDDn/DA//DAHvDAPvjAPvjAPvjAPvh///gf/vAAD+AAB8AAAAA==") : atob("GBiBAAAAAAAAAAAAAAAAAAAAAB//+D///D///A//8CP/xDj/HD48DD+B8D/D+D/3vD/vvj/vvj/vvj/vvh/v/gfnvAAD+AAB8AAAAA=="), this.x, this.y-1);
+    for(let i = 0;i < msgsShown;i++) {
+      const msg = msgs[i];
+      const colors = [g.theme.bg, g.setColor(require("messages").getMessageImageCol(msg)).getColor()];
+      if (settings.flash && (c&1)) {
+        colors[1] = g.theme.fg;
+      }
+      g.setColor(colors[1]).setBgColor(colors[0]);
+      g.drawImage(i == 2 ? atob("GBgBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH4H4H4H4H4H4H4H4H4H4H4H4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA") : require("messages").getMessageImage(msg), this.x + i * this.iconwidth, this.y - 1);
+    }
   }
   if (settings.repeat===undefined) settings.repeat = 4;
   if (c<120 && (Date.now()-this.l)>settings.repeat*1000) {
@@ -21,18 +43,19 @@ draw:function(recall) {
   }
   WIDGETS["messages"].i=setTimeout(()=>WIDGETS["messages"].draw(true), 1000);
   if (process.env.HWVERSION>1) Bangle.on('touch', this.touch);
-},show:function(quiet) {
-  WIDGETS["messages"].t=Date.now(); // first time
-  WIDGETS["messages"].l=Date.now()-10000; // last buzz
-  if (quiet) WIDGETS["messages"].t -= 500000; // if quiet, set last time in the past so there is no buzzing
-  WIDGETS["messages"].width=this.iconwidth;
+},update:function(quiet) {
+  const msgs = getMessages();
+  if (msgs.length === 0) {
+    delete WIDGETS["messages"].t;
+    delete WIDGETS["messages"].l;
+  } else {
+    WIDGETS["messages"].t=Date.now(); // first time
+    WIDGETS["messages"].l=Date.now()-10000; // last buzz
+    if (quiet) WIDGETS["messages"].t -= 500000; // if quiet, set last time in the past so there is no buzzing
+  }
+  WIDGETS["messages"].width=this.iconwidth * E.clip(msgs.length, 0, 3);
   Bangle.drawWidgets();
-  Bangle.setLCDPower(1);// turns screen on
-},hide:function() {
-  delete WIDGETS["messages"].t;
-  delete WIDGETS["messages"].l;
-  WIDGETS["messages"].width=0;
-  Bangle.drawWidgets();
+  if (msgs.length !== 0) Bangle.setLCDPower(1);// turns screen on
 },buzz:function() {
   if ((require('Storage').readJSON('setting.json',1)||{}).quiet) return; // never buzz during Quiet Mode
   require("buzz").pattern((require('Storage').readJSON("messages.settings.json", true) || {}).vibrate || ".");
@@ -41,10 +64,10 @@ draw:function(recall) {
   if (!w||!w.width||c.x<w.x||c.x>w.x+w.width||c.y<w.y||c.y>w.y+w.iconwidth) return;
   load("messages.app.js");
 }};
+
 /* We might have returned here if we were in the Messages app for a
 message but then the watch was never viewed. In that case we don't
 want to buzz but should still show that there are unread messages. */
-if (global.MESSAGES===undefined) (function() {
-  var messages = require("Storage").readJSON("messages.json",1)||[];
-  if (messages.some(m=>m.new&&m.id!="music")) WIDGETS["messages"].show(true);
+WIDGETS["messages"].update(true);
+
 })();
