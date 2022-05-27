f763cd962f88d5393ee3a91e4b377e82dfb3e2a9
Martin Boonk <martin@boonk.info>
2022-05-27 13:16:47 +0200

    Clip values to display range

--- apps/calibration/boot.js
+++ apps/calibration/boot.js
@@ -9,6 +9,6 @@ Bangle.on('touch', function(button, xy) {
   }
 
   // apply the calibration offset
-  xy.x = Math.round((xy.x + cal_settings.xoffset) * cal_settings.xscale);
-  xy.y = Math.round((xy.y + cal_settings.yoffset) * cal_settings.yscale);
+  xy.x = E.clip(Math.round((xy.x + (cal_settings.xoffset || 0)) * (cal_settings.xscale || 1)),0,g.getWidth());
+  xy.y = E.clip(Math.round((xy.y + (cal_settings.yoffset || 0)) * (cal_settings.yscale || 1)),0,g.getHeight());
 });
