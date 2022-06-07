d09f85413b0f0d13307ed0fdf15039fb1f10c76e
Richard de Boer <git@tubul.net>
2022-06-07 21:44:54 +0200

    cogclock: Use ClockFace_menu.addSettingsFile

--- apps/cogclock/settings.js
+++ apps/cogclock/settings.js
@@ -1,19 +1,10 @@
 (function(back) {
-  let s = require('Storage').readJSON("cogclock.settings.json", true) || {};
-
-  function save(key, value) {
-    s[key] = value;
-    require("Storage").writeJSON("cogclock.settings.json", s);
-  }
-
   let menu = {
     "": {"title": /*LANG*/"Cog Clock"},
     /*LANG*/"< Back": back,
   };
-  require("ClockFace_menu").addItems(menu, save, {
-    showDate: s.showDate,
-    loadWidgets: s.loadWidgets,
-  });
-
+  require("ClockFace_menu").addSettingsFile(menu, "cogclock.settings.json", [
+    "showDate", "loadWidgets"
+  ]);
   E.showMenu(menu);
 });
