d07580cc907d3bd7d3de221231ccf32936394d2d
Gordon Williams <gw@pur3.co.uk>
2022-06-09 10:41:03 +0100

    Add minified layout library, which should improve layout speed a lot.

--- modules/Layout.js
+++ modules/Layout.js
@@ -2,6 +2,14 @@
 
 // See Layout.md for documentation
 
+/* Minify to 'Layout.min.js' by:
+
+   * checking out: https://github.com/espruino/EspruinoDocs
+   * run: ../EspruinoDocs/bin/minify.js modules/Layout.js modules/Layout.min.js
+   
+*/   
+ 
+
 function Layout(layout, options) {
   this._l = this.l = layout;
   // Do we have >1 physical buttons?
