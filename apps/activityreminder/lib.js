85a52afbf55725bd999bb94e2e8c1b929a3cc4ed
Stiralbios <aurelien.vezin.dev@gmail.com>
2022-06-12 17:50:43 +0200

    Fix the const reassign bug (cutting edge firmware) and better indentation and scoping

--- apps/activityreminder/lib.js
+++ apps/activityreminder/lib.js
@@ -1,5 +1,3 @@
-const storage = require("Storage");
-
 exports.loadSettings = function () {
     return Object.assign({
         enabled: true,
@@ -10,15 +8,15 @@ exports.loadSettings = function () {
         pauseDelayMin: 120,
         minSteps: 50,
         tempThreshold: 27
-    }, storage.readJSON("activityreminder.s.json", true) || {});
+    }, require("Storage").readJSON("activityreminder.s.json", true) || {});
 };
 
 exports.writeSettings = function (settings) {
-    storage.writeJSON("activityreminder.s.json", settings);
+    require("Storage").writeJSON("activityreminder.s.json", settings);
 };
 
 exports.saveData = function (data) {
-    storage.writeJSON("activityreminder.data.json", data);
+    require("Storage").writeJSON("activityreminder.data.json", data);
 };
 
 exports.loadData = function () {
@@ -31,7 +29,7 @@ exports.loadData = function () {
         dismissDate: new Date(1970),
         pauseDate: new Date(1970),
     },
-    storage.readJSON("activityreminder.data.json") || {});
+    require("Storage").readJSON("activityreminder.data.json") || {});
 
     if(typeof(data.stepsDate) == "string")
         data.stepsDate = new Date(data.stepsDate);
