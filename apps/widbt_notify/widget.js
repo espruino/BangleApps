c99a0de9a90d35a4b8db7822ba41e57dfd8841d8
Hank <hank@kueper.info>
2022-07-01 19:49:35 +0200

    widbt_notify: Added "connection restored" notification. Fixed restoring of the watchface.

--- apps/widbt_notify/widget.js
+++ apps/widbt_notify/widget.js
@@ -15,13 +15,28 @@ WIDGETS.bluetooth_notify = {
     
     redrawCurrentApp: function(){
         if(typeof(draw)=='function'){
+			g.clear();
             draw();
+			Bangle.loadWidgets();
+			Bangle.drawWidgets();
         }else{
             load(); // fallback. This might reset some variables
         }
     },
     
     connect: function() {
+        if(WIDGETS.bluetooth_notify.warningEnabled == 1){
+            E.showMessage(/*LANG*/'Connection\n restored.', 'Bluetooth');
+            setTimeout(()=>{WIDGETS.bluetooth_notify.redrawCurrentApp();}, 3000); // clear message - this will reload the widget, resetting 'warningEnabled'.
+            
+            WIDGETS.bluetooth_notify.warningEnabled = 0;
+            setTimeout('WIDGETS.bluetooth_notify.warningEnabled = 1;', 30000); // don't buzz for the next 30 seconds.
+            
+            var quiet       = (require('Storage').readJSON('setting.json',1)||{}).quiet;
+            if(!quiet){
+                Bangle.buzz(700, 1); // buzz on connection loss
+            }
+        }		
         WIDGETS.bluetooth_notify.draw();
     },
     
