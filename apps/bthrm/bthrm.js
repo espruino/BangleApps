72f5769486c7e84cdb31c4f81f03d0f05832e92f
Gordon Williams <gw@pur3.co.uk>
2022-07-01 11:07:22 +0100

    fix clear amount

--- apps/bthrm/bthrm.js
+++ apps/bthrm/bthrm.js
@@ -51,7 +51,7 @@ var firstEventInt = true;
 // This can get called for the boot code to show what's happening
 function showStatusInfo(txt) {
   var R = Bangle.appRect;
-  g.reset().clearRect(R.x,R.y2-24,R.x2,R.y2).setFont("6x8");
+  g.reset().clearRect(R.x,R.y2-8,R.x2,R.y2).setFont("6x8");
   txt = g.wrapString(txt, R.w)[0];
   g.setFontAlign(0,1).drawString(txt, (R.x+R.x2)/2, R.y2);
 }
