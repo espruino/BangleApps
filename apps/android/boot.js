aaa18568921e2100af938cdce2d0aef745091423
Martin Boonk <martin@boonk.info>
2022-07-02 17:00:23 +0200

    android - Fix timeout not beeing cleared

--- apps/android/boot.js
+++ apps/android/boot.js
@@ -122,14 +122,14 @@
       "http":function() {
         //get the promise and call the promise resolve
         if (Bangle.httpRequest === undefined) return;
-        var objID=Bangle.httpRequest[event.id];
-        if (objID === undefined) return; //already timedout or wrong id
+        var request=Bangle.httpRequest[event.id];
+        if (request === undefined) return; //already timedout or wrong id
         delete Bangle.httpRequest[event.id];
-        clearInterval(objID.t); //t = timeout variable
+        clearTimeout(request.t); //t = timeout variable
         if(event.err!==undefined) //if is error
-          objID.j(event.err); //r = reJect function
+          request.j(event.err); //r = reJect function
         else
-          objID.r(event); //r = resolve function
+          request.r(event); //r = resolve function
       }
     };
     var h = HANDLERS[event.t];
