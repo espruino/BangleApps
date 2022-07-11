2f940d901e6425693b184b420000bb6f9624a8f7
deirdreobyrne <deirdre.dub@gmail.com>
2022-07-11 14:37:09 +0100

    Checks firmware; E.setDST(...) moved to boot.js

--- apps/widdst/widget.js
+++ apps/widdst/widget.js
@@ -40,26 +40,16 @@
       clear();
     }
   }
-  
-  function setDst() {
-    var dstSettings = require('Storage').readJSON('widdst.json',1)||{};
-    if (dstSettings.has_dst) {
-      E.setDST(60*dstSettings.dst_size, 60*dstSettings.tz, dstSettings.dst_start.dow_number, dstSettings.dst_start.dow,
-        dstSettings.dst_start.month, dstSettings.dst_start.day_offset, 60*dstSettings.dst_start.at,
-        dstSettings.dst_end.dow_number, dstSettings.dst_end.dow, dstSettings.dst_end.month, dstSettings.dst_end.day_offset,
-        60*dstSettings.dst_end.at);
-    } else {
-      E.setDST(0,0,0,0,0,0,0,0,0,0,0,0);
-    }
-  }
 
-  setDst();
-  
   // Register ourselves
-  WIDGETS["widdst"] = {
-    area: "tl",
-    width: 0,
-    draw: draw
-  };
+  if (E.setDST) {
+    WIDGETS["widdst"] = {
+      area: "tl",
+      width: 0,
+      draw: draw
+    };
+  } else {
+    E.showAlert("Firmware update needed to support Daylight Saving Time");
+  }
 
 })()
