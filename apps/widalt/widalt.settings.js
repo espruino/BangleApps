974b1238da2e72fa93399772bd0346675109237e
xxDUxx <96152564+xxDUxx@users.noreply.github.com>
2022-08-02 13:46:41 +0200

    Create widalt.settings.js

---
+++ apps/widalt/widalt.settings.js
@@ -0,0 +1,18 @@
+(function(back) {
+  o=Bangle.getOptions();
+  Bangle.getPressure().then((p)=>{
+    E.showMenu({
+      "" : { "title" : "Altimeter Widget" },
+      "< Back" : () => back(),
+      'QNH: ': {
+        value: 1013 | o.seaLevelPressure,  // 0| converts undefined to 0
+        min: 100, max: 10000,
+        format: v=>(v+"hPa\nAlt: "+(44330 * (1.0 - Math.pow(p.pressure/v, 0.1903))).toFixed(0)+"m"),
+        onchange: v => {
+          o.seaLevelPressure = v;
+          Bangle.setOptions(o);
+        }
+      }
+    });
+  });
+})
