f264f8e043fc4baa38f25f1705a949d80e81f616
xxDUxx <96152564+xxDUxx@users.noreply.github.com>
2022-08-03 08:08:07 +0200

    Update widalt.settings.js

--- apps/widalt/widalt.settings.js
+++ apps/widalt/widalt.settings.js
@@ -8,10 +8,7 @@
         value: o.seaLevelPressure,
         min: 100, max: 10000,
         format: v=>(v+"hPa\nAlt: "+(44330 * (1.0 - Math.pow(p.pressure/v, 0.1903))).toFixed(0)+"m"),
-        onchange: v => {
-          o.seaLevelPressure = v;
-          Bangle.setOptions(o);
-        }
+        onchange: v => {Bangle.setOptions({seaLevelPressure:v});}
       }
     });
   });
