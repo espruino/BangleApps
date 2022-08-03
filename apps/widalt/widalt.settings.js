7032496bcf54b3fddaf31a4d9759fd255069e2f8
xxDUxx <96152564+xxDUxx@users.noreply.github.com>
2022-08-03 08:12:49 +0200

    Update widalt.settings.js

--- apps/widalt/widalt.settings.js
+++ apps/widalt/widalt.settings.js
@@ -4,12 +4,12 @@
     E.showMenu({
       "" : { "title" : "Altimeter Widget" },
       "< Back" : () => back(),
-      'QNH: ': {
-        value: o.seaLevelPressure,
+      'QNH': {
+        value: Math.floor(o.seaLevelPressure),
         min: 100, max: 10000,
         format: v=>(v+"hPa\nAlt: "+(44330 * (1.0 - Math.pow(p.pressure/v, 0.1903))).toFixed(0)+"m"),
         onchange: v => {Bangle.setOptions({seaLevelPressure:v});}
       }
     });
   });
-})
+})(load)
