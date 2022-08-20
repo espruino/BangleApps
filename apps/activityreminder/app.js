392866d2de1e5777896b08be545797be79f94773
Stiralbios <aurelien.vezin.dev@gmail.com>
2022-08-20 16:11:10 +0200

    [ActivityReminder] New app screen

--- apps/activityreminder/app.js
+++ apps/activityreminder/app.js
@@ -1,20 +1,54 @@
 (function () {
-  // todo do something more interesting than just showing the settings
-  /*
   // load variable before defining functions cause it can trigger a ReferenceError
   const activityreminder = require("activityreminder");
-  const storage = require("Storage");
-  const activityreminder_settings = activityreminder.loadSettings();
   let activityreminder_data = activityreminder.loadData();
-  */
+  let W = g.getWidth();
+  // let H = g.getHeight();
 
-  function run() {
-    eval(storage.read("activityreminder.settings.js"))(() => load());
+  function getHoursMins(date){
+    var h = date.getHours();
+    var m = date.getMinutes();
+    return (""+h).substr(-2) + ":" + ("0"+m).substr(-2);
   }
 
-  g.clear();
-  Bangle.loadWidgets();
-  Bangle.drawWidgets();
+  function drawData(name, value, y){
+    g.drawString(name, 10, y);
+    g.drawString(value, 100, y);
+  }
+  
+  function drawInfo() {
+    var h=18, y = h;
+    g.setColor(g.theme.fg);
+    g.setFont("Vector",h).setFontAlign(-1,-1);
+
+    // Header
+    g.drawLine(0,25,W,25);
+    g.drawLine(0,26,W,26);
+  
+    g.drawString("Current Cycle", 10, y+=h);
+    drawData("Start", getHoursMins(activityreminder_data.stepsDate), y+=h);
+    drawData("Steps", getCurrentSteps(), y+=h);
+
+    /*
+    g.drawString("Button Press", 10, y+=h*2);
+    drawData("Ok", getHoursMins(activityreminder_data.okDate), y+=h);
+    drawData("Dismiss", getHoursMins(activityreminder_data.dismissDate), y+=h);
+    drawData("Pause", getHoursMins(activityreminder_data.pauseDate), y+=h);
+    */
+  }
+
+  function getCurrentSteps(){
+    let health = Bangle.getHealthStatus("day");
+    return health.steps - activityreminder_data.stepsOnDate;
+  }
+
+  function run() {
+    g.clear();
+    Bangle.loadWidgets();
+    Bangle.drawWidgets();
+    drawInfo();
+  }
+
   run();
 
 })();
