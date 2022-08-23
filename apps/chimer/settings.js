c2b93e33254b0c0680de81cbf2d19389e21a0ad3
aaronrolls <public@aaronrolls.nz>
2022-08-23 00:58:13 +0000

    Fixed a bug

--- apps/chimer/settings.js
+++ apps/chimer/settings.js
@@ -28,7 +28,7 @@ var showMainMenu = () => {
     "Frequency": {
       value: settings.freq,
       min: 0, max: 2,
-      format: v => frequency[v],
+      format: v => v,
       onchange: v => {
         settings.freq = v;
         writeSettings(settings);
