f6640e10ce2133e5ec43cf116627bb1e98bd12be
storm64 <github@storm64.de>
2022-09-06 12:15:52 +0200

    [ptlaunch] Add setUI custom clock recognition
    Update boot.js to support clocks in custom setUI mode, started with: `Bangle.setUI({ mode: "custom", clock: 1, ... });

--- apps/ptlaunch/boot.js
+++ apps/ptlaunch/boot.js
@@ -76,13 +76,8 @@
   var sui = Bangle.setUI;
   Bangle.setUI = function (mode, cb) {
     sui(mode, cb);
-    if ("object"==typeof mode) mode = mode.mode;
-    if (!mode) {
-      Bangle.removeListener("drag", dragHandler);
-      storedPatterns = {};
-      return;
-    }
-    if (!mode.startsWith("clock")) {
+    if (typeof mode === "object") mode = { mode: mode };
+    if (!mode || !(mode.clock || mode.startsWith("clock"))) {
       storedPatterns = {};
       Bangle.removeListener("drag", dragHandler);
       return;
