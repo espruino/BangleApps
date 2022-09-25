cd4efcb57782de6ce43389952d00b597c78ec165
Spencer Churchill <spence@duck.com>
2022-09-25 14:36:53 -0700

    dont buzz on sunday

--- apps/twenties/boot.js
+++ apps/twenties/boot.js
@@ -2,12 +2,13 @@
   const move = 20 * 60 * 1000; // 20 minutes
   const look = 20 * 1000;      // 20 seconds
 
-  buzz = _ => {
+  const buzz = _ => {
     const date = new Date();
     const day = date.getDay();
     const hour = date.getHours();
     // buzz at work
-    if (day <= 5 && hour >= 8 && hour <= 17) {
+    if (day >= 1 && day <= 5 &&
+      hour >= 8 && hour <= 17) {
       Bangle.buzz().then(_ => {
         setTimeout(Bangle.buzz, look);
       });
