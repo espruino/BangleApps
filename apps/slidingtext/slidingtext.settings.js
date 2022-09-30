cc551b64964cd13a561481a3c717d6ff4ecc0f59
adrian w kirk <adrianwkirk@gmail.com>
2022-09-30 10:46:32 +0100

    sliding text clock: BUGFIX - settings not working map remove not supported

--- apps/slidingtext/slidingtext.settings.js
+++ apps/slidingtext/slidingtext.settings.js
@@ -103,7 +103,7 @@
         if(styling.row_types != null)
             settings.row_types = styling.row_types;
 
-        settings.delete("row_defs");
+        settings.row_defs = [];
         if(styling.row_defs != null)
             settings.row_defs = styling.row_defs;
 
