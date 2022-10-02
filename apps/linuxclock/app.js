fc9ef3fd69b6192dfef59a666ddb6e518b77dd28
David Peer <david.peer@outlook.com>
2022-10-02 14:14:32 +0200

    Minor design change

--- apps/linuxclock/app.js
+++ apps/linuxclock/app.js
@@ -231,13 +231,10 @@ function drawCursor(){
    c += g.stringWidth(":");
 
    g.setColor("#0ff");
-   g.drawString("~", x+c, y);
-   c += g.stringWidth("~");
+   g.drawString("~$ ", x+c, y);
+   c += g.stringWidth("~$ ");
 
    g.setColor(g.theme.fg);
-   g.drawString("$", x+c, y);
-   c += g.stringWidth("$ ");
-
    g.drawString(cmd, x+c, y);
  }
 
