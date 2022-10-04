74166eaedc951528a5af04f131c7a8a4729ce36d
storm64 <github@storm64.de>
2022-10-04 17:42:08 +0200

    [waypointer] Correct wpindex=0 check

--- apps/waypointer/app.js
+++ apps/waypointer/app.js
@@ -263,7 +263,7 @@ function nextwp(inc){
 }
 
 function doselect(){
-  if (selected && wpindex!=0 && waypoints[wpindex].lat===undefined && savedfix.fix) {
+  if (selected && wpindex>=0 && waypoints[wpindex].lat===undefined && savedfix.fix) {
      waypoints[wpindex] ={name:"@"+wp.name, lat:savedfix.lat, lon:savedfix.lon};
      wp = waypoints[wpindex];
      require("waypoints").save(waypoints);
