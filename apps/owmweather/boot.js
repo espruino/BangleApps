0d145328a01b3703ee5fd007bd2d349fdfe2af29
Martin Boonk <martin@boonk.info>
2022-10-30 15:52:06 +0100

    owmweather - Schedule the first update for 5s after boot

--- apps/owmweather/boot.js
+++ apps/owmweather/boot.js
@@ -7,16 +7,19 @@
   let completion = function(){
     waiting = false;
   }
-  
+
   if (settings.enabled) {    
     let weather = require("Storage").readJSON('weather.json') || {};
     let lastUpdate;
     if (weather && weather.weather && weather.weather.time) lastUpdate = weather.weather.time;
+
     if (!lastUpdate || lastUpdate + settings.refresh * 1000 * 60 < Date.now()){
-      if (!waiting){
-        waiting = true;
-        require("owmweather").pull(completion);
-      }
+      setTimeout(() => {
+        if (!waiting){
+          waiting = true;
+          require("owmweather").pull(completion);
+        }
+      }, 5000);
     }
     setInterval(() => {
       if (!waiting && NRF.getSecurityStatus().connected){
