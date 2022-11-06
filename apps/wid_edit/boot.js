9f17ea5c98c3854fac64429b589e8209035d541d
Martin Boonk <martin@boonk.info>
2022-11-06 13:36:14 +0100

    wid_edit - Access global variables explicitly

--- apps/wid_edit/boot.js
+++ apps/wid_edit/boot.js
@@ -7,14 +7,14 @@ Bangle.loadWidgets = (o => ()=>{
     // store defaults of customized values in _WIDGETS
     if (!global._WIDGETS) global._WIDGETS = {};
     if (!global._WIDGETS[w]) global._WIDGETS[w] = {};
-    Object.keys(c[w]).forEach(k => _WIDGETS[w][k] = WIDGETS[w][k]);
+    Object.keys(c[w]).forEach(k => global._WIDGETS[w][k] = global.WIDGETS[w][k]);
     //overide values in widget with configured ones
-    Object.assign(WIDGETS[w], c[w]);
+    Object.assign(global.WIDGETS[w], c[w]);
   }
-  const W = WIDGETS;
-  WIDGETS = {};
+  const W = global.WIDGETS;
+  global.WIDGETS = {};
   Object.keys(W)
     .sort()
     .sort((a, b) => (0|W[b].sortorder)-(0|W[a].sortorder))
-    .forEach(k => WIDGETS[k] = W[k]);
+    .forEach(k => global.WIDGETS[k] = W[k]);
 })(Bangle.loadWidgets);
