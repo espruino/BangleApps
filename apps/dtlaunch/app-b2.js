5e2819bedcb7aed55cc3d143320dd21da70cf53d
thyttan <6uuxstm66@mozmail.com>
2022-11-06 19:07:01 +0100

    new try at fix

--- apps/dtlaunch/app-b2.js
+++ apps/dtlaunch/app-b2.js
@@ -12,8 +12,10 @@ let settings = Object.assign({
   swipeExit: false,
 }, require('Storage').readJSON("dtlaunch.json", true) || {});
 
-if (settings.oneClickExit) let buttonWatch = setWatch(_=> returnToClock, BTN1);
-  
+if (settings.oneClickExit) {
+  let buttonWatch = setWatch(_=> returnToClock, BTN1);
+} 
+
 let s = require("Storage");
   var apps = s.list(/\.info$/).map(app=>{
   let a=s.readJSON(app,1);
