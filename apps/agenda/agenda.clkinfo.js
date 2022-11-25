602689ed28fbe8866fae44b176a41307a137c11c
Gordon Williams <gw@pur3.co.uk>
2022-11-25 16:26:38 +0000

    Fix clkinfo icon

--- apps/agenda/agenda.clkinfo.js
+++ apps/agenda/agenda.clkinfo.js
@@ -1,7 +1,7 @@
 (function() {
   var agendaItems = {
       name: "Agenda",
-      img: atob("GBiBAf////////85z/AAAPAAAPgAAP////AAAPAAAPAAAPAAAOAAAeAAAeAAAcAAA8AAAoAABgAADP//+P//8PAAAPAAAPgAAf///w=="),
+      img: atob("GBiBAAAAAAAAAADGMA///w///wf//wAAAA///w///w///w///x///h///h///j///D///X//+f//8wAABwAADw///w///wf//gAAAA=="),
       items: []
     };
   var locale = require("locale");
@@ -12,13 +12,13 @@
 
   agenda.forEach((entry, i) => {
 
-    var title = entry.title.slice(0,18);
+    var title = entry.title.slice(0,12);
     var date = new Date(entry.timestamp*1000);
     var dateStr = locale.date(date).replace(/\d\d\d\d/,"");
     dateStr += entry.durationInSeconds < 86400 ? "/ " + locale.time(date,1) : "";
 
     agendaItems.items.push({
-      name: null,
+      name: "Agenda "+i,
       get: () => ({ text: title + "\n" + dateStr, img: null}),
       show: function() { agendaItems.items[i].emit("redraw"); },
       hide: function () {}
