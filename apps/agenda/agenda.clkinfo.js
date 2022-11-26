a011f929fd7e29f67fe3b01e86bc41477f1c4ed0
glemco <glemco@github>
2022-11-26 20:12:08 +0100

    Rolled back agenda clkinfo

--- apps/agenda/agenda.clkinfo.js
+++ apps/agenda/agenda.clkinfo.js
@@ -1,34 +1,25 @@
 (function() {
   var agendaItems = {
-    name: "Agenda",
-    img: atob("GBiBAAAAAAAAAADGMA///w///wf//wAAAA///w///w///w///x///h///h///j///D///X//+f//8wAABwAADw///w///wf//gAAAA=="),
-    dynamic: true,
-    items: []
-  };
-  var storage = require("Storage");
+      name: "Agenda",
+      img: atob("GBiBAAAAAAAAAADGMA///w///wf//wAAAA///w///w///w///x///h///h///j///D///X//+f//8wAABwAADw///w///wf//gAAAA=="),
+      items: []
+    };
   var locale = require("locale");
   var now = new Date();
-  var agenda = (storage.readJSON("android.calendar.json",true)||[])
-    .filter(ev=>ev.timestamp + ev.durationInSeconds > now/1000)
-    .sort((a,b)=>a.timestamp - b.timestamp);
-  var settings = storage.readJSON("agenda.settings.json",true)||{};
+  var agenda = require("Storage").readJSON("android.calendar.json")
+          .filter(ev=>ev.timestamp + ev.durationInSeconds > now/1000)
+          .sort((a,b)=>a.timestamp - b.timestamp);
 
   agenda.forEach((entry, i) => {
+
     var title = entry.title.slice(0,12);
     var date = new Date(entry.timestamp*1000);
     var dateStr = locale.date(date).replace(/\d\d\d\d/,"");
-    var dateStrToday = locale.date(new Date()).replace(/\d\d\d\d/,"");
-    var timeStr = locale.time(date);
-    //maybe not the most efficient..
-    var shortTxt = (dateStrToday == dateStr) ? timeStr : dateStr;
     dateStr += entry.durationInSeconds < 86400 ? "/ " + locale.time(date,1) : "";
 
-    if(!settings.pastEvents && entry.timestamp + entry.durationInSeconds < (new Date())/1000)
-      return;
-
     agendaItems.items.push({
-      name: null,
-      get: () => ({ text: title + "\n" + dateStr, short: shortTxt, img: null}),
+      name: "Agenda "+i,
+      get: () => ({ text: title + "\n" + dateStr, img: null}),
       show: function() { agendaItems.items[i].emit("redraw"); },
       hide: function () {}
     });
