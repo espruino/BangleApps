52c807aea5dcd888bbc90a8997038446b23da334
glemco <glemco@github>
2022-11-27 15:03:28 +0100

    weather: added weather condition to clkinfo
    picture is dynamically generated with the weather lib

--- apps/weather/clkinfo.js
+++ apps/weather/clkinfo.js
@@ -3,6 +3,7 @@
         temp: "?",
         hum: "?",
         wind: "?",
+        txt: "?",
     };
 
     var weatherJson = require("Storage").readJSON('weather.json');
@@ -14,17 +15,34 @@
         weather.wind = Math.round(weather.wind[1]) + "kph";
     }
 
+    function weatherIcon(code) {
+      var ovr = Graphics.createArrayBuffer(24,24,1,{msb:true});
+      require("weather").drawIcon({code:code},12,12,12,ovr);
+      var img = ovr.asImage();
+      img.transparent = 0;
+      //for (var i=0;i<img.buffer.length;i++) img.buffer[i]^=255;
+      return img;
+      //g.setColor("#0ff").drawImage(img, 42, 42);
+    }
+
     //FIXME ranges are somehow arbitrary
     var weatherItems = {
         name: "Weather",
         img: atob("GBiBAf+///u5//n7//8f/9wHP8gDf/gB//AB/7AH/5AcP/AQH/DwD/uAD84AD/4AA/wAAfAAAfAAAfAAAfgAA/////+bP/+zf/+zfw=="),
         items: [
             {
+                name: "condition",
+                get: () => ({ text: weather.txt, img: weatherIcon(weather.code),
+                  v: weather.code}),
+                show: function() { weatherItems.items[0].emit("redraw"); },
+                hide: function () {}
+            },
+            {
                 name: "temperature",
                 hasRange : true,
                 get: () => ({ text: weather.temp, img: atob("GBiBAAA8AAB+AADnAADDAADDAADDAADDAADDAADbAADbAADbAADbAADbAADbAAHbgAGZgAM8wAN+wAN+wAM8wAGZgAHDgAD/AAA8AA=="),
                   v: parseInt(weather.temp), min: -30, max: 55}),
-                show: function() { weatherItems.items[0].emit("redraw"); },
+                show: function() { weatherItems.items[1].emit("redraw"); },
                 hide: function () {}
             },
             {
@@ -32,7 +50,7 @@
                 hasRange : true,
                 get: () => ({ text: weather.hum, img: atob("GBiBAAAEAAAMAAAOAAAfAAAfAAA/gAA/gAI/gAY/AAcfAA+AQA+A4B/A4D/B8D/h+D/j+H/n/D/n/D/n/B/H/A+H/AAH/AAD+AAA8A=="),
                   v: parseInt(weather.hum), min: 0, max: 100}),
-                show: function() { weatherItems.items[1].emit("redraw"); },
+                show: function() { weatherItems.items[2].emit("redraw"); },
                 hide: function () {}
             },
             {
@@ -40,7 +58,7 @@
                 hasRange : true,
                 get: () => ({ text: weather.wind, img: atob("GBiBAAHgAAPwAAYYAAwYAAwMfAAY/gAZh3/xg//hgwAAAwAABg///g//+AAAAAAAAP//wH//4AAAMAAAMAAYMAAYMAAMcAAP4AADwA=="),
                   v: parseInt(weather.wind), min: 0, max: 118}),
-                show: function() { weatherItems.items[2].emit("redraw"); },
+                show: function() { weatherItems.items[3].emit("redraw"); },
                 hide: function () {}
             },
         ]
