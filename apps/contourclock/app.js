2abdfd991c10785e1a180fdfb29e685b9b78aff1
xxDUxx <96152564+xxDUxx@users.noreply.github.com>
2022-12-03 14:19:04 +0100

    Update app.js

--- apps/contourclock/app.js
+++ apps/contourclock/app.js
@@ -5,7 +5,11 @@
   let settings = require('Storage').readJSON("contourclock.json", true) || {};
   if (settings.fontIndex==undefined) {
     settings.fontIndex=0;
-    require('Storage').writeJSON("myapp.json", settings);
+    settings.widgets=true;
+    settings.hide=false;
+    settings.weekday=true;
+    settings.hideWhenLocked=false;
+    settings.date=true;    require('Storage').writeJSON("myapp.json", settings);
   }
 
   let queueDraw = function() {
@@ -23,23 +27,31 @@
     g.reset();
     g.setFont("Teletext10x18Ascii");
     g.clearRect(0,138,g.getWidth()-1,176);
-    g.setFontAlign(0,1).drawString(require("locale").dow(date).toUpperCase(),g.getWidth()/2,g.getHeight()-18);
+    if (settings.weekday) g.setFontAlign(0,1).drawString(require("locale").dow(date).toUpperCase(),g.getWidth()/2,g.getHeight()-18);
     // Draw Date
-    g.setFontAlign(0,1).drawString(require('locale').date(new Date(),1),g.getWidth()/2,g.getHeight());
+    if (settings.date) g.setFontAlign(0,1).drawString(require('locale').date(new Date(),1),g.getWidth()/2,g.getHeight());
     require('contourclock').drawClock(settings.fontIndex);
   };
+  
+  if (settings.hideWhenLocked) Bangle.on('lock', function (locked) {
+    g.clear();
+    if (locked) require('contourclock').drawClock(settings.fontIndex);
+    else draw();
+  });
 
   require("FontTeletext10x18Ascii").add(Graphics);
   g.clear();
   draw();
   Bangle.setUI({mode:"clock", remove:function() {
     if (drawTimeout) clearTimeout(drawTimeout);
-    require("widget_utils").show();
+    if (settings.widgets && settings.hide) require("widget_utils").show();
     g.reset();
     g.clear();
   }});
-  Bangle.loadWidgets();
-  require("widget_utils").swipeOn();
-  //Bangle.drawWidgets();
+  if (settings.widgets) {
+    Bangle.loadWidgets();
+    if (settings.hide) require("widget_utils").swipeOn();
+    else Bangle.drawWidgets();
+  }
   queueDraw();
 }
