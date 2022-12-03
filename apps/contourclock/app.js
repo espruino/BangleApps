5308b42db1218457e965d96c05b1fd43c3e63836
xxDUxx <96152564+xxDUxx@users.noreply.github.com>
2022-12-03 13:07:33 +0100

    Update app.js

--- apps/contourclock/app.js
+++ apps/contourclock/app.js
@@ -1,35 +1,45 @@
-var digits = [];
-var drawTimeout;
-var fontName="";
-var settings = require('Storage').readJSON("contourclock.json", true) || {};
-if (settings.fontIndex==undefined) {
-  settings.fontIndex=0;
-  require('Storage').writeJSON("myapp.json", settings);
-}
+{
+  let digits = [];
+  let drawTimeout;
+  let fontName="";
+  let settings = require('Storage').readJSON("contourclock.json", true) || {};
+  if (settings.fontIndex==undefined) {
+    settings.fontIndex=0;
+    require('Storage').writeJSON("myapp.json", settings);
+  }
 
-function queueDraw() {
-  setTimeout(function() {
-    draw();
-    queueDraw();
-  }, 60000 - (Date.now() % 60000));
-}
+  let queueDraw = function() {
+    if (drawTimeout) clearTimeout(drawTimeout);
+    drawTimeout = setTimeout(function() {
+      drawTimeout = undefined;
+      draw();
+      queueDraw();
+    }, 60000 - (Date.now() % 60000));
+  };
 
-function draw() {
-  var date = new Date();
-  // Draw day of the week
-  g.reset();
-  g.setFont("Teletext10x18Ascii");
-  g.clearRect(0,138,g.getWidth()-1,176);
-  g.setFontAlign(0,1).drawString(require("locale").dow(date).toUpperCase(),g.getWidth()/2,g.getHeight()-18);
-  // Draw Date
-  g.setFontAlign(0,1).drawString(require('locale').date(new Date(),1),g.getWidth()/2,g.getHeight());
-  require('contourclock').drawClock(settings.fontIndex);
-}
+  let draw = function() {
+    var date = new Date();
+    // Draw day of the week
+    g.reset();
+    g.setFont("Teletext10x18Ascii");
+    g.clearRect(0,138,g.getWidth()-1,176);
+    g.setFontAlign(0,1).drawString(require("locale").dow(date).toUpperCase(),g.getWidth()/2,g.getHeight()-18);
+    // Draw Date
+    g.setFontAlign(0,1).drawString(require('locale').date(new Date(),1),g.getWidth()/2,g.getHeight());
+    require('contourclock').drawClock(settings.fontIndex);
+  };
 
-require("FontTeletext10x18Ascii").add(Graphics);
-Bangle.setUI("clock");
-g.clear();
-Bangle.loadWidgets();
-Bangle.drawWidgets();
-queueDraw();
-draw();
+  require("FontTeletext10x18Ascii").add(Graphics);
+  g.clear();
+  draw();
+  Bangle.setUI({mode:"clock", remove:function() {
+    if (drawTimeout) clearTimeout(drawTimeout);
+    require("widget_utils").show();
+    g.reset();
+    g.clear();
+  }});
+  Bangle.loadWidgets();
+  require("widget_utils").swipeOn();
+  //Bangle.drawWidgets();
+  queueDraw();
+}
