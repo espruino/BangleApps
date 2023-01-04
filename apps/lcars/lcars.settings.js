bcc60776b80180db59040d872f08efaa8fbfec80
Ronin0000 <89286474+Ronin0000@users.noreply.github.com>
2023-01-03 16:24:56 -0800

    Update lcars.settings.js

--- apps/lcars/lcars.settings.js
+++ apps/lcars/lcars.settings.js
@@ -14,6 +14,7 @@
     themeColor2BG: "#FF00DC",
     themeColor3BG: "#0094FF",
     disableAlarms: false,
+    disableData: false,
   };
   let saved_settings = storage.readJSON(SETTINGS_FILE, 1) || settings;
   for (const key in saved_settings) {
@@ -27,8 +28,8 @@
 
   var dataOptions = ["Steps", "Battery", "VREF", "HRM", "Temp", "Humidity", "Wind", "Altitude", "CoreT"];
   var speedOptions = ["kph", "mph"];
-  var color_options = ['Green','Orange','Cyan','Purple','Red','Blue','Yellow','White'];
-  var bg_code = ['#00ff00','#FF9900','#0094FF','#FF00DC','#ff0000','#0000ff','#ffef00','#FFFFFF'];
+  var color_options = ['Green','Orange','Cyan','Purple','Red','Blue','Yellow','White','Purple','Pink','Light Green','Dark Green'];
+  var bg_code = ['#00ff00','#FF9900','#0094FF','#FF00DC','#ff0000','#0000ff','#ffef00','#FFFFFF','#FF00FF','#6C00FF','#99FF00',"#556B2F'];
 
   E.showMenu({
     '': { 'title': 'LCARS Clock' },
@@ -109,6 +110,14 @@
       format: () => (settings.disableAlarms ? 'Yes' : 'No'),
       onchange: () => {
         settings.disableAlarms = !settings.disableAlarms;
+        save();
+      },
+    },
+    'Disable data pages functionality': {
+      value: settings.disableData,
+      format: () => (settings.disableData ? 'Yes' : 'No'),
+      onchange: () => {
+        settings.disableData = !settings.disableData;
         save();
       },
     },
