364888d959297e4e41f1395704483b451a73fe47
sir-indy <53864146+sir-indy@users.noreply.github.com>
2023-01-12 20:46:34 +0000

    Modify fuzzyw clock to use fastload & widget_utils

---
+++ apps/fuzzyw/fuzzyw.settings.js
@@ -0,0 +1,33 @@
+(function(back) {
+  const SETTINGS_FILE = "fuzzy.settings.json";
+
+  // initialize with default settings...
+  let s = {'showWidgets': false}
+
+  // ...and overwrite them with any saved values
+  // This way saved values are preserved if a new version adds more settings
+  const storage = require('Storage')
+  let settings = storage.readJSON(SETTINGS_FILE, 1) || s;
+  const saved = settings || {}
+  for (const key in saved) {
+    s[key] = saved[key]
+  }
+
+  function save() {
+    settings = s
+    storage.write(SETTINGS_FILE, settings)
+  }
+
+  E.showMenu({
+    '': { 'title': 'Fuzzy Word Clock' },
+    '< Back': back,
+    'Show Widgets': {
+      value: settings.showWidgets,
+      format: () => (settings.showWidgets ? 'Yes' : 'No'),
+      onchange: () => {
+        settings.showWidgets = !settings.showWidgets;
+        save();
+      }
+    },
+  });
+})
