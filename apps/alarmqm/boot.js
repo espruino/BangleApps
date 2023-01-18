22d8ca43dc96602e8c17d91eea81de1fca18faa6
Kedlub <jakubkyzr@gmail.com>
2023-01-18 14:16:28 +0100

    Disable quiet mode after alarm dismiss

---
+++ apps/alarmqm/boot.js
@@ -0,0 +1,16 @@
+(function() {
+  function dismissAlarm(alarm) {
+    if ("qmsched" in WIDGETS) {
+      require("qmsched").setMode(0);
+    }
+    else {
+      // Code from qmsched.js, so we can work without it
+      require("Storage").writeJSON("setting.json", Object.assign(
+        require("Storage").readJSON("setting.json", 1) || {},
+        {quiet:0}
+        ));
+      }
+    }
+
+    Bangle.on('alarmDismiss', dismissAlarm);
+  })();
