124c3578a9fa4fec5d4874d1a9a4ba651e5ec38a
Gordon Williams <gw@pur3.co.uk>
2023-02-20 15:45:25 +0000

    Better font for rendering japanese characters

--- bin/language_render.js
+++ bin/language_render.js
@@ -4,17 +4,22 @@
   with its built-in fonts, and pre-render them.
 */
 
-const FONT_SIZE = 18;
+//const FONT_SIZE = 18;
+//const FONT_NAME = 'Sans';
+const FONT_SIZE = 16; // 12pt
+const FONT_NAME = '"Unifont Regular"'; // or just 'Sans'
 
-var createCanvas;
+var createCanvas, registerFont;
 try {
   createCanvas = require("canvas").createCanvas;
+  registerFont = require("canvas").registerFont;
 } catch(e) {
   console.log("ERROR: needc canvas library");
   console.log("Try: npm install canvas");
   process.exit(1);
 }
-// we could use registerFont here to ensure a consitent experience?
+// Use font from https://unifoundry.com/unifont/ as it scales well at 16px high
+registerFont(__dirname+'/unifont-15.0.01.ttf', { family: 'Unifont Regular' })
 
 var imageconverter  = require(__dirname+"/../webtools/imageconverter.js");
 
@@ -23,13 +28,14 @@ const ctx = canvas.getContext('2d')
 
 function renderText(txt) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.font = FONT_SIZE+'px Sans';
+  ctx.font = FONT_SIZE+'px '+FONT_NAME;
   ctx.fillStyle = "white";
   ctx.fillText(txt, 0, FONT_SIZE);
   var str = imageconverter.canvastoString(canvas, { autoCrop:true, output:"raw", mode:"1bit", transparent:true } );
   // for testing: 
-  //console.log("g.drawImage(",imageconverter.canvastoString(canvas, { autoCrop:true, output:"string", mode:"1bit" } ),");");
-  //process.exit(1);
+//  console.log(txt);
+//  console.log("g.drawImage(",imageconverter.canvastoString(canvas, { autoCrop:true, output:"string", mode:"1bit" } ),");");
+//  process.exit(1);
   return "\0"+str;
 }
 
