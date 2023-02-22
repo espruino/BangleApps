5128c99343d24588ebdfedcf4f6df3f472d1bcd9
thyttan <6uuxstm66@mozmail.com>
2023-02-22 18:43:53 +0100

    Revert multiple commits to the run app.
    Revert "move changes to run to new app runplus"
    This reverts commit 026b266686238152d6e21944a7a48860c0b92136.
    
    Revert "spelling comment"
    This reverts commit 9824460add766ca48c6e18c1033333dc721fafd4.
    
    Revert "remove export in karvonnen.js"
    This reverts commit c681bd8fadbfb163bb8ec65e52d79040de1e45a2.
    
    Revert "change to use eval for launching karvonnen ui"
    This reverts commit 92a3c48d633304fc2b56f99f3258594d7d9fab6d.
    
    Revert "tweaks to colors and placings"
    This reverts commit ff6337b2eeeeb04fca575df3481713155c54c632.
    
    Revert "tweaks to placing of things"
    This reverts commit 3ee5c239a7d624b0b196ce29f8c9e56f0c282576.
    
    Revert "fix HRM settings default"
    This reverts commit cd001d827644a80d5c9233cecee24ba37b0714d6.
    
    Revert "ChangeLog: alpha->beta"
    This reverts commit 6c1d709a8c0a4b0f9f42ae0bd1f183a85c29e792.
    
    Revert "remove unnecessary semicolon"
    This reverts commit 918ab8158c9380ce595231683baedc6b849385f3.
    
    Revert "switch to run UI whenever pressing physical button"
    This reverts commit ebc2916f6f97f8520e9a3481f6f29107d0e99697.
    
    Revert "after many changes and some testing many things seem to function very well"
    This reverts commit 5a05b47267f7742aca2d36775f1782330f03adc0.
    
    Revert "add defaults to hrm settings"
    This reverts commit 71892cf91730477aa4ce9190034b4fbfc8a23a78.
    
    Revert "update UI pretty lazily, fix color of bpm"
    This reverts commit 2e6489240d828feccd53b72acc2547d71010e544.
    
    Revert "karvonnen UI now updates with bpm from hrm"
    This reverts commit d4db5bed6f29318ee1d2ec65f80f1f7a26a05f99.
    
    Revert "maybe now hrm setting work..."
    This reverts commit 646ffa21fabb4e4b5c0418571b3f4c1943a2b99a.
    
    Revert "settings fix again"
    This reverts commit 5a8046b6b6d6da0ab1ce857228458c5d4574333b.
    
    Revert "try fix settings min"
    This reverts commit 671a407513251a86877552366db8aebabb859ac4.
    
    Revert "fix hrm min setting"
    This reverts commit ea005672f6de1df640557b281714c538a2126a4a.
    
    Revert "fix hrm min setting"
    This reverts commit 7572ddd433c30d7a5523e234146b01eae86c72b8.
    
    Revert "fix hrm max setting"
    This reverts commit 2a83b7bf3226eae88990f5ee069b5709f65a4e40.
    
    Revert "fix hrm max setting"
    This reverts commit 4494b4863f688ade6b5e7958a68c5ba709628adf.
    
    Revert "add hrm min/max settings"
    This reverts commit 52c59702b08316f6572393616a5f6bfe1771c368.
    
    Revert "add/rm semicolons"
    This reverts commit 1d78542167647ed2994262ef87bf4d72f4a08202.
    
    Revert "fix accidental removal of interval"
    This reverts commit f2af13281cab2bff3f4af4d5690dbcc492ac68eb.
    
    Revert "move karvonnen out of app.js and require it into run.app.js when loading app to the watch"
    This reverts commit 1df4962018fc8a31d7ffa5f2ae316cc360de66ac.
    
    Revert "Revert "remove run.app.karvonnen.js, moved into app.js""
    This reverts commit 2ed76d600b27dcd9d8e791bfc26e0366806f6f22.
    
    Revert "add a comment, remove a print-statement"
    This reverts commit 9035cc3a2723235e625f880e50d79e18655239aa.
    
    Revert "remove commented out lines"
    This reverts commit 32267561a15b8e2c1781d01ab824c9eb5c6ae9ba.
    
    Revert "remove run.app.karvonnen.js, moved into app.js"
    This reverts commit 705c2c53954d3921bacb4d5bd41b8f14d713d355.
    
    Revert "fix version metadata"
    This reverts commit ef7f4d3a963a5ca3b0ce1709992733ca6a69f066.
    
    Revert "ChangLog version"
    This reverts commit 502b992b29ca746a4785444b1acca8f2cbef3bcf.
    
    Revert "version metadata"
    This reverts commit 270c3d3f5b870dc3c5b2a2a3044dedfd305f2657.
    
    Revert "Move karvonnen code into the run app"
    This reverts commit e72a4d3082f7cace8425e503ee20129f8ce41400.
    
    Revert "Update run.app.karvonnen.js"
    This reverts commit 5313d415d5d0f4a79e2728ed42bffc4a87e3a963.
    
    Revert "Add files via upload"
    This reverts commit 51d62c816fa8eaf0a00b7b06957fab1f47d0efb7.

--- apps/run/app.js
+++ apps/run/app.js
@@ -1,23 +1,16 @@
-// Use widget utils to show/hide widgets
-let wu = require("widget_utils");
+var ExStats = require("exstats");
+var B2 = process.env.HWVERSION===2;
+var Layout = require("Layout");
+var locale = require("locale");
+var fontHeading = "6x8:2";
+var fontValue = B2 ? "6x15:2" : "6x8:3";
+var headingCol = "#888";
+var fixCount = 0;
+var isMenuDisplayed = false;
 
-let runInterval;
-let karvonnenActive = false;
-// Run interface wrapped in a function
-let ExStats = require("exstats");
-let B2 = process.env.HWVERSION===2;
-let Layout = require("Layout");
-let locale = require("locale");
-let fontHeading = "6x8:2";
-let fontValue = B2 ? "6x15:2" : "6x8:3";
-let headingCol = "#888";
-let fixCount = 0;
-let isMenuDisplayed = false;
-
-g.reset().clear();
+g.clear();
 Bangle.loadWidgets();
 Bangle.drawWidgets();
-wu.show();
 
 // ---------------------------
 let settings = Object.assign({
@@ -42,20 +35,16 @@ let settings = Object.assign({
       value: 0,
       notifications: [],
     },
-    HRM: {
-      min: 65,
-      max: 170,
-    }
   },
 }, require("Storage").readJSON("run.json", 1) || {});
-let statIDs = [settings.B1,settings.B2,settings.B3,settings.B4,settings.B5,settings.B6].filter(s=>s!=="");
-let exs = ExStats.getStats(statIDs, settings);
+var statIDs = [settings.B1,settings.B2,settings.B3,settings.B4,settings.B5,settings.B6].filter(s=>s!=="");
+var exs = ExStats.getStats(statIDs, settings);
 // ---------------------------
 
 // Called to start/stop running
 function onStartStop() {
-  let running = !exs.state.active;
-  let prepPromises = [];
+  var running = !exs.state.active;
+  var prepPromises = [];
 
   // start/stop recording
   // Do this first in case recorder needs to prompt for
@@ -83,25 +72,25 @@ function onStartStop() {
 
   Promise.all(prepPromises)
     .then(() => {
-    if (running) {
-      exs.start();
-    } else {
-      exs.stop();
-    }
-    layout.button.label = running ? "STOP" : "START";
-    layout.status.label = running ? "RUN" : "STOP";
-    layout.status.bgCol = running ? "#0f0" : "#f00";
-    // if stopping running, don't clear state
-    // so we can at least refer to what we've done
-    layout.render();
-  });
+      if (running) {
+        exs.start();
+      } else {
+        exs.stop();
+      }
+      layout.button.label = running ? "STOP" : "START";
+      layout.status.label = running ? "RUN" : "STOP";
+      layout.status.bgCol = running ? "#0f0" : "#f00";
+      // if stopping running, don't clear state
+      // so we can at least refer to what we've done
+      layout.render();
+    });
 }
 
-let lc = [];
+var lc = [];
 // Load stats in pair by pair
-for (let i=0;i<statIDs.length;i+=2) {
-  let sa = exs.stats[statIDs[i+0]]
-  let sb = exs.stats[statIDs[i+1]];
+for (var i=0;i<statIDs.length;i+=2) {
+  var sa = exs.stats[statIDs[i+0]];
+  var sb = exs.stats[statIDs[i+1]];
   lc.push({ type:"h", filly:1, c:[
     sa?{type:"txt", font:fontHeading, label:sa.title.toUpperCase(), fillx:1, col:headingCol }:{},
     sb?{type:"txt", font:fontHeading, label:sb.title.toUpperCase(), fillx:1, col:headingCol }:{}
@@ -119,25 +108,25 @@ lc.push({ type:"h", filly:1, c:[
   {type:"txt", font:fontHeading, label:"STOP", id:"status", fillx:1 }
 ]});
 // Now calculate the layout
-let layout = new Layout( {
+var layout = new Layout( {
   type:"v", c: lc
-},{lazy:true, btns:[{ label:"START", cb: ()=>{if (karvonnenActive) {stopKarvonnenUI();run();} onStartStop();}, id:"button"}]});
+},{lazy:true, btns:[{ label:"START", cb: onStartStop, id:"button"}]});
 delete lc;
 layout.render();
 
 function configureNotification(stat) {
   stat.on('notify', (e)=>{
     settings.notify[e.id].notifications.reduce(function (promise, buzzPattern) {
-      return promise.then(function () {
-        return Bangle.buzz(buzzPattern[0], buzzPattern[1]);
-      });
+        return promise.then(function () {
+          return Bangle.buzz(buzzPattern[0], buzzPattern[1]);
+        });
     }, Promise.resolve());
   });
 }
 
 Object.keys(settings.notify).forEach((statType) => {
   if (settings.notify[statType].increment > 0 && exs.stats[statType]) {
-    configureNotification(exs.stats[statType]);
+      configureNotification(exs.stats[statType]);
   }
 });
 
@@ -149,46 +138,8 @@ Bangle.on("GPS", function(fix) {
     Bangle.buzz(); // first fix, does not need to respect quiet mode
   }
 });
-
-// run() function used to switch between traditional run UI and karvonnen UI
-function run() {
-  wu.show();
-  layout.lazy = false;
-  layout.render();
-  layout.lazy = true;
-  // We always call ourselves once a second to update
-  if (!runInterval){
-    runInterval = setInterval(function() {
-      layout.clock.label = locale.time(new Date(),1);
-      if (!isMenuDisplayed && !karvonnenActive) layout.render();
-    }, 1000);
-  }
-}
-run();
-
-///////////////////////////////////////////////
-//                Karvonnen
-///////////////////////////////////////////////
-
-function stopRunUI() {
-  // stop updating and drawing the traditional run app UI
-  clearInterval(runInterval);
-  runInterval = undefined;
-  karvonnenActive = true;
-}
-
-function stopKarvonnenUI() {
-  g.reset().clear();
-  clearInterval(karvonnenInterval);
-  karvonnenInterval = undefined;
-  karvonnenActive = false;
-}
-
-let karvonnenInterval;
-// Define the function to go back and forth between the different UI's
-function swipeHandler(LR,_) {
-  if (LR==-1 && karvonnenActive && !isMenuDisplayed) {stopKarvonnenUI(); run();}
-  if (LR==1 && !karvonnenActive && !isMenuDisplayed) {stopRunUI(); karvonnenInterval = eval(require("Storage").read("run_karvonnen"))(settings.HRM, exs.stats.bpm);}
-}
-// Listen for swipes with the swipeHandler
-Bangle.on("swipe", swipeHandler);
+// We always call ourselves once a second to update
+setInterval(function() {
+  layout.clock.label = locale.time(new Date(),1);
+  if (!isMenuDisplayed) layout.render();
+}, 1000);
