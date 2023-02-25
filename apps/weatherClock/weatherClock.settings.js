f635e37263566ed5f406f73a862f27ec6118dd05
lunctis-viribus <josdaboss@gmail.com>
2023-02-25 19:35:00 +0100

    weatherclock v0.06

---
+++ apps/weatherClock/weatherClock.settings.js
@@ -0,0 +1,50 @@
+(function(back) {
+  const SETTINGS_FILE = "weatherClock.json";
+
+  // initialize with default settings...
+  let s = {
+    'day': true,
+    'date': true,
+    'wind': true
+  }
+
+  // ...and overwrite them with any saved values
+  // This way saved values are preserved if a new version adds more settings
+  const storage = require('Storage')
+  let settings = storage.readJSON(SETTINGS_FILE, 1) || {}
+  const saved = settings || {}
+  for (const key in saved) {
+    s[key] = saved[key]
+  }
+
+  function save() {
+    settings = s
+    storage.write(SETTINGS_FILE, settings)
+  }
+
+  E.showMenu({
+    '': { 'title': 'Weather Clock' },
+    '< Back': back,
+	'Day Of Week': {
+      value: !!s.day,
+      onchange: v => {
+        s.day = v;
+        save();
+      },
+    },
+    'Date': {
+      value: !!s.date,
+      onchange: v => {
+        s.date = v;
+        save();
+      },
+    },
+    'Wind Speed': {
+      value: !!s.wind,
+      onchange: v => {
+        s.wind = v;
+        save();
+      },
+    }
+  })
+})
