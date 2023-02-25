6a21608322d2a605e273b8b74cf7227129d152ee
Rob Pilling <robpilling@gmail.com>
2023-02-25 21:58:12 +0000

    widlockunlock: convert to typescript

--- apps/widlockunlock/widget.js
+++ apps/widlockunlock/widget.js
@@ -1,45 +1,34 @@
+"use strict";
 WIDGETS["lockunlock"] = {
-  area: (() => {
-    const settings = require("Storage")
-        .readJSON("lockunlock.settings.json", true) || {};
-    return settings.location || "tl";
-  })(),
-  sortorder: 10,
-  width: 14,
-  draw: w => {
-    g.reset()
-      .drawImage(
-        atob(Bangle.isLocked()
-        ? "DBGBAAAA8DnDDCBCBP////////n/n/n//////z/A"
-        : "DBGBAAAA8BnDDCBABP///8A8A8Y8Y8Y8A8A//z/A"),
-        w.x + 1,
-        w.y + 3
-      );
-  },
+    area: (function () {
+        var _a;
+        var settings = require("Storage")
+            .readJSON("lockunlock.settings.json", true) || {};
+        return (_a = settings.location) !== null && _a !== void 0 ? _a : "tl";
+    })(),
+    sortorder: 10,
+    width: 14,
+    draw: function (w) {
+        g.reset()
+            .drawImage(atob(Bangle.isLocked()
+            ? "DBGBAAAA8DnDDCBCBP////////n/n/n//////z/A"
+            : "DBGBAAAA8BnDDCBABP///8A8A8Y8Y8Y8A8A//z/A"), w.x + 1, w.y + 3);
+    },
 };
-Bangle.on("lock", () => Bangle.drawWidgets());
-
-Bangle.on('touch', (_btn, xy) => {
-  const oversize = 5;
-
-  const w = WIDGETS.lockunlock;
-
-  const x = xy.x;
-  const y = xy.y;
-
-  if(w.x - oversize <= x && x < w.x + 14 + oversize
-  && w.y - oversize <= y && y < w.y + 24 + oversize)
-  {
-    Bangle.setLocked(true);
-
-    const backlightTimeout = Bangle.getOptions().backlightTimeout; // ms
-
-    // seems to be a race/if we don't give the firmware enough time,
-    // it won't timeout the backlight and we'll restore it in our setTimeout below
-    Bangle.setOptions({ backlightTimeout: 100 });
-
-    setTimeout(() => {
-      Bangle.setOptions({ backlightTimeout });
-    }, 300);
-  }
+Bangle.on("lock", function () { return Bangle.drawWidgets(); });
+Bangle.on("touch", function (_btn, e) {
+    var oversize = 5;
+    if (!e)
+        return;
+    var x = e.x, y = e.y;
+    var w = WIDGETS["lockunlock"];
+    if (w.x - oversize <= x && x < w.x + 14 + oversize
+        && w.y - oversize <= y && y < w.y + 24 + oversize) {
+        Bangle.setLocked(true);
+        var backlightTimeout_1 = Bangle.getOptions().backlightTimeout;
+        Bangle.setOptions({ backlightTimeout: 100 });
+        setTimeout(function () {
+            Bangle.setOptions({ backlightTimeout: backlightTimeout_1 });
+        }, 300);
+    }
 });
