463c3fec238f531ac19accad0766f0264ee40332
Le~Kat <Le3Kat@protonmail.com>
2023-02-25 08:05:35 -0500

    IDK how to use promises properly

--- apps/multidice/app.js
+++ apps/multidice/app.js
@@ -132,7 +132,7 @@ function random (max) {
 function vibrate() {
 	
 	Bangle.on ('accel', voidFn);
-	Bangle.buzz (200, 1).then(
+	Bangle.buzz (200, 1).then ((value) =>
 		
 		Bangle.on ('accel', accelHandler)
 	);
