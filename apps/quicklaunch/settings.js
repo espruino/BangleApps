6d80f784b40c7695884b32850edb4fe13fa45493
thyttan <6uuxstm66@mozmail.com>
2023-03-12 12:22:02 +0100

    handle app name "(none)" vs empty string "" in settings.js instead of in app.js"

--- apps/quicklaunch/settings.js
+++ apps/quicklaunch/settings.js
@@ -4,7 +4,7 @@ var settings = Object.assign(storage.readJSON("quicklaunch.json", true) || {});
 
 // Add default settings if they haven't been configured before. 
 for (let c of ["lapp","rapp","uapp","dapp","tapp"]){ // l=left, r=right, u=up, d=down, t=tap.
-  if (!settings[c]) settings[c] = {"name":"(none)"};
+  if (!settings[c]) settings[c] = {"name":""};
 }
 
 // Convert settings object from before v0.12 to v0.12.
@@ -13,6 +13,7 @@ for (let c of ["leftapp","rightapp","upapp","downapp","tapapp"]){
     let cNew = c.substring(0,1)+"app";
     settings[cNew] = settings[c];
     delete settings[c];
+    if (settings[cNew].name=="(none)") settings[cNew].name = "";
 
     if (settings[cNew].name=="Quick Launch Extension"){
       settings[cNew].name = "Extension";
@@ -20,6 +21,7 @@ for (let c of ["leftapp","rightapp","upapp","downapp","tapapp"]){
         if (settings[d]) {
           let dNew = cNew.substring(0,1)+d.substring(3,4)+"app";
           settings[dNew] = settings[d];
+          if (settings[dNew].name=="(none)") settings[dNew].name = "";
         }
       }
     }
@@ -71,7 +73,7 @@ function save(key, value) {
   // If changing to extension app (from something else) add downstream settings entries.
   if (value.name == extension.name && settings[key].name != extension.name) {
       for (let c of [path+"lapp", path+"rapp", path+"uapp", path+"dapp", path+"tapp"]) {
-        settings[c] = {"name":"(none)"};
+        settings[c] = {"name":""};
         storage.write("quicklaunch.json",settings);
       }
     }
@@ -88,7 +90,7 @@ function showMainMenu() {
     "< Back" : ()=>{load();}
   };
 
-  //List all selected apps
+  // List all selected apps.
   for (let key of Object.keys(settings)) {
     if (key == "trace") continue;
     let keyCurrent = key;
@@ -98,7 +100,9 @@ function showMainMenu() {
     if (entry=="U") entry = "Up";
     if (entry=="D") entry = "Down";
     if (entry=="T") entry = "Tap";
-    mainmenu[entry+ ": "+settings[key].name] = function() {showSubMenu(keyCurrent);};
+    // If no app is selected the name is an empty string, but we want to display "(none)".
+    let appName = settings[key].name==""?"(none)":settings[key].name;
+    mainmenu[entry+ ": "+appName] = function() {showSubMenu(keyCurrent);};
   }
 
   return E.showMenu(mainmenu);
@@ -111,7 +115,7 @@ function showSubMenu(key) {
   };
   
   submenu["(none)"] = function() {
-    save(key, {"name":"(none)"});
+    save(key, {"name":""});
     showMainMenu();
   };
   apps.forEach((a)=>{
