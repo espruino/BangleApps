647118c3db860adb9c319eb515f37b32ed7f7990
KungPhoo <gf@glbasic.com>
2023-03-23 09:36:58 +0100

    first try of xxlmessage

--- apps/xxlmessage/app.js
+++ apps/xxlmessage/app.js
@@ -1,95 +1,20 @@
-﻿
-var drawTimeout; // = undefined;
-var xpos = 2*g.getWidth();
-var loopCount=0;
-
-// schedule a draw for the next 30 FPS
-function queueDraw() {
-    if (drawTimeout) {clearTimeout(drawTimeout);}
-    drawTimeout = setTimeout(function() {
-        drawTimeout = undefined;
-        draw();
-    }, 32 - (Date.now() % 32));
-}
-
-function drawBackground() {
-    g.setBgColor(0,0,0);
-    g.setColor(1,1,1);
+﻿function stop() {
+    g.setBgColor(0, 1, 1);
     g.clear();
-}
-
-function stop(){
-    if (drawTimeout) {clearTimeout(drawTimeout);}
+    g.reset();
     load();
 }
 
-function draw(){
-    Bangle.setLocked(false); // keep the touch input active
-    drawBackground();
+g.setBgColor(1, 0, 0);
+g.setColor(1, 1, 1);
+g.clear();
 
-    // g.setFontLECO1976Regular42();
-    g.setFont('6x8:5x10');
-    g.setFontAlign(-1, -1);
+g.setFont('6x8:5x10');
+g.setFontAlign(-1, -1);
+g.setColor('#ffffff');
+g.drawString('DUMMY', 0, 24);
 
-
-    var gw = g.getWidth();
-    var text = "This is a test with a longer message to try...";
-    var wtot = g.stringMetrics(text).width;
-
-    g.setBgColor('#000000');
-    // shadow - just wastes battery
-    // g.setColor('#ff0000');
-    // g.drawString(text, 3+xpos, 3);
-    // g.drawString(text, 3+xpos-gw-32, 3+g.getHeight()/2);
-
-    g.setColor('#ffffff');
-    g.drawString(text, xpos, 0);
-    g.drawString(text, xpos-gw-32, g.getHeight()/2);
-
-    g.reset();
-    xpos -= 3;
-    if(xpos < -wtot-gw*2 ) {
-        ++loopCount;
-        if(loopCount > 2){
-            stop();
-        }
-        xpos=3*gw;
-    }
-
-
-    // widget redraw
-    Bangle.drawWidgets();
-    queueDraw();
-}
 
 Bangle.loadWidgets();
-// draw();
-
-
-Bangle.on("message", (type, msg) => {
-    if (!msg) return;
-    if(type === 'text' && msg.t !== 'remove'){
-        msg.handled = true; // don't do anything else with the message
-        draw();
-    }
-});
-
-
-// 
-// Bangle.on('lcdPower',on=>{
-//   if (on) {
-//     draw(); // draw immediately, queue redraw
-//   } else { // stop draw timer
-//     if (drawTimeout) clearTimeout(drawTimeout);
-//     drawTimeout = undefined;
-//   }
-// });
-
-Bangle.on('touch', function(b,xy){
-    stop();
-});
-
-
-Bangle.setUI("clock");
-
 Bangle.drawWidgets();
+setTimeout(function () { stop() }, 2000);
--- apps/xxlmessage/lib.js
+++ apps/xxlmessage/lib.js
@@ -1,4 +1,110 @@
-exports.listener = function(type, event) {
+
+
+var xxl = {
+// private:
+    msg: [],
+    drawTimeout: undefined, // = undefined;
+    xpos : 0,
+    loopCount : 0,
+//public:
+    show: function(theMessage){
+        xxl.msg = theMessage;
+        xxl.xpos = 2 * g.getWidth();
+
+        Bangle.loadWidgets();
+
+        Bangle.on('touch', function (b, xy) {
+            xxl.stop();
+        });
+        Bangle.buzz(500,1);
+        xxl.draw();
+    },
+//private:
+// schedule a draw for the next 30 FPS
+    queueDraw: function() {
+        if (xxl.drawTimeout) { return; } // clearTimeout(xxl.drawTimeout); }
+            xxl.drawTimeout = setTimeout(function () {
+            xxl.drawTimeout = undefined;
+            xxl.draw();
+        }, 17 - (Date.now() % 17));
+    },
+
     
+    stop:function() {
+        if (xxl.drawTimeout) { clearTimeout(xxl.drawTimeout); }
+        xxl.drawTimeout = undefined;
+        g.reset();
+        g.setBgColor('#ffff00');
+        g.setColor('#ffffff');
+        g.setFont('6x8:5x9');
+        g.setFontAlign(0, 0);
+        g.drawString('load...', 0, g.getHeight()/2);
+        g.clear();
+                   
+        Bangle.setLCDPower(0); // light off
+        Bangle.setLocked(true); // disable touch
+
+        setTimeout(load, 1000);
+    },
+
+    draw: function() {
+        Bangle.setLCDPower(1); // light on
+        Bangle.setLocked(false); // keep the touch input active
+        g.setBgColor(0, 0, 0);
+        g.setColor(1, 1, 1);
+        g.clear();
+
+        // g.setFontLECO1976Regular42();
+        g.setFont('6x8:5x9');
+        g.setFontAlign(-1, -1);
+
+
+        var gw = g.getWidth();
+        var text = xxl.msg.title + ': ' + xxl.msg.body;
+        var wtot = g.stringMetrics(text).width;
+
+        g.setBgColor('#000000');
+
+        g.setColor('#ffffff');
+        wh = 24; // widgets height
+        h = g.getHeight() - wh;
+        g.drawString(text, xxl.xpos, wh);// widgets height
+        g.drawString(text, xxl.xpos - gw - 32, h / 2 + wh);
+
+        g.reset();
+        xxl.xpos -= 6;
+        if (xxl.xpos < -wtot - gw * 2) {
+            ++xxl.loopCount;
+            if (xxl.loopCount > 2) {
+                xxl.stop();
+            }
+            xxl.xpos = 3 * gw;
+        }
+
+        // widget redraw
+        Bangle.drawWidgets();
+        xxl.queueDraw();
+    }
+};
+
+
+// for IDE
+// var exports={};
+exports.listener = function (type, msg) {
+    // msg = {t:"add",id:int, src,title,subject,body,sender,tel, important:bool, new:bool}
+    if (!msg) return;
+    if (type === 'text' && msg.t !== 'remove') {
+        msg.handled = true; // don't do anything else with the message
+        xxl.show(msg);
+    }
     
-}
+};
+
+// debug
+// var mymsg = {t:'add', new:true, handled:false, title:'SMS', body:'Hello World'};
+// exports.listener('text', mymsg);
+
+
+
+
+
