ab75d5b3530d9622ab275f52b62c9033182f6b61
Rob Pilling <robpilling@gmail.com>
2023-04-13 18:05:59 +0100

    Regenerate JS

--- apps/drained/boot.js
+++ apps/drained/boot.js
@@ -1,7 +1,13 @@
 "use strict";
-var _a = require("Storage")
-    .readJSON("".concat(app, ".setting.json"), true) || {}, _b = _a.battery, battery = _b === void 0 ? 5 : _b, _c = _a.interval, interval = _c === void 0 ? 10 : _c;
-var drainedInterval = setInterval(function () {
+var _a = require("Storage").readJSON("drained.setting.json", true) || {}, _b = _a.battery, battery = _b === void 0 ? 5 : _b, _c = _a.interval, interval = _c === void 0 ? 10 : _c, _d = _a.disableBoot, disableBoot = _d === void 0 ? false : _d;
+if (disableBoot) {
+    require("Storage").erase(".boot0");
+    Bangle.on("charging", function (charging) {
+        if (charging)
+            eval(require('Storage').read('bootupdate.js'));
+    });
+}
+drainedInterval = setInterval(function () {
     if (Bangle.isCharging())
         return;
     if (E.getBattery() > battery)
--- apps/drained/settings.js
+++ apps/drained/settings.js
@@ -1,17 +1,26 @@
 "use strict";
 (function (back) {
-    var _a, _b;
+    var _a, _b, _c;
     var SETTINGS_FILE = "drained.setting.json";
     var storage = require("Storage");
     var settings = storage.readJSON(SETTINGS_FILE, true) || {};
     (_a = settings.battery) !== null && _a !== void 0 ? _a : (settings.battery = 5);
     (_b = settings.interval) !== null && _b !== void 0 ? _b : (settings.interval = 10);
+    (_c = settings.disableBoot) !== null && _c !== void 0 ? _c : (settings.disableBoot = false);
     var save = function () {
         storage.writeJSON(SETTINGS_FILE, settings);
     };
     E.showMenu({
         "": { "title": "Drained" },
         "< Back": back,
+        "Keep startup code": {
+            value: settings.disableBoot,
+            format: function () { return settings.disableBoot ? "No" : "Yes"; },
+            onchange: function () {
+                settings.disableBoot = !settings.disableBoot;
+                save();
+            },
+        },
         "Trigger at batt%": {
             value: settings.battery,
             min: 0,
