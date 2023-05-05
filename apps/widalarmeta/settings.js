0746730d9830b1e382bfb7a119ce011b9ec6b513
Erik Andresen <erik@vontaene.de>
2023-05-05 05:02:43 +0200

    widalarmeta: settings fix

--- apps/widalarmeta/settings.js
+++ apps/widalarmeta/settings.js
@@ -53,7 +53,7 @@
     /*LANG*/'Font': {
       value: settings.font,
       min: 0, max: 2,
-      format: v => [/*LANG*/"Segment", /*LANG*/"Teletext", /*LANG*/"6x8"][v || 1],
+      format: v => [/*LANG*/"Segment", /*LANG*/"Teletext", /*LANG*/"6x8"][v === undefined ? 1 : v],
       onchange: v => {
         settings.font = v;
         writeSettings();
