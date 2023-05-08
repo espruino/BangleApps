69bfcfeb992de8262b4e3b1d63c6a6ca89402c4a
Rob Pilling <robpilling@gmail.com>
2023-05-08 15:02:54 +0100

    widhid: regenerate JS

--- apps/widhid/wid.js
+++ apps/widhid/wid.js
@@ -1,72 +1,103 @@
 (function () {
-    var settings = require('Storage').readJSON('setting.json', true) || { HID: false };
+    var settings = require("Storage").readJSON("setting.json", true) || { HID: false };
     if (settings.HID !== "kbmedia") {
         console.log("widhid: can't enable, HID setting isn't \"kbmedia\"");
         return;
     }
-    var start = { x: 0, y: 0 }, end = { x: 0, y: 0 };
+    var anchor = { x: 0, y: 0 };
+    var start = { x: 0, y: 0 };
     var dragging = false;
     var activeTimeout;
+    var waitForRelease = true;
     Bangle.on("swipe", function (_lr, ud) {
-        if (ud > 0) {
+        if (!activeTimeout && ud > 0) {
             listen();
-            Bangle.buzz();
+            Bangle.buzz(20);
         }
     });
-    Bangle.on('drag', function (e) {
-        if (!activeTimeout)
-            return;
-        if (!dragging) {
-            dragging = true;
-            start.x = e.x;
-            start.y = e.y;
-            return;
-        }
-        var released = e.b === 0;
-        if (released) {
-            var dx = end.x - start.x;
-            var dy = end.y - start.y;
-            if (Math.abs(dy) < 10) {
-                if (dx > 40)
-                    next();
-                else if (dx < 40)
-                    prev();
+    var onDrag = (function (e) {
+        if (e.b === 0) {
+            var wasDragging = dragging;
+            dragging = false;
+            if (waitForRelease) {
+                waitForRelease = false;
+                return;
             }
-            else if (Math.abs(dx) < 10) {
-                if (dy > 40)
-                    down();
-                else if (dy < 40)
-                    up();
-            }
-            else if (dx === 0 && dy === 0) {
+            if (!wasDragging
+                || (Math.abs(e.x - anchor.x) < 2 && Math.abs(e.y - anchor.y) < 2)) {
                 toggle();
+                onEvent();
+                return;
             }
-            Bangle.buzz();
-            listen();
+        }
+        if (waitForRelease)
+            return;
+        if (e.b && !dragging) {
+            dragging = true;
+            setStart(e);
+            Object.assign(anchor, start);
             return;
         }
-        end.x = e.x;
-        end.y = e.y;
+        var dx = e.x - start.x;
+        var dy = e.y - start.y;
+        if (Math.abs(dy) > 25 && Math.abs(dx) > 25) {
+            setStart(e);
+            return;
+        }
+        if (dx > 40) {
+            next();
+            onEvent();
+            waitForRelease = true;
+        }
+        else if (dx < -40) {
+            prev();
+            onEvent();
+            waitForRelease = true;
+        }
+        else if (dy > 30) {
+            down();
+            onEvent();
+            setStart(e);
+        }
+        else if (dy < -30) {
+            up();
+            onEvent();
+            setStart(e);
+        }
     });
+    var setStart = function (_a) {
+        var x = _a.x, y = _a.y;
+        start.x = x;
+        start.y = y;
+    };
+    var onEvent = function () {
+        Bangle.buzz(20);
+        listen();
+    };
     var listen = function () {
-        suspendOthers();
         var wasActive = !!activeTimeout;
-        clearTimeout(activeTimeout);
+        if (!wasActive) {
+            suspendOthers();
+            waitForRelease = true;
+            Bangle.on("drag", onDrag);
+            redraw();
+        }
+        if (activeTimeout)
+            clearTimeout(activeTimeout);
         activeTimeout = setTimeout(function () {
             activeTimeout = undefined;
+            Bangle.removeListener("drag", onDrag);
             resumeOthers();
             redraw();
-        }, 5000);
-        if (!wasActive)
-            redraw();
+        }, 3000);
     };
     WIDGETS["hid"] = {
         area: "tr",
         sortorder: -20,
         draw: function () {
             g.drawImage(activeTimeout
-                ? require("heatshrink").decompress(atob("mEwxH+AH4A/AH4A/AG8gkAvvAAYvvGVIvIGcwvMGMQv/F/4vTGpQvmNJAvqBggvtAEQv/F/4v/F/4nbFIYvlFooAHF1wvgFxwvfFx4v/Fz4v/F/4v/F/4wfFzwvwGBwugGBouiGBYukGJAtnAH4A/AH4A/ACIA=="))
-                : require("heatshrink").decompress(atob("mEwxH+AH4A/AH4A/AG9lsovvAAYvvGVIvIGcwvMGMQv/F/4vTGpQvmNJAvqBggvtAEQv/F/4v/F/4nbFIYvlFooAHF1wvgFxwvfFx4v/Fz4v/F/4v/F/4wfFzwvwGBwugGBouiGBYukGJAtnAH4A/AH4A/ACIA==")), this.x + 2, this.y + 2);
+                ? require("heatshrink").decompress(atob("jEYxH+AEfH44XXAAYXXDKIXZDYp3pC/6KHUMwWHC/4XvUy4YGdqoA/AFoA=="))
+                : require("heatshrink").decompress(atob("jEYxH+AEcdjoXXAAYXXDKIXZDYp3pC/6KHUMwWHC/4XvUy4YGdqoA/AFoA==")), this.x + 2, this.y + 2);
         },
         width: 24,
     };
