9fa59106b3649b70859c9e759571ff79c3650a36
ikades <49671784+ikades@users.noreply.github.com>
2023-05-16 17:42:56 +0200

    Update noanalo_ikades.app.js

--- apps/noanalo_ikades/noanalo_ikades.app.js
+++ apps/noanalo_ikades/noanalo_ikades.app.js
@@ -299,7 +299,7 @@ function drawData() {
     g.setColor(g.theme.fg);
     g.setFontAlign(0,0,0);
     // drawBatteryHand(parseInt(state.bat*360/100));
-    drawBatteryHand(parseInt(state.currentDate.getHours()*360/12 + state.currentDate.getMinutes()/720));
+    drawBatteryHand(parseInt(state.currentDate.getHours()*360/12 + state.currentDate.getMinutes()*360/720));
 
     // Draw data hand - depending on state
     g.setColor(state.color);
