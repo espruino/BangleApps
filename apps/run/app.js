21517916c123a374e21deb6f659a8052432e448e
André Büsgen <andre.buesgen@posteo.de>
2023-06-06 21:03:34 +0200

    feat: :sparkles: initial ble hrm support for run app

--- apps/run/app.js
+++ apps/run/app.js
@@ -1,5 +1,5 @@
 var ExStats = require("exstats");
-var B2 = process.env.HWVERSION===2;
+var B2 = process.env.HWVERSION === 2;
 var Layout = require("Layout");
 var locale = require("locale");
 var fontHeading = "6x8:2";
@@ -7,12 +7,16 @@ var fontValue = B2 ? "6x15:2" : "6x8:3";
 var headingCol = "#888";
 var fixCount = 0;
 var isMenuDisplayed = false;
+let isHRMAdvertisingSetUp = false;
+
 
 g.clear();
 Bangle.loadWidgets();
 Bangle.drawWidgets();
 
-// ---------------------------
+/*********************************************************************
+ * READ SETTINGS AND INITIALIZE EXSTATS WITH CONFIGURED MEASUREMENTS *
+ *********************************************************************/
 let settings = Object.assign({
   record: true,
   B1: "dist",
@@ -36,11 +40,74 @@ let settings = Object.assign({
       notifications: [],
     },
   },
+  exposeHRM: false
 }, require("Storage").readJSON("run.json", 1) || {});
-var statIDs = [settings.B1,settings.B2,settings.B3,settings.B4,settings.B5,settings.B6].filter(s=>s!=="");
+var statIDs = [settings.B1, settings.B2, settings.B3, settings.B4, settings.B5, settings.B6].filter(s => s !== "");
 var exs = ExStats.getStats(statIDs, settings);
-// ---------------------------
 
+/*********************************************************
+ * FUNCTIONS FOR SHARING AND SETTING UP HRM DATA VIA BLE *
+ *********************************************************/
+function setupHRMAdvertising() {
+  /*
+   * This function preparse BLE heart rate Advertisement.
+   */
+  NRF.setAdvertising(
+    {
+      0x180D: undefined,
+      0x2A37: [0x06, 10]
+    },
+    {
+      whenConnected: true,  // Allow advertisement while connected
+      discoverable: true,   // Make discoverable
+      connectable: true,    // One must connect in order to receive measurements
+      scannable: true
+    }
+  );
+  NRF.setServices({
+    0x180D: { // heart_rate
+      0x2A37: { // heart_rate_measurement
+        notify: true,
+        value: [0x06, 0],
+      }
+    }
+  });
+
+}
+
+function updateBLEHeartRate(hrm) {
+  /*
+   * Send updated heart rate measurement via BLE
+   */
+  if (hrm === undefined) return;
+  try {
+    NRF.updateServices({
+      '180d': {
+        '2a37': {
+          value: [
+            0x06, //
+            hrm
+          ],
+          notify: true
+        }
+      }
+    });
+  } catch (error) {
+    // After setupHRMAdvertising() BLE needs to restart.
+    // We force a disconnect if the Bangle was connected while setting HRM
+    NRF.disconnect();
+  }
+}
+
+if (settings.exposeHRM && !isHRMAdvertisingSetUp) {
+  setupHRMAdvertising();
+  Bangle.on("HRM", function (hrm) { updateBLEHeartRate(hrm.bpm); });
+  isHRMAdvertisingSetUp = true;
+}
+
+/**********************************
+ * SETUP LAYOUT AND HANDLE INPUTS *
+ **********************************/
 function setStatus(running) {
   layout.button.label = running ? "STOP" : "START";
   layout.status.label = running ? "RUN" : "STOP";
@@ -58,9 +125,9 @@ function onStartStop() {
     promise = promise.
       then(() => {
         isMenuDisplayed = true;
-        return E.showPrompt("Resume run?",{title:"Run"});
+        return E.showPrompt("Resume run?", { title: "Run" });
       }).then(r => {
-        isMenuDisplayed=false;shouldResume=r;
+        isMenuDisplayed = false; shouldResume = r;
       });
   }
 
@@ -71,7 +138,7 @@ function onStartStop() {
     if (running) {
       isMenuDisplayed = true;
       promise = promise.
-        then(() => WIDGETS["recorder"].setRecording(true, { force : shouldResume?"append":undefined })).
+        then(() => WIDGETS["recorder"].setRecording(true, { force: shouldResume ? "append" : undefined })).
         then(() => {
           isMenuDisplayed = false;
           layout.setUI(); // grab our input handling again
@@ -102,51 +169,57 @@ function onStartStop() {
 
 var lc = [];
 // Load stats in pair by pair
-for (var i=0;i<statIDs.length;i+=2) {
-  var sa = exs.stats[statIDs[i+0]];
-  var sb = exs.stats[statIDs[i+1]];
-  lc.push({ type:"h", filly:1, c:[
-    sa?{type:"txt", font:fontHeading, label:sa.title.toUpperCase(), fillx:1, col:headingCol }:{},
-    sb?{type:"txt", font:fontHeading, label:sb.title.toUpperCase(), fillx:1, col:headingCol }:{}
-  ]}, { type:"h", filly:1, c:[
-    sa?{type:"txt", font:fontValue, label:sa.getString(), id:sa.id, fillx:1 }:{},
-    sb?{type:"txt", font:fontValue, label:sb.getString(), id:sb.id, fillx:1 }:{}
-  ]});
-  if (sa) sa.on('changed', e=>layout[e.id].label = e.getString());
-  if (sb) sb.on('changed', e=>layout[e.id].label = e.getString());
+for (var i = 0; i < statIDs.length; i += 2) {
+  var sa = exs.stats[statIDs[i + 0]];
+  var sb = exs.stats[statIDs[i + 1]];
+  lc.push({
+    type: "h", filly: 1, c: [
+      sa ? { type: "txt", font: fontHeading, label: sa.title.toUpperCase(), fillx: 1, col: headingCol } : {},
+      sb ? { type: "txt", font: fontHeading, label: sb.title.toUpperCase(), fillx: 1, col: headingCol } : {}
+    ]
+  }, {
+    type: "h", filly: 1, c: [
+      sa ? { type: "txt", font: fontValue, label: sa.getString(), id: sa.id, fillx: 1 } : {},
+      sb ? { type: "txt", font: fontValue, label: sb.getString(), id: sb.id, fillx: 1 } : {}
+    ]
+  });
+  if (sa) sa.on('changed', e => layout[e.id].label = e.getString());
+  if (sb) sb.on('changed', e => layout[e.id].label = e.getString());
 }
 // At the bottom put time/GPS state/etc
-lc.push({ type:"h", filly:1, c:[
-  {type:"txt", font:fontHeading, label:"GPS", id:"gps", fillx:1, bgCol:"#f00" },
-  {type:"txt", font:fontHeading, label:"00:00", id:"clock", fillx:1, bgCol:g.theme.fg, col:g.theme.bg },
-  {type:"txt", font:fontHeading, label:"---", id:"status", fillx:1 }
-]});
+lc.push({
+  type: "h", filly: 1, c: [
+    { type: "txt", font: fontHeading, label: "GPS", id: "gps", fillx: 1, bgCol: "#f00" },
+    { type: "txt", font: fontHeading, label: "00:00", id: "clock", fillx: 1, bgCol: g.theme.fg, col: g.theme.bg },
+    { type: "txt", font: fontHeading, label: "---", id: "status", fillx: 1 }
+  ]
+});
 // Now calculate the layout
-var layout = new Layout( {
-  type:"v", c: lc
-},{lazy:true, btns:[{ label:"---", cb: onStartStop, id:"button"}]});
+var layout = new Layout({
+  type: "v", c: lc
+}, { lazy: true, btns: [{ label: "---", cb: onStartStop, id: "button" }] });
 delete lc;
 setStatus(exs.state.active);
 layout.render();
 
 function configureNotification(stat) {
-  stat.on('notify', (e)=>{
+  stat.on('notify', (e) => {
     settings.notify[e.id].notifications.reduce(function (promise, buzzPattern) {
-        return promise.then(function () {
-          return Bangle.buzz(buzzPattern[0], buzzPattern[1]);
-        });
+      return promise.then(function () {
+        return Bangle.buzz(buzzPattern[0], buzzPattern[1]);
+      });
     }, Promise.resolve());
   });
 }
 
 Object.keys(settings.notify).forEach((statType) => {
   if (settings.notify[statType].increment > 0 && exs.stats[statType]) {
-      configureNotification(exs.stats[statType]);
+    configureNotification(exs.stats[statType]);
   }
 });
 
 // Handle GPS state change for icon
-Bangle.on("GPS", function(fix) {
+Bangle.on("GPS", function (fix) {
   layout.gps.bgCol = fix.fix ? "#0f0" : "#f00";
   if (!fix.fix) return; // only process actual fixes
   if (fixCount++ === 0) {
@@ -154,7 +227,7 @@ Bangle.on("GPS", function(fix) {
   }
 });
 // We always call ourselves once a second to update
-setInterval(function() {
-  layout.clock.label = locale.time(new Date(),1);
+setInterval(function () {
+  layout.clock.label = locale.time(new Date(), 1);
   if (!isMenuDisplayed) layout.render();
 }, 1000);
--- apps/run/settings.js
+++ apps/run/settings.js
@@ -1,9 +1,9 @@
-(function(back) {
+(function (back) {
   const SETTINGS_FILE = "run.json";
   var ExStats = require("exstats");
   var statsList = ExStats.getList();
-  statsList.unshift({name:"-",id:""}); // add blank menu item
-  var statsIDs = statsList.map(s=>s.id);
+  statsList.unshift({ name: "-", id: "" }); // add blank menu item
+  var statsIDs = statsList.map(s => s.id);
 
   // ...and overwrite them with any saved values
   // This way saved values are preserved if a new version adds more settings
@@ -31,6 +31,7 @@
         notifications: [],
       },
     },
+    exposeHRM: false
   }, storage.readJSON(SETTINGS_FILE, 1) || {});
   function saveSettings() {
     storage.write(SETTINGS_FILE, settings)
@@ -38,8 +39,8 @@
 
   function getBoxChooser(boxID) {
     return {
-      min: 0, max: statsIDs.length-1,
-      value: Math.max(statsIDs.indexOf(settings[boxID]),0),
+      min: 0, max: statsIDs.length - 1,
+      value: Math.max(statsIDs.indexOf(settings[boxID]), 0),
       format: v => statsList[v].name,
       onchange: v => {
         settings[boxID] = statsIDs[v];
@@ -50,9 +51,9 @@
 
   function sampleBuzz(buzzPatterns) {
     return buzzPatterns.reduce(function (promise, buzzPattern) {
-        return promise.then(function () {
-            return Bangle.buzz(buzzPattern[0], buzzPattern[1]);
-        });
+      return promise.then(function () {
+        return Bangle.buzz(buzzPattern[0], buzzPattern[1]);
+      });
     }, Promise.resolve());
   }
 
@@ -60,18 +61,18 @@
     '': { 'title': 'Run' },
     '< Back': back,
   };
-  if (global.WIDGETS&&WIDGETS["recorder"])
+  if (global.WIDGETS && WIDGETS["recorder"])
     menu[/*LANG*/"Record Run"] = {
-      value : !!settings.record,
-      onchange : v => {
+      value: !!settings.record,
+      onchange: v => {
         settings.record = v;
         saveSettings();
       }
     };
   var notificationsMenu = {
-    '< Back': function() { E.showMenu(menu) },
+    '< Back': function () { E.showMenu(menu) },
   }
-  menu[/*LANG*/"Notifications"] = function() { E.showMenu(notificationsMenu)};
+  menu[/*LANG*/"Notifications"] = function () { E.showMenu(notificationsMenu) };
   ExStats.appendMenuItems(menu, settings, saveSettings);
   ExStats.appendNotifyMenuItems(notificationsMenu, settings, saveSettings);
   var vibPatterns = [/*LANG*/"Off", ".", "-", "--", "-.-", "---"];
@@ -80,43 +81,43 @@
     [[100, 1]],
     [[300, 1]],
     [[300, 1], [300, 0], [300, 1]],
-    [[300, 1],[300, 0], [100, 1], [300, 0], [300, 1]],
-    [[300, 1],[300, 0],[300, 1],[300, 0],[300, 1]],
+    [[300, 1], [300, 0], [100, 1], [300, 0], [300, 1]],
+    [[300, 1], [300, 0], [300, 1], [300, 0], [300, 1]],
   ];
   notificationsMenu[/*LANG*/"Dist Pattern"] = {
-    value: Math.max(0,vibTimes.findIndex((p) => JSON.stringify(p) === JSON.stringify(settings.notify.dist.notifications))),
-      min: 0, max: vibTimes.length - 1,
-      format: v => vibPatterns[v]||/*LANG*/"Off",
-      onchange: v => {
-        settings.notify.dist.notifications = vibTimes[v];
-        sampleBuzz(vibTimes[v]);
-        saveSettings();
-      }
+    value: Math.max(0, vibTimes.findIndex((p) => JSON.stringify(p) === JSON.stringify(settings.notify.dist.notifications))),
+    min: 0, max: vibTimes.length - 1,
+    format: v => vibPatterns[v] ||/*LANG*/"Off",
+    onchange: v => {
+      settings.notify.dist.notifications = vibTimes[v];
+      sampleBuzz(vibTimes[v]);
+      saveSettings();
+    }
   }
   notificationsMenu[/*LANG*/"Step Pattern"] = {
-    value: Math.max(0,vibTimes.findIndex((p) => JSON.stringify(p) === JSON.stringify(settings.notify.step.notifications))),
-      min: 0, max: vibTimes.length - 1,
-      format: v => vibPatterns[v]||/*LANG*/"Off",
-      onchange: v => {
-        settings.notify.step.notifications = vibTimes[v];
-        sampleBuzz(vibTimes[v]);
-        saveSettings();
-      }
+    value: Math.max(0, vibTimes.findIndex((p) => JSON.stringify(p) === JSON.stringify(settings.notify.step.notifications))),
+    min: 0, max: vibTimes.length - 1,
+    format: v => vibPatterns[v] ||/*LANG*/"Off",
+    onchange: v => {
+      settings.notify.step.notifications = vibTimes[v];
+      sampleBuzz(vibTimes[v]);
+      saveSettings();
+    }
   }
   notificationsMenu[/*LANG*/"Time Pattern"] = {
-    value: Math.max(0,vibTimes.findIndex((p) => JSON.stringify(p) === JSON.stringify(settings.notify.time.notifications))),
-      min: 0, max: vibTimes.length - 1,
-      format: v => vibPatterns[v]||/*LANG*/"Off",
-      onchange: v => {
-        settings.notify.time.notifications = vibTimes[v];
-        sampleBuzz(vibTimes[v]);
-        saveSettings();
-      }
+    value: Math.max(0, vibTimes.findIndex((p) => JSON.stringify(p) === JSON.stringify(settings.notify.time.notifications))),
+    min: 0, max: vibTimes.length - 1,
+    format: v => vibPatterns[v] ||/*LANG*/"Off",
+    onchange: v => {
+      settings.notify.time.notifications = vibTimes[v];
+      sampleBuzz(vibTimes[v]);
+      saveSettings();
+    }
   }
   var boxMenu = {
-    '< Back': function() { E.showMenu(menu) },
+    '< Back': function () { E.showMenu(menu) },
   }
-  Object.assign(boxMenu,{
+  Object.assign(boxMenu, {
     'Box 1': getBoxChooser("B1"),
     'Box 2': getBoxChooser("B2"),
     'Box 3': getBoxChooser("B3"),
@@ -124,6 +125,15 @@
     'Box 5': getBoxChooser("B5"),
     'Box 6': getBoxChooser("B6"),
   });
-  menu[/*LANG*/"Boxes"] = function() { E.showMenu(boxMenu)};
+  menu[/*LANG*/"Boxes"] = function () { E.showMenu(boxMenu) };
+
+  menu[/*LANG*/"Expose HRM"] = {
+    value: false,
+    onchange: v => {
+      settings.exposeHRM = v;
+      saveSettings();
+    }
+  }
+
   E.showMenu(menu);
 })
