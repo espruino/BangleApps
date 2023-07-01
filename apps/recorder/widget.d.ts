dedaa93e66d31ccb6a584fde1ebc0f8bfb1c492a
Rob Pilling <robpilling@gmail.com>
2023-07-01 11:52:17 +0100

    recorder: add widget typescript definitions

---
+++ apps/recorder/widget.d.ts
@@ -0,0 +1,40 @@
+type RecorderWidget = Widget & {
+	getRecorders(): Recorders;
+
+	reload(): void,
+
+	isRecording(): boolean,
+
+	setRecording(
+		isOn: boolean,
+		options?: { force?: "append" | "new" | "overwrite" },
+	): Promise<boolean>;
+
+	plotTrack(
+		m: unknown /* osm module */,
+		options?: {
+			async: true,
+			callback?: ()=>void,
+		}
+	): { stop(): void };
+	plotTrack(
+		m: unknown /* osm module */,
+		options?: {
+			async?: false,
+			callback?: ()=>void,
+		}
+	): void;
+};
+
+type Recorders = {
+	[key: string]: Recorder;
+};
+
+type Recorder = () => {
+	name: string,
+	fields: string[],
+	getValues(): unknown[],
+	start(): void,
+	stop(): void,
+	draw(x: number, y: number): void,
+};
