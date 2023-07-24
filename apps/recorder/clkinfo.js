9fa01034439532a20bc934b136a20a45211db555
Rob Pilling <robpilling@gmail.com>
2023-07-24 20:50:48 +0100

    recorder: change record & pause icons

--- apps/recorder/clkinfo.js
+++ apps/recorder/clkinfo.js
@@ -1,13 +1,9 @@
 (function () {
 	const recimg = () =>
-		require("heatshrink").decompress(atob("jEYxH+AHHCAAgVQ4fDCwYFCCpotFDQgZJCxYYLCxgYCOJgALFygwHLpphJIyJIFC9O72oXU3m02h3UC4O7U6m7FwhIQIwwwPCxJhMCwSNEDBm83hbBCxQZEDQQUCIhIZIAAO1UAwAzA="));
+		atob("GBiBAAAAABwAAD4MAH8eAH8OAH8AAD4QABx8AAD8AAH+AAE+AAM/AAN7wAN4wAB4AAB8AAD8AADOAAHGAAOHAAMDAAIBAAAAAAAAAA==");
 
-	const pauseimg = () => ({
-		palette: new Uint16Array([0, g.theme.fg]),
-		buffer: require("heatshrink").decompress(atob("jEYxH+AH4Am64ABAxQWLCIYGGC6AHEF9QX/C/4X/C64HEF8YRDAxQA/AEQA=")),
-		width: 16,
-		height: 20,
-	});
+	const pauseimg = () =>
+		atob("GBiBAAAAAAAAAAAAAAAAAAHDgAPnwAPjwAPnwAPnwAPnwAPnwAPnwAPnwAPnwAPnwAPnwAPnwAPjwAPnwAHDgAAAAAAAAAAAAAAAAA==");
 
 	return {
 		name: "Bangle",
