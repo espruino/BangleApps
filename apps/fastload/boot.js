espruino/BangleApps

commit e2ba3ca64da7ce5b433e152c3c5887992b7f8897

fastload - Replace E.CRC32 with Storage.hash

apps/fastload/boot.js
@@ -22,8 +22,8 @@ let cache = s.readJSON("fastload.cache") || {};
 const SYS_SETTINGS="setting.json";
 
 let appFastloadPossible = function(n){
-  if(SETTINGS.detectSettingsChange && (!cache[SYS_SETTINGS] || E.CRC32(SYS_SETTINGS) != cache[SYS_SETTINGS])){
-    cache[SYS_SETTINGS] = E.CRC32(SYS_SETTINGS);
+  if(SETTINGS.detectSettingsChange && (!cache[SYS_SETTINGS] || s.hash(SYS_SETTINGS) != cache[SYS_SETTINGS])){
+    cache[SYS_SETTINGS] = s.hash(SYS_SETTINGS);
     s.writeJSON("fastload.cache", cache);
     return false;
   }
@@ -31,11 +31,11 @@ let appFastloadPossible = function(n){
   // no widgets, no problem
   if (!global.WIDGETS) return true;
   let app = s.read(n);
-  if (cache[n] && E.CRC32(app) == cache[n].crc)
+  if (cache[n] && s.hash(app) == cache[n].hash)
     return cache[n].fast;
   cache[n] = {};
   cache[n].fast = app.includes("Bangle.loadWidgets");
-  cache[n].crc = E.CRC32(app);
+  cache[n].hash = s.hash(app);
   s.writeJSON("fastload.cache", cache);
   return cache[n].fast;
 };

commit 97fa54c2d6131af7c1a231993df6a667c39e9827

fastload: fix caching
broken since e2ba3ca64 (fastload - Replace E.CRC32 with Storage.hash, 2023-07-24)

Storage.hash() expects a regex (or apparently the name as a string) to match files that should be hashed, but was called with the contents of the file

apps/fastload/boot.js
@@ -30,12 +30,13 @@ let appFastloadPossible = function(n){
 
   // no widgets, no problem
   if (!global.WIDGETS) return true;
-  let app = s.read(n);
-  if (cache[n] && s.hash(app) == cache[n].hash)
+  let hash = s.hash(n);
+  if (cache[n] && hash == cache[n].hash)
     return cache[n].fast;
+  let app = s.read(n);
   cache[n] = {};
   cache[n].fast = app.includes("Bangle.loadWidgets");
-  cache[n].hash = s.hash(app);
+  cache[n].hash = hash;
   s.writeJSON("fastload.cache", cache);
   return cache[n].fast;
 };