9909ff74cf1e8a6b73a875b2d2672160011ea0f1
thyttan <6uuxstm66@mozmail.com>
2023-08-10 02:19:59 +0200

    libslider: init w current android audio level

--- apps/libslider/app.js
+++ apps/libslider/app.js
@@ -1,8 +1,16 @@
 let callback = (mode,fb)=>{
   if (mode =="map") Bangle.musicControl({cmd:"volumesetlevel",extra:Math.round(100*fb/30)});
   if (mode =="incr") Bangle.musicControl(fb>0?"volumedown":"volumeup");
+  if (mode =="remove") {audioLevels.c = fb;}
 };
 
 g.reset().clear().setColor(1,0,0).fillRect(0,0,176,176);
 
-Bangle.on('drag', ()=>{require("SliderInput").interface(callback, {useMap:true});});
+let audioLevels;
+let audioHandler = (e)=>{audioLevels = e;};
+Bangle.on('audio', audioHandler);
+Bangle.musicControl("volumegetlevel");
+
+Bangle.on('drag', ()=>{
+  require("SliderInput").interface(callback, {useMap:true, steps:audioLevels.u, currLevel:audioLevels.c});}
+);
--- apps/libslider/lib.js
+++ apps/libslider/lib.js
@@ -15,9 +15,11 @@ const TIMEOUT = conf.timeout || 1;
 const STEP_SIZE = HEIGHT/STEPS;
 
 // Initialize the level
-let level; // TODO: Depend on parameter.
-let lastLevel=10;
+let level;
+let prevLevel = conf.currLevel || 0;
 let levelHeight;
+
+let firstRun = true;
 
 let dragSlider = e=>{
   "ram";
@@ -44,7 +46,7 @@ let dragSlider = e=>{
         else { dy+=32; incr = -1;}
         Bangle.buzz(20);
 
-        level = Math.min(Math.max(lastLevel-incr,0),STEPS);
+        level = Math.min(Math.max(prevLevel-incr,0),STEPS);
         cb("incr",incr);
         draw(level);
       }
@@ -63,19 +65,19 @@ let draw = (level)=>{
     // Pauses and resets the time out when interacted with.
     // TODO: Lazy, keep track of last level and only draw again if it changed.
 
-  if (level == lastLevel) {print("hi"); return;}
+  if (level == prevLevel) {if (!firstRun) return; if (firstRun) firstRun = false;}
 
   levelHeight = level==0?WIDTH:level*STEP_SIZE; // Math.max(level*STEP_SIZE,STEP_SIZE);
-  lastLevel = level;
+  prevLevel = level;
 
   ovr.clear().setColor(1).
-    fillRect({x:0,y:0,w:WIDTH,y2:ovr.getHeight(),r:30}). // To get outer border...
+    fillRect({x:0,y:0,w:WIDTH,y2:HEIGHT,r:30}). // To get outer border...
     setColor(0).
-    fillRect({x:2,y:2,w:WIDTH-5,y2:ovr.getHeight()-2,r:30}). // ... and here it's made hollow.
+    fillRect({x:2,y:2,w:WIDTH-5,y2:HEIGHT-2,r:30}). // ... and here it's made hollow.
     setColor(0==level?0:1).
-    fillRect({x:4,y:ovr.getHeight()-levelHeight+4,w:WIDTH-9,y2:ovr.getHeight()-4,r:30}); // Here the bar is drawn.
+    fillRect({x:4,y:HEIGHT-levelHeight+4,w:WIDTH-9,y2:ovr.getHeight()-4,r:30}); // Here the bar is drawn.
     Bangle.setLCDOverlay({
-      width:ovr.getWidth(), height:ovr.getHeight(),
+      width:WIDTH, height:HEIGHT,
       bpp:1, transparent:0,
       buffer:ovr.buffer
     },X_START,Y_START);
@@ -88,6 +90,7 @@ let remove = ()=> {
   ovr.clear().reset();
   Bangle.setLCDOverlay();
   Bangle.removeListener('drag', dragSlider);
+  cb("remove", level);
 };
 
 let timeout = setTimeout(remove, 1000*TIMEOUT);
