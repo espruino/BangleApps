6414859eda397544ad91280290ecbce679cb5dae
Matija Tosic <tosicmatija@gmail.com>
2023-08-13 00:49:32 +0200

    Add prompt before shutdown

--- apps/poweroff/app.js
+++ apps/poweroff/app.js
@@ -1,13 +1,23 @@
 g.clear();
 
-g.setFont("6x8",2).setFontAlign(0,0);
+E.showPrompt('Are you sure?', {
+  title: 'Power off',
+  buttons: { Yes: true, No: false },
+}).then((confirm) => {
+  if (!confirm) {
+    setTimeout(load, 100);
+    return;
+  }
+
+  g.setFont("6x8",2).setFontAlign(0,0);
   var x = g.getWidth()/2;
   var y = g.getHeight()/2 + 10;
   g.drawString("Powering off...", x, y);
 
-setTimeout(function() {
-  if (Bangle.softOff) Bangle.softOff(); else Bangle.off();
-}, 1000);
+  setTimeout(function() {
+    if (Bangle.softOff) Bangle.softOff(); else Bangle.off();
+  }, 1000);
+});
 
 Bangle.loadWidgets();
 Bangle.drawWidgets();
