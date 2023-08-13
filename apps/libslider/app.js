3824a5e603d5667dab5ec0a3126ea1fa64cb2430
thyttan <6uuxstm66@mozmail.com>
2023-08-14 01:45:13 +0200

    libslider: update and wait for volume level...
    ... from android before initiating the slider.

--- apps/libslider/app.js
+++ apps/libslider/app.js
@@ -1,7 +1,8 @@
+{
 let callback = (mode,fb)=>{
   if (mode =="map") Bangle.musicControl({cmd:"volumesetlevel",extra:Math.round(100*fb/30)});
   if (mode =="incr") Bangle.musicControl(fb>0?"volumedown":"volumeup");
-  if (mode =="remove") {audioLevels.c = fb;}
+  if (mode =="remove") {audioLevels.c = fb; ebLast = 0;}
 };
 
 g.reset().clear().setColor(1,0,0).fillRect(0,0,176,176);
@@ -11,6 +12,13 @@ let audioHandler = (e)=>{audioLevels = e;};
 Bangle.on('audio', audioHandler);
 Bangle.musicControl("volumegetlevel");
 
-Bangle.on('drag', ()=>{
-  require("SliderInput").interface(callback, {useMap:true, steps:audioLevels.u, currLevel:audioLevels.c});}
+let ebLast = 0;
+Bangle.on('drag', (e)=>{
+  if (ebLast==0) {
+  Bangle.musicControl("volumegetlevel");
+  setTimeout(()=>{require("SliderInput").interface(callback, {useMap:true, steps:audioLevels.u, currLevel:audioLevels.c});},200);
+  }
+  ebLast = e.b;
+}
 );
+}
