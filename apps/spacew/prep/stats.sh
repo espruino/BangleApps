337be5ff27278d7588e27616958e205551ce25fa
Pavel Machek <pavel@ucw.cz>
2023-08-19 22:59:19 +0200

    Prepare version 0.02 of Space Weaver. Major changes are move towards binary format, which saves a lot of space, and including world map, so that there's somethign to play with without extensive setup.

--- apps/spacew/prep/stats.sh
+++ apps/spacew/prep/stats.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 zoom() {
-    echo "Zoom $1"
-    cat delme/z$1-* | wc -c
-    echo "M..k..."
+    VAL=`cat delme/z$1-* | wc -c`
+    echo "Zoom $1 -- " $[$VAL/1024]
 }
 
 echo "Total data"
@@ -16,7 +15,17 @@ zoom 14
 zoom 13
 zoom 12
 zoom 11
-zoom 10
+zoom 9
+zoom 8
+zoom 7
+zoom 6
+zoom 5
+zoom 4
+zoom 3
+zoom 2
+zoom 1
+zoom 0
+
 echo "Zoom 1..9"
 cat delme/z?-* | wc -c
 echo "M..k..."
