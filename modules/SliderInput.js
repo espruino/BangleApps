7e906ede2566579f9b969ccf673b2a1818c8c273
thyttan <6uuxstm66@mozmail.com>
2023-09-02 11:17:04 +0200

    SliderInput:refactor to return obj w funcs & vars
    ... making it possible to interact with the slider reachable in the scope
    where the slider was initiated from.

--- modules/SliderInput.js
+++ modules/SliderInput.js
@@ -4,9 +4,13 @@ exports.interface = function(cb, conf) {
 
 const R = Bangle.appRect;
 
+let o = {};
+o.v = {}; // variables go here.
+o.f = {}; // functions go here.
+
 // configuration for the indicator:
 
-let c = Object.assign({
+o.c = Object.assign({ // constants go here.
 useMap:false,
 useIncr:true,
 horizontal:false,
@@ -23,137 +27,131 @@ colorBG:g.theme.bg2,
 lazy:true,
 rounded:0,
 propagateDrag:false,
-immediateDraw:false,
+immediatedraw:false,
 autoProgress:false,
 },conf);
 
-if (c.xStart) c.xStart += 4; // +4 to compensate for the border.
-if (c.width) c.width -= 8; // +8 to compensate for the border.
-if (c.yStart) c.yStart += 4; // +4 to compensate for the border.
-if (c.height) c.height -= 8; // +8 to compensate for the border.
-if (c.rounded) c.rounded = 40;
+o.c._xStart = o.c.xStart + 4; // +4 to compensate for the border.
+o.c._width = o.c.width - 8; // -8 to compensate for the border.
+o.c._yStart = o.c.yStart + 4; // +4 to compensate for the border.
+o.c._height = o.c.height - 8; // -8 to compensate for the border.
+if (o.c.rounded) o.c.rounded = 40;
 
-const STEP_SIZE = c.height/c.steps;
+o.c.STEP_SIZE = o.c._height/o.c.steps;
 
-if (c.horizontal) {
-  let mediator = c.xStart;
-  c.xStart = c.yStart;
-  c.yStart = mediator;
-  mediator = c.width;
-  c.width = c.height;
-  c.height = mediator;
+if (o.c.horizontal) {
+  let mediator = o.c._xStart;
+  o.c._xStart = o.c._yStart;
+  o.c._yStart = mediator;
+  mediator = o.c._width;
+  o.c._width = o.c._height;
+  o.c._height = mediator;
+
+  mediator = o.c.xStart;
+  o.c.xStart = o.c.yStart;
+  o.c.yStart = mediator;
+  mediator = o.c.width;
+  o.c.width = o.c.height;
+  o.c.height = mediator;
   delete mediator;
 }
 
 // Initialize the level
-let level = c.currLevel || c.steps/2;
-let prevLevel;
+o.v.level = o.c.currLevel || o.c.steps/2;
 
-let firstRun = true;
-let ebLast = 0;
-let exFirst;
+o.v.firstRun = true;
+o.v.ebLast = 0;
 
-let wasOnIndicator = (exFirst)=>{
+o.f.wasOnIndicator = (exFirst)=>{
   "ram";
-  if (!c.horizontal) return exFirst>c.xStart-c.oversizeL*c.width && exFirst<c.xStart+c.width+c.oversizeR*c.width;
-  if (c.horizontal) return exFirst>c.yStart-c.oversizeL*c.height && exFirst<c.yStart+c.height+c.oversizeR*c.height;
+  if (!o.c.horizontal) return exFirst>o.c._xStart-o.c.oversizeL*o.c._width && exFirst<o.c._xStart+o.c._width+o.c.oversizeR*o.c._width;
+  if (o.c.horizontal) return exFirst>o.c._yStart-o.c.oversizeL*o.c._height && exFirst<o.c._yStart+o.c._height+o.c.oversizeR*o.c._height;
 };
 
-const borderRect = {x:c.xStart-4,y:c.yStart-4,w:c.width+8,h:c.height+8,r:c.rounded};
-const hollowRect = {x:c.xStart-2,y:c.yStart-2,w:c.width+4,h:c.height+4,r:c.rounded};
+o.c.borderRect = {x:o.c._xStart-4,y:o.c._yStart-4,w:o.c._width+8,h:o.c._height+8,r:o.c.rounded};
+o.c.hollowRect = {x:o.c._xStart-2,y:o.c._yStart-2,w:o.c._width+4,h:o.c._height+4,r:o.c.rounded};
 
-let updateBar = (levelHeight)=>{
+o.f.updateBar = (levelHeight)=>{
   "ram";
-  if (!c.horizontal) return {x:c.xStart,y:c.yStart+c.height-levelHeight,w:c.width,y2:c.yStart+c.height,r:c.rounded};
-  if (c.horizontal) return {x:c.xStart,y:c.yStart,w:levelHeight,h:c.height,r:c.rounded};
+  if (!o.c.horizontal) return {x:o.c._xStart,y:o.c._yStart+o.c._height-levelHeight,w:o.c._width,y2:o.c._yStart+o.c._height,r:o.c.rounded};
+  if (o.c.horizontal) return {x:o.c._xStart,y:o.c._yStart,w:levelHeight,h:o.c._height,r:o.c.rounded};
 };
 
-let dragSlider = e=>{
+o.f.dragSlider = e=>{
   "ram";
-  if (!c.propagateDrag) E.stopEventPropagation&&E.stopEventPropagation();
+  if (!o.c.propagateDrag) E.stopEventPropagation&&E.stopEventPropagation();
 
-  if (timeout) {clearTimeout(timeout); timeout = undefined;}
-  if (e.b==0 && !timeout && (c.timeout || c.timeout===0)) timeout = setTimeout(remove, 1000*c.timeout);
+  if (o.v.timeoutID) {clearTimeout(o.v.timeoutID); o.v.timeoutID = undefined;}
+  if (e.b==0 && !o.v.timeoutID && (o.c.timeout || o.c.timeout===0)) o.v.timeoutID = setTimeout(o.f.remove, 1000*o.c.timeout);
 
-  let input = Math.min(c.horizontal?175-e.x:e.y, 170);
-  input = Math.round(input/STEP_SIZE);
+  let input = Math.min(o.c.horizontal?175-e.x:e.y, 170);
+  input = Math.round(input/o.c.STEP_SIZE);
 
-  if (ebLast==0) exFirst = c.horizontal?e.y:e.x;
+  if (o.v.ebLast==0) exFirst = o.c.horizontal?e.y:e.x;
 
-  // If draging on the indicator, adjust one-to-one.
-    if (c.useMap && wasOnIndicator(exFirst)) {
+    if (o.c.useMap && o.f.wasOnIndicator(exFirst)) { // If draging starts on the indicator, adjust one-to-one.
 
-      level = Math.min(Math.max(c.steps-input,0),c.steps);
+      o.v.level = Math.min(Math.max(o.c.steps-input,0),o.c.steps);
 
-      if (level != prevLevel) cb("map",level);
-      draw(level);
+      if (o.v.level != o.v.prevLevel) cb("map",o.v.level);
+      o.f.draw(o.v.level);
 
-    } else if (c.useIncr) { // Heavily inspired by "updown" mode of setUI.
+    } else if (o.c.useIncr) { // Heavily inspired by "updown" mode of setUI.
 
-      dy += c.horizontal?-e.dx:e.dy;
-      //if (!e.b) dy=0;
+      o.v.dy += o.c.horizontal?-e.dx:e.dy;
+      //if (!e.b) o.v.dy=0;
 
       let incr;
-      while (Math.abs(dy)>32) {
-        if (dy>0) { dy-=32; incr = 1;}
-        else { dy+=32; incr = -1;}
+      while (Math.abs(o.v.dy)>32) {
+        if (o.v.dy>0) { o.v.dy-=32; incr = 1;}
+        else { o.v.dy+=32; incr = -1;}
         Bangle.buzz(20);
 
-        level = Math.min(Math.max(level-incr,0),c.steps);
+        o.v.level = Math.min(Math.max(o.v.level-incr,0),o.c.steps);
         cb("incr",incr);
-        draw(level);
+        o.f.draw(o.v.level);
       }
     }
-  ebLast = e.b;
+  o.v.ebLast = e.b;
 };
 
-let draw = (level)=>{
+o.f.draw = (level)=>{
   "ram";
-  // Draw the indicator.
-    // Should be displayed when a relevant drag event is detected.
-    // Should time out.
-    // If user drags directly on the draw area, adjust level one-to-one.
-    // Pauses and resets the time out when interacted with.
 
-  if (firstRun || !c.lazy) {
-    g.setColor(c.colorFG).fillRect(borderRect); // To get outer border...
+  if (true || o.v.firstRun || !o.c.lazy) {
+    g.setColor(o.c.colorFG).fillRect(o.c.borderRect); // To get outer border...
   }
-  if (level == prevLevel) {if (!firstRun) return; if (firstRun) firstRun = false;}
+  if (false && level == o.v.prevLevel) {if (!o.v.firstRun) return; if (o.v.firstRun) o.v.firstRun = false;}
 
-  prevLevel = level;
+  o.v.prevLevel = level;
 
-    g.setColor(c.colorBG).
-    fillRect(hollowRect). // ... and here it's made hollow.
-    setColor(0==level?c.colorBG:c.colorFG).
-    fillRect(updateBar(level*STEP_SIZE)); // Here the bar is drawn.
+    g.setColor(o.c.colorBG).
+    fillRect(o.c.hollowRect). // ... and here it's made hollow.
+    setColor(0==level?o.c.colorBG:o.c.colorFG).
+    fillRect(o.f.updateBar(level*o.c.STEP_SIZE)); // Here the bar is drawn.
 
-  print(level);
+  //print(level);
   //print(process.memory().usage);
 };
 
-let remove = ()=> {
-  Bangle.removeListener('drag', dragSlider);
-  cb("remove", prevLevel);
+o.f.remove = ()=> {
+  Bangle.removeListener('drag', o.f.dragSlider);
+  cb("remove", o.v.prevLevel);
 };
 
-let timeout;
-if (c.timeout || c.timeout===0) timeout = setTimeout(remove, 1000*c.timeout);
-
-let dy = 0;
-g.reset();
-Bangle.prependListener('drag', dragSlider);
-if (c.immediateDraw) draw(level);
-
-if (c.autoProgress) {
-  draw(level);
-  let autoUpdate = ()=>{
-    level = level?level+1:0;
-    draw(level);
-    if (level==c.steps) {clearInterval(autoInterval); return;}
+if (o.c.autoProgress) {
+  o.f.autoUpdate = ()=>{
+    //if (o.v.level===undefined) o.v.level = -1;
+    o.v.level = o.v.level+1;
+    o.f.draw(o.v.level);
+    if (o.v.level==o.c.steps) {clearInterval(o.v.autoIntervalID);}
   };
-  let autoInterval;
-  autoInterval = setInterval(autoUpdate,1000);
+  o.f.startAutoUpdate = ()=>{o.v.autoIntervalID = setInterval(o.f.autoUpdate,1000)};
 }
+
+//o.f.printThis = ()=>(print(this));
+
+return o;
 }
 
 } catch (e) {
