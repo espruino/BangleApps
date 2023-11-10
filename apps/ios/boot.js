1cd63b0261d7b893120003476ba2e1bd7602f8c5
Mineinjava <65673396+Mineinjava@users.noreply.github.com>
2023-11-10 14:33:46 -0800

    try this

--- apps/ios/boot.js
+++ apps/ios/boot.js
@@ -190,7 +190,7 @@ E.on('notify',msg=>{
       color: 0,
       allday: false
     }
-    Terminal.println("'" + msg.name + "'");
+    Terminal.println("'" + msg.title&&E.decodeUTF8(msg.title, unicodeRemap, replacer) + "'");
     calEvent.allday = calEvent.durationInSeconds >= 24 * 56 * 60 - 1; // 24 hours for IOS is 23:59:59
 
     var cal = require("Storage").readJSON("android.calendar.json",true);
