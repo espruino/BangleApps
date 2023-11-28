96778b6be1317fa1dc902681fdaf99264124a513
lauzonhomeschool <85599144+lauzonhomeschool@users.noreply.github.com>
2023-11-27 22:27:13 -0500

    Update app.js compact showAlarm boolean

--- apps/alarm/app.js
+++ apps/alarm/app.js
@@ -84,7 +84,7 @@ function showMainMenu(scroll, group) {
   var showAlarm;
 
   alarms.forEach((e, index) => {
-    showAlarm = !settings.showGroup || (!group && !e.group) || (group && e.group === group);
+    showAlarm = !settings.showGroup || (group ? e.group === group : !e.group);
     if(showAlarm) {
       menu[trimLabel(getLabel(e),40)] = {
         value: e.on ? (e.timer ? iconTimerOn : iconAlarmOn) : (e.timer ? iconTimerOff : iconAlarmOff),
