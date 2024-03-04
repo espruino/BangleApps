22d7ef7ae9b22a0e193dd9c984a4075e8478b700
Logan B <3870583+thinkpoop@users.noreply.github.com>
2024-03-03 21:00:54 -0600

    a_dndtoggle: add comments to explain settings a bit better

--- apps/a_dndtoggle/settings.js
+++ apps/a_dndtoggle/settings.js
@@ -8,13 +8,15 @@
     }
 
     function buildMainMenu(){
+      // 0-Noisy is only a placeholder so that the other values map to the Bangle quiet mode options
       const modes = [/*LANG*/"Noisy",/*LANG*/"Alarms",/*LANG*/"Silent"];
       let mainmenu = {
         '': { 'title': 'A_DND Toggle' },
         '< Back': back,
         /*LANG*/"Quiet Mode": {
           value: settings.mode || 2,
-          min: 1, max: modes.length - 1,
+          min: 1, // don't allow choosing 0-Noisy
+          max: modes.length - 1,
           format: v => modes[v],
           onchange: v => {
             settings.mode = v;
