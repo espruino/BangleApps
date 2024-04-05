8391a4750a2fec717000786581b0b9f9f66b2ff6
thyttan <6uuxstm66@mozmail.com>
2024-04-06 01:26:27 +0200

    messagegui: use settings wake on facup/twist

--- apps/messagegui/app-newmessage.js
+++ apps/messagegui/app-newmessage.js
@@ -3,3 +3,18 @@ BUZZ_ON_NEW_MESSAGE is set so when messagegui.app.js loads it knows
 that it should buzz */
 global.BUZZ_ON_NEW_MESSAGE = true;
 eval(require("Storage").read("messagegui.app.js"));
+
+// Add temporary listeners for unlocking watch. `settings` is init:ed in the eval:ed file "messagegui.app.js".
+if (settings.tempWakeOnFaceUp || settings.tempWakeOnTwist) {
+  const options = Bangle.getOptions();
+  if (!options.wakeOnFaceUp && !options.wakeOnTwist) { // If the user already uses a one arm method of unlocking it's unnecessary to do anything more.
+    if (settings.tempWakeOnFaceUp) {
+      Bangle.setOptions({wakeOnFaceUp:true});
+      E.on("kill", ()=>{Bangle.setOptions({wakeOnFaceUp:false});});
+    }
+    if (settings.tempWakeOnTwist) {
+      Bangle.setOptions({wakeOnTwist:true});
+      E.on("kill", ()=>{Bangle.setOptions({wakeOnTwist:false});});
+    }
+  }
+}
