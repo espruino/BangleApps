1628c3ccf139eefcff3f5764071338594462ff80
thyttan <6uuxstm66@mozmail.com>
2024-04-07 12:16:41 +0200

    messagegui: avoid lint warning

--- apps/messagegui/app-newmessage.js
+++ apps/messagegui/app-newmessage.js
@@ -1,10 +1,11 @@
 /* Called when we have a new message when we're in the clock...
 BUZZ_ON_NEW_MESSAGE is set so when messagegui.app.js loads it knows
 that it should buzz */
+var settings; // Init:ed here to avoid lint warn, defined in eval below.
 global.BUZZ_ON_NEW_MESSAGE = true;
 eval(require("Storage").read("messagegui.app.js"));
 
-// Add temporary listeners for unlocking watch. `settings` is init:ed in the eval:ed file "messagegui.app.js".
+// Add temporary listeners for unlocking watch.
 if (settings.tempWakeOnFaceUp || settings.tempWakeOnTwist) {
   const options = Bangle.getOptions();
   if (!options.wakeOnFaceUp && !options.wakeOnTwist) { // If the user already uses a one arm method of unlocking it's unnecessary to do anything more.
