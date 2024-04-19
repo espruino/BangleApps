7d592664381f6f26d6de0bf1771475c148828ab9
Gordon Williams <gw@pur3.co.uk>
2024-04-19 15:30:18 +0100

    Ensure calendar clockinfo is transparent and remove an unused font!

--- apps/clkinfocal/clkinfo.js
+++ apps/clkinfocal/clkinfo.js
@@ -1,9 +1,7 @@
 (function() {
-  require("Font4x8Numeric").add(Graphics);
-
   var settings = require("Storage").readJSON("clkinfocal.json",1)||{};
   settings.fmt = settings.fmt||"DDD";
-  
+
   var getDateString = function(dt) {
     switch(settings.fmt) {
     case "dd MMM":
@@ -22,6 +20,7 @@
         get : () => {
           let d = new Date();
           let g = Graphics.createArrayBuffer(24,24,1,{msb:true});
+          g.transparent = 1;
           g.drawImage(atob("FhgBDADAMAMP/////////////////////8AADwAAPAAA8AADwAAPAAA8AADwAAPAAA8AADwAAPAAA8AADwAAP///////"),1,0);
           g.setFont("6x15").setFontAlign(0,0).drawString(d.getDate(),11,17);
           return {
