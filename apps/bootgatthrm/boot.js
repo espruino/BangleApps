9b5295189e26e50562ed503a8caa85a1d0e2087e
Rob Pilling <robpilling@gmail.com>
2024-04-20 17:41:37 +0100

    bootgatthrm: whitespace

--- apps/bootgatthrm/boot.js
+++ apps/bootgatthrm/boot.js
@@ -53,14 +53,14 @@
     } catch (error) {
       if (error.message.includes("BLE restart")) {
         /*
-         * BLE has to restart after service setup.  
+         * BLE has to restart after service setup.
          */
         if(!settings.keepConnected)
           NRF.disconnect();
       }
       else if (error.message.includes("UUID 0x2a37")) {
         /*
-         * Setup service if it wasn't setup correctly for some reason 
+         * Setup service if it wasn't setup correctly for some reason
          */
         setupHRMAdvertising();
       } else {
