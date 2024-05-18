a4677f5a06ba2316f8f9d8efc1fe1126e8c6c2b8
Rob Pilling <robpilling@gmail.com>
2024-05-18 10:45:34 +0100

    .eslintrc.js: disable duplicate TS rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -235,7 +235,7 @@ module.exports = {
                 "@typescript-eslint/no-delete-var": "off",
                 "@typescript-eslint/no-explicit-any": "off",
                 "@typescript-eslint/no-this-alias": "off",
-                "@typescript-eslint/no-unused-vars": ["error", { "args": "none" } ],
+                "@typescript-eslint/no-unused-vars": "off",
                 "@typescript-eslint/no-var-requires": "off",
             }
         },
