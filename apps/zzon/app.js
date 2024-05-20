125ccdd660f48722787699a5819ccc94d95d2689
nhq12 <113136877+nhq12@users.noreply.github.com>
2024-05-20 03:23:40 +0300

    Update app.js

--- apps/zzon/app.js
+++ apps/zzon/app.js
@@ -72,7 +72,7 @@ const ummAlQuraCoordinates = [
   // }
   
   // function deg2rad(deg) {
-  //   return deg * (Math.PI/180)
+  //   return deg * (Math.PI/180);
   // }
   
   // function createAndUploadZoneArea(watch, zoneName, centerLat, centerLon, radius) {
@@ -108,3 +108,60 @@ const ummAlQuraCoordinates = [
   //     });
   //   }
   // });
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//   const Bangle = require('banglejs');
+// function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+//   var R = 6371; // Radius of the earth in km
+//   var dLat = deg2rad(lat2 - lat1);
+//   var dLon = deg2rad(lon2 - lon1); 
+//   var a = 
+//     Math.sin(dLat/2) * Math.sin(dLat/2) +
+//     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+//     Math.sin(dLon/2) * Math.sin(dLon/2)
+//     ; 
+//   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+//   var d = R * c; // Distance in km
+//   return d;
+// }
+
+// function deg2rad(deg) {
+//   return deg * (Math.PI/180);
+// }
+
+// function createAndUploadZoneArea(watch, zoneName, centerLat, centerLon, radius) {
+//   let newZone = {
+//     name: zoneName,
+//     center: { lat: centerLat, lon: centerLon },
+//     radius: radius
+//   };
+//   watch.uploadZoneArea(newZone, (error, response) => {
+//     if (error) {
+//       console.log('Error uploading zone area:', error);
+//     } else {
+//       console.log('Zone area uploaded successfully:', response);
+//     }
+//   });
+// }
+
+// const zoneName = 'Your Zone';
+// const radius = 0.05; // replace with your radius in km
+
+// Bangle.connect('your-watch-id', (error, watch) => {
+//   if (error) {
+//     console.log('Error connecting to watch:', error);
+//   } else {
+//     watch.getGPS().then(location => {
+//       const centerLatitude = location.lat;
+//       const centerLongitude = location.lon;
+//       createAndUploadZoneArea(watch, zoneName, centerLatitude, centerLongitude, radius);
+//       watch.on('location', (location) => {
+//         const distance = getDistanceFromLatLonInKm(centerLatitude, centerLongitude, location.lat, location.lon);
+//         if (distance > radius) {
+//           console.log('You are out of the zone');
+//         }
+//       });
+//     });
+//   }
+// });
