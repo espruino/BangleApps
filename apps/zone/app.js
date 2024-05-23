556f9290762df2d58f43f47604992c5c443e03f0
nhq12 <113136877+nhq12@users.noreply.github.com>
2024-05-23 18:55:40 +0300

    Update app.js

--- apps/zone/app.js
+++ apps/zone/app.js
@@ -85,6 +85,15 @@ function drawPOI() {
   });
 }
 
+// Draw a circle on the map
+function drawCircle(lat, lon, radius) {
+  var center = m.latLonToXY(lat, lon);
+  var scaleFactor = m.getScaleFactor();
+  var pixelRadius = radius / scaleFactor;
+  g.setColor(1, 0, 0); // Red color
+  g.drawCircle(center.x, center.y, pixelRadius);
+}
+
 function isInside(rect, e, w, h) {
   return e.x-w/2>=rect.x && e.x+w/2<rect.x+rect.w
     && e.y-h/2>=rect.y && e.y+h/2<=rect.y+rect.h;
@@ -118,7 +127,7 @@ function drawLocation() {
 
   ovLoc.clear();
   if (isInside(R, p, ovLoc.getWidth(), ovLoc.getHeight())) { // avoid drawing over widget area
-    const angle = settings.dirSrc ===    1 ? fix.course : Bangle.getCompass().heading;
+    const angle = settings.dirSrc === 1 ? fix.course : Bangle.getCompass().heading;
     if (!isNaN(angle)) {
       ovLoc.drawImage(imgLoc, ovLoc.getWidth()/2, ovLoc.getHeight()/2, {rotate: angle*Math.PI/180});
     }
@@ -147,140 +156,136 @@ if (HASWIDGETS) {
   Bangle.loadWidgets();
   if (!WIDGETS["gps"]) { // one GPS Widget is enough
     WIDGETS["sats"] = { area:"tl", width:24, draw:w=>{
-      var sats = 0|fix.satellites;
-      g.reset().clearRect(w.x,w.y,w.x+23,w.y+23).           drawImage(atob("EhPCAP//vmU0Cv++BUAAAANVQAAANVUAAANVVSoA/VVWqA/VqqqA/1VVoA/9VVkAP/VVlAP/1VVAP/9VVQD//WVUA//2VUA//9VUAX//1UBV///0BVX//ABVVAAABVVAAAA="), w.x,w.y+5); // 18x19
-      if (fix.fix) g.setFont("6x8").setFontAlign(1,-1).drawString(sats,w.x+23,w.y);
-      else g.setFont("4x6:2").setColor("#f00").setFontAlign(1,-1).drawString("X",w.x+23,w.y);
-    }};
+        var sats = 0|fix.satellites;
+        g.reset().clearRect(w.x,w.y,w.x+23,w.y+23).           drawImage(atob("EhPCAP//vmU0Cv++BUAAAANVQAAANVUAAANVVSoA/VVWqA/VqqqA/1VVoA/9VVkAP/VVlAP/1VVAP/9VVQD//WVUA//2VUA//9VUAX//1UBV///0BVX//ABVVAAABVVAAAA="), w.x,w.y+5); // 18x19
+        if (fix.fix) g.setFont("6x8").setFontAlign(1,-1).drawString(sats,w.x+23,w.y);
+        else g.setFont("4x6:2").setColor("#f00").setFontAlign(1,-1).drawString("X",w.x+23,w.y);
+      }};
+    }
+    Bangle.drawWidgets();
   }
-  Bangle.drawWidgets();
-}
-R = Bangle.appRect;
-
-function writeSettings() {
-  settings.lat = m.lat;
-  settings.lon = m.lon;
-  settings.scale = m.scale;
-  require("Storage").writeJSON("zone.json",settings);
-}
-
-function showMenu() {
-  if (plotTrack && plotTrack.stop)
-    plotTrack.stop();
-  mapVisible = false;
-  drawLocation();
-  var menu = {
-    "":{title:/*LANG*/"Map"},
-    "< Back": ()=> showMap(),
-  };
-  // If we have a GPS fix, add a menu item to center it
-  if (fix.fix) menu[/*LANG*/"Center GPS"]=() =>{
-    m.lat = fix.lat;
-    m.lon = fix.lon;
-    showMap();
-  };
-  menu = Object.assign(menu, {
-  /*LANG*/"Zoom In": () =>{
-    m.scale /= 2;
-    showMap();
-  },
-  /*LANG*/"Zoom Out": () =>{
-    m.scale *= 2;
-    showMap();
-  },
-  /*LANG*/"Draw Track": {
-    value : !!settings.drawTrack,
-    onchange : v => { settings.drawTrack=v; writeSettings(); }
-  },
-  /*LANG*/"Draw cont. position": {
-    value : !!settings.drawMarker,
-    onchange : v => { settings.drawMarker=v; writeSettings(); }
-  },
-  });
-
-  if (Bangle.setLCDOverlay) {
-    menu[/*LANG*/"Direction source"] = {
-      value: settings.dirSrc,
-      min: 0, max: 2,
-      format: v => [/*LANG*/"None", /*LANG*/"GPS", /*LANG*/"Compass"][v],
-      onchange: v => {
-        settings.dirSrc = v;
-        Bangle.setCompassPower(settings.dirSrc === 2, "zone");
-        writeSettings();
-      }
+  R = Bangle.appRect;
+  
+  function writeSettings() {
+    settings.lat = m.lat;
+    settings.lon = m.lon;
+    settings.scale = m.scale;
+    require("Storage").writeJSON("zone.json",settings);
+  }
+  
+  function showMenu() {
+    if (plotTrack && plotTrack.stop)
+      plotTrack.stop();
+    mapVisible = false;
+    drawLocation();
+    var menu = {
+      "":{title:/*LANG*/"Map"},
+      "< Back": ()=> showMap(),
     };
-    menu[/*LANG*/"Reset compass"] = () => {
-      Bangle.resetCompass();
+    // If we have a GPS fix, add a menu item to center it
+    if (fix.fix) menu[/*LANG*/"Center GPS"]=() =>{
+      m.lat = fix.lat;
+      m.lon = fix.lon;
       showMap();
     };
-  }
-
-  menu[/*LANG*/"Center Map"] = () =>{
-    m.lat = m.map.lat;
-    m.lon = m.map.lon;
-    m.scale = m.map.scale;
-    showMap();
-  };
-
-  // If we have the recorder widget, add a menu item to start/stop recording
-  if (WIDGETS.recorder) {
-    menu[/*LANG*/"Record"] = {
-      value : WIDGETS.recorder.isRecording(),
-      onchange : isOn => {
-        E.showMessage(/*LANG*/"Please Wait...");
-        WIDGETS.recorder.setRecording(isOn).then(showMap);
-      }
-    };
-  }
-  menu[/*LANG*/"Exit"] = () => load();
-  E.showMenu(menu);
-}
-
-function showMap() {
-  mapVisible = true;
-  g.reset().clearRect(R);
-  redraw();
-  Bangle.setUI({mode:"custom",drag:e=>{
-    if (plotTrack && plotTrack.stop) plotTrack.stop();
-    if (e.b) {
-      if (!startDrag)
-        startDrag = getTime();
-      g.setClipRect(R.x,R.y,R.x2,R.y2);
-      g.scroll(e.dx,e.dy);
-      m.scroll(e.dx,e.dy);
-      g.setClipRect(0,0,g.getWidth()-1,g.getHeight()-1);
-      hasScrolled = true;
-      drawLocation();
-    } else if (hasScrolled) {
-      const delta = getTime() - startDrag;
-      startDrag = 0;
-      hasScrolled = false;
-      if (delta < 0.2) {
-        if (e.y > g.getHeight() / 2) {
-          if (e.x < g.getWidth() / 2) {
-            m.scale /= 2;
-          } else {
-            m.scale *= 2;
-          }
+    menu = Object.assign(menu, {
+    /*LANG*/"Zoom In": () =>{
+      m.scale /= 2;
+      showMap();
+    },
+    /*LANG*/"Zoom Out": () =>{
+      m.scale *= 2;
+      showMap();
+    },
+    /*LANG*/"Draw Track": {
+      value : !!settings.drawTrack,
+      onchange : v => { settings.drawTrack=v; writeSettings(); }
+    },
+    /*LANG*/"Draw cont. position": {
+      value : !!settings.drawMarker,
+      onchange : v => { settings.drawMarker=v; writeSettings(); }
+    },
+    });
+  
+    if (Bangle.setLCDOverlay) {
+      menu[/*LANG*/"Direction source"] = {
+        value: settings.dirSrc,
+        min: 0, max: 2,
+        format: v => [/*LANG*/"None", /*LANG*/"GPS", /*LANG*/"Compass"][v],
+        onchange: v => {
+          settings.dirSrc = v;
+          Bangle.setCompassPower(settings.dirSrc === 2, "zone");
+          writeSettings();
         }
-        g.reset().clearRect(R);
-      }
-      redraw();
+      };
+      menu[/*LANG*/"Reset compass"] = () => {
+        Bangle.resetCompass();
+        showMap();
+      };
     }
-  }, btn: () => showMenu() });
-}
-
-
-if (m.maps.length === 0) {
-  E.showPrompt(/*LANG*/'Please upload a map first.', {buttons : {/*LANG*/"Ok":true}}).then(v => load());
-} else {
-  showMap();
-}
-
-// Write settings on exit via button
-setWatch(() => writeSettings(), BTN, { repeat: true, edge: "rising" });
-
-// Adding code for circle with a radius of 100 meters
-g.setColor(1, 1, 1); // Set color to white
-const circleCenter = m.latLonToXY(fix.lat, fix.lon); // Get the center point of the circle
-g.drawCircle(circleCenter.x, circleCenter.y, m.getDistanceX(100), 1); // Draw circle with a radius of 100 meters
+  
+    menu[/*LANG*/"Center Map"] = () =>{
+      m.lat = m.map.lat;
+      m.lon = m.map.lon;
+      m.scale = m.map.scale;
+      showMap();
+    };
+  
+    // If we have the recorder widget, add a menu item to start/stop recording
+    if (WIDGETS.recorder) {
+      menu[/*LANG*/"Record"] = {
+        value : WIDGETS.recorder.isRecording(),
+        onchange : isOn => {
+          E.showMessage(/*LANG*/"Please Wait...");
+          WIDGETS.recorder.setRecording(isOn).then(showMap);
+        }
+      };
+    }
+    menu[/*LANG*/"Exit"] = () => load();
+    E.showMenu(menu);
+  }
+  
+  function showMap() {
+    mapVisible = true;
+    g.reset().clearRect(R);
+    redraw();
+    Bangle.setUI({mode:"custom",drag:e=>{
+      if (plotTrack && plotTrack.stop) plotTrack.stop();
+      if (e.b) {
+        if (!startDrag)
+          startDrag = getTime();
+        g.setClipRect(R.x,R.y,R.x2,R.y2);
+        g.scroll(e.dx,e.dy);
+        m.scroll(e.dx,e.dy);
+        g.setClipRect(0,0,g.getWidth()-1,g.getHeight()-1);
+        hasScrolled = true;
+        drawLocation();
+      } else if (hasScrolled) {
+        const delta = getTime() - startDrag;
+        startDrag = 0;
+        hasScrolled = false;
+        if (delta < 0.2) {
+          if (e.y > g.getHeight() / 2) {
+            if (e.x < g.getWidth() / 2) {
+              m.scale /= 2;
+            } else {
+              m.scale *= 2;
+            }
+          }
+          g.reset().clearRect(R);
+        }
+        redraw();
+      }
+    }, btn: () => showMenu() });
+  }
+  
+  
+  if (m.maps.length === 0) {
+    E.showPrompt(/*LANG*/'Please upload a map first.', {buttons : {/*LANG*/"Ok":true}}).then(v => load());
+  } else {
+    showMap();
+  }
+  
+  // Write settings on exit via button
+  setWatch(() => writeSettings(), BTN, { repeat: true, edge: "rising" });
+  
