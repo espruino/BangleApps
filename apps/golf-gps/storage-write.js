e3160a1106ca38816ba5de952664238782186a28
jeonlab <37269397+jeonlab@users.noreply.github.com>
2024-06-21 14:20:58 -0400

    Update storage-write.js

--- apps/golf-gps/storage-write.js
+++ apps/golf-gps/storage-write.js
@@ -1,15 +1,18 @@
 /*
- A script to create, overwrite or append golf course GPS data
- Format: latitude (6 decimal places), longitude (6 decimal places), par
- There are two course data in below code, but you can put any number of course data.
- 
- Save this file to your computer and open it from Espruino Web IDE (https://www.espruino.com/ide/)
- and run in RAM mode
- 
- Tip: In the Web IDE editor, enter GPS cordinates and par per hole, and then SHFT-ALT drag all line, hit HOME and type " and hit END and type \n"+
-*/
+  Before you run Golf GPS, you need to save your favourite golf course data into the storage area. 
+  Download this script and open it from Espruino Web IDE (https://www.espruino.com/ide/) and enter your course data and run in RAM mode. 
+  With this script, you can create, overwrite or append golf course GPS data.
 
-f = require("Storage").open("course-data","a"); // "r" to create or overwrite, "a" to append
+  You can put any number of course data. 
+  Once the data file is created in the storage area, you can add more data to the same file using append ("a" ) mode with the same script. 
+  Just replace the data and change the mode to "a" and run in RAM mode.
+
+  One tip to wrap each line with "----\n"+ is using the built-in edit feature of the Web IDE. 
+  In the Web IDE editor, enter GPS cordinates and par per hole, and then SHFT-ALT drag all lines including the course name, 
+  and hit HOME and type " and hit END and type \n"+.
+*/
+ 
+f = require("Storage").open("course-data","w"); // "w" to create or overwrite, "a" to append
 f.write(
 "course name\n"+
 "00.000000,00.000000,0\n"+
