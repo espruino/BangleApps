324e17ff957aacb335403a23a67752b2bbd75232
deirdreobyrne <deirdre.dub@gmail.com>
2024-06-26 23:26:08 +0100

    Using the ble_advert module

--- apps/bootbthomebatt/boot.js
+++ apps/bootbthomebatt/boot.js
@@ -3,25 +3,10 @@ var btHomeBatterySequence = 0;
 function advertiseBTHomeBattery() {
   var advert = [0x40, 0x00, btHomeBatterySequence, 0x01, E.getBattery()];
 
-  if(Array.isArray(Bangle.bleAdvert)){
-    var found = false;
-    for(var ad in Bangle.bleAdvert){
-      if(ad[0xFCD2]){
-        ad[0xFCD2] = advert;
-        found = true;
-        break;
-      }
-    }
-    if(!found)
-      Bangle.bleAdvert.push({ 0xFCD2: advert });
-  } else {
-    Bangle.bleAdvert[0xFCD2] = advert;
-  }
-  NRF.setAdvertising(Bangle.bleAdvert);
+  require("ble_advert").set(0xFCD2, advert);
   btHomeBatterySequence = (btHomeBatterySequence + 1) & 255;
 }
 
-if (!Bangle.bleAdvert) Bangle.bleAdvert = {};
 setInterval(function() {
   advertiseBTHomeBattery();
 }, 300000); // update every 5 min
