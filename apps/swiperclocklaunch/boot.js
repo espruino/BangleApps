14a37529c9bacb3506e24e43b15cd001d70092c4
Rob Pilling <robpilling@gmail.com>
2024-07-03 12:07:45 +0100

    swiperclocklaunch: fix detection of clock

--- apps/swiperclocklaunch/boot.js
+++ apps/swiperclocklaunch/boot.js
@@ -8,19 +8,14 @@
     sui(mode,cb);
     oldSwipe = Bangle.swipeHandler;
 
-    if ("object"==typeof mode) mode = mode.mode;
-    if (!mode) return;
-
-    if (mode.startsWith("clock")) {
+    if (Bangle.CLOCK) {
       // clock -> launcher
       Bangle.swipeHandler = dir => { if (dir<0) Bangle.showLauncher(); };
       Bangle.on("swipe", Bangle.swipeHandler);
-    } else {
-      if (global.__FILE__ && __FILE__.endsWith(".app.js") && (require("Storage").readJSON(__FILE__.slice(0,-6)+"info",1)||{}).type=="launch") {
-        // launcher -> clock
-        Bangle.swipeHandler = dir => { if (dir>0) load(); };
-        Bangle.on("swipe", Bangle.swipeHandler);
-      }
+    } else if (global.__FILE__ && __FILE__.endsWith(".app.js") && (require("Storage").readJSON(__FILE__.slice(0,-6)+"info",1)||{}).type==="launch") {
+      // launcher -> clock
+      Bangle.swipeHandler = dir => { if (dir>0) load(); };
+      Bangle.on("swipe", Bangle.swipeHandler);
     }
   };
 })();
