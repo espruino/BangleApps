aae8479231b119ff71835b2fc3a45cd032282f45
Martin Zwigl <martin@zwigl.info>
2024-08-01 16:40:33 +0200

    :sparkles: presp to display GPS data

--- apps_ts/papam_clockinfo_tests/src/app.ts
+++ apps_ts/papam_clockinfo_tests/src/app.ts
@@ -1,7 +1,52 @@
 import appConfig = require("./app-config");
 
-setInterval(() => {
-    const temp = E.getTemperature();
-    console.log("Current temp:", temp);
-}, appConfig.default.refreshPeriodSec * 1000);
+interface GpsData {
+    text: string;
+    img: string;
+}
 
+interface Item {
+    name: string;
+    get: () => GpsData;
+    show: () => void;
+    hide: () => void;
+    hasRange?: boolean;
+}
+
+interface Result {
+    name: string;
+    items: Item[];
+}
+
+(function() {
+    function show(this: { interval?: number; emit: (event: string) => void }) {
+        this.interval = setTimeout(() => {
+            this.emit("redraw");
+            this.interval = setInterval(() => {
+                this.emit("redraw");
+            }, 60000);
+        }, 60000 - (Date.now() % 60000));
+    }
+
+    function hide(this: { interval?: number }) {
+        clearInterval(this.interval);
+        this.interval = undefined;
+    }
+
+    return {
+        name: "Bangle",
+        items: [
+            {
+                name: "GPS-Data",
+                get: () => {
+                    return {
+                        text: "lat/lon",
+                        img: atob("GBiBAAAAAAAAAAAAAAAYAAA8AAB+AAD/AAAAAAAAAAAAAAAYAAAYAAQYIA4AcAYAYAA8AAB+AAD/AAH/gD///D///AAAAAAAAAAAAA==")
+                    };
+                },
+                show: show,
+                hide: hide
+            },
+        ]
+    } as Result;
+})();
