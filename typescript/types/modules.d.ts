4b08b4b24cc49978162b7fbca5d1e95dc518fa87
Rob Pilling <robpilling@gmail.com>
2024-09-21 12:51:49 +0100

    typescript: add exstats

--- typescript/types/modules.d.ts
+++ typescript/types/modules.d.ts
@@ -6,3 +6,4 @@ declare function require(moduleName: "ClockFace"): typeof ClockFace_.ClockFace;
 declare function require(moduleName: "clock_info"): typeof ClockInfo;
 declare function require(moduleName: "Layout"): typeof Layout.Layout;
 declare function require(moduleName: "power_usage"): PowerUsageModule;
+declare function require(moduleName: "exstats"): typeof ExStats;
