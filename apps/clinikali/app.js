0bb30499b26f3fa4321a202a2c195c9c855c9931
Laurent Chau <laurent.chau@capgemini.com>
2024-10-09 14:00:03 +0200

    change icons and run prettier

--- apps/clinikali/app.js
+++ apps/clinikali/app.js
@@ -4,21 +4,19 @@ Bangle.drawWidgets();
 let settings;
 
 function loadSettings() {
-  settings = require("Storage").readJSON("clinikali.json",1)||{};
+  settings = require("Storage").readJSON("clinikali.json", 1) || {};
   let changed = false;
   if (!settings.file) {
     changed = true;
     settings.file = "clinikali.log0.csv";
   }
-  if (changed)
-    require("Storage").writeJSON("clinikali.json", settings);
+  if (changed) require("Storage").writeJSON("clinikali.json", settings);
 }
 loadSettings();
 
 function updateSettings() {
   require("Storage").writeJSON("clinikali.json", settings);
-  if (WIDGETS.recorder)
-    WIDGETS.recorder.reload();
+  if (WIDGETS.recorder) WIDGETS.recorder.reload();
 }
 
 function getTrackNumber(filename) {
@@ -31,20 +29,22 @@ function showMainMenu() {
   function menuRecord(id) {
     return {
       value: settings.record.includes(id),
-      onchange: v => {
+      onchange: (v) => {
         settings.recording = false; // stop recording if we change anything
-        settings.record = settings.record.filter(r=>r!==id);
+        settings.record = settings.record.filter((r) => r !== id);
         if (v) settings.record.push(id);
         updateSettings();
-      }
+      },
     };
   }
   const mainmenu = {
-    '': { 'title': 'Recorder' },
-    '< Back': ()=>{load();},
-    'RECORD': {
+    "": { title: "Recorder" },
+    "< Back": () => {
+      load();
+    },
+    RECORD: {
       value: !!settings.recording,
-      onchange: v => {
+      onchange: (v) => {
         setTimeout(() => {
           E.showMenu();
           WIDGETS.recorder.setRecording(v).then(() => {
@@ -54,115 +54,115 @@ function showMainMenu() {
             showMainMenu();
           });
         }, 1);
-      }
+      },
     },
-    'File' : {value:getTrackNumber(settings.file)},
-    'View Tracks': ()=>{viewTracks();},
-    'Time Period': {
-      value: settings.period||10,
+    File: { value: getTrackNumber(settings.file) },
+    "View Tracks": () => {
+      viewTracks();
+    },
+    "Time Period": {
+      value: settings.period || 10,
       min: 1,
       max: 120,
       step: 1,
-      format: v=>`${v}s`,
-      onchange: v => {
+      format: (v) => `${v}s`,
+      onchange: (v) => {
         settings.recording = false; // stop recording if we change anything
         settings.period = v;
         updateSettings();
-      }
-    }
+      },
+    },
   };
   const recorders = WIDGETS.recorder.getRecorders();
-  Object.keys(recorders).forEach(id=>{
+  Object.keys(recorders).forEach((id) => {
     mainmenu[`Log ${recorders[id]().name}`] = menuRecord(id);
   });
   delete recorders;
   return E.showMenu(mainmenu);
 }
 
-
-
 function viewTracks() {
   const menu = {
-    '': { 'title': 'Tracks' }
+    "": { title: "Tracks" },
   };
   let found = false;
-  require("Storage").list(/^clinikali\.log.*\.csv$/,{sf:true}).reverse().forEach(filename=>{
-    found = true;
-    menu[getTrackNumber(filename)] = ()=>viewTrack(filename,false);
-  });
-  if (!found)
-    menu["No Tracks found"] = ()=> {};
-  menu['< Back'] = () => { showMainMenu(); };
+  require("Storage")
+    .list(/^clinikali\.log.*\.csv$/, { sf: true })
+    .reverse()
+    .forEach((filename) => {
+      found = true;
+      menu[getTrackNumber(filename)] = () => viewTrack(filename, false);
+    });
+  if (!found) menu["No Tracks found"] = () => {};
+  menu["< Back"] = () => {
+    showMainMenu();
+  };
   return E.showMenu(menu);
 }
 
 function getTrackInfo(filename) {
-  "ram"
-    let starttime;
-    let duration=0;
-  const f = require("Storage").open(filename,"r");
-  if (f===undefined) return;
+  "ram";
+  let starttime;
+  let duration = 0;
+  const f = require("Storage").open(filename, "r");
+  if (f === undefined) return;
   let l = f.readLine(f);
   let fields;
   let timeIdx;
-  let nl = 0;
   let c;
-  if (l!==undefined) {
+  if (l !== undefined) {
     fields = l.trim().split(",");
     timeIdx = fields.indexOf("Time");
     l = f.readLine(f);
   }
-  if (l!==undefined) {
+  if (l !== undefined) {
     c = l.split(",");
     starttime = Number.parseInt(c[timeIdx]);
   }
-  // pushed this loop together to try and bump loading speed a little
-  while(l!==undefined) {
-    ++nl;c=l.split(",");l = f.readLine(f);
-      }
+
   if (c) duration = Number.parseInt(c[timeIdx]) - starttime;
   return {
-    fn : getTrackNumber(filename),
-    fields : fields,
-    filename : filename,
-    time : new Date(starttime*1000),
-    records : nl,
-    duration : Math.round(duration)
+    fn: getTrackNumber(filename),
+    fields: fields,
+    filename: filename,
+    time: new Date(starttime * 1000),
+    duration: Math.round(duration),
   };
 }
 
-function asTime(v){
-  const mins = Math.floor(v/60);
-  const secs = v-mins*60;
+function asTime(v) {
+  const mins = Math.floor(v / 60);
+  const secs = v - mins * 60;
   return `${mins.toString()}m ${secs.toString()}s`;
 }
 
 function viewTrack(filename, info) {
   if (!info) {
-    E.showMessage("Loading...",`Track ${getTrackNumber(filename)}`);
+    E.showMessage("Loading...", `Track ${getTrackNumber(filename)}`);
     info = getTrackInfo(filename);
   }
   //console.log(info);
   const menu = {
-    '': { 'title': `Track ${info.fn}` }
+    "": { title: `Track ${info.fn}` },
   };
   if (info.time)
-    menu[info.time.toISOString().substr(0,16).replace("T"," ")] = {};
-  menu.Duration = { value : asTime(info.duration)};
-  menu.Records = { value : `${info.records}` };
-    menu.Erase = () => {
+    menu[info.time.toISOString().substr(0, 16).replace("T", " ")] = {};
+  menu.Duration = { value: asTime(info.duration) };
+  menu.Records = { value: `${info.records}` };
+  menu.Erase = () => {
     E.showPrompt("Delete Track?").then((v) => {
       if (v) {
         settings.recording = false;
         updateSettings();
-        const f = require("Storage").open(filename,"r");
+        const f = require("Storage").open(filename, "r");
         f.erase();
         viewTracks();
-      } else
-        viewTrack(filename, info);
+      } else viewTrack(filename, info);
     });
   };
-  menu['< Back'] = () => { viewTracks(); };
+  menu["< Back"] = () => {
+    viewTracks();
+  };
   return E.showMenu(menu);
 }
 
--- apps/clinikali/settings.js
+++ apps/clinikali/settings.js
@@ -1,4 +1,4 @@
 (back) => {
-	// just go right to our app - we need all the memory
-	load("clinikali.app.js");
+  // just go right to our app - we need all the memory
+  load("clinikali.app.js");
 };
