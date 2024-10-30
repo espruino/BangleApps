8c0380f48e5198f23d8e75689b2b72088e95face
thyttan <6uuxstm66@mozmail.com>
2024-10-30 20:24:21 +0100

    gpsrec + gpssetup: tweak comments

--- apps/gpsrec/settings.js
+++ apps/gpsrec/settings.js
@@ -1,4 +1,4 @@
 (function(back) {
-  // just go right to our app - we need all the memory
+  // just go right to our app - we need all the memory */
   load("gpsrec.app.js");
-})() // FIXME: Since this is called directly, should it still have a semi-colon?
+})()
