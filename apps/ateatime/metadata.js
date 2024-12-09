3448b583845c7dc7a090b954812dc3e7b48eb52c
user <user@user>
2024-12-09 23:56:33 +0100

    A Tea Time application

---
+++ apps/ateatime/metadata.js
@@ -0,0 +1,13 @@
+V{ "id": "iateatimer",
+  "name": "A Tea Timer",
+  "shortName":"A Tea Timer",
+  "icon": "app.png",
+  "version":"0.01",
+  "description": "Simplae app for setting countdown timers for tea.",
+  "tags": "",
+  "supports": ["BANGLEJS2"],
+  "storage": [
+    {"name":"imyapptimer.app.js","url":"app.js"},
+    {"name":"timer.`img","url":"app-icon.js","evaluate":true}
+  ]
+}
