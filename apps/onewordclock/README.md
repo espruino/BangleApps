77444e575872eb78d3f0b2d86090e3fb3abb6e80
kkayam <korre.remix@gmail.com>
2025-01-22 15:21:02 +0000

    added oneword clock

---
+++ apps/onewordclock/README.md
@@ -0,0 +1,38 @@
+# Exact Words
+
+This is a clock for expressing the time in exact words. Each minute of 
+the day has a different phrase. 
+
+Ranging from "Twelve" to "Coming up to midnight" to "A little after 
+twenty-five past four in the early hours"
+
+Screenshots best demonstrate 
+
+![1200.png](1200.png)
+![2358.png](2358.png)
+![1616.png](1616.png)
+![0634.png](0634.png)
+![1517.png](1517.png)
+![2020.png](2020.png)
+
+
+"just gone " - as in Just gone quarter past four is 16:16
+
+"a little after ", as in A little after quarter past three is 15:17 
+
+"coming up to ", as in Coming up to midnight is 23:58
+
+"almost " as in Almost twenty-five to seven is 06:34
+
+## To Do
+
+Add localisation. 
+
+## Requests
+
+Written by: [Brendan Sleight](https://github.com/bmsleight/) For support and discussion please post in the Bangle JS Forum
+
+
+## Creator
+
+[Brendan Sleight](https://github.com/bmsleight/) 
---
+++ apps/onewordclock/app.js
@@ -0,0 +1,136 @@
+// timeout used to update every minute
+var drawTimeout;
+
+// https://www.espruino.com/Bangle.js+Locale
+
+// schedule a draw for the next minute
+function queueDraw() {
+  if (drawTimeout) clearTimeout(drawTimeout);
+  drawTimeout = setTimeout(function () {
+    drawTimeout = undefined;
+    draw();
+  }, 60000 - (Date.now() % 60000));
+}
+
+function wordFromHour(h) {
+  const HOUR_WORDS = [
+    "Midnight", "Early", "Quiet", "Still", "Dawn", "Earlybird",
+    "Sunrise", "Morning", "Bright", "Active", "Busy", "Pre-noon",
+    "Noon", "Post-noon", "Afternoon", "Siesta", "Breezy", "Evening",
+    "Twilight", "Dinner", "Cozy", "Relax", "Quietude", "Night"
+  ];
+
+  return HOUR_WORDS[h];
+}
+
+function wordsFromDayMonth(day, date, month) {
+  // Tests
+
+  // Example 12:00 = Twelve
+  //  New Year's Day
+  //  date = 1;
+  //  month = 0;
+  //  on the Ides of March
+  //  date = 15;
+  //  month = 2;
+  // , ERROR C Nonsense in BASIC
+  //  date = 1;
+  //  month = 3;
+  //  - O'Canada
+  //  date = 1;
+  //  month = 6;
+  //  -  on Halloween
+  //  date = 31;
+  //  month = 9;
+  //  -  Christmas Eve
+  //  date = 24;
+  //  month = 11;
+  //  -  Christmas Day
+  //  date = 25;
+  //  month = 11;
+  //  -  Boxing day
+  //  date = 26;
+  //  month = 11;
+  //  New Year's eve
+  //  date = 31;
+  //  month = 11;
+  //  longest
+  //  date = 29;
+  //  month = 10;
+
+
+  const DAY_WORD_ARRAY = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const DATE_WORD_ARRAY = ["zero", "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth", "thirteenth", "fourteenth", "fifteenth", "sixteenth", "seventeenth", "eighteenth", "nineteenth", "twentieth", "twenty-first", "twenty-second", "twenty-third", "twenty-fourth", "twenty-fifth", "twenty-sixth", "twenty-seventh", "twenty-eighth", "twenty-ninth", "thirtieth", "thirty-first"];
+  const MONTH_WORD_ARRAY = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var words = " ";
+  words = DAY_WORD_ARRAY[day] + ", " + DATE_WORD_ARRAY[date] + " of " + MONTH_WORD_ARRAY[month];
+  if ((date == 1) && (month == 0)) {
+    words = "New Year's Day";
+  } else if ((date == 15) && (month == 2)) {
+    words = DAY_WORD_ARRAY[day] + " on the Ides of March";
+  } else if ((date == 1) && (month == 3)) {
+    words = DAY_WORD_ARRAY[day] + ", ERROR C Nonsense in BASIC";
+  } else if ((date == 1) && (month == 6)) {
+    words = DAY_WORD_ARRAY[day] + "  - O'Canada";
+  } else if ((date == 31) && (month == 9)) {
+    words = DAY_WORD_ARRAY[day] + " - on Halloween";
+  } else if ((date == 24) && (month == 11)) {
+    words = "Christmas Eve";
+  } else if ((date == 25) && (month == 11)) {
+    words = "Christmas Day";
+  } else if ((date == 26) && (month == 11)) {
+    words = "Boxing Day";
+  } else if ((date == 31) && (month == 11)) {
+    words = "New Year's eve";
+  }
+  return words;
+}
+
+function draw() {
+  var x = g.getWidth() / 2;
+  var y = g.getHeight() / 2;
+  g.reset();
+
+  var d = new Date();
+  var h = d.getHours();
+  var date = d.getDate();
+  var day = d.getDay();
+  var month = d.getMonth();
+
+  var timeStr = wordFromHour(h);
+  var dateStr = wordsFromDayMonth(day, date, month);
+
+  // draw time
+  g.setBgColor(g.theme.bg);
+  g.setColor(g.theme.fg);
+  g.clear();
+  g.setFontAlign(0, 0).setFont("Vector", 36);  // Made font bigger since we're showing one word
+  g.drawString(timeStr, x, y - 24);
+
+  // draw date
+  g.setFontAlign(0, 0).setFont("Vector", 12);
+  g.drawString(g.wrapString(dateStr, g.getWidth()).join("\n"), x, y + 24);
+
+  // queue draw in one minute
+  queueDraw();
+}
+
+// Clear the screen once/, at startup
+g.clear();
+// draw immediately at first, queue update
+draw();
+// Stop updates when LCD is off, restart when on
+Bangle.on('lcdPower', on => {
+  if (on) {
+    draw(); // draw immediately, queue redraw
+  } else { // stop draw timer
+    if (drawTimeout) clearTimeout(drawTimeout);
+    drawTimeout = undefined;
+  }
+});
+
+// Show launcher when middle button pressed
+Bangle.setUI("clock");
+// Load widgets
+Bangle.loadWidgets();
+Bangle.drawWidgets();
