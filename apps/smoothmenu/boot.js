f4722659242fe955a62a639d77717c004312bbd5
kkayam <korre.remix@gmail.com>
2025-01-23 17:07:11 +0000

    original menu code

--- apps/smoothmenu/boot.js
+++ apps/smoothmenu/boot.js
@@ -1,199 +1,150 @@
-E.showMenu = function (items) {
-    var ar = Bangle.appRect;
-    Bangle.removeAllListeners("drag");
-    if (!items) {
-        g.clearRect(ar.x, ar.y, ar.x2, ar.y2);
-        return false;
+E.showMenu = (function (menu) {
+    const H = 40;
+    if (menu === undefined) {
+        g.clearRect(Bangle.appRect);
+        return Bangle.setUI();
     }
-    var loc = require("locale");
-    const ITEM_HEIGHT = 48;
-    var m = {
-        info: {
-            cB: g.theme.bg,
-            cF: g.theme.fg,
-            cHB: g.theme.bgH,
-            cHF: g.theme.fgH,
-            cAB: g.theme.bg2,
-            cAF: g.theme.fg2,
-            predraw: () => { },
-            preflip: () => { }
-        },
-        scroll: 0,
-        items: [],
-        selected: -1,
-        draw: () => {
-            g.reset().setFont('12x20');
-            m.info.predraw(g);
-            g.setColor(m.info.cB).fillRect(ar.x, ar.y, ar.x2, ar.y2).setColor(m.info.cF);
-            m.items.forEach((e, i) => {
-                const s = (i * ITEM_HEIGHT) - m.scroll + ar.y;
-                if (s + ITEM_HEIGHT < ar.y || s > ar.y2 - 10) {
-                    return false;
-                }
-                if (i == m.selected) {
-                    g.setColor(m.info.cHB).fillRect(ar.x, s, ar.x2, Math.min(s + ITEM_HEIGHT, ar.y2)).setColor(m.info.cHF);
-                } else {
-                    g.setColor(m.info.cF);
-                }
-                g.setFontAlign(0, -1, 0);
-                if (e.icon) {
-                    g.drawString(e.title, ar.x + ((ar.x2 - ar.x) / 2) + 10, s + 5);
-                    g.drawImage(e.icon, ar.x + ((ar.x2 - ar.x) / 2) - g.stringWidth(e.title) / 2 - 15, s + 5);
-                } else {
-                    g.drawString(e.title, ar.x + ((ar.x2 - ar.x) / 2), s + 5);
-                }
-                if (e.type && s < ar.y2 - 10) {
-                    if (e.format) {
-                        g.setFontAlign(0, -1, 0).drawString(e.format(e.value), ar.x + ((ar.x2 - ar.x) / 2), s + 25);
-                    } else {
-                        g.setFontAlign(0, -1, 0).drawString(e.value, ar.x + ((ar.x2 - ar.x) / 2), s + 25);
-                    }
+    var menuIcon = "\0\f\f\x81\0\xFF\xFF\xFF\0\0\0\0\x0F\xFF\xFF\xF0\0\0\0\0\xFF\xFF\xFF";
+    var options = menu[""] || {};
+    if (!options.title) options.title = "Menu";
+    var back = options.back || menu["< Back"];
+    var keys = Object.keys(menu).filter(k => k !== "" && k != "< Back");
+    keys.forEach(k => {
+        var item = menu[k];
+        if ("object" != typeof item) return;
+        if ("boolean" == typeof item.value &&
+            !item.format)
+            item.format = v => "\0" + atob(v ? "EhKBAH//v/////////////5//x//j//H+eP+Mf/A//h//z//////////3//g" : "EhKBAH//v//8AA8AA8AA8AA8AA8AA8AA8AA8AA8AA8AA8AA8AA8AA///3//g");
+    });
+    // Submenu for editing menu options...
+    function showSubMenu(item, title) {
+        /*if ("number"!=typeof item.value)
+          return console.log("Unhandled item type");*/
+        var step = item.step || 1;
+        if (!item.noList && item.min !== undefined && item.max !== undefined &&
+            ((item.max - item.min) / step) < 20) {
+            // show scrolling menu of options
+            E.showScroller({
+                h: H, c: (item.max + step - item.min) / step,
+                back: show, // redraw original menu
+                remove: options.remove,
+                draw: (idx, r) => {
+                    g.setColor(g.theme.bg2).fillRect({ x: r.x + 4, y: r.y + 2, w: r.w - 8, h: r.h - 4, r: 5 });
+                    var v = idx * step + item.min;
+                    g.setColor(g.theme.fg2).setFont("12x20").setFontAlign(-1, 0).drawString((item.format) ? item.format(v, 1) : v, r.x + 12, r.y + H / 2);
+                    g.drawImage(/* 20x20 */atob(v == item.value ? "FBSBAAH4AH/gHgeDgBww8MY/xmf+bH/jz/88//PP/zz/88f+Nn/mY/xjDww4AcHgeAf+AB+A" : "FBSBAAH4AH/gHgeDgBwwAMYABmAAbAADwAA8AAPAADwAA8AANgAGYABjAAw4AcHgeAf+AB+A"), r.x + r.w - 32, r.y + H / 2 - 10);
+                },
+                select: function (idx) {
+                    Bangle.buzz(20);
+                    item.value = item.min + idx * step;
+                    if (item.onchange) item.onchange(item.value);
+                    scr.scroll = l.scroller.scroll; // set scroll to prev position
+                    show(); // redraw original menu
                 }
             });
-            m.info.preflip(g, m.scroll > 0, m.scroll < (m.items.length - 1) * ITEM_HEIGHT);
-        },
-        select: (x, y) => {
-            if (m.selected == -1 || m.selected !== Math.max(Math.min(Math.floor((y + m.scroll - ar.y) / ITEM_HEIGHT), m.items.length - 1), 0)) {
-                if (y) {
-                    if (y < ar.y || y > ar.y2) {
-                        return false;
-                    } else {
-                        m.selected = Math.max(Math.min(Math.floor((y + m.scroll - ar.y) / ITEM_HEIGHT), m.items.length - 1), 0);
-                    }
-                } else {
-                    m.selected = Math.floor(m.scroll / ITEM_HEIGHT);
-                }
-                m.draw();
-            } else {
-                if (m.items[m.selected].type && m.items[m.selected].type === "boolean") {
-                    m.items[m.selected].value = !m.items[m.selected].value;
-                    m.items[m.selected].onchange(m.items[m.selected].value);
-                    m.draw();
-                } else if (m.items[m.selected].type && m.items[m.selected].type === "number") {
-                    if (x && x < ((ar.x + ar.x2) / 2)) {
-                        m.items[m.selected].value = m.items[m.selected].value - (m.items[m.selected].step ? m.items[m.selected].step : 1);
-                    } else {
-                        m.items[m.selected].value = m.items[m.selected].value + (m.items[m.selected].step ? m.items[m.selected].step : 1);
-                    }
-                    if (m.items[m.selected].value > (m.items[m.selected].max ? m.items[m.selected].max : Infinity)) {
-                        m.items[m.selected].value = m.items[m.selected].min ? m.items[m.selected].min : 0;
-                    }
-                    if (m.items[m.selected].value < (m.items[m.selected].min ? m.items[m.selected].min : 0)) {
-                        m.items[m.selected].value = m.items[m.selected].max ? m.items[m.selected].max : 10;
-                    }
-                    m.items[m.selected].onchange(m.items[m.selected].value);
-                    m.draw();
-                } else {
-                    if (m.items[m.selected]) {
-                        m.items[m.selected]();
-                    }
+        } else {
+            // show simple box for scroll up/down
+            var R = Bangle.appRect;
+            var v = item.value;
+            g.reset().clearRect(R);
+            g.setFont("12x20").setFontAlign(0, 0).drawString(
+                menuIcon + " " + title, R.x + R.w / 2, R.y + 12);
+
+            function draw() {
+                var mx = R.x + R.w / 2, my = 12 + R.y + R.h / 2, txt = item.format ? item.format(v, 2) : v, s = 30;
+                g.reset().setColor(g.theme.bg2).fillRect({ x: R.x + 24, y: R.y + 36, w: R.w - 48, h: R.h - 48, r: 5 });
+                g.setColor(g.theme.fg2).setFontVector(Math.min(30, (R.w - 52) * 100 / g.setFontVector(100).stringWidth(txt))).setFontAlign(0, 0).drawString(txt, mx, my);
+                g.fillPoly([mx, my - 45, mx + 15, my - 30, mx - 15, my - 30]).fillPoly([mx, my + 45, mx + 15, my + 30, mx - 15, my + 30]);
+            }
+            function cb(dir) {
+                if (dir) {
+                    v -= (dir || 1) * (item.step || 1);
+                    if (item.min !== undefined && v < item.min) v = item.wrap ? item.max : item.min;
+                    if (item.max !== undefined && v > item.max) v = item.wrap ? item.min : item.max;
+                    draw();
+                } else { // actually selected
+                    item.value = v;
+                    if (item.onchange) item.onchange(item.value);
+                    scr.scroll = l.scroller.scroll; // set scroll to prev position
+                    show(); // redraw original menu
                 }
             }
+            draw();
+            var dy = 0;
+            Bangle.setUI({
+                mode: "custom",
+                back: show,
+                remove: options.remove,
+                redraw: draw,
+                drag: e => {
+                    dy += e.dy; // after a certain amount of dragging up/down fire cb
+                    if (!e.b) dy = 0;
+                    while (Math.abs(dy) > 32) {
+                        if (dy > 0) { dy -= 32; cb(1); }
+                        else { dy += 32; cb(-1); }
+                        Bangle.buzz(20);
+            }
+                },
+                touch: (_, e) => {
+                    Bangle.buzz(20);
+                    if (e.y < 82) cb(-1); // top third
+                    else if (e.y > 142) cb(1); // bottom third
+                    else cb(); // middle = accept
+                }
+            });
+        }
+    }
+    var l = {
+        draw: () => l.scroller.draw(),
+        scroller: undefined
+    };
+    var scr = {
+        h: H, c: keys.length,
+        scroll: options.scroll ?? 0,
+        back: back,
+        remove: options.remove,
+        draw: (idx, r) => {
+            g.setColor(g.theme.bg2).fillRect({ x: r.x + 4, y: r.y + 2, w: r.w - 8, h: r.h - 4, r: 5 });
+            g.setColor(g.theme.fg2).setFont("12x20");
+            var pad = 24;
+            var item = menu[keys[idx]];
+            if ("object" == typeof item) {
+                var v = item.value;
+                if (item.format) v = item.format(v);
+                if (g.stringMetrics(v).width > r.w / 2) // bodge for broken wrapString with image
+                    v = g.wrapString(v, r.w / 2).join("\n");
+                g.setFontAlign(1, 0).drawString(v, r.x + r.w - 8, r.y + H / 2);
+                pad += g.stringWidth(v);
+            } else if ("function" == typeof item) {
+                g.drawImage(/* 9x18 */atob("CRKBAGA4Hg8DwPB4HgcDg8PB4eHg8HAwAA=="), r.x + r.w - 21, r.y + H / 2 - 9);
+                pad += 16;
+            }
+            var title = (item && item.title) ?? keys[idx];
+            var l = g.wrapString(title, r.w - pad);
+            if (l.length > 1)
+                l = g.setFont("6x15").wrapString(title, r.w - pad);
+            g.setFontAlign(-1, 0).drawString(l.join("\n"), r.x + 12, r.y + H / 2);
         },
-        move: d => {
-            m.scroll += (d * ITEM_HEIGHT);
-            m.scroll = Math.min(Math.max(m.scroll, 0), (m.items.length - 1) * ITEM_HEIGHT);
-            m.selected = Math.max(Math.min(Math.floor((m.scroll - ar.y) / ITEM_HEIGHT), m.items.length - 1), 0);
-            m.draw();
+        select: function (idx, touch) {
+            var item = menu[keys[idx]];
+            Bangle.buzz(20);
+            if ("function" == typeof item) item(touch);
+            else if ("object" == typeof item) {
+                if ("number" == typeof item.value) {
+                    showSubMenu(item, keys[idx]);
+                } else {
+                    // if a bool, just toggle it
+                    if ("boolean" == typeof item.value)
+                        item.value = !item.value;
+                    if (item.onchange) item.onchange(item.value, touch);
+                    if (l.scroller.isActive()) l.scroller.drawItem(idx);
+                }
+            }
         }
     };
-    Object.keys(items).forEach(i => {
-        if (i == "") {
-            m.info = Object.assign(m.info, items[i]);
-        } else if (i === "< Back" && items[i]) {
-            m.back = items[i];
-        } else if (items[i]) {
-            m.items.push(items[i]);
-            m.items[m.items.length - 1].title = loc.translate(i);
-            if (items[i].hasOwnProperty("value")) {
-                if (typeof items[i].value === "boolean") {
-                    m.items[m.items.length - 1].type = "boolean";
-                } else {
-                    m.items[m.items.length - 1].type = "number";
-                }
-            }
-        }
-    });
-    m.draw();
-
-    // Add BTN1 handler for back functionality
-    if (m.back) {
-        if (Bangle.backHandler) clearWatch(Bangle.backHandler);
-        Bangle.backHandler = setWatch(m.back, BTN1, { debounce: 100 });
+    function show() {
+        l.scroller = E.showScroller(scr);
     }
-
-    Bangle.on("drag", d => {
-        if (!d.b) return false;
-        if (d.dx == 0 && d.dy == 0) {
-            m.select(d.x, d.y);
-        } else {
-            m.selected = -1;
-            m.scroll -= d.dy;
-            m.scroll = Math.min(Math.max(m.scroll, 0), (m.items.length - 1) * ITEM_HEIGHT);
-            m.draw();
-        }
-    });
-    return m;
-};
-
-E.showAlert = function (e, t) {
-    if (!e) {
-        E.showMenu();
-        return false;
-    }
-    return new Promise(r => {
-        const menu = {
-            "": {
-                "title": (t ? t : "Alert")
-            },
-            Ok: () => {
-                E.showMenu();
-                r();
-            }
-        };
-        menu[e] = () => { };
-        E.showMenu(menu);
-    });
-};
-E.showMessage = E.showAlert;
-
-E.showPrompt = function (e, t) {
-    if (!e) {
-        E.showMenu();
-        return false;
-    }
-    return new Promise(r => {
-        const menu = {
-            "": {
-                "title": (t && t.title ? t.title : "Choose")
-            }
-        };
-        menu[e] = () => { };
-        if (t && t.buttons) {
-            Object.keys(t.buttons).forEach(b => {
-                menu[b] = () => {
-                    E.showMenu();
-                    r(t.buttons[b]);
-                };
-            });
-        } else {
-            menu.Yes = () => {
-                E.showMenu();
-                r(true);
-            };
-            menu.No = () => {
-                E.showMenu();
-                r(false);
-            };
-        }
-        E.showMenu(menu);
-    });
-};
-
-const bsl = Bangle.showLauncher;
-Bangle.showLauncher = function () {
-    Bangle.removeAllListeners("drag");
-    if (Bangle.backHandler) clearWatch(Bangle.backHandler);
-    delete Bangle.backHandler;
-    bsl();
-};
+    show();
+    return l;
+});
