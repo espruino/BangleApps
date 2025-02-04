06b2e3e52f441b1dca201071c49967c583dc9ad3
David Volovskiy <devolov@gmail.com>
2025-02-03 19:32:49 -0500

    Added Detroit and Raleigh to Location

--- apps/mylocation/settings.js
+++ apps/mylocation/settings.js
@@ -5,9 +5,9 @@ let settings;
 
 // initialize with default settings...
 let s = {
-  'lat': 51.5072,
-  'lon': 0.1276,
-  'location': "London"
+  'lat': 35.7796,
+  'lon': -78.6382,
+  'location': "Raleigh"
 };
 
 function loadSettings() {
@@ -22,9 +22,9 @@ function saveSettings() {
   require('Storage').write(SETTINGS_FILE, settings);
 }
 
-const locations = ["London" ,"Newcastle","Edinburgh", "Paris" , "New York" , "Tokyo"  , "Frankfurt", "Auckland", "???"];
-const lats =      [ 51.5072 ,  54.9783  , 55.9533   , 48.8566 ,  40.7128   ,  35.6762 ,  50.1236   ,  -36.9    ,  0.0 ];
-const lons =      [ -0.1276 ,  -1.6178  , -3.1883   ,  2.3522 , -74.0060   , 139.6503 ,   8.6553   ,  174.7832 ,  0.0 ];
+const locations = ["Raleigh", "Detroit", "London" ,"Newcastle","Edinburgh", "Paris" , "New York" , "Tokyo"  , "Frankfurt", "Auckland", "???"];
+const lats =      [ 35.7796, 42.3314, 51.5072 ,  54.9783  , 55.9533   , 48.8566 ,  40.7128   ,  35.6762 ,  50.1236   ,  -36.9    ,  0.0 ];
+const lons =      [ -78.6382, -83.0458, -0.1276 ,  -1.6178  , -3.1883   ,  2.3522 , -74.0060   , 139.6503 ,   8.6553   ,  174.7832 ,  0.0 ];
 
 function setFromGPS() {
   Bangle.on('GPS', (gps) => {
