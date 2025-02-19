b0bdd78549aa4db850d01ddd28e11ba037c11665
Travis Evans <travisgevans@gmail.com>
2025-02-18 19:50:20 -0600

    Try to restore timer snooze functionality

--- apps/tevtimer/alarm.js
+++ apps/tevtimer/alarm.js
@@ -1,9 +1,19 @@
 const tt = require('tevtimer');
 
 function showAlarm(alarm) {
+  const alarmIndex = alarms.indexOf(alarm);
   const settings = require("sched").getSettings();
   const timer = tt.TIMERS[alarm.data.idx];
-  const message =  timer.display_name() + '\n' + alarm.msg;
+  let message = timer.display_name() + '\n' + alarm.msg;
+
+  if (alarm.msg) {
+    message += "\n" + alarm.msg;
+  } else {
+    message = (alarm.timer
+      ? atob("ACQswgD//33vRcGHIQAAABVVVAAAAAAAABVVVAAAAAAAABVVVAAAAAAAABVVVAAAAAAAABVVVAAAAAAAABVVVAAAAAAAAAP/wAAAAAAAAAP/wAAAAAAAAAqqoAPAAAAAAqqqqoP8AAAAKqqqqqv/AAACqqqqqqq/wAAKqqqlWqqvwAAqqqqlVaqrAACqqqqlVVqqAAKqqqqlVVaqgAKqaqqlVVWqgAqpWqqlVVVqoAqlWqqlVVVaoCqlV6qlVVVaqCqVVfqlVVVWqCqVVf6lVVVWqKpVVX/lVVVVqqpVVV/+VVVVqqpVVV//lVVVqqpVVVfr1VVVqqpVVVfr1VVVqqpVVVb/lVVVqqpVVVW+VVVVqqpVVVVVVVVVqiqVVVVVVVVWqCqVVVVVVVVWqCqlVVVVVVVaqAqlVVVVVVVaoAqpVVVVVVVqoAKqVVVVVVWqgAKqlVVVVVaqgACqpVVVVVqqAAAqqlVVVaqoAAAKqqVVWqqgAAACqqqqqqqAAAAAKqqqqqgAAAAAAqqqqoAAAAAAAAqqoAAAAA==")
+      : atob("AC0swgF97///RcEpMlVVVVVVf9VVVVVVVVX/9VVf9VVf/1VVV///1Vf9VX///VVX///VWqqlV///1Vf//9aqqqqpf//9V///2qqqqqqn///V///6qqqqqqr///X//+qqoAAKqqv//3//6qoAAAAKqr//3//qqAAAAAAqq//3/+qoAADwAAKqv/3/+qgAADwAACqv/3/aqAAADwAAAqp/19qoAAADwAAAKqfV1qgAAADwAAACqXVWqgAAADwAAACqlVWqAAAADwAAAAqlVWqAAAADwAAAAqlVWqAAAADwAAAAqlVaoAAAADwAAAAKpVaoAAAADwAAAAKpVaoAAAADwAAAAKpVaoAAAAOsAAAAKpVaoAAAAOsAAAAKpVaoAAAAL/AAAAKpVaoAAAAgPwAAAKpVaoAAACAD8AAAKpVWqAAAIAA/AAAqlVWqAAAgAAPwAAqlVWqAACAAADwAAqlVWqgAIAAAAAACqlVVqgAgAAAAAACqVVVqoAAAAAAAAKqVVVaqAAAAAAAAqpVVVWqgAAAAAACqlVVVWqoAAAAAAKqlVVVVqqAAAAAAqqVVVVVaqoAAAAKqpVVVVVeqqoAAKqqtVVVVV/6qqqqqqr/VVVVX/2qqqqqqn/1VVVf/VaqqqqpV/9VVVf9VVWqqlVVf9VVVf1VVVVVVVVX9VQ==")
+    ) + " " + message
+  }
 
   Bangle.loadWidgets();
   Bangle.drawWidgets();
@@ -12,11 +22,44 @@ function showAlarm(alarm) {
   let buzzCount = timer.buzz_count - 1;
 
   E.showPrompt(message, {
-    title: 'Timer',
-    buttons: { "OK": true }
-  }).then(function (go) {
+    title: 'tev timer',
+    buttons: { "Snooze": true, "Stop": false } // default is sleep so it'll come back in some mins
+  }).then(function (sleep) {
     buzzCount = 0;
-    Bangle.emit("alarmDismiss", alarm);
+
+    if (sleep) {
+      if (alarm.ot === undefined) {
+        alarm.ot = alarm.t;
+      }
+      let time = new Date();
+      let currentTime = (time.getHours()*3600000)+(time.getMinutes()*60000)+(time.getSeconds()*1000);
+      alarm.t = currentTime + settings.defaultSnoozeMillis;
+      alarm.t %= 86400000;
+      Bangle.emit("alarmSnooze", alarm);
+    } else {
+      let del = alarm.del === undefined ? settings.defaultDeleteExpiredTimers : alarm.del;
+      if (del) {
+        alarms.splice(alarmIndex, 1);
+      } else {
+        if (alarm.date && alarm.rp) {
+          setNextRepeatDate(alarm);
+        } else if (!alarm.timer) {
+          alarm.last = new Date().getDate();
+        }
+        if (alarm.ot !== undefined) {
+          alarm.t = alarm.ot;
+          delete alarm.ot;
+        }
+        if (!alarm.rp) {
+          alarm.on = false;
+        }
+      }
+      Bangle.emit("alarmDismiss", alarm);
+    }
+
+    // The updated alarm is still a member of 'alarms'
+    // so writing to array writes changes back directly
+    require("sched").setAlarms(alarms);
     load();
   });
 
