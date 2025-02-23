9e05058a3d2e6965e793859a8133ebf85f83f62d
Martin Zwigl <martin@zwigl.info>
2025-02-23 21:37:48 +0100

    :sparkles: countdown app to show something

---
+++ apps/coin_info/app.js
@@ -0,0 +1,22 @@
+{
+    Bangle.loadWidgets();
+    Bangle.drawWidgets();
+    
+    function countDown() {
+        counter--;
+
+        g.clear();
+        // draw the current counter value
+        g.drawString(counter, g.getWidth()/2, g.getHeight()/2);
+        // optional - this keeps the watch LCD lit up
+        Bangle.setLCDPower(1);
+    }
+
+    // call countDown every second
+    var interval = setInterval(countDown, 1000);
+    //
+    Bangle.setUI({
+        mode : 'custom',
+        back : Bangle.showClock,
+    });
+}
