dbb26cfee4dd3ca372f5474ce095aca952396432
thyttan <6uuxstm66@mozmail.com>
2025-02-26 20:20:11 +0100

    messagegui: refactor `global` -> `globalThis`
    [global is used in Node.js. Consider using the identical globalThis as it was introduced in the ECMAScript spec.](https://www.espruino.com/ReferenceBANGLEJS2#l__global_global)

--- apps/messagegui/app.js
+++ apps/messagegui/app.js
@@ -504,10 +504,10 @@ function checkMessages(options) {
     delete newMessages[0].show; // stop us getting stuck here if we're called a second time
     showMessage(newMessages[0].id, false);
     // buzz after showMessage, so being busy during layout doesn't affect the buzz pattern
-    if (global.BUZZ_ON_NEW_MESSAGE) {
+    if (globalThis.BUZZ_ON_NEW_MESSAGE) {
       // this is set if we entered the messages app by loading `messagegui.new.js`
       // ... but only buzz the first time we view a new message
-      global.BUZZ_ON_NEW_MESSAGE = false;
+      globalThis.BUZZ_ON_NEW_MESSAGE = false;
       // messages.buzz respects quiet mode - no need to check here
       require("messages").buzz(newMessages[0].src);
     }
