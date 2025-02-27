1cda9a71a0fddc2fcd1050adaf42aa60a88f89b9
Martin Zwigl <martin@zwigl.info>
2025-02-28 00:57:47 +0100

    :construction: improve clk-info

--- apps/coin_info/clkinfo.js
+++ apps/coin_info/clkinfo.js
@@ -2,35 +2,30 @@
     const SETTINGS_FILE = "coin_info.settings.json";
     const settings = require("Storage").readJSON(SETTINGS_FILE,1) || {};
 
-    function debug(...args) {
-        console.log("[CoinInfo]", ...args);
-    }
-
     function retrieveClkInfo(token) {
         // TODO: do something useful here -> http request to CMC
-        debug("Retrieving:", token);
+        console.log("Retrieving:", token);
         return {
             text : token,
             // color: "#f00",
             img : atob("MDCBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/AAAAAA//8AAAAB///AAAAB8A/gAAAAgAH4AAAwAAB8AAB4AAA+AADwA4APAADgA4APAAHgA4AHgADAf/ADwAAAf/gBwAAAf/wB4AAAcB4A4AAAcA4A4AAAcA4A4AYAcA4AcA8AcB4AcB+Af/wDdj/Af/4D/n/Af/8D/n/AcAcB/A4AcAOA+A4AcAOAcAcAcAOAAAcAcAAAAAcAcAAAAAeAf/AAAAOAf/AAAAPAf/ADAAHgA4AHgADwA4AHAADwA4APAAB8AAA+AAA+AAB8AAAfgAH4AAAH8A/gAAAD///AAAAA//8AAAAAD/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==")
-            v: 0
         }
     }
-    
+
     function showClkInfo() {
-        debug("Show called");
+        console.log("Show called");
         const self = this;
         this.interval = setTimeout(() => {
             self.emit("redraw");
             self.interval = setInterval(() => {
-                debug("Interval refresh");
+                console.log("Interval refresh");
                 self.emit("redraw");
             }, 60000);
         }, 60000 - (Date.now() % 60000));
     }
 
     function hideClkInfo() {
-        debug("Hide called");
+        console.log("Hide called");
         if (this.interval) {
             clearInterval(this.interval);
             this.interval = null;
@@ -38,7 +33,7 @@
     }
 
     function createClkInfoItems() {
-        debug("Creating items from:", settings.tokenSelected);
+        console.log("Creating items from:", settings.tokenSelected);
         return (settings.tokenSelected || []).map(token => ({
             name: token,
             get: () => retrieveClkInfo(token),
@@ -52,4 +47,4 @@
         name: "CoinInfo",
         items: createClkInfoItems
     };
-})();
+});
