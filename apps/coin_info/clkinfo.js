ef6c04b1cca90125d3e41381be4a70231c076412
Martin Zwigl <martin@zwigl.info>
2025-03-01 00:06:32 +0100

    :construction: at least make icon a constant

--- apps/coin_info/clkinfo.js
+++ apps/coin_info/clkinfo.js
@@ -1,15 +1,6 @@
-const COIN_ICON = atob("MDCBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/AAAAAA//8AAAAB///AAAAB8A/gAAAAgAH4AAAwAAB8AAB4AAA+AADwA4APAADgA4APAAHgA4AHgADAf/ADwAAAf/gBwAAAf/wB4AAAcB4A4AAAcA4A4AAAcA4A4AYAcA4AcA8AcB4AcB+Af/wDdj/Af/4D/n/Af/8D/n/AcAcB/A4AcAOA+A4AcAOAcAcAcAOAAAcAcAAAAAcAcAAAAAeAf/AAAAOAf/AAAAPAf/ADAAHgA4AHgADwA4AHAADwA4APAAB8AAA+AAA+AAB8AAAfgAH4AAAH8A/gAAAD///AAAAA//8AAAAAD/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="); // (full base64 string here)
-
-function getTokenInfo(token) {
-    return function() {
-        return {
-            text: token,
-            img: COIN_ICON
-        };
-    };
-}
-
 (function() {
+    const COIN_ICON = atob("MDCBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/AAAAAA//8AAAAB///AAAAB8A/gAAAAgAH4AAAwAAB8AAB4AAA+AADwA4APAADgA4APAAHgA4AHgADAf/ADwAAAf/gBwAAAf/wB4AAAcB4A4AAAcA4A4AAAcA4A4AYAcA4AcA8AcB4AcB+Af/wDdj/Af/4D/n/Af/8D/n/AcAcB/A4AcAOA+A4AcAOAcAcAcAOAAAcAcAAAAAcAcAAAAAeAf/AAAAOAf/AAAAPAf/ADAAHgA4AHgADwA4AHAADwA4APAAB8AAA+AAA+AAB8AAAfgAH4AAAH8A/gAAAD///AAAAA//8AAAAAD/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="); // (full base64 string here)
+
     const settings = require("Storage").readJSON("coin_info.settings.json",1)||{};
     if (!(settings.tokenSelected instanceof Array))
         settings.tokenSelected = [];
@@ -17,7 +8,13 @@ function getTokenInfo(token) {
         name: "CoinInfo",
         items: settings.tokenSelected.map(token => {
             return { name : token,
-                get : getTokenInfo(token),
+                get : function()
+                {
+                    return {
+                        text : token,
+                        img : COIN_ICON
+                    }
+                },
                 show : function() {
                     var self = this;
                     // Set timeout to align to the next minute
@@ -38,4 +35,4 @@ function getTokenInfo(token) {
             }
         })
     };
-})()
+})
