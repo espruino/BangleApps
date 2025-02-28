9330250ed977a795236bf7f851218efc70f47c6b
Martin Zwigl <martin@zwigl.info>
2025-02-28 23:57:27 +0100

    :construction: extract getting info for token

--- apps/coin_info/clkinfo.js
+++ apps/coin_info/clkinfo.js
@@ -1,3 +1,14 @@
+const COIN_ICON = atob("MDCBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/AAAAAA//8AAAAB///AAAAB8A/gAAAAgAH4AAAwAAB8AAB4AAA+AADwA4APAADgA4APAAHgA4AHgADAf/ADwAAAf/gBwAAAf/wB4AAAcB4A4AAAcA4A4AAAcA4A4AYAcA4AcA8AcB4AcB+Af/wDdj/Af/4D/n/Af/8D/n/AcAcB/A4AcAOA+A4AcAOAcAcAcAOAAAcAcAAAAAcAcAAAAAeAf/AAAAOAf/AAAAPAf/ADAAHgA4AHgADwA4AHAADwA4APAAB8AAA+AAA+AAB8AAAfgAH4AAAH8A/gAAAD///AAAAA//8AAAAAD/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="); // (full base64 string here)
+
+function getTokenInfo(token) {
+    return function() {
+        return {
+            text: token,
+            img: COIN_ICON
+        };
+    };
+}
+
 (function() {
     const settings = require("Storage").readJSON("coin_info.settings.json",1)||{};
     if (!(settings.tokenSelected instanceof Array))
@@ -6,13 +17,7 @@
         name: "CoinInfo",
         items: settings.tokenSelected.map(token => {
             return { name : token,
-                get : function()
-                {
-                    return {
-                        text : token,
-                        img : atob("MDCBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/AAAAAA//8AAAAB///AAAAB8A/gAAAAgAH4AAAwAAB8AAB4AAA+AADwA4APAADgA4APAAHgA4AHgADAf/ADwAAAf/gBwAAAf/wB4AAAcB4A4AAAcA4A4AAAcA4A4AYAcA4AcA8AcB4AcB+Af/wDdj/Af/4D/n/Af/8D/n/AcAcB/A4AcAOA+A4AcAOAcAcAcAOAAAcAcAAAAAcAcAAAAAeAf/AAAAOAf/AAAAPAf/ADAAHgA4AHgADwA4AHAADwA4APAAB8AAA+AAA+AAB8AAAfgAH4AAAH8A/gAAAD///AAAAA//8AAAAAD/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==")
-                    }
-                },
+                get : getTokenInfo(token),
                 show : function() {
                     var self = this;
                     // Set timeout to align to the next minute
@@ -33,4 +38,4 @@
             }
         })
     };
-})
+})()
