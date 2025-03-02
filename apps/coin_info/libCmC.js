5ebc00f311fd6fce4722bcdcc6d94e833d8fbaa3
Martin Zwigl <martin@zwigl.info>
2025-03-02 20:07:24 +0100

    :construction: stub for CoinmarketCap lib

---
+++ apps/coin_info/libCmC.js
@@ -0,0 +1,22 @@
+exports.getQuoteLatest = function(slug) {
+    // Bangle.http("https://pur3.co.uk/hello.txt").then(data=>{
+    //     console.log("Got ",data);
+    //     // actual response is in data.resp
+    // });
+    // -------------
+    // Prints:
+    //Got  {
+    //  "t": "http",
+    //  "id": "89875941444",
+    //  "resp": "Hello World!\n"
+    // }
+
+    // return Bangle.http(serverUrl, {
+    //     method: 'GET',
+    //     headers: {
+    //         'Authorization': `Basic ${credentials}`
+    //     }
+    // });
+
+    return { result: "yo"};
+}
