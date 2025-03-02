536aeb510d788d36f66c189cd377b17eb75dcdc8
Martin Zwigl <martin@zwigl.info>
2025-03-02 23:22:17 +0100

    :construction: back to basic

--- apps/coin_info/clkinfo.js
+++ apps/coin_info/clkinfo.js
@@ -1,52 +1,32 @@
-(function() { // IIFE wrapper required
-    const COIN_ICON = atob("MDCBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/AAAAAA//8AAAAB///AAAAB8A/gAAAAgAH4AAAwAAB8AAB4AAA+AADwA4APAADgA4APAAHgA4AHgADAf/ADwAAAf/gBwAAAf/wB4AAAcB4A4AAAcA4A4AAAcA4A4AYAcA4AcA8AcB4AcB+Af/wDdj/Af/4D/n/Af/8D/n/AcAcB/A4AcAOA+A4AcAOAcAcAcAOAAAcAcAAAAAcAcAAAAAeAf/AAAAOAf/AAAAPAf/ADAAHgA4AHgADwA4AHAADwA4APAAB8AAA+AAA+AAB8AAAfgAH4AAAH8A/gAAAD///AAAAA//8AAAAAD/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="); // (full base64 string here)
+(function() {
+    const COIN_ICON = atob("MDCBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/AAAAAA//8AAAAB///AAAAB8A/gAAAAgAH4AAAwAAB8AAB4AAA+AADwA4APAADgA4APAAHgA4AHgADAf/ADwAAAf/gBwAAAf/wB4AAAcB4A4AAAcA4A4AAAcA4A4AYAcA4AcA8AcB4AcB+Af/wDdj/Af/4D/n/Af/8D/n/AcAcB/A4AcAOA+A4AcAOAcAcAcAOAAAcAcAAAAAcAcAAAAAeAf/AAAAOAf/AAAAPAf/ADAAHgA4AHgADwA4AHAADwA4APAAB8AAA+AAA+AAB8AAAfgAH4AAAH8A/gAAAD///AAAAA//8AAAAAD/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==");
     const settings = require("Storage").readJSON("coin_info.settings.json",1)||{};
-    const db = require("Storage").readJSON("coin_info.cmc_key.json",1)||{};
+    const apiKey = (require("Storage").readJSON("coin_info.cmc_key.json",1)||{}).apiKey||"";
 
-    const module = {
-        name: "Coin Price",
-        items: []
-    };
+    return {
+        name: "CoinInfo",
+        items: (settings.tokenSelected||[]).map(token => {
+            let current = {text:"-", img:COIN_ICON};
+            let tmr;
 
-    settings.tokenSelected.forEach(token => {
-        let current = {text:"-", img: COIN_ICON};
-        let timer = null;
-        let lastUpdate = 0;
+            return {
+                name: token,
+                get: () => current,
+                show: function() {
+                    const update = () => Bangle.http(
+                        `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?slug=${token}`,
+                        {headers: {'CMC_PRO_API_KEY': apiKey}}
+                    ).then(r => {
+                        // current.text = `${r.data["1"].symbol} $${r.data["1"].quote.USD.price.toFixed(2)}`;
+                        current.text = `${r.data["1"].symbol}`;
+                        Bangle.drawWidgets();
+                    }).catch(e => current.text = `Err:${e}`);
 
-        function update() {
-            Bangle.http(
-                `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?slug=${token}`,
-                {headers: {'CMC_PRO_API_KEY': db.apiKey}}
-            ).then(res => {
-                // current.text = `${res.data["1"].symbol}\n$${res.data["1"].quote.USD.price.toFixed(2)}`;
-                current.text = `${res.data["1"].symbol}`;
-                lastUpdate = Date.now();
-            }).catch(err => {
-                current.text = `Err: ${err}`;
-                lastUpdate = Date.now() + 300000; // Retry in 5min
-            }).finally(() => Bangle.drawWidgets());
-        }
-
-        module.items.push({
-            name: token,
-            get: () => current,
-            show: function() {
-                if(!timer) {
                     update();
-                    timer = setInterval(update, 3600000); // 60min
-                }
-            },
-            hide: function() {
-                if(timer) {
-                    clearInterval(timer);
-                    timer = null;
-                }
-            }
-        });
-    });
-
-    return module;
+                    tmr = setInterval(update, 3600000);
+                },
+                hide: () => tmr && clearInterval(tmr)
+            };
+        })
+    };
 })();
-
-
-
