ff906ffe253b5c909c45075a187e427ba49eada4
David Volovskiy <devolov@gmail.com>
2025-03-06 08:11:03 -0500

    Failed attempt on on-the-fly redraw of menu when selecting Steps

--- apps/daisy/settings.js
+++ apps/daisy/settings.js
@@ -32,61 +32,69 @@
   var ring_options = ['Hours', 'Minutes', 'Seconds', 'Steps', 'Battery', 'Sun'];
   var step_options = [100, 1000, 5000, 10000, 15000, 20000];
 
-  let appMenu = {
-    '': { 'title': 'Daisy Clock' },
-    '< Back': back,
-    'Colour': {
-      value: 0 | color_options.indexOf(s.color),
-      min: 0, max: color_options.length - 1,
-      format: v => color_options[v],
-      onchange: v => {
-        s.color = color_options[v];
-        s.fg = fg_code[v];
-        s.gy = gy_code[v];
-        save();
+  function buildMainMenu() {
+    let appMenu = {
+      '': { 'title': 'Daisy Clock' },
+      '< Back': back,
+      'Colour': {
+        value: 0 | color_options.indexOf(s.color),
+        min: 0, max: color_options.length - 1,
+        format: v => color_options[v],
+        onchange: v => {
+          s.color = color_options[v];
+          s.fg = fg_code[v];
+          s.gy = gy_code[v];
+          save();
+        },
       },
-    },
-    'Idle Warning': {
-      value: !!s.idle_check,
-      onchange: v => {
-        s.idle_check = v;
-        save();
+      'Idle Warning': {
+        value: !!s.idle_check,
+        onchange: v => {
+          s.idle_check = v;
+          save();
+        },
       },
-    },
-    'Expected Battery Life In Days Not Percentage': {
-      value: !!s.batt_hours,
-      onchange: v => {
-        s.batt_hours = v;
-        save();
+      'Expected Battery Life In Days Not Percentage': {
+        value: !!s.batt_hours,
+        onchange: v => {
+          s.batt_hours = v;
+          save();
+        },
       },
-    },
-    '12 Hr Time': {
-      value: !!s.hr_12,
-      onchange: v => {
-        s.hr_12 = v;
-        save();
+      '12 Hr Time': {
+        value: !!s.hr_12,
+        onchange: v => {
+          s.hr_12 = v;
+          save();
+        },
       },
-    },
-    'Ring Display': {
-      value: 0 | ring_options.indexOf(s.ring),
-      min: 0, max: ring_options.length - 1,
-      format: v => ring_options[v],
-      onchange: v => {
-        s.ring = ring_options[v];
-        save();
-      },
-    }
-  };
-  if (s.ring == 'Steps') {
-    appMenu[/*LANG*/"Step Target"] = {
-      value: 0 | step_options.indexOf(s.step_target),
-      min: 0, max: step_options.length - 1,
-      format: v => step_options[v],
-      onchange: v => {
-        s.step_target = step_options[v];
-        save();
-      },
+      'Ring Display': {
+        value: 0 | ring_options.indexOf(s.ring),
+        min: 0, max: ring_options.length - 1,
+        format: v => ring_options[v],
+        onchange: v => {
+          let prev = s.ring;
+          s.ring = ring_options[v];
+          save();
+          if (prev != s.ring && (prev === 'Steps' || s.ring === 'Steps')) {
+            E.showMenu(buildMainMenu());
+          }
+        },
+      }
     };
+    if (s.ring == 'Steps') {
+      appMenu[/*LANG*/"Step Target"] = {
+        value: 0 | step_options.indexOf(s.step_target),
+        min: 0, max: step_options.length - 1,
+        format: v => step_options[v],
+        onchange: v => {
+          s.step_target = step_options[v];
+          save();
+        },
+      };
+    } 
+    return appMenu;
   }
-  E.showMenu(appMenu);
+
+  E.showMenu(buildMainMenu());
 })
