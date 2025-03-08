18711f385ee276eb81aa7277873fb3f4e0b7fff6
Martin Zwigl <martin@zwigl.info>
2025-03-08 02:38:15 +0100

    Better retrieval mechanism; also caching for promise

--- apps/coin_info/clkinfo.js
+++ apps/coin_info/clkinfo.js
@@ -1,5 +1,5 @@
 (function() {
-    const COIN_ICON = atob("MDCBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/AAAAAA//8AAAAB///AAAAB8A/gAAAAgAH4AAAwAAB8AAB4AAA+AADwA4APAADgA4APAAHgA4AHgADAf/ADwAAAf/gBwAAAf/wB4AAAcB4A4AAAcA4A4AAAcA4A4AYAcA4AcA8AcB4AcB+Af/wDdj/Af/4D/n/Af/8D/n/AcAcB/A4AcAOA+A4AcAOAcAcAcAOAAAcAcAAAAAcAcAAAAAeAf/AAAAOAf/AAAAPAf/ADAAHgA4AHgADwA4AHAADwA4APAAB8AAA+AAA+AAB8AAAfgAH4AAAH8A/gAAAD///AAAAA//8AAAAAD/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==");
+    const COIN_ICON = atob("MDCBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/AAAAAA//8AAAAB///AAAAB8A/gAAAAgAH4AAAwAAB8AAB4AAA+AADwA4APAADgA4APAAHgA4AHgADAf/ADwAAAf/gBwAAAf/wB4AAAcB4A4AAAcA4A4AAAcA4A4AYAcA4AcA8AcB4AcB+Af/wDdj/Af/4D/n/Af/8D/n/AcAcB/A4AcAOA+A4AcAOAcAcAcAOAAAcAcAAAAAcAcAAAAAeAf/AAAAOAf/AAAAPAf/ADAAHgA4AHgADwA4AHAADwA4APAAB8AAA+AAA+AAB8AAAfgAH4AAAH8A/gAAAD///AAAAA//8AAAAAD/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="); // (full base64 string here)
 
     const settings = require("Storage").readJSON("coin_info.settings.json", 1) || {};
     const db = require("Storage").readJSON("coin_info.cmc_key.json", 1) || {};
@@ -7,7 +7,6 @@
 
     if (!(settings.tokenSelected instanceof Array)) settings.tokenSelected = [];
 
-    // Cache for storing results
     let cache = {};
 
     return {
@@ -16,10 +15,12 @@
             return {
                 name: token,
                 get: function() {
-                    // Immediate return from cache or placeholder
+                    // Return cached data if available
                     if (cache[token]) {
                         return cache[token];
                     }
+
+                    // Return placeholder while waiting for data
                     return {
                         text: "Load...",
                         img: COIN_ICON
@@ -28,10 +29,10 @@
                 show: function() {
                     var self = this;
 
-                    // Fetch data when shown
-                    const url = `https://api.binance.com/api/v3/ticker/24hr?symbol=${token.toUpperCase()}USDT`;
+                    // Function to fetch data from API
+                    const fetchData = (callback) => {
+                        const url = `https://api.binance.com/api/v3/ticker/24hr?symbol=${token.toUpperCase()}USDT`;
 
-                    (function fetchData() {
                         Bangle.http(url, { method: 'GET' })
                             .then(cmcResult => {
                                 logFile.write("HTTP resp:" + JSON.stringify(cmcResult));
@@ -40,13 +41,11 @@
 
                                 // Update cache with fetched data
                                 cache[token] = {
-                                    // text: `${apiData.symbol}\n${apiData.lastPrice} USD`,
                                     text: `${apiData.symbol}`,
                                     img: COIN_ICON
                                 };
 
-                                // Trigger a redraw of this item
-                                self.emit("redraw");
+                                callback();
                             })
                             .catch(err => {
                                 logFile.write("API Error: " + JSON.stringify(err));
@@ -54,14 +53,22 @@
                                     text: "Error",
                                     img: COIN_ICON
                                 };
+                                callback();
+                            });
+                    };
+
+                    // Set timeout to align to the next hour and then continue updating every hour
+                    self.interval = setTimeout(function timerTimeout() {
+                        fetchData(() => {
+                            self.emit("redraw");
+                        });
+                        // Continue updating every hour
+                        self.interval = setInterval(function intervalCallback() {
+                            fetchData(() => {
                                 self.emit("redraw");
                             });
-                    })();
-
-                    // Set interval to refresh data every hour
-                    self.interval = setInterval(() => {
-                        fetchData(); // Call the fetch function again
-                    }, 3600000); // Refresh every hour
+                        }, 3600000);
+                    }, 3600000 - (Date.now() % 3600000));
                 },
                 hide: function() {
                     if (this.interval) {
@@ -72,4 +79,4 @@
             };
         })
     };
-})();
+});
