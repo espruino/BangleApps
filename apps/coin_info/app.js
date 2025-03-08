95a2927b99799213a19f57fd7c0c17398d59a200
Martin Zwigl <martin@zwigl.info>
2025-03-08 21:09:59 +0100

    Simple layout test

--- apps/coin_info/app.js
+++ apps/coin_info/app.js
@@ -2,11 +2,16 @@
     Bangle.loadWidgets();
     Bangle.drawWidgets();
 
+    var Layout = require("Layout");
+    var layout = new Layout( {
+        type:"v", c: [
+            {type:"txt", font:"20%", label:"12:00", id:"time" },
+            {type:"txt", font:"6x8", label:"The Date", id:"date" }
+        ]
+    });
     g.clear();
-    // draw the current counter value
-    g.drawString("TEST", g.getWidth()/2, g.getHeight()/2);
-
-
+    layout.render();
+    
     Bangle.setUI({
         mode : 'custom',
         back : Bangle.showClock,
