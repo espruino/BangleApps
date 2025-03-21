108a932a581bb8cbbaf792663b21638ba2b62594
thyttan <6uuxstm66@mozmail.com>
2025-03-21 18:59:22 +0100

    Revert "voldisp: some optimization"
    This reverts commit 500296af191730b4eff2b7f489f7658fff84a37d.

--- apps/voldisp/boot.js
+++ apps/voldisp/boot.js
@@ -1,36 +1,36 @@
 {
-  const WIDGET_UTILS_HIDE = function() {
-    //exports.cleanup();
-    if (!global.WIDGETS) return;
-    g.reset(); // reset colors
-    for (var w of global.WIDGETS) {
-      if (w._draw) return; // already hidden
-      w._draw = w.draw;
-      w.draw = () => {};
-      w._area = w.area;
-      w.area = "";
-      if (w.x!=undefined) g.clearRect(w.x,w.y,w.x+w.width-1,w.y+23);
-    }
-  };
-  /// Show any hidden widgets
-  const WIDGET_UTILS_SHOW = function() {
-    //exports.cleanup();
-    if (!global.WIDGETS) return;
-    for (var w of global.WIDGETS) {
-      if (!w._draw) return; // not hidden
-      w.draw = w._draw;
-      w.area = w._area;
-      delete w._draw;
-      delete w._area;
-      w.draw(w);
-    }
-  };
-
   let timeout;
   let onMusicVolume = (volPercent)=>{
-    if (timeout) {clearTimeout(timeout);}
+    if (timeout) {clearTimeout(timeout); timeout=undefined;}
 
     if (Bangle.CLOCK) {
+
+      const WIDGET_UTILS_HIDE = function() {
+        //exports.cleanup();
+        if (!global.WIDGETS) return;
+        g.reset(); // reset colors
+        for (var w of global.WIDGETS) {
+          if (w._draw) return; // already hidden
+          w._draw = w.draw;
+          w.draw = () => {};
+          w._area = w.area;
+          w.area = "";
+          if (w.x!=undefined) g.clearRect(w.x,w.y,w.x+w.width-1,w.y+23);
+        }
+      };
+      /// Show any hidden widgets
+      const WIDGET_UTILS_SHOW = function() {
+        //exports.cleanup();
+        if (!global.WIDGETS) return;
+        for (var w of global.WIDGETS) {
+          if (!w._draw) return; // not hidden
+          w.draw = w._draw;
+          w.area = w._area;
+          delete w._draw;
+          delete w._area;
+          w.draw(w);
+        }
+      };
 
       let isAllWidgetsHidden = true;
       if (global.WIDGETS) {
@@ -41,7 +41,7 @@
           }
         }
       }
-      if (!timeout) {WIDGET_UTILS_HIDE();}
+      WIDGET_UTILS_HIDE();
       let barWidth = g.getWidth()*volPercent/100;
       g.
         setColor(0x0000).fillRect(0,0,g.getWidth(),24).
@@ -58,7 +58,6 @@
         } else {
           Bangle.load();
         }
-        timeout = undefined;
       };
       timeout = setTimeout(goAway, 3*1000);
     }
