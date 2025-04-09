3e8ceaacd4b43c6045abe2f7984c8ac04e90a336
David Volovskiy <devolov@gmail.com>
2025-04-09 07:51:22 -0400

    Daisy settings reorder

--- apps/daisy/settings.js
+++ apps/daisy/settings.js
@@ -3,35 +3,34 @@
 
   // initialize with default settings...
   let s = {'gy' : '#020',
-           'fg' : '#0f0',
-           'color': 'Green',
-           'check_idle' : true,
-           'batt_hours' : false,
-           'hr_12' : false,
-           'hourly_buzz' : false,
-           'ring' : 'Steps',
-           'idxInfo' : 0,
-           'step_target' : 10000};
+    'fg' : '#0f0',
+    'color': 'Green',
+    'check_idle' : true,
+    'batt_hours' : false,
+    'hourly_buzz' : false,
+    'ring' : 'Steps',
+    'idxInfo' : 0,
+    'step_target' : 10000};
 
-  // ...and overwrite them with any saved values
-  // This way saved values are preserved if a new version adds more settings
-  const storage = require('Storage');
-  let settings = storage.readJSON(SETTINGS_FILE, 1) || s;
-  const saved = settings || {};
-  for (const key in saved) {
-    s[key] = saved[key];
-  }
+// ...and overwrite them with any saved values
+// This way saved values are preserved if a new version adds more settings
+const storage = require('Storage');
+let settings = storage.readJSON(SETTINGS_FILE, 1) || s;
+const saved = settings || {};
+for (const key in saved) {
+s[key] = saved[key];
+}
 
-  function save() {
-    settings = s;
-    storage.write(SETTINGS_FILE, settings);
-  }
+function save() {
+settings = s;
+storage.write(SETTINGS_FILE, settings);
+}
 
-  var color_options = ['Green','Orange','Cyan','Purple','Red','Blue'];
-  var fg_code = ['#0f0','#ff0','#0ff','#f0f','#f00','#00f'];
-  var gy_code = ['#020','#220','#022','#202','#200','#002'];
-  var ring_options = ['Hours', 'Minutes', 'Seconds', 'Day', 'Sun', 'Steps', 'Battery'];
-  var step_options = [100, 1000, 5000, 10000, 15000, 20000];
+var color_options = ['Green','Orange','Cyan','Purple','Red','Blue'];
+var fg_code = ['#0f0','#ff0','#0ff','#f0f','#f00','#00f'];
+var gy_code = ['#020','#220','#022','#202','#200','#002'];
+var ring_options = ['Hours', 'Minutes', 'Seconds', 'Day', 'Sun', 'Steps', 'Battery'];
+var step_options = [100, 1000, 5000, 10000, 15000, 20000];
 
   function showMainMenu() {
     let appMenu = {
@@ -64,7 +63,7 @@
         },
       };
     } 
-    appMenu['Colour'] = {
+    appMenu['Color'] = {
       value: 0 | color_options.indexOf(s.color),
       min: 0, max: color_options.length - 1,
       format: v => color_options[v],
@@ -75,13 +74,6 @@
         save();
       },
     };
-    appMenu['Idle Warning'] = {
-      value: !!s.idle_check,
-      onchange: v => {
-        s.idle_check = v;
-        save();
-      },
-    };
     appMenu['Hourly Buzz'] = {
       value: !!s.hourly_buzz,
       onchange: v => {
@@ -89,8 +81,16 @@
         save();
       },
     };
-    appMenu['Expected Battery Life In Days Not Percentage'] = {
+    appMenu['Idle Warning'] = {
+      value: !!s.idle_check,
+      onchange: v => {
+        s.idle_check = v;
+        save();
+      },
+    };
+    appMenu['Battery Life Format'] = {
       value: !!s.batt_hours,
+      format: value => value?"Days":"%",
       onchange: v => {
         s.batt_hours = v;
         save();
