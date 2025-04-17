a058b08d1406f616c87d400c767c573b8f27e4c3
thyttan <6uuxstm66@mozmail.com>
2025-04-18 00:18:44 +0200

    swipeinv: wip non-working changes

--- apps/swipeinv/boot.js
+++ apps/swipeinv/boot.js
@@ -1,4 +1,7 @@
 {
+  // TODO: Should the global case just hijack the swipe and drag events, modifying them before passing on to other listeners?
+  //   - That could be a new separate bootloader app instead though?
+
   const settings = Object.assign({
     global: false,
     apps: []
@@ -10,7 +13,17 @@
       if (typeof mode === "object" && mode.swipe) {
         if (settings.global ^ settings.apps.includes(global.__FILE__)) {
           const origSwipeCb = mode.swipe;
-          mode.swipe = (dirLR, dirUD) => origSwipeCb(dirLR*-1, dirUD*-1);
+          mode.swipe = (dirLR, dirUD) => origSwipeCb(dirLR * -1, dirUD * -1);
+        }
+      }
+      if (typeof mode === "object" && mode.drag) {
+        if (settings.global ^ settings.apps.includes(global.__FILE__)) {
+          const origDragCb = mode.drag;
+          mode.drag = (e) => {
+            e.dx *= -1;
+            e.dy *= -1;
+            origDragCb(e);
+          }
         }
       }
       return setURIOrig(mode, callback);
