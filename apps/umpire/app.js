ce691968f9a698b8ce60a1ac68ecbdabe260b9d0
Andy Smy <47543767+AndySmee@users.noreply.github.com>
2025-04-19 21:04:46 +0100

    Update app.js

--- apps/umpire/app.js
+++ apps/umpire/app.js
@@ -209,4 +209,16 @@ var menuItems = {
   }
 };
 countdownDebounce = true;
-var menu = E.showMenu(menuItems);
+//var menu = E.showMenu(menuItems);
+
+var scrollMenuItems =[
+  "Play", "Wicket"
+];
+var scrollMenu = E.showScroller({
+  h : 60, c : 2,
+  draw : (idx, r) => {
+    g.setBgColor((idx&1)?"#000":"#333").clearRect(r.x,r.y,r.x+r.w-1,r.y+r.h-1);
+    g.setFont("6x8:2").drawString(scrollMenuItems[idx],r.x+10,r.y+4);
+  },
+  select : (idx) => console.log("You selected ", idx)
+});
