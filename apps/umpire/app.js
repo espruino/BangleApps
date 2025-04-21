9a10edffcb5c22a104521038662d63893b76d1d3
Andy Smy <47543767+AndySmee@users.noreply.github.com>
2025-04-21 11:37:05 +0100

    Update app.js

--- apps/umpire/app.js
+++ apps/umpire/app.js
@@ -25,6 +25,26 @@ function addLog(timeSig, over, ball, matchEvent, metaData) {
 
 function showLog() {
   processing = true;
+  Bangle.setUI({
+      mode : "custom",
+      swipe : (directionLR, directionUD)=>{
+        Bangle.setUI();
+        if(counter==6) {
+          startOver();
+        } else {
+          menu = resumeGame();
+        }      
+      }
+      ,
+      btn : ()=>{
+        Bangle.setUI();
+        if(counter==6) {
+          startOver();
+        } else {
+          menu = resumeGame();
+        }
+      }
+    });
   return E.showScroller({
     h : 40, c : log.length,
   draw : (idx, r) => {
