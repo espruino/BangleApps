b1d48c11dee7b68f8cd6ed5498d4f8637943aae0
thyttan <6uuxstm66@mozmail.com>
2025-04-29 00:48:48 +0200

    swipeinv: (WIP) update settings

--- apps/swipeinv/settings.js
+++ apps/swipeinv/settings.js
@@ -3,12 +3,25 @@
   // Load settings
   const settings = Object.assign({
     global: false,
-    apps: []
+    apps: {}
   }, require("Storage").readJSON(FILE, true) || {});
 
   function writeSettings() {
     require('Storage').writeJSON(FILE, settings);
   }
+
+  // Convert settings when coming from ver < 4
+  function convertSettings() {
+    if ("array" === typeof(settings.apps)) {
+      let newObject = {};
+      settings.apps.forEach((source)=>{
+      let idExtractedFromSource = source.split(".")[0];
+        newObject[idExtractedFromSource] = {};
+      })
+      settings.apps = newObject;
+    }
+  }
+  convertSettings();
 
   function appMenu() {
     let menu = {
@@ -21,15 +34,14 @@
       return 0;
     }).forEach(app => {
       menu[app.name] = {
-        value: settings.apps.includes(app.src),
+        value: settings.apps.keys().includes(app.id),
         onchange: v => {
           if (v) {
-            settings.apps.push(app.src);
+            if (!settings.apps[app.id] || settings.apps[app.id].keys().length===0) {
+                settings.apps[app.id] = {"swipeH":true, "swipeV":true, "dragH":true, "dragV":true};
+              }
           } else {
-            const idx = settings.apps.indexOf(app.src);
-            if (idx !== -1) {
-              settings.apps.splice(idx, 1);
-            }
+            if (settings.apps[app.id]) {delete settings.apps[app.id];}
           }
         }
       };
@@ -38,7 +50,7 @@
   }
 
   function mainMenu() {
-    E.showMenu({
+    let menu = {
       "" : { "title" : /*LANG*/"Swipe inversion" },
       "< Back" : () => back(),
 
@@ -50,8 +62,40 @@
         }
       },
 
-      /*LANG*/'Select apps': () => appMenu()
+      /*LANG*/'Select apps': () => appMenu(),
+    };
+
+    if (!settings.appTune) {settings.appTune = {};}
+
+    settings.apps.forEach((appSrc,index,array) => {
+      // Create a sub menu and show it.
+      let subMenu = {
+        "" : { "title" : /*LANG*/"Tune"+" "+appSrc },
+        "< Back" : () => { writeSettings(); mainMenu();}
+      }
+      let entries = [{name:"Swipe Horizontal", id:"swipeH"}, {name:"Swipe Vertical", id:"swipeV"}, {name:"Drag Horizontal", id:"dragH"}, {name:"Drag Vertical", id:"dragV"}];
+      entries.forEach((setting, index, array)=>{
+        if (!settings.appTune[app[setting.id]) {settings.appTune[settings.id] = }
+        subMenu[setting.name] = {
+          value: settings.appTune.includes(app.src),
+          onchange: v => {
+            if (v) {
+              settings.apps.push(app.src);
+            } else {
+              const idx = settings.apps.indexOf(app.src);
+              if (idx !== -1) {
+                settings.apps.splice(idx, 1);
+              }
+            }
+          }
+        };
+
+      })
+
+      menu[appSrc] = ()=>E.showMenu(subMenu);
     });
+    
+    E.showMenu(menu);
   }
 
   mainMenu();
