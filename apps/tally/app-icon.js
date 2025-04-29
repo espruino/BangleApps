cc2978fb07bb6f7ad126da20ec887df9d794effe
Rob Pilling <robpilling@gmail.com>
2025-04-29 08:48:45 +0100

    tally: app icon
    compression + transparency + inverted

--- apps/tally/app-icon.js
+++ apps/tally/app-icon.js
@@ -1,1 +1,1 @@
-require("heatshrink").decompress(atob("mEwwMB/4Ai/gsDn/4Aocf+AFDh4FGDgYFU/k/Aq08n4FTAgIFtO66bKVoq5F//wj4fD+AIBAod/AojYE+IFE+YFE+4FKABQ="))
+require("heatshrink").decompress(atob("mEwwInkgf/AAXAg4FD8EPAofwAowcDAqkD4AFW4fAAqYEBAtp3XTZStFXIsAh/gD4cPBAIFDwAFEbAkOAokMAokIApQAKA=="))
