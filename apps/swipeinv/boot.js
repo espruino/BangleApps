20321056edf8dbbb2eef42b5c886e9bcf770e88c
thyttan <6uuxstm66@mozmail.com>
2025-05-04 16:30:31 +0200

    swipeinv: adhere to code style guide re consts

--- apps/swipeinv/boot.js
+++ apps/swipeinv/boot.js
@@ -1,30 +1,30 @@
 {
-  const settings = Object.assign({
+  const SETTINGS = Object.assign({
     global: false,
     apps: []
   }, require("Storage").readJSON("swipeinv.json", true) || {});
 
-  const clockAppID = require("Storage").readJSON("setting.json",true).clock.split(".")[0];
+  const CLOCK_APP_ID = require("Storage").readJSON("setting.json",true).clock.split(".")[0];
 
   let getAppIdFromCurrentFile = ()=> {
     if (!global.__FILE__ || global.__FILE__===".bootcde") {
-      return clockAppID;
+      return CLOCK_APP_ID;
     } else {return global.__FILE__.split(".")[0];}
   }
 
   setTimeout(() => { // Timeout so we prepend listeners late, hopefully after all other listerners were added.
-    if (settings.global || Object.keys(settings.apps).length > 0) {
+    if (SETTINGS.global || Object.keys(SETTINGS.apps).length > 0) {
 
       let swipeInverter = (dirLR, dirUD, obj) => {
         "ram"
-        const appID = getAppIdFromCurrentFile();
-        if (settings.global ^ Object.keys(settings.apps).includes(appID)) {
+        const APP_ID = getAppIdFromCurrentFile();
+        if (SETTINGS.global ^ Object.keys(SETTINGS.apps).includes(APP_ID)) {
           if (!(obj && obj.inverted)) {
             E.stopEventPropagation();
             obj = Object.assign({inverted:true}, obj);
 
-            if (settings.global ^ (settings.apps[appID]&&settings.apps[appID].swipeH)) {dirLR *= -1;}
-            if (settings.global ^ (settings.apps[appID]&&settings.apps[appID].swipeV)) {dirUD *= -1;}
+            if (SETTINGS.global ^ (SETTINGS.apps[APP_ID]&&SETTINGS.apps[APP_ID].swipeH)) {dirLR *= -1;}
+            if (SETTINGS.global ^ (SETTINGS.apps[APP_ID]&&SETTINGS.apps[APP_ID].swipeV)) {dirUD *= -1;}
 
             Bangle.emit("swipe", dirLR, dirUD, obj)
           }
@@ -33,14 +33,14 @@
 
       let dragInverter = (e) => {
         "ram"
-        const appID = getAppIdFromCurrentFile();
-        if (settings.global ^ Object.keys(settings.apps).includes(appID)) {
+        const APP_ID = getAppIdFromCurrentFile();
+        if (SETTINGS.global ^ Object.keys(SETTINGS.apps).includes(APP_ID)) {
           if (!e.inverted) {
             E.stopEventPropagation();
             e.inverted = true;
 
-            if (settings.global ^ (settings.apps[appID]&&settings.apps[appID].dragH)) {e.dx *= -1;}
-            if (settings.global ^ (settings.apps[appID]&&settings.apps[appID].dragV)) {e.dy *= -1;}
+            if (SETTINGS.global ^ (SETTINGS.apps[APP_ID]&&SETTINGS.apps[APP_ID].dragH)) {e.dx *= -1;}
+            if (SETTINGS.global ^ (SETTINGS.apps[APP_ID]&&SETTINGS.apps[APP_ID].dragV)) {e.dy *= -1;}
 
             Bangle.emit("drag", e);
           }
