4696b43c93f9c3c25aabca24c0b3a8bfa21d78b9
thyttan <6uuxstm66@mozmail.com>
2025-05-04 16:28:05 +0200

    swipeinv: look up filename less often

--- apps/swipeinv/boot.js
+++ apps/swipeinv/boot.js
@@ -4,10 +4,11 @@
     apps: []
   }, require("Storage").readJSON("swipeinv.json", true) || {});
 
-  let getAppIdFromSrc = ()=> {
-    "ram"
+  const clockAppID = require("Storage").readJSON("setting.json",true).clock.split(".")[0];
+
+  let getAppIdFromCurrentFile = ()=> {
     if (!global.__FILE__ || global.__FILE__===".bootcde") {
-      return require("Storage").readJSON("setting.json",true).clock
+      return clockAppID;
     } else {return global.__FILE__.split(".")[0];}
   }
 
@@ -16,13 +17,14 @@
 
       let swipeInverter = (dirLR, dirUD, obj) => {
         "ram"
-        if (settings.global ^ Object.keys(settings.apps).includes(getAppIdFromSrc())) {
+        const appID = getAppIdFromCurrentFile();
+        if (settings.global ^ Object.keys(settings.apps).includes(appID)) {
           if (!(obj && obj.inverted)) {
             E.stopEventPropagation();
             obj = Object.assign({inverted:true}, obj);
 
-            if (settings.global ^ (settings.apps[getAppIdFromSrc()]&&settings.apps[getAppIdFromSrc()].swipeH)) {dirLR *= -1;}
-            if (settings.global ^ (settings.apps[getAppIdFromSrc()]&&settings.apps[getAppIdFromSrc()].swipeV)) {dirUD *= -1;}
+            if (settings.global ^ (settings.apps[appID]&&settings.apps[appID].swipeH)) {dirLR *= -1;}
+            if (settings.global ^ (settings.apps[appID]&&settings.apps[appID].swipeV)) {dirUD *= -1;}
 
             Bangle.emit("swipe", dirLR, dirUD, obj)
           }
@@ -31,13 +33,14 @@
 
       let dragInverter = (e) => {
         "ram"
-        if (settings.global ^ Object.keys(settings.apps).includes(getAppIdFromSrc())) {
+        const appID = getAppIdFromCurrentFile();
+        if (settings.global ^ Object.keys(settings.apps).includes(appID)) {
           if (!e.inverted) {
             E.stopEventPropagation();
             e.inverted = true;
 
-            if (settings.global ^ (settings.apps[getAppIdFromSrc()]&&settings.apps[getAppIdFromSrc()].dragH)) {e.dx *= -1;}
-            if (settings.global ^ (settings.apps[getAppIdFromSrc()]&&settings.apps[getAppIdFromSrc()].dragV)) {e.dy *= -1;}
+            if (settings.global ^ (settings.apps[appID]&&settings.apps[appID].dragH)) {e.dx *= -1;}
+            if (settings.global ^ (settings.apps[appID]&&settings.apps[appID].dragV)) {e.dy *= -1;}
 
             Bangle.emit("drag", e);
           }
