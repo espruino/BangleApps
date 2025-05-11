b9c3f40ebaae5084bd823e29440c2b2f3b7932f9
Andy Smy <47543767+AndySmee@users.noreply.github.com>
2025-05-11 18:44:54 +0100

    Update app.js

--- apps/umpire/app.js
+++ apps/umpire/app.js
@@ -204,7 +204,7 @@ function countDown(dir) {
     g.drawString('\¦\¦\¦', 173, 15);
     g.setFontAlign(-1,0);
     g.setFont("Vector",16); // vector font, 80px
-    var heartRateString = 'BPM: ' + heartRate;
+    var heartRateString = 'HR:' + heartRate;
     if(heartRateEventSeconds <= 0) heartRateString = '';
     g.drawString(battery + '% ' + heartRateString, 5, 11);
     g.setFontAlign(0,0);
