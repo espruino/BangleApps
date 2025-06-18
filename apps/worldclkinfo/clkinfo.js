db43b51cb15ce8dbb178bf7b293877264d15f313
RKBoss6 <onkarka@northvilleschools.net>
2025-06-18 16:01:36 -0400

    Create clkinfo.js

---
+++ apps/worldclkinfo/clkinfo.js
@@ -0,0 +1,18 @@
+(function() {
+  return {
+    name: "Bangle",
+    // img: 24x24px image for this list of items. The default "Bangle" list has its own image so this is not needed
+    items: [
+      { name : "Item1",
+        get : function() { return { text : "TextOfItem1",
+                       // v : 10, min : 0, max : 100, - optional
+                      img : atob("GBiBAAAAAAAAAAAYAAD/AAOBwAYAYAwAMAgAEBgAGBAACBCBCDHDjDCBDBAACBAACBhCGAh+EAwYMAYAYAOBwAD/AAAYAAAAAAAAAA==") }},
+        show : function() {},
+        hide : function() {},
+        // run : function() {} optional (called when tapped)
+        // focus : function() {} optional (called when focussed)
+        // blur : function() {} optional (called when unfocussed)
+      }
+    ]
+  };
+}) // must not have a semi-colon!
