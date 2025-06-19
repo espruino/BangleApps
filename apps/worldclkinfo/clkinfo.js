689e5640ef14e0e356822f2beec98c0d0c5027d1
RKBoss6 <onkarka@northvilleschools.net>
2025-06-18 20:20:25 -0400

    Test times

--- apps/worldclkinfo/clkinfo.js
+++ apps/worldclkinfo/clkinfo.js
@@ -1,20 +1,162 @@
+
 (function() {
+  
+  
+  //All offsets from UTC in minutes. Positive: behind UTC. Negative: Ahead of UTC. 
+
+  const londonTimeOffset=60;
+  const mumbaiTimeOffset=330;
+  const nycTimeOffset=-240;
+  const tokyoTimeOffset=540;
+  const dubaiTimeOffset=240;
+  const laTimeOffset=-420;
+  const parisTimeOffset=120;
+
+  
+  
+  function getWorldDate(string city){
+    //Gets difference between UTC and local time
+    var currOffset = new Date().getTimezoneOffset();
+    var date=newDate();
+    var timeOffset;
+    
+    switch (city) {
+      case "London":
+        timeOffset=londonTimeOffset;
+        break; 
+      case "Mumbai":
+        timeOffset=mumbaiTimeOffset;
+        break;
+      case "New York":
+        timeOffset=nycTimeOffset;
+        break;
+      case "Tokyo":
+        timeOffset=tokyoTimeOffset;
+        break;
+      case "Dubai":
+        timeOffset=dubaiTimeOffset;
+        break;
+      case "Los Angeles":
+        timeOffset=laTimeOffset;
+        break;
+      case "Paris":
+        timeOffset=parisTimeOffset;
+        break;
+
+      default:
+        //Nothing else matches
+        timeOffset=0
+
+    }
+    
+    //go to UTC time
+    date.setMinutes(date.getMinutes() - currOffset);
+    //from there, go to city time
+    date.setMinutes(date.getMinutes() + timeOffset);
+    
+    var clockStr = require("locale").time(date, 1 /*omit seconds*/);
+    
+    return clockStr;
+    
+    
+  }
+  
+  
+  
+  
+  
   return {
-    name: "World Clock",
-    img: atob("GBiBAAAAAAAAAAAYAAD/AAOBwAYAYAwAMAgAEBgAGBAACBCBCDHDjDCBDBAACBAACBhCGAh+EAwYMAYAYAOBwAD/AAAYAAAAAAAAAA=="),
+    name: "World Clocks",
     items: [
+      
       { name : "London",
-        get : function() { return { text : "London \n3:24",
-                       // v : 10, min : 0, max : 100, - optional
-                      img : atob("") 
-      }},
-       
-        show : function() {},
-        hide : function() {},
-        // run : function() {} optional (called when tapped)
-        // focus : function() {} optional (called when focussed)
-        // blur : function() {} optional (called when unfocussed)
-      }
+        get : () => {
+          return {
+            text : "London"+"\n"+getWorldDate("London"),
+            //blank image
+            img : atob("")
+          };
+        },
+        show : function() {
+          this.interval = setTimeout(()=>{
+            this.emit("redraw");
+            this.interval = setInterval(()=>{
+              this.emit("redraw");
+            }, 60000);
+          }, 60000 - (Date.now() % 60000));
+        },
+       hide : function() {
+          clearInterval(this.interval);
+          this.interval = undefined;
+        }
+      },
+      
+      { name : "Mumbai",
+        get : () => {
+          return {
+            text : "Mumbai"+"\n"+getWorldDate("Mumbai"),
+            //blank image
+            img : atob("")
+          };
+        },
+        show : function() {
+          this.interval = setTimeout(()=>{
+            this.emit("redraw");
+            this.interval = setInterval(()=>{
+              this.emit("redraw");
+            }, 60000);
+          }, 60000 - (Date.now() % 60000));
+        },
+       hide : function() {
+          clearInterval(this.interval);
+          this.interval = undefined;
+        }
+      },
+      
+      { name : "New York",
+        get : () => {
+          return {
+            text : "New York"+"\n"+getWorldDate("New York"),
+            //blank image
+            img : atob("")
+          };
+        },
+        show : function() {
+          this.interval = setTimeout(()=>{
+            this.emit("redraw");
+            this.interval = setInterval(()=>{
+              this.emit("redraw");
+            }, 60000);
+          }, 60000 - (Date.now() % 60000));
+        },
+       hide : function() {
+          clearInterval(this.interval);
+          this.interval = undefined;
+        }
+      },
+      
+      { name : "Tokyo",
+        get : () => {
+          return {
+            text : "Tokyo"+"\n"+getWorldDate("Tokyo"),
+            //blank image
+            img : atob("")
+          };
+        },
+        show : function() {
+          this.interval = setTimeout(()=>{
+            this.emit("redraw");
+            this.interval = setInterval(()=>{
+              this.emit("redraw");
+            }, 60000);
+          }, 60000 - (Date.now() % 60000));
+        },
+       hide : function() {
+          clearInterval(this.interval);
+          this.interval = undefined;
+        }
+      },
+      
     ]
   };
-}) // must not have a semi-colon!
+})
