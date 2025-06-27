67f04a5249fcad4936fef3f49f5eea86b148fbd0
RKBoss6 <onkarka@northvilleschools.net>
2025-06-27 19:56:45 -0400

    Update lib.js

--- apps/textfacts/lib.js
+++ apps/textfacts/lib.js
@@ -3,9 +3,11 @@ exports.getCount = function() {
   return 162;
 };
 exports.getText = function(n) {
+  /*
   var s = require("Storage").read("textsource.txt");
   var idx = s.indexOf("\n");
   var lengths = [idx];
+  
   while (idx>=0) {
     var next = s.indexOf("\n",idx+1);
     if (next>=0) {
@@ -16,8 +18,8 @@ exports.getText = function(n) {
     idx = next;
   }
   print(`Count = ${lengths.length}`);
-  print(`var idxs = E.toUint8Array(atob("${btoa(lengths)}"));`);
-  var idxs = E.toUint8Array(atob("PVguLS4qQScgVTtRLC4vMTwyKiowJjI+PzAnSigsLCssHTFSOkRCMiZHSjhUWVNSPUNCLU8uRzVCNk1RQ0Y7ZDwxMDU7QFgkMTM1RTJpIzFFO0I8KDRJPVNBQDIuPzsyMjFQOEdGPyguODI4LkhCQB5BNi8uRCsqMTVOVjNFTmI1O0QyPyk5PC89M0gwTjE7QixTRSI2QSY1RkY1SUs5OBsw"));
+  print(`var idxs = E.toUint8Array(atob("${btoa(lengths)}"));`);*/
+  var idxs = E.toUint8Array(atob("J1guOzAsMVJHQ0I1WEVpRUlBPzhCHkQzYj8zMUVJGylDIS8tKk5XRSc3RCZBLipBO08qRCU9LTgoSDlDIlgbIWEkOSkbMjckPzkYQTI0WD0nH1Y1JzchJEhGVUUoOTpMJUNYNSJbQk5UKFoxK0wyOBwqNSdIPD5ALmMmVFkxUiYnN0RBLipBO08qRCU9LTs4KEg5QyJYGyFhJDkpGzI3JD85GEEyNFg9Jx9WNSc3ISRIRlVFKDk6TCVDWDUiW0JOVChaMStMMjgcKjUnSDw+QC5jJlRZVzJPRS8iNBM5IlpFH1Y1NUJOOzs4SDlDIlgbIWEkORsyNz85GEEyNFg9JzchSEZVRCg5OkxDV1taMStLMjgcKjVIPD5ALmNUWShuNj07OTpYSUQlPzwyMkBFVyQjIiclJCgmIyckKQ=="));
   if (n<0 || n>=idxs.length) return;
   var idx = n ? E.sum(new Uint8Array(idxs.buffer,0,n))+1 : 0;
   var len = idxs[n]-1;
