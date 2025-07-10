b2e23e18c6eb20c66bd294941fff9c89be3da147
RKBoss6 <onkarka@northvilleschools.net>
2025-07-10 17:17:19 -0400

    Update boot.js

--- apps/backlite/boot.js
+++ apps/backlite/boot.js
@@ -1,14 +1,19 @@
 
-//Set LCD to zero through settings every reboot
+//Set LCD to zero every reboot
 let s = require("Storage").readJSON("setting.json", 1) || {};
-s.brightness = 0; // Set brightness to 70% (value from 0 to 1)
+s.brightness = 0;
+if (!("lcdTimeout" in s)) s.lcdTimeout = 5; // fallback so  logic doesn't break
 require("Storage").writeJSON("setting.json", s);
+
+
 
 
 let unlockedWithLongPress = false;
 const longPressTime=300; //(ms)
 
 Bangle.on('lock', function(isLocked) {
+  Bangle.setLCDBrightness(0);
+
   if (!isLocked) {
     // Just unlocked — give a short delay and check if BTN1 is still pressed
     setTimeout(() => {
@@ -17,7 +22,7 @@ Bangle.on('lock', function(isLocked) {
         Bangle.setLCDBrightness(0.1);
         setTimeout(()=>{
           Bangle.setLCDBrightness(0);
-        },Bangle.getOptions().lcdTimeout);
+        },s.lcdTimeout*1000);
       } else {
         Bangle.setLCDBrightness(0);
       }
