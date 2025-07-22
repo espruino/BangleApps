c588a254d0b9fcceb71c10152ed1102dca6120cd
RKBoss6 <178680496+RKBoss6@users.noreply.github.com>
2025-07-21 21:17:51 -0400

    Update boot to use its own block

--- apps/backlite/boot.js
+++ apps/backlite/boot.js
@@ -1,33 +1,32 @@
-
-//Set LCD to zero every reboot
-let s = require("Storage").readJSON("setting.json", 1) || {};
-s.brightness = 0;
-if (!("lcdTimeout" in s)) s.lcdTimeout = 5; // fallback so  logic doesn't break
-require("Storage").writeJSON("setting.json", s);
-
-
+{
+  //Set LCD to zero every reboot
+  let s = require("Storage").readJSON("setting.json", 1) || {};
+  s.brightness = 0;
+  if (!("lcdTimeout" in s)) s.lcdTimeout = 5; // fallback so  logic doesn't break
+  require("Storage").writeJSON("setting.json", s);
 
 
-const longPressTime=400; //(ms)
 
-Bangle.on('lock', function(isLocked) {
-  Bangle.setLCDBrightness(0);
 
-  if (!isLocked) {
-    // Just unlocked — give a short delay and check if BTN1 is still pressed
-    setTimeout(() => {
-      if (digitalRead(BTN1)) {
-        Bangle.setLCDBrightness(0.1);
-        setTimeout(()=>{
+  let unlockedWithLongPress = false;
+  const longPressTime=400; //(ms)
+
+  Bangle.on('lock', function(isLocked) {
+    Bangle.setLCDBrightness(0);
+
+    if (!isLocked) {
+      // Just unlocked — give a short delay and check if BTN1 is still pressed
+      setTimeout(() => {
+        if (digitalRead(BTN1)) {
+          unlockedWithLongPress = true;
+          //set brightness until. locked.
+          Bangle.setLCDBrightness(0.1);
+          
+        } else {
           Bangle.setLCDBrightness(0);
-        },s.lcdTimeout*1000);
-      } else {
-        Bangle.setLCDBrightness(0);
-      }
-    }, longPressTime); // Slight delay to allow unlock to settle
-  }
-});
+        }
+      }, longPressTime); // Slight delay to allow unlock to settle
+    }
+  });
 
-
-
-
+}
