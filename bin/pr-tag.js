580573215c581283edbddb1bf055d9bbdc905bc6
Rob Pilling <robpilling@gmail.com>
2025-09-13 11:49:50 +0100

    pr-tag.js: allow specifying to skip tagging (in PR description)

--- bin/pr-tag.js
+++ bin/pr-tag.js
@@ -1,15 +1,20 @@
 const { execSync } = require("child_process");
 const { readFileSync } = require("fs");
+const https = require("https");
 
 function usage(){
-  console.log(`Usage: pr-tag.js [revision-range]`);
+  console.log(`Usage: pr-tag.js [--local] [revision-range]`);
+  console.log(`--local: don't fetch the PR description - useful for local testing`);
   process.exit(2);
 }
 
-function main() {
+async function main() {
+  let local = false;
   let rev;
   for(const arg of process.argv.slice(2)){
-    if(/^-/.test(arg))
+    if(arg === "--local")
+      local = true;
+    else if(/^-/.test(arg))
       usage();
     else if(!rev)
       rev = arg;
@@ -18,6 +23,11 @@ function main() {
   }
 
   if(!rev) rev = "origin/master...";
+
+  if(!local && await shouldSkip()){
+    console.log("Skipping PR");
+    process.exit(0);
+  }
 
   const apps = execSync(
     `git diff --name-only ${rev} -- apps/ |
@@ -51,6 +61,60 @@ function main() {
 
     console.log(`tagging ${author} for ${apps.join(", ")}`);
   }
+}
+
+// allow skipping of tagging if the PR description contains a specific message
+async function shouldSkip() {
+  const desc = await fetchPRDesc({
+    prNumber: getenv("PR_NUM"),
+    repo: getenv("REPO"),
+    token: getenv("GITHUB_TOKEN"),
+  });
+
+  return desc
+    .split("\n")
+    .filter(line => /^tag-bot: skip\s*$/.test(line))
+    .length > 0;
+}
+
+function fetchPRDesc({ prNumber, repo, token }) {
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: "api.github.com",
+      path: `/repos/${repo}/pulls/${prNumber}`,
+      method: "GET",
+      headers: {
+        "User-Agent": "node.js",
+        "Authorization": `Bearer ${token}`,
+        "Accept": "application/vnd.github+json"
+      }
+    };
+
+    https.get(options, res => {
+      let data = "";
+      res.on("data", chunk => data += chunk);
+      res.on("end", () => {
+        if (res.statusCode === 200) {
+          try {
+            resolve(JSON.parse(data).body);
+          } catch (e) {
+            reject(e);
+          }
+        } else {
+          reject(new Error(`GitHub API error: ${res.statusCode}`));
+        }
+      });
+    }).on("error", reject);
+  });
+}
+
+function getenv(name) {
+  const val = process.env[name];
+  if(!val){
+    console.error(`Need environment variable $${name}`);
+    process.exit(1);
+  }
+  return val;
 }
 
 main().catch(e => {
