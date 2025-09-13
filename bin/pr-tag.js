e8d6da149f64ebfa57fd09e30079c507c23c9d2f
Rob Pilling <robpilling@gmail.com>
2025-09-13 11:49:29 +0100

    pr-tag.js: move to main() function

--- bin/pr-tag.js
+++ bin/pr-tag.js
@@ -6,47 +6,54 @@ function usage(){
   process.exit(2);
 }
 
-let rev;
-for(const arg of process.argv.slice(2)){
-  if(/^-/.test(arg))
-    usage();
-  else if(!rev)
-    rev = arg;
-  else
-    usage();
-}
+function main() {
+  let rev;
+  for(const arg of process.argv.slice(2)){
+    if(/^-/.test(arg))
+      usage();
+    else if(!rev)
+      rev = arg;
+    else
+      usage();
+  }
 
-if(!rev) rev = "origin/master...";
+  if(!rev) rev = "origin/master...";
 
-const apps = execSync(
-  `git diff --name-only ${rev} -- apps/ |
-    grep -o "apps/[^/]*" |
-    sort |
-    uniq`,
-  { encoding: "utf8" }
-).split("\n").filter(x => x);
+  const apps = execSync(
+    `git diff --name-only ${rev} -- apps/ |
+      grep -o "apps/[^/]*" |
+      sort |
+      uniq`,
+    { encoding: "utf8" }
+  ).split("\n").filter(x => x);
 
-if(apps.length === 0) process.exit(1); // pipeline failed
+  if(apps.length === 0) process.exit(1); // pipeline failed
 
-const authorToApp = {};
+  const authorToApp = {};
 
-for(const app of apps){
-  const metadata = JSON.parse(readFileSync(`${app}/metadata.json`, "utf8"));
-  let authorField = metadata.author;
-  if(!authorField) continue;
+  for(const app of apps){
+    const metadata = JSON.parse(readFileSync(`${app}/metadata.json`, "utf8"));
+    let authorField = metadata.author;
+    if(!authorField) continue;
 
-  const authors = Array.isArray(authorField) ? authorField : [authorField];
+    const authors = Array.isArray(authorField) ? authorField : [authorField];
 
-  for(const author of authors){
-    if(!authorToApp[author])
-      authorToApp[author] = new Set();
+    for(const author of authors){
+      if(!authorToApp[author])
+        authorToApp[author] = new Set();
 
-    authorToApp[author].add(app);
+      authorToApp[author].add(app);
+    }
+  }
+
+  for(let [author, apps] of Object.entries(authorToApp)){
+    apps = [...apps].sort();
+
+    console.log(`tagging ${author} for ${apps.join(", ")}`);
   }
 }
 
-for(let [author, apps] of Object.entries(authorToApp)){
-  apps = [...apps].sort();
-
-  console.log(`tagging ${author} for ${apps.join(", ")}`);
-}
+main().catch(e => {
+  console.error(e);
+  process.exit(1);
+});
