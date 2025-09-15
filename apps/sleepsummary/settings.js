274ec8b8e9c1312bee454c359c9d8c7b0b99d8fd
RKBoss6 <kabironkar@gmail.com>
2025-09-15 16:11:53 -0400

    Add settings management for Sleep Summary app

---
+++ apps/sleepsummary/settings.js
@@ -0,0 +1,71 @@
+(function(back) {
+  var FILE = "sleepsummary.settings.json";
+  // Load settings
+  var settings = Object.assign({
+      useTrueSleep:true,
+      showMessage:true,
+      deepSleepHours:5,
+      idealSleepHours:10,
+
+    }, require('Storage').readJSON(FILE, true) || {});
+
+  function writeSettings() {
+    require('Storage').writeJSON(FILE, settings);
+  }
+
+  // Show the menu
+  E.showMenu({
+    "" : { "title" : "Sleep Summary" },
+    "< Back" : () => back(),
+    'Use True Sleep': {
+      value: !!settings.useTrueSleep, 
+      onchange: v => {
+        settings.useTrueSleep = v;
+        writeSettings();
+      }
+    },
+    'Show Message': {
+      value: !!settings.showMessage,  
+      onchange: v => {
+        settings.showMessage = v;
+        writeSettings();
+      }
+      
+    },
+    'Ideal Deep Sleep': {
+      value: 0|settings.deepSleepHours*60,
+      min: 60, max: 600,
+      step:15,
+      onchange: v => {
+        settings.deepSleepHours = v/60; //Convert minutes to hours
+        writeSettings();
+      },
+      format : v => {
+        let h = Math.floor(v/60);
+        let m = v % 60;
+        let str = "";
+        if (h) str += h+"h";
+        if (m) str += " "+m+"m";
+        return str || "0m";
+      }
+    },
+    'Ideal Sleep Time': {
+      value: 0|settings.idealSleepHours*60, 
+      min: 120, max: 60*14 ,
+      step:15,
+      onchange: v => {
+        settings.idealSleepHours = v/60,
+        writeSettings();
+      },
+      format : v => {
+        let h = Math.floor(v/60);
+        let m = v % 60;
+        let str = "";
+        if (h) str += h+"h";
+        if (m) str += " "+m+"m";
+        return str || "0m";
+      }
+    },
+  });
+})
+
