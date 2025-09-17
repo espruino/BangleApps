b2de15569ca1ed890899193e92a9648d9b6eb38e
RKBoss6 <kabironkar@gmail.com>
2025-09-17 17:45:45 -0400

    Set default lcdTimeout and optimize memory usage

--- apps/backlite/boot.js
+++ apps/backlite/boot.js
@@ -11,26 +11,36 @@
   //Set LCD to zero every reboot
   let s = require("Storage").readJSON("setting.json", 1) || {};
   s.brightness = 0;
+  if (!("lcdTimeout" in s)) s.lcdTimeout = 5; // fallback so  logic doesn't break
   require("Storage").writeJSON("setting.json", s);
   //remove large settings object from memory
-  delete s;
+  s=null;
   const longPressTime=400; //(ms)
-
+  var longPressTimer;
   Bangle.on('lock', function(isLocked) {
     Bangle.setLCDBrightness(0);
 
     if (!isLocked) {
       // Just unlocked — give a short delay and check if BTN1 is still pressed
-      setTimeout(() => {
+      longPressTimer=setTimeout(() => {
         if (digitalRead(BTN1)) {
           //set brightness until. locked.
           Bangle.setLCDBrightness(getSettings().brightness);
         } else {
           Bangle.setLCDBrightness(0);
         }
+        clearTimeout(longPressTimer)
       }, longPressTime); // Slight delay to allow unlock to settle
     }
   });
 
+  
+  
+  setWatch(() => {
+    if (longPressTimer) {
+      clearTimeout(longPressTimer);
+      longPressTimer = null;
+    }
+  }, BTN1, { repeat:true, edge:'rising' });
 }
 
