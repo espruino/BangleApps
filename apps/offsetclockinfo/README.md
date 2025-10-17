6b469f853fb1805a36b65cae60b4606cae17016f
David Holdeman <dave@holdemanenterprises.com>
2025-10-17 14:59:50 +0800

    Create offset clockinfo

---
+++ apps/offsetclockinfo/README.md
@@ -0,0 +1,14 @@
+# Offset Clock Info
+
+Create a list of clockinfos, each with a name and an offset from UTC.
+
+# Display modes
+
+Tapping the clockinfo changes its mode.
+
+Examples of possible modes:
+- `5:45`
+- `5:45 Fri`
+- `NYC 5:45`
+- `NYC 5:45 Fri`
+- `NYC`
---
+++ apps/offsetclockinfo/clkinfo.js
@@ -0,0 +1,57 @@
+(function() {
+  let config = require("Storage").readJSON("offsetclockinfo.settings.json") || {};
+
+  var clocks = {
+    name: "Offset Clocks",
+    items: []
+  };
+
+  if (config.rows) {
+    config.rows.forEach((row, id) => {
+      clocks.items.push({
+        name: "OC " + row.name,
+        show: () => {},
+        hide: () => {},
+        run: function() {
+          if ( config.rows[id].mode == 5 || config.rows[id].mode == null) {
+             config.rows[id].mode = 1
+          } else {
+             config.rows[id].mode += 1
+          }
+          this.emit("redraw");
+          require('Storage').writeJSON("offsetclockinfo.settings.json", config);
+        },
+        get: () => {
+          var d = new Date();
+          var gmt = new Date(d.getTime() + d.getTimezoneOffset() * 60 * 1000);
+          var dx = new Date(gmt.getTime() + row.offset * 60 * 60 * 1000);
+          var odx = require("locale").time(dx, 1).replace(" ", "")
+          var odd = require('locale').dow(dx, 1)
+          var txt = ""
+          switch ( config.rows[id].mode) {
+            case 2:
+              txt = odx + " " + odd;
+              break;
+            case 3:
+              txt = row.name + " " + odx;
+              break;
+            case 4:
+              txt = row.name + " " + odx + " " + odd;
+              break;
+            case 5:
+              txt = row.name;
+              break;
+            default:
+              txt = odx;
+          }
+          return {
+            text: txt,
+            short: odx
+          };
+        }
+      });
+    });
+  }
+
+  return clocks;
+})
