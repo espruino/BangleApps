0f312815a8402377c2b92524c398754ff46010fe
thyttan <6uuxstm66@mozmail.com>
2025-12-09 23:40:55 +0100

    bigclkinfo: new app - one fullscreen clock info

---
+++ apps/bigclkinfo/README.md
@@ -0,0 +1,23 @@
+# Big Clock Info app
+
+For die hard clock info fans...
+
+## Features
+
+All the clock infos you have installed displayed full screen.
+
+## Controls
+
+Interact with it like one big clock info, which it is!
+
+## Settings
+
+TBA: Should the app be treated as a clock face app?
+
+## Requests
+
+Open an issue on the espruino/BangleApps issue tracker mentioning @thyttan.
+
+## Creator
+
+thyttan
---
+++ apps/bigclkinfo/app.js
@@ -0,0 +1,32 @@
+// TODO:
+// Add setting to decide if the app shoud set Bangle.CLOCK=1 ?
+// Make an updating analog clock info entry to use as start card. (update clkinfoclk or make a new one?)
+
+Bangle.CLOCK = 1; // TODO: make optional.
+
+// Load the clock infos
+let clockInfoItems = require("clock_info").load();
+// Add the
+let clockInfoMenu = require("clock_info").addInteractive(clockInfoItems, {
+  // Add the dimensions we're rendering to here - these are used to detect taps on the clock info area
+  x : 0, y: 0, w: Bangle.appRect.w, h: Bangle.appRect.h,
+  // You can add other information here you want to be passed into 'options' in 'draw'
+  // This function draws the info
+  draw : (itm, info, options) => {
+    // itm: the item containing name/hasRange/etc
+    // info: data returned from itm.get() containing text/img/etc
+    // options: options passed into addInteractive
+    // Clear the background
+    g.reset().clearRect(options.x, options.y, options.x+options.w-2, options.y+options.h-1);
+    // indicate focus - we're using a border, but you could change color?
+    if (options.focus) g.drawRect(options.x, options.y, options.x+options.w-2, options.y+options.h-1); // show if focused
+    // we're drawing center-aligned here
+    var midx = options.x+options.w/2;
+    let scale = 5;
+    if (info.img) g.drawImage(info.img, midx-12*scale,options.y+4, {scale:scale}); // draw the image
+    g.setFont("6x8:2").setFontAlign(0,1).drawString(info.text, midx,options.y+160); // draw the text
+  }
+});
+clockInfoMenu.menuA = 0;
+clockInfoMenu.menuB = 6; // Assume clkinfoclk is at indices (0,6)
+clockInfoMenu.redraw();
