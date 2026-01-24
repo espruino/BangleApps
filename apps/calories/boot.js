2a7994f70cc83f9be21fa7a05948988f6a04b300
RKBoss6 <kabironkar@gmail.com>
2026-01-23 20:26:33 -0500

    Refactor calorie tracking and data saving logic

--- apps/calories/boot.js
+++ apps/calories/boot.js
@@ -2,56 +2,42 @@ let myProfile = require("Storage").readJSON("myprofile.json",1)||{};
 let savedData = require("Storage").readJSON("calories.json",1)||{};
 //init global var
 global.calories = {
-  caloriesBurned:0
+  activeCaloriesBurned:0,
+  totalCaloriesBurned:0,
+  bmrCaloriesBurned:0
 };
-global.calories.caloriesBurned=savedData.caloriesBurned||0;
+
+global.calories.totalCaloriesBurned=savedData.totalCaloriesBurned||0;
+global.calories.activeCaloriesBurned=savedData.activeCaloriesBurned||0;
+global.calories.bmrCaloriesBurned=savedData.bmrCaloriesBurned||0;
+
+
+
 function writeData() {
   require('Storage').writeJSON("calories.json", savedData);
 }
 
-// takes object with bpm, movement (in duration), steps (in duration), and duration in minutes
-let calcCalories = function(healthData) {
-  if (!healthData || !healthData.duration) return;
-  if(myProfile=={}||!myProfile.weight||!myProfile.restingHr||!myProfile.maxHrm) return;
-  let weight = myProfile.weight;
-    //current HR-rest HR)/(max HR - rest HR) )
-  let hrr = healthData.bpm
-  ? (healthData.bpm - myProfile.restingHr) / (myProfile.maxHrm - myProfile.restingHr)
-  : 0;
-  if(myProfile.gender!=undefined){
-    if(myProfile.gender==1){
-      //female
 
-    }else{
-      //male, but use as default
-
-    }
-  }else{
-    //not defined, so we'll use an avg formula
-    
-  }
-  let bpmMet = 1 + (5 * Math.max(0,hrr));
-  let stepsPerMin = (healthData.steps/healthData.duration);
-  let stepsMet = 1 + 0.0175 * stepsPerMin;
-  let weightedHR = (healthData.bpm>0)?0.7:0.0;
-  let weightedSteps = 1 - weightedHR;
-  let met = bpmMet*weightedHR + stepsMet*weightedSteps;
-
-  //cals burned in duration
-  return Math.round(met * weight * (healthData.duration/60));
-
-}
 
 Bangle.on('health',function(hd){
-  global.calories.caloriesBurned+=calcCalories(Object.assign(hd,{duration:10}))
-  Bangle.emit("calories",{totalCalories:global.calories.caloriesBurned})
+  let calData=require("calories").calcCalories(Object.assign(hd,{duration:10}),myProfile)
+  global.calories.activeCaloriesBurned+=calData.activeCalories;
+  global.calories.totalCaloriesBurned+=calData.totalCalories;
+  global.calories.bmrCaloriesBurned+=calData.bmrCalories;
+  Bangle.emit("calories",global.calories)
 })
 
 Bangle.on('midnight', function(){
-  global.calories.caloriesBurned=0;
+  global.calories.activeCaloriesBurned=0,
+  global.calories.totalCaloriesBurned=0,
+  global.calories.bmrCaloriesBurned=0
 })
 E.on('kill', function() {
  //save cals counted
-  savedData.caloriesBurned=global.calories.caloriesBurned;
+  savedData.totalCaloriesBurned=global.calories.totalCaloriesBurned
+  savedData.activeCaloriesBurned=global.calories.activeCaloriesBurned;
+  savedData.bmrCaloriesBurned=global.calories.bmrCaloriesBurned;
+
   writeData()
 });
+
