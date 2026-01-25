ba759ecd1af64879a6799553a6630b181ca8427b
RKBoss6 <kabironkar@gmail.com>
2026-01-25 11:04:05 -0500

    Refactor calorie event handlers for efficiency

--- apps/calories/clkinfo.js
+++ apps/calories/clkinfo.js
@@ -5,46 +5,39 @@
       { name : "TotalCalories",
         get : () => ({ 
           text : global.calories.totalCaloriesBurned==0?"--":global.calories.totalCaloriesBurned,
-          img : atob("GBiBAAAAAAAAAAAAAAAAAAAQAAAQAAAYAAA8AAA+AAB+AAD+AAH+AAH+AAP+QAP+wAf/wAf/wAf/wAP/wAP/gAH/gAD/AAAAAAAAAA==") }),
+          img : atob("GBiBAAAAAAAAAAAAAAAAAAAQAAAQAAAYAAA8AAA+AAB+AAD+AAH+AAH+AAP+QAP+wAf/wAf/wAf/wAP/wAP/gAH/gAH/AAB8AAAAAA==") }),
         show : function() { // shown - if running, start animation
-          Bangle.on("calories",function(v){
-            this.emit('redraw')
-          })
+          this.caloriesHandler = () => this.emit('redraw');
+          Bangle.on("calories",this.caloriesHandler);
         },
         hide : function() { // hidden - stop animation
-          Bangle.removeListener("calories",function(v){
-            this.emit('redraw')
-          })
+          Bangle.removeListener("calories",this.caloriesHandler)
         }
       },
       { name : "ActiveCalories",
         get : () => ({ 
           text : global.calories.activeCaloriesBurned==0?"--":global.calories.activeCaloriesBurned,
-          img : atob("GBiBAAAAAAAAAAAAAAAAAAAQAAAQAAAYAAA8AAA+AAB+AAD+AAH+AAH+AAP+QAP+wAf/wAf/wAf/wAP/wAP/gAH/gAD/AAAAAAAAAA==") }),
+          img : atob("GBiBAAAAAAOAAAfAAAfAAAOAAAAAAAfAAA/gAB/gIBfwMDfYMDeceCeM+AeB+APB+gXB/gbj/gZx/g4x/Aww+Bw4ABgYABgYAAAAAA==") }),
         show : function() { // shown - if running, start animation
-          Bangle.on("calories",function(v){
-            this.emit('redraw')
-          })
+          this.caloriesHandler = () => this.emit('redraw');
+          Bangle.on("calories",this.caloriesHandler);
         },
         hide : function() { // hidden - stop animation
-          Bangle.removeListener("calories",function(v){
-            this.emit('redraw')
-          })
+          Bangle.removeListener("calories",this.caloriesHandler)
+
         }
       },
       { name : "BmrCalories",
         get : () => ({ 
           text : global.calories.bmrCaloriesBurned==0?"--":global.calories.bmrCaloriesBurned,
-          img : atob("GBiBAAAAAAAAAAAAAAAAAAAQAAAQAAAYAAA8AAA+AAB+AAD+AAH+AAH+AAP+QAP+wAf/wAf/wAf/wAP/wAP/gAH/gAD/AAAAAAAAAA==") }),
+          img : atob("GBiBAAAAAAAAAAA/AAH/zAHB7AEIfAwMPB4M/H4e/H8+AFv+ABh+hhh/hhj/hhh/jgx/DAw+HA4DGAcGMAOG8AHPwABPAAAHgAABgA==") }),
         show : function() { // shown - if running, start animation
-          Bangle.on("calories",function(v){
-            this.emit('redraw')
-          })
+          this.caloriesHandler = () => this.emit('redraw');
+          Bangle.on("calories",this.caloriesHandler);
         },
         hide : function() { // hidden - stop animation
-          Bangle.removeListener("calories",function(v){
-            this.emit('redraw')
-          })
+          Bangle.removeListener("calories",this.caloriesHandler)
+
         }
       }
     ]
