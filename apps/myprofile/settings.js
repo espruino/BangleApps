33e60b170f6295a4fb216dd7b958e9de1eb59a6c
RKBoss6 <kabironkar@gmail.com>
2026-01-27 16:07:41 -0500

    Add resting heart rate functionality to settings directly instead of going through settings

--- apps/myprofile/settings.js
+++ apps/myprofile/settings.js
@@ -1,6 +1,9 @@
 (function(back) {
   const FILE = "myprofile.json";
-
+  //RHR reading vars
+  let rhrData = [];
+  let seconds = 60;
+  let counter = seconds;
   const myprofile = Object.assign({
     maxHrm: 200,
     strideLength: 0, // 0 = not set
@@ -12,6 +15,95 @@
   function writeProfile() {
     require('Storage').writeJSON(FILE, myprofile);
   }
+  function finish() {
+  Bangle.setHRMPower(0);
+  Bangle.removeListener('HRM', onRHRHrm);
+  if (rhrData.length > 0) {
+    // Calculate average, ignoring outliers
+    let avgRHR = Math.round(rhrData.reduce((a, b) => a + b) / rhrData.length);
+    myprofile.restingHrm = avgRHR;
+    myprofile.minHrm = avgRHR;
+    require("Storage").writeJSON("myprofile.json", myprofile);
+    E.showPrompt(" ",{
+      buttonHeight:35,
+      buttons:{"Back":true}
+    }).then(function(v){
+      mainMenu();
+    })
+      g.clearRect(0,Bangle.appRect.y,g.getWidth(),g.getHeight()-40)
+      g.setColor("#f00"); g.drawImage(atob("Mi2BAAAAAAAAAAAP4AAf4AAf/wAf/gAP/+Af/+AH//wP//wD//+H//+B///z///w///+///8P///////n///////5///////+f///////3///////9////////f///////3///////9////////f///////j///////4///////+P///////B///////wf//////4D//////+Af//////AH//////gA//////4AH/////8AA/////+AAH/////AAB/////gAAP////wAAA////4AAAH///8AAAA///+AAAAH///AAAAA///AAAAAH//gAAAAAf/wAAAAAD/4AAAAAAf4AAAAAAB8AAAAAAAOAAAAAAAAAAAAAAAAAAAAAA=="),g.getWidth()-80,70);
+      g.setColor(g.theme.fg); 
+      g.setFont("Vector", 25).setFontAlign(0,0);
+          g.drawString("Saved!", g.getWidth()/2, 35);
+      g.setFont("Vector", 30).setFontAlign(0,0);
+          g.drawString(avgRHR, g.getWidth()/2-30, g.getHeight()/2);
+      g.setFont("Vector", 18).setFontAlign(0,0);
+          g.drawString("RHR", g.getWidth()/2-30, g.getHeight()/2+20);
+    
+  } 
+}
+
+function onRHRHrm(hrm) {
+  // Only record if the watch is confident in the reading
+  if (hrm.confidence > 80) {
+    rhrData.push(hrm.bpm);
+  }
+
+  // UI Update
+  g.clearRect(Bangle.appRect);
+  g.setColor(g.theme.fg); 
+  g.setFont("Vector", 20).setFontAlign(0,0);
+  g.drawString("Measuring...", g.getWidth()/2, 40);
+  g.setFont("Vector", 40);
+  g.drawString(hrm.bpm, g.getWidth()/2-30, g.getHeight()/2-5);
+  g.setColor("#f00"); g.drawImage(atob("Mi2BAAAAAAAAAAAP4AAf4AAf/wAf/gAP/+Af/+AH//wP//wD//+H//+B///z///w///+///8P///////n///////5///////+f///////3///////9////////f///////3///////9////////f///////j///////4///////+P///////B///////wf//////4D//////+Af//////AH//////gA//////4AH/////8AA/////+AAH/////AAB/////gAAP////wAAA////4AAAH///8AAAA///+AAAAH///AAAAA///AAAAAH//gAAAAAf/wAAAAAD/4AAAAAAf4AAAAAAB8AAAAAAAOAAAAAAAAAAAAAAAAAAAAAA=="),g.getWidth()-80,60)
+  g.setColor(g.theme.fg); 
+  g.setFont("Vector", 16);
+  g.drawString(counter + "s remaining", g.getWidth()/2, g.getHeight() - 55);
+  if(hrm.confidence<=80){
+    g.setFont("Vector", 14).drawString("Low confidence\nKeep still", g.getWidth()/2, g.getHeight() - 20);
+  }
+};
+
+function startRHR(){
+    // Start the process
+    
+    g.clearRect(Bangle.appRect)
+    g.setColor(g.theme.fg); 
+    g.setFont("Vector", 20).setFontAlign(0,0);
+    g.drawString("Starting...", g.getWidth()/2, g.getHeight()/2);
+    rhrData = [];
+    counter = seconds;
+    Bangle.on('HRM',onRHRHrm);
+    Bangle.setHRMPower(1);
+    let interval = setInterval(() => {
+      counter--;
+      if (counter <= 0) {
+        clearInterval(interval);
+        finish();
+      }
+    }, 1000);
+  };
+function RHRReading(){
+  E.showPrompt("Resting Heart Rate reading requires you to be resting and still. Takes approx. 1 minute.",{
+      title:"Continue?",
+      buttonHeight:50,
+      buttons:{"Continue":true,"Back":false}
+    }).then(function(v){
+    if(v){
+          E.showPrompt("Make sure Bangle.js is snug around your wrist, about 1 cm under your wrist bone.",{
+          buttonHeight:40,
+          buttons:{"Continue":true}
+        }).then(function(v){
+            startRHR();
+      });
+
+    }else{
+      mainMenu()
+    }
+  });
+}
+  
   const genderOpts = ["Male","Female","Not Set"];
   // if we have old min hr data and no resting hr data, upgrade minHRM to restingHRM
   if(myprofile.minHrm&&!myprofile.restingHrm)myprofile.restingHrm=myprofile.minHrm;
@@ -137,6 +229,15 @@
           writeProfile();
         }
       },
+      /*LANG*/'HR min': {
+        format: v => /*LANG*/`${v} BPM`,
+        value: myprofile.minHrm,
+        min: 30, max: 220,
+        onchange: v => {
+          myprofile.minHrm = v;
+          writeProfile();
+        }
+      },
       /*LANG*/"Stride length": {
         value: myprofile.strideLength,
         min:0.00,
@@ -149,22 +250,8 @@
       },
     };
   
-  menu[/*LANG*/`Resting/min Hr: ${myprofile.restingHrm?myprofile.restingHrm:"--"}`]=function(){
-    E.showPrompt("To take an RHR reading, go to the Health app.",{
-      buttons: {
-        "Go to Health": true,
-        "Back": false
-      }
-    }).then(function(v){
-      if (v === true) {
-        load("health.app.js");
-      }else{
-        mainMenu();
-      }
-    });
+  menu[/*LANG*/`Resting HR: ${myprofile.restingHrm?myprofile.restingHrm:"--"}`]=RHRReading;
+   E.showMenu(menu)
   };
-  E.showMenu(menu);
-};
-  
   mainMenu();
-})
+})(load)
