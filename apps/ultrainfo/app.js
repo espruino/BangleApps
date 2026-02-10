fc68802a32183de5830ca5bbc856fd8e515ab944
RKBoss6 <onkarka@northvilleschools.net>
2026-02-10 10:09:16 -0500

    Add settings page for accessing accent color

--- apps/ultrainfo/app.js
+++ apps/ultrainfo/app.js
@@ -1,5 +1,9 @@
 // Load fonts
 let background = require("clockbg");
+var settings = Object.assign({
+  accentColor: "#f00",
+}, require('Storage').readJSON("ultrainfo.json", true) || {});
+
 require("Font7x11Numeric7Seg").add(Graphics);
 // X/Y are the position of the bottom right of the HH:MM text - make it central!
 const X = g.getWidth()/2 + 45,
@@ -8,19 +12,34 @@ const X = g.getWidth()/2 + 45,
 let drawTimeout;
 
 
+let draw = function() {
+  var x = g.getWidth() / 2;
+  var y = g.getHeight() / 2;
+  var date =new Date()
+  background.fillRect(0,62,g.getWidth(),110);
+  
+  var timeStr = require("locale").time(date, 1); // Hour and minute
+  g.setFontAlign(0, -1).setColor(g.theme.bg).setFont("Vector",30).drawString(timeStr, g.getWidth()/2,62+3);
+  
+  // Show date and day of week
+  var dateStr = require("locale").dow(date, 1)+", " +require("locale").month(date, 1)+" "+ date.getDate();
+  g.setFont("14").setFontAlign(0, 1).drawString(dateStr, g.getWidth()/2,110-2);
+
+  // queue next draw
+  if (drawTimeout) clearTimeout(drawTimeout);
+  drawTimeout = setTimeout(function() {
+    drawTimeout = undefined;
+    draw();
+  }, 60000 - (Date.now() % 60000));
+};
 
 
 // Clear the screen once, at startup
 g.clear();
-// draw immediately at first
+
 background.fillRect(Bangle.appRect);
 
-/* Show launcher when middle button pressed
-This should be done *before* Bangle.loadWidgets so that
-widgets know if they're being loaded into a clock app or not */
-
-
-let highlightColor="#f00"
+draw()
 
 // CLOCKINFOS:
 
@@ -28,7 +47,7 @@ let clockInfoItems = require("clock_info").load();
 
 function drawProgBar(x,y,w,val,col){
   g.setColor(col)
-    .fillRect({x:x,y:y-2,w:w*Math.max(1,val),h:3,r:8})
+    .fillRect({x:x,y:y-2,w:w*Math.min(1,val),h:3,r:8})
 }
 
 let drawInlineClockInfo = function(itm, info, options, left) {
@@ -42,7 +61,7 @@ let drawInlineClockInfo = function(itm, info, options, left) {
   });
   const mid = options.y + (options.h /2);
   g.setFont(font.font)
-    .setColor(options.focus ? highlightColor : g.theme.bg)
+    .setColor(options.focus ? settings.accentColor : g.theme.bg)
     .drawImage(info.img,
       left ? options.x + options.w - 24-3 : options.x+3,
       mid - 12)
@@ -58,7 +77,7 @@ let drawRectClockInfo=function(itm,info,options,top){
     drawProgBar(options.x+3,top?options.y+options.h-3:options.y+3,options.w-6,1,"#000")
     if(info.max){
     
-      drawProgBar(options.x+3,top?options.y+options.h-3:options.y+3,options.w-6,+parseFloat((""+info.text).match(/[\d.]+/))/info.max,highlightColor)
+      drawProgBar(options.x+3,top?options.y+options.h-3:options.y+3,options.w-6,+parseFloat((""+info.text).match(/[\d.]+/))/info.max,settings.accentColor)
     }
     let font=g.findFont(info.text,{w : options.w-5,
                             h : 30, 
@@ -67,7 +86,7 @@ let drawRectClockInfo=function(itm,info,options,top){
                             trim : true 
                            })
     g.setFont(font.font)
-        .setColor(options.focus?highlightColor:g.theme.bg)
+        .setColor(options.focus?settings.accentColor:g.theme.bg)
         .drawImage(info.img,options.x+(options.w/2)-12,options.y+(options.h/2)+(top?-3:2),)
         .setFontAlign(0,0)
         .drawString(font.text,options.x+(options.w/2),options.y+(options.h/2)+(top?-15:2-10))
@@ -109,18 +128,7 @@ let brClkInfo = require("clock_info").addInteractive(clockInfoItems, {
   draw: function(itm, info,options){drawRectClockInfo(itm,info,options,false)}
 });
 
-let leftInlineClkInfo = require("clock_info").addInteractive(clockInfoItems, {
-  x: 0, y: g.getHeight()/2-20, w: g.getWidth()/2-5, h: 40,
-  draw: function(itm,info,options){
-    drawInlineClockInfo(itm,info,options,false)
-  }
-});
-let rightInlineClkInfo = require("clock_info").addInteractive(clockInfoItems, {
-  x: g.getWidth()/2+5, y: g.getHeight()/2-20, w: g.getWidth()/2-5, h: 40,
-  draw: function(itm,info,options){
-    drawInlineClockInfo(itm,info,options,true)
-  }
-});
+
 
 
 Bangle.setUI("clock");
