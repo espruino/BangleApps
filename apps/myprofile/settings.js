a7b1c22617ec9a079747cb7ecfaecebad352f035
thyttan <6uuxstm66@mozmail.com>
2026-05-05 12:10:23 +0200

    myprofile: autoformat settings.js after new code

--- apps/myprofile/settings.js
+++ apps/myprofile/settings.js
@@ -16,58 +16,58 @@
   function writeProfile() {
     require('Storage').writeJSON(FILE, myprofile);
   }
-  
+
   function finishRHRReading() {
     Bangle.setHRMPower(0);
     Bangle.removeListener('HRM', onRHRHrm);
     if (rhrData.length > 0) {
       // Calculate average, ignoring outliers
       let avgRHR = Math.round(rhrData.reduce((a, b) => a + b) / rhrData.length);
-      
-      E.showPrompt(" ",{
-        buttonHeight:35,
-        buttons:{"Yes":true,"No":false}
-      }).then(function(v){
-        if(v){
+
+      E.showPrompt(" ", {
+        buttonHeight: 35,
+        buttons: { "Yes": true, "No": false }
+      }).then(function (v) {
+        if (v) {
           myprofile.minHrm = avgRHR;
           writeProfile();
-          
-          E.showPrompt(" ",{
-            buttonHeight:35,
-            buttons:{"Back":true}
-          }).then(function(v){
+
+          E.showPrompt(" ", {
+            buttonHeight: 35,
+            buttons: { "Back": true }
+          }).then(function (v) {
             mainMenu();
           })
-          
-          g.clearRect(0,Bangle.appRect.y,g.getWidth(),g.getHeight()-40)
+
+          g.clearRect(0, Bangle.appRect.y, g.getWidth(), g.getHeight() - 40)
             .setColor("#f00")
-            .drawImage(atob("Mi2BAAAAAAAAAAAP4AAf4AAf/wAf/gAP/+Af/+AH//wP//wD//+H//+B///z///w///+///8P///////n///////5///////+f///////3///////9////////f///////3///////9////////f///////j///////4///////+P///////B///////wf//////4D//////+Af//////AH//////gA//////4AH/////8AA/////+AAH/////AAB/////gAAP////wAAA////4AAAH///8AAAA///+AAAAH///AAAAA///AAAAAH//gAAAAAf/wAAAAAD/4AAAAAAf4AAAAAAB8AAAAAAAOAAAAAAAAAAAAAAAAAAAAAA=="),g.getWidth()-80,70)
+            .drawImage(atob("Mi2BAAAAAAAAAAAP4AAf4AAf/wAf/gAP/+Af/+AH//wP//wD//+H//+B///z///w///+///8P///////n///////5///////+f///////3///////9////////f///////3///////9////////f///////j///////4///////+P///////B///////wf//////4D//////+Af//////AH//////gA//////4AH/////8AA/////+AAH/////AAB/////gAAP////wAAA////4AAAH///8AAAA///+AAAAH///AAAAA///AAAAAH//gAAAAAf/wAAAAAD/4AAAAAAf4AAAAAAB8AAAAAAAOAAAAAAAAAAAAAAAAAAAAAA=="), g.getWidth() - 80, 70)
             .setColor(g.theme.fg)
-            .setFont("Vector", 25).setFontAlign(0,0)
-            .drawString("Saved!", g.getWidth()/2, 35)
-            .setFont("Vector", 30).setFontAlign(0,0)
-            .drawString(avgRHR, g.getWidth()/2-30, g.getHeight()/2)
-            .setFont("Vector", 18).setFontAlign(0,0)
-            .drawString("RHR", g.getWidth()/2-30, g.getHeight()/2+20);
-          
-        }else{
+            .setFont("Vector", 25).setFontAlign(0, 0)
+            .drawString("Saved!", g.getWidth() / 2, 35)
+            .setFont("Vector", 30).setFontAlign(0, 0)
+            .drawString(avgRHR, g.getWidth() / 2 - 30, g.getHeight() / 2)
+            .setFont("Vector", 18).setFontAlign(0, 0)
+            .drawString("RHR", g.getWidth() / 2 - 30, g.getHeight() / 2 + 20);
+
+        } else {
           mainMenu();
         }
-    }) 
-  
-    g.clearRect(0,Bangle.appRect.y,g.getWidth(),g.getHeight()-40)
-      .setColor("#f00")
-      .drawImage(atob("Mi2BAAAAAAAAAAAP4AAf4AAf/wAf/gAP/+Af/+AH//wP//wD//+H//+B///z///w///+///8P///////n///////5///////+f///////3///////9////////f///////3///////9////////f///////j///////4///////+P///////B///////wf//////4D//////+Af//////AH//////gA//////4AH/////8AA/////+AAH/////AAB/////gAAP////wAAA////4AAAH///8AAAA///+AAAAH///AAAAA///AAAAAH//gAAAAAf/wAAAAAD/4AAAAAAf4AAAAAAB8AAAAAAAOAAAAAAAAAAAAAAAAAAAAAA=="),g.getWidth()-80,70-15)
-      .setColor(g.theme.fg)
-      .setFont("Vector", 25).setFontAlign(0,0)
-      .drawString("Finished!", g.getWidth()/2, 35)
-      .setFont("Vector", 30).setFontAlign(0,0)
-      .drawString(avgRHR, g.getWidth()/2-30, g.getHeight()/2-15)
-      .setFont("Vector", 18).setFontAlign(0,0)
-      .drawString("RHR", g.getWidth()/2-30, g.getHeight()/2+20-15)
-      .drawString("Save?", g.getWidth()/2, g.getHeight()/2+30)
-      
-    } 
+      })
+
+      g.clearRect(0, Bangle.appRect.y, g.getWidth(), g.getHeight() - 40)
+        .setColor("#f00")
+        .drawImage(atob("Mi2BAAAAAAAAAAAP4AAf4AAf/wAf/gAP/+Af/+AH//wP//wD//+H//+B///z///w///+///8P///////n///////5///////+f///////3///////9////////f///////3///////9////////f///////j///////4///////+P///////B///////wf//////4D//////+Af//////AH//////gA//////4AH/////8AA/////+AAH/////AAB/////gAAP////wAAA////4AAAH///8AAAA///+AAAAH///AAAAA///AAAAAH//gAAAAAf/wAAAAAD/4AAAAAAf4AAAAAAB8AAAAAAAOAAAAAAAAAAAAAAAAAAAAAA=="), g.getWidth() - 80, 70 - 15)
+        .setColor(g.theme.fg)
+        .setFont("Vector", 25).setFontAlign(0, 0)
+        .drawString("Finished!", g.getWidth() / 2, 35)
+        .setFont("Vector", 30).setFontAlign(0, 0)
+        .drawString(avgRHR, g.getWidth() / 2 - 30, g.getHeight() / 2 - 15)
+        .setFont("Vector", 18).setFontAlign(0, 0)
+        .drawString("RHR", g.getWidth() / 2 - 30, g.getHeight() / 2 + 20 - 15)
+        .drawString("Save?", g.getWidth() / 2, g.getHeight() / 2 + 30)
+
+    }
   }
 
   function onRHRHrm(hrm) {
@@ -75,34 +75,34 @@
     if (hrm.confidence > 80) {
       rhrData.push(hrm.bpm);
     }
-  
+
     // Update UI
     g.clearRect(Bangle.appRect)
       .setColor(g.theme.fg)
-      .setFont("Vector", 20).setFontAlign(0,0)
-      .drawString("Measuring...", g.getWidth()/2, 40)
+      .setFont("Vector", 20).setFontAlign(0, 0)
+      .drawString("Measuring...", g.getWidth() / 2, 40)
       .setFont("Vector", 40)
-      .drawString(hrm.bpm, g.getWidth()/2-30, g.getHeight()/2-5)
+      .drawString(hrm.bpm, g.getWidth() / 2 - 30, g.getHeight() / 2 - 5)
       .setColor("#f00")
-      .drawImage(atob("Mi2BAAAAAAAAAAAP4AAf4AAf/wAf/gAP/+Af/+AH//wP//wD//+H//+B///z///w///+///8P///////n///////5///////+f///////3///////9////////f///////3///////9////////f///////j///////4///////+P///////B///////wf//////4D//////+Af//////AH//////gA//////4AH/////8AA/////+AAH/////AAB/////gAAP////wAAA////4AAAH///8AAAA///+AAAAH///AAAAA///AAAAAH//gAAAAAf/wAAAAAD/4AAAAAAf4AAAAAAB8AAAAAAAOAAAAAAAAAAAAAAAAAAAAAA=="),g.getWidth()-80,60)
+      .drawImage(atob("Mi2BAAAAAAAAAAAP4AAf4AAf/wAf/gAP/+Af/+AH//wP//wD//+H//+B///z///w///+///8P///////n///////5///////+f///////3///////9////////f///////3///////9////////f///////j///////4///////+P///////B///////wf//////4D//////+Af//////AH//////gA//////4AH/////8AA/////+AAH/////AAB/////gAAP////wAAA////4AAAH///8AAAA///+AAAAH///AAAAA///AAAAAH//gAAAAAf/wAAAAAD/4AAAAAAf4AAAAAAB8AAAAAAAOAAAAAAAAAAAAAAAAAAAAAA=="), g.getWidth() - 80, 60)
       .setColor(g.theme.fg)
       .setFont("Vector", 16)
-      .drawString(counter + "s remaining", g.getWidth()/2, g.getHeight() - 55)
-    
-    if(hrm.confidence<=80){
-      g.setFont("Vector", 14).drawString("Low confidence\nKeep still", g.getWidth()/2, g.getHeight() - 20);
+      .drawString(counter + "s remaining", g.getWidth() / 2, g.getHeight() - 55)
+
+    if (hrm.confidence <= 80) {
+      g.setFont("Vector", 14).drawString("Low confidence\nKeep still", g.getWidth() / 2, g.getHeight() - 20);
     }
   }
 
-  function startRHR(){
+  function startRHR() {
     // Start the reading
     g.clearRect(Bangle.appRect)
-    g.setColor(g.theme.fg); 
-    g.setFont("Vector", 20).setFontAlign(0,0);
-    g.drawString("Starting...", g.getWidth()/2, g.getHeight()/2);
+    g.setColor(g.theme.fg);
+    g.setFont("Vector", 20).setFontAlign(0, 0);
+    g.drawString("Starting...", g.getWidth() / 2, g.getHeight() / 2);
     rhrData = [];
     counter = seconds;
-    Bangle.on('HRM',onRHRHrm);
+    Bangle.on('HRM', onRHRHrm);
     Bangle.setHRMPower(1);
     let interval = setInterval(() => {
       counter--;
@@ -112,47 +112,45 @@
       }
     }, 1000);
   }
-  
-  function RHRReading(){
-      E.showPrompt("Resting Heart Rate reading requires you to be resting and still. Takes approx. 1 minute.",{
-        title:"Continue?",
-        buttonHeight:50,
-        buttons:{"Continue":true,"Back":false}
-      }).then(function(v){
-      if(v){
-          E.showPrompt("Make sure Bangle.js is snug around your wrist, about 1 cm under your wrist bone.",{
-            buttonHeight:40,
-            buttons:{"Continue":true}
-          }).then(function(v){
-              startRHR();
-          });
-      }else{
+
+  function RHRReading() {
+    E.showPrompt("Resting Heart Rate reading requires you to be resting and still. Takes approx. 1 minute.", {
+      title: "Continue?",
+      buttonHeight: 50,
+      buttons: { "Continue": true, "Back": false }
+    }).then(function (v) {
+      if (v) {
+        E.showPrompt("Make sure Bangle.js is snug around your wrist, about 1 cm under your wrist bone.", {
+          buttonHeight: 40,
+          buttons: { "Continue": true }
+        }).then(function (v) {
+          startRHR();
+        });
+      } else {
         mainMenu()
       }
     });
   }
-  
-  const genderOpts = ["Male","Female","Not Set"];
+
+  const genderOpts = ["Male", "Female", "Not Set"];
 
   const ageMenu = () => {
     const date = new Date(myprofile.birthday);
 
     E.showMenu({
-      "" : { "title" : /*LANG*/"Birthday" },
-
-      "< Back" : () => {
+      "": { "title": /*LANG*/"Birthday" },
+      "< Back": () => {
         if (date != new Date(myprofile.birthday)) {
           // Birthday changed
           if (date > new Date()) {
             E.showPrompt(/*LANG*/"Birthday must not be in future!", {
-              
-              buttons : {"Ok":true},
+              buttons: { "Ok": true },
             }).then(() => ageMenu());
-            
+
           } else {
             const age = (new Date()).getFullYear() - date.getFullYear();
-            const newMaxHRM = Math.round(208-0.7*age); // Tanaka formula: https://my.clevelandclinic.org/health/articles/24649-heart-rate-reserve
-            E.showPrompt(/*LANG*/`Set HR max to ${newMaxHRM} calculated from age?`).then(function(v) {
+            const newMaxHRM = Math.round(208 - 0.7 * age); // Tanaka formula: https://my.clevelandclinic.org/health/articles/24649-heart-rate-reserve
+            E.showPrompt(/*LANG*/`Set HR max to ${newMaxHRM} calculated from age?`).then(function (v) {
               myprofile.birthday = date.getFullYear() + "-" + (date.getMonth() + 1).toString().padStart(2, '0') + "-" + date.getDate().toString().padStart(2, '0');
               if (v) {
                 myprofile.maxHrm = newMaxHRM;
@@ -179,7 +177,7 @@
         value: date ? date.getMonth() + 1 : null,
         format: v => require("date_utils").month(v),
         onchange: v => {
-          date.setMonth((v+11)%12);
+          date.setMonth((v + 11) % 12);
         }
       },
       /*LANG*/"Year": {
@@ -194,17 +192,17 @@
   };
 
   const mainMenu = () => {
-    var menu={
-      "" : { "title" : /*LANG*/"My Profile" },
+    var menu = {
+      "": { "title": /*LANG*/"My Profile" },
 
-      "< Back" : () => back(),
+      "< Back": () => back(),
 
-      /*LANG*/"Birthday" : () => ageMenu(),
+      /*LANG*/"Birthday": () => ageMenu(),
 
       /*LANG*/'Height': {
         value: myprofile.height,
         min: 0, max: 300,
-        step:0.01,
+        step: 0.01,
         format: v => v ? require("locale").distance(v, 2) : '-',
         onchange: v => {
           if (v !== myprofile.height) {
@@ -212,7 +210,7 @@
             myprofile.height = v;
             setTimeout(() => {
               const newStrideLength = myprofile.height * 0.414;
-              E.showPrompt(/*LANG*/`Set Stride length to ${require("locale").distance(newStrideLength, 2)} calculated from height?`).then(function(v) {
+              E.showPrompt(/*LANG*/`Set Stride length to ${require("locale").distance(newStrideLength, 2)} calculated from height?`).then(function (v) {
                 if (v) {
                   myprofile.strideLength = newStrideLength;
                 }
@@ -226,21 +224,21 @@
 
       /*LANG*/"Weight": {
         value: myprofile.weight,
-        min:0,
-        step:1,
+        min: 0,
+        step: 1,
         format: v => v ? v + "kg" : '-',
         onchange: v => {
-          myprofile.weight=v;
+          myprofile.weight = v;
           writeProfile();
         },
       },
 
       /*LANG*/"Gender": {
         value: (typeof myprofile.gender === "number" &&
-                myprofile.gender >= 0 &&
-                myprofile.gender < genderOpts.length) ? myprofile.gender : 2,
-        min:0,
-        max: genderOpts.length-1,
+          myprofile.gender >= 0 &&
+          myprofile.gender < genderOpts.length) ? myprofile.gender : 2,
+        min: 0,
+        max: genderOpts.length - 1,
         format: v => genderOpts[v],
         onchange: v => {
           myprofile.gender = v;
@@ -258,20 +256,20 @@
         }
       },
     };
-  
-  menu[/*LANG*/`Resting/Min HR: ${myprofile.minHrm?myprofile.minHrm:"--"}`]=RHRReading;
-  menu[/*LANG*/"Stride length"]= {
-        value: myprofile.strideLength,
-        min:0.00,
-        step:0.01,
-        format: v => v ? require("locale").distance(v, 2) : '-',
-        onchange: v => {
-          myprofile.strideLength=v;
-          writeProfile();
-        },
-      }
-  E.showMenu(menu)
+
+    menu[/*LANG*/`Resting/Min HR: ${myprofile.minHrm ? myprofile.minHrm : "--"}`] = RHRReading;
+    menu[/*LANG*/"Stride length"] = {
+      value: myprofile.strideLength,
+      min: 0.00,
+      step: 0.01,
+      format: v => v ? require("locale").distance(v, 2) : '-',
+      onchange: v => {
+        myprofile.strideLength = v;
+        writeProfile();
+      },
+    }
+    E.showMenu(menu)
   };
-  
+
   mainMenu();
 })
