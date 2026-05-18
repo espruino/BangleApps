c8920cfbfe12944165f9f40f568e5d66cb51723b
Gordon Williams <gw@pur3.co.uk>
2026-05-18 16:13:58 +0100

    widgets: use reset("widget") to allow separate widget bar colour in 2v30. Minor efficiency tweaks as well.

--- apps/widlock/widget.js
+++ apps/widlock/widget.js
@@ -4,5 +4,5 @@ Bangle.on("lock", function() {
 });
 WIDGETS["lock"]={area:"tl",sortorder:10,width:Bangle.isLocked()?16:1,draw:function(w) {
   if (Bangle.isLocked())
-    g.reset().drawImage(atob("DhABH+D/wwMMDDAwwMf/v//4f+H/h/8//P/z///f/g=="), w.x+1, w.y+4);
+    g.reset("widget").drawImage(atob("DhABH+D/wwMMDDAwwMf/v//4f+H/h/8//P/z///f/g=="), w.x+1, w.y+4);
 }};
