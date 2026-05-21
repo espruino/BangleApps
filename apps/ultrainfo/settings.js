1e4e56bcd7930eaacc111b01a448773f827369bf
RKBoss6 <kabironkar@gmail.com>
2026-05-21 10:10:17 -0400

    Use colorpicker module instead, and reduce bloat

--- apps/ultrainfo/settings.js
+++ apps/ultrainfo/settings.js
@@ -10,65 +10,37 @@
     require('Storage').writeJSON(FILE, settings);
   }
   
-  // same as in clock_bg
-  function getColorsImage(cols) {
-    var bpp = 1;
-    if (cols.length>4) bpp=4;
-    else if (cols.length>2) bpp=2;
-    var w = (cols.length>8)?8:16;
-    var b = Graphics.createArrayBuffer(w*cols.length,16,bpp);
-    b.palette = new Uint16Array(1<<bpp);
-    cols.forEach((c,i)=>{
-      b.setColor(i).fillRect(i*w,0,i*w+w-1,15);
-      b.palette[i] = g.toColor(c);
-    });
-    return "\0"+b.asImage("string");
-  }
-  function showAccentColor(){
-    var menu={
-    "" : { "title" : "Accent Color" },
-    "< Back" : showMainMenu,
-
-    }
-    var cols = ["#F00","#0F0","#FF0",
-                    "#00F","#F0F","#0FF",
-                    "#000","#888","#fff",];
-    cols.forEach(col => {
-        menu[" "+getColorsImage([col])] = () => {
-          settings.accentColor=col;
-          writeSettings();
-          showMainMenu();
-        };
-      });
-    E.showMenu(menu);
-  }
-  
-function showMainColor(){
-    var menu={
-    "" : { "title" : "Main Color" },
-    "< Back" : showMainMenu,
-
-    }
-    var cols = ["#F00","#0F0","#FF0",
-                    "#00F","#F0F","#0FF",
-                    "#000","#888","#fff",];
-    cols.forEach(col => {
-        menu[" "+getColorsImage([col])] = () => {
-          settings.mainColor=col;
-          writeSettings();
-          showMainMenu();
-        };
-      });
-    E.showMenu(menu);
-  }
-  
   function showMainMenu(){
   // Show the menu
     E.showMenu({
       "" : { "title" : "Ultra Info" },
       "< Back" : () => back(),
-      'Main Color': showMainColor,
-      'Accent Color': showAccentColor
+      'Main Color': function(){
+        require("colorpicker").show({
+          onSelect:function(color){
+            settings.mainColor=color;
+            writeSettings();
+          },
+          showPreview:true,
+          back:function(){
+            showMainMenu()
+          }
+
+        });
+       },
+      'Accent Color': function(){
+        require("colorpicker").show({
+          onSelect:function(color){
+            settings.accentColor=color;
+            writeSettings();
+          },
+          showPreview:true,
+          back:function(){
+            showMainMenu()
+          }
+
+        });
+       },
     });
   }
   showMainMenu()
