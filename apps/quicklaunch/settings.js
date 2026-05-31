7a9a12a63f2d8da0ff959177da861eda424a470d
thyttan <6uuxstm66@mozmail.com>
2026-06-01 00:16:59 +0200

    quicklaunch: WIP tie updating settings object to menu entry

--- apps/quicklaunch/settings.js
+++ apps/quicklaunch/settings.js
@@ -7,43 +7,45 @@ for (let c of ["lapp","rapp","uapp","dapp","tapp"]){ // l=left, r=right, u=up, d
   if (!settings[c]) settings[c] = {"name":""};
 }
 
-// Convert settings object from before v0.12 to v0.12.
-for (let c of ["leftapp","rightapp","upapp","downapp","tapapp"]){
-  if (settings[c]) {
-    let cNew = c.substring(0,1)+"app";
-    settings[cNew] = settings[c];
-    delete settings[c];
-    if (settings[cNew].name=="(none)") settings[cNew].name = "";
-
-    if (settings[cNew].name=="Quick Launch Extension"){
-      settings[cNew].name = "Extension";
-      for (let d of ["extleftapp","extrightapp","extupapp","extdownapp","exttapapp"]){
-        if (settings[d]) {
-          let dNew = cNew.substring(0,1)+d.substring(3,4)+"app";
-          settings[dNew] = settings[d];
-          delete settings[d];
-          if (settings[dNew].name=="(none)") settings[dNew].name = "";
-        }
-      }
-    }
-    storage.writeJSON("quicklaunch.json",settings);
-  } 
-}
-for (let d of ["extleftapp","extrightapp","extupapp","extdownapp","exttapapp"]){
-  if (settings[d]) delete settings[d];
-}
 
 var launchCache = require("launch_utils").cache({showClocks:true,showLaunchers:true});
-launchCache = require("launch_utils").cacheWidgetsCheck(launchCache.apps);
 var apps = launchCache.apps;
 
+function updateSettingsObject() {
+  // Convert settings object from before v0.12 to v0.12.
+  for (let c of ["leftapp","rightapp","upapp","downapp","tapapp"]){
+    if (settings[c]) {
+      let cNew = c.substring(0,1)+"app";
+      settings[cNew] = settings[c];
+      delete settings[c];
+      if (settings[cNew].name=="(none)") settings[cNew].name = "";
+  
+      if (settings[cNew].name=="Quick Launch Extension"){
+        settings[cNew].name = "Extension";
+        for (let d of ["extleftapp","extrightapp","extupapp","extdownapp","exttapapp"]){
+          if (settings[d]) {
+            let dNew = cNew.substring(0,1)+d.substring(3,4)+"app";
+            settings[dNew] = settings[d];
+            delete settings[d];
+            if (settings[dNew].name=="(none)") settings[dNew].name = "";
+          }
+        }
+      }
+      storage.writeJSON("quicklaunch.json",settings);
+    } 
+  }
+  for (let d of ["extleftapp","extrightapp","extupapp","extdownapp","exttapapp"]){
+    if (settings[d]) delete settings[d];
+  }
+launchCache = require("launch_utils").cacheWidgetsCheck(launchCache.apps);
+}
 // Add required launch_utils properties to settings object from versions older than 0.17
 //delete settings.trace;
 var settingsKeys = Object.keys(settings);
-settingsKeys.forEach(entry => {
-  let name = settings[entry].name; 
+settingsKeys.forEach(key => {
+  let name = settings[key].name; 
   let app  = apps.find(app=>app.name === name);
-  if (app) settings[entry].wid = app.wid;
+  if (app) settings[key].wid = app.wid;
 });
 storage.writeJSON("quicklaunch.json",settings);
 
@@ -118,6 +120,7 @@ function showMainMenu() {
     let appName = settings[key].name==""?"(none)":settings[key].name;
     mainmenu[entry+ ": "+appName] = function() {showSubMenu(keyCurrent);};
   }
+    mainmenu.updateSettingsObject = updateSettingsObject(); // FIXME: Make this a good user experience by wrapping in a alert/prompt or something.
 
   return E.showMenu(mainmenu);
 }
