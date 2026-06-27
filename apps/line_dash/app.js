020b950f5478efc70b9031d189683f430c599e0f
Patrick Heeren <phother@mailbox.org>
2026-06-27 16:47:37 +0200

    Reorder screens: Steps before Distance

--- apps/line_dash/app.js
+++ apps/line_dash/app.js
@@ -35,8 +35,8 @@ for (const key in saved_settings) {
 }
 
 let screens = ["clock"];
-if (initialSettings.showDistance) screens.push("distance");
 if (initialSettings.showSteps) screens.push("steps");
+if (initialSettings.showDistance) screens.push("distance");
 if (initialSettings.showHrm) screens.push("hrm");
 if (initialSettings.showBattery) screens.push("battery");
 let currentScreenIdx = 0;
