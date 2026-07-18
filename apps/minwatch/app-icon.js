03bf88e14e365491b579eaf59f03b0581ac904d0
fozzy-ag <fozzy-ag@users.noreply.github.com>
2026-07-18 23:34:00 +0200

    Fix minwatch icon: use heatshrink-compressed format
    The previous app-icon.js used Graphics.createArrayBuffer() with wrong
    color values (0x07E0 truncated to red in 8bpp mode). Replaced with
    standard heatshrink-compressed 4bit image matching app.png exactly.

--- apps/minwatch/app-icon.js
+++ apps/minwatch/app-icon.js
@@ -1,12 +1,1 @@
-(function() {
-  var g = Graphics.createArrayBuffer(48,48,8);
-  g.setColor(0x07E0);
-  g.fillCircle(24,24,22);
-  g.setColor(0);
-  g.fillCircle(24,24,19);
-  g.setColor(0x07E0);
-  g.fillCircle(24,24,2);
-  g.fillRect(23,10,25,24);
-  g.fillRect(24,23,34,25);
-  return g.asImage();
-})()
+require("heatshrink").decompress(atob("mEwghC/AFegCieqAAOKAQIaQ1WIAAmKDBwWGDB4WIDBsK1AXJxGqFx52BGBwuGC4wwJRY6TPLowXHxAXGhSJHC4+KC7qkHC5BIGC6xGHC5RIEC/4XqAAadDC4imHxAWEC4ioEF64XIxA5IC5wAIC/4XMVBIAIUwYXZJCBGFC7BIQIwoAB1QuOC48KGBuqCwwwOFxAwOFxIwMFxQYLCxoYICx4YCAAOKAQIWQAAYUUAHw"))
