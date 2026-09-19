7954858c737db5df9253e17351e54f7be0e0f30b
ZubaZ21 <82243853+ZubaZ21@users.noreply.github.com>
2026-09-19 19:47:04 -0400

    Added theme support

--- apps/approxclockplus/app.js
+++ apps/approxclockplus/app.js
@@ -301,14 +301,22 @@ function drawTime() {
 
 function clearFace() {
   g.reset();
-  g.setBgColor(0, 0, 0);
+
+  if (settings.theme === "Dark") {
+    g.setBgColor(0, 0, 0);
+    g.setColor(1, 1, 1);
+
+  } else if (settings.theme === "Light") {
+    g.setBgColor(1, 1, 1);
+    g.setColor(0, 0, 0);
+  }
+
   g.clearRect(
     0,
     0,
     g.getWidth(),
     g.getHeight()
   );
-  g.setColor(1, 1, 1);
 }
 
 
