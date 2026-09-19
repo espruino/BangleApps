21c3e69679049254e650cf7e8233679794dc4f70
ZubaZ21 <82243853+ZubaZ21@users.noreply.github.com>
2026-09-19 16:07:31 -0400

    Adding in level zero

--- apps/approxclockplus/app.js
+++ apps/approxclockplus/app.js
@@ -14,31 +14,31 @@ function drawTime() {
   const date = new Date();
 
   const hour = date.getHours();
-  const minutes = date.getMinutes();
+
+  let text;
+
+  if (hour < 4) {
+    text = "Just after yesterday";
+  } else if (hour < 20) {
+    text = "Today";
+  } else {
+    text = "Almost tomorrow";
+  }
 
   g.reset();
   g.setBgColor(0, 0, 0);
   g.clearRect(0, 0, g.getWidth(), g.getHeight());
   g.setColor(1, 1, 1);
 
-  g.setFont("Vector", 30);
+  g.setFont("Vector", 24);
+
+  const x =
+    (g.getWidth() - g.stringWidth(text)) / 2;
 
   g.drawString(
-    "Level: " + level,
-    20,
-    60
-  );
-
-  g.drawString(
-    "Hour: " + hour,
-    20,
-    110
-  );
-
-  g.drawString(
-    "Minute: " + minutes,
-    20,
-    160
+    text,
+    x,
+    100
   );
 }
 
